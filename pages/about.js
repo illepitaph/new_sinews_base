d08e77e653689d0982bf20bde005468dc4313561
Steve Barbaro <steve.barbaro@gmail.com>
2021-09-21 19:27:37 -0500

    updates new_sinews about page

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,109 @@
 import styles from '../styles/About.module.css'
 
 export default function About() {
-  return <div>About Page</div>
+  return (
+
+    <body>
+  <section className={styles.top}>
+    <div className={styles.iconPlusSitebar}>
+    <a href="https://newnewsinews.com"></a>
+    <nav className={styles.siteNav}>
+      <button>issue_4</button>
+      <button>issue_3</button>
+      <button>issue_2</button>
+      <button>issue_1</button>
+      <button>new sinews</button>
+      <button>about</button>
+      </nav>
+    </div>
+</section>
+    <section className={styles.middle}>
+<p><span className={styles.darkTypeface}>edited + designed by :: steve barbaro</span></p>
+
+<div className={styles.jpegBox1}>
+
+    <img src="images/Siracusa Wall.jpg"></img>
+    <img src="images/Bubbles.jpg"></img>
+    <img src="images/Archaic Entity.jpg"></img>
+    <img src="images/Security.jpg"></img>
+    <img src="images/Archaic Entity.jpg"></img>
+    <img src="images/Siracusa Wall.jpg"></img>
+    <img src="images/Security.jpg"></img>
+    <img src="images/Bubbles.jpg"></img>
+    <img src="images/Security.jpg"></img>
+    <img src="images/Archaic Entity.jpg"></img>
+    <img src="images/Bubbles.jpg"></img>
+    <img src="images/Siracusa Wall.jpg"></img>
+    <img src="images/Siracusa Wall.jpg"></img>
+    <img src="images/Bubbles.jpg"></img>
+    <img src="images/Archaic Entity.jpg"></img>
+    <img src="images/Security.jpg"></img>
+    <img src="images/Archaic Entity.jpg"></img>
+    <img src="images/Siracusa Wall.jpg"></img>
+    <img src="images/Bubbles.jpg"></img>
+    <img src="images/Archaic Entity.jpg"></img>
+    <img src="images/Security.jpg"></img>
+    <img src="images/Archaic Entity.jpg"></img>
+
+</div>
+
+<p>send poems & fiction (very much open to speculative/fantasy) or even aggressively creative nonfiction in a <code>word DOC</code> to be considered to:</p>
+
+<p><a href="mailto:newsinewsmag@gmail.com">newsinewsmag@gmail.com</a></p>
+
+<p>looking for diverse voices & dynamic styles</p>
+
+<p>generally: striving to make unexpected juxtapositions</p>
+
+<p>especially: seeking writing that is historically keen</p>
+    </section>
+
+<section className={styles.bottom}>
+    <div className={styles.jpegBox2}>
+      <img src="images/Siracusa Wall.jpg"></img>
+      <img src="images/Bubbles.jpg"></img>
+      <img src="images/Archaic Entity.jpg"></img>
+      <img src="images/Security.jpg"></img>
+      <img src="images/Archaic Entity.jpg"></img>
+      <img src="images/Siracusa Wall.jpg"></img>
+      <img src="images/Security.jpg"></img>
+      <img src="images/Bubbles.jpg"></img>
+      <img src="images/Security.jpg"></img>
+      <img src="images/Archaic Entity.jpg"></img>
+      <img src="images/Bubbles.jpg"></img>
+      <img src="images/Siracusa Wall.jpg"></img>
+      <img src="images/Siracusa Wall.jpg"></img>
+      <img src="images/Bubbles.jpg"></img>
+      <img src="images/Archaic Entity.jpg"></img>
+      <img src="images/Security.jpg"></img>
+      <img src="images/Archaic Entity.jpg"></img>
+      <img src="images/Siracusa Wall.jpg"></img>
+      <img src="images/Security.jpg"></img>
+      <img src="images/Bubbles.jpg"></img>
+      <img src="images/Security.jpg"></img>
+      <img src="images/Archaic Entity.jpg"></img>
+      <img src="images/Bubbles.jpg"></img>
+      <img src="images/Siracusa Wall.jpg"></img>
+      <img src="images/Siracusa Wall.jpg"></img>
+      <img src="images/Bubbles.jpg"></img>
+      <img src="images/Archaic Entity.jpg"></img>
+      <img src="images/Security.jpg"></img>
+      <img src="images/Archaic Entity.jpg"></img>
+      <img src="images/Siracusa Wall.jpg"></img>
+      <img src="images/Security.jpg"></img>
+      <img src="images/Bubbles.jpg"></img>
+      <img src="images/Security.jpg"></img>
+      <img src="images/Archaic Entity.jpg"></img>
+      <img src="images/Bubbles.jpg"></img>
+      <img src="images/Siracusa Wall.jpg"></img>
+
+    </div>
+</section>
+
+
+    <footer className={styles.socialLinkFooter}>
+  <a className={styles.socialLinkTag} href="https://www.twitter.com/new_sinews">twitter</a>
+      </footer>
+    </body>
+  )
 }
