fbb4925ca4c5e097391421c67fa30700334c924e
Steve Barbaro <steve.barbaro@gmail.com>
2021-10-04 17:04:30 -0500

    instantiates even flesh is a formality page

--- components/authorName.js
+++ components/authorName.js
@@ -2,8 +2,8 @@ import Image from 'next/image'
 import Link from 'next/link'
 import styles from '../styles/AuthorName.module.css'
 
-export default function AuthorName() {
+export default function AuthorName(props) {
   return (
-    <p className={styles.authorName}></p>
+    <p className={styles.authorName}>{this.props.name}</p>
   )
 }
