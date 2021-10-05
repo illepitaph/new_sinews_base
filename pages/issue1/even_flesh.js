05a50c9092472d14bbf0978abeea8d5192c8cf2a
Steve Barbaro <steve.barbaro@gmail.com>
2021-10-04 22:25:39 -0500

    finishes issue1's even flesh

--- pages/issue1/even_flesh.js
+++ pages/issue1/even_flesh.js
@@ -24,14 +24,14 @@ export default function EvenFlesh() {
 
        <article className={styles.standardProse}>
        <br/>
-       <span className={styles.authorItalic}>Steve Barbaro</span>
+       <span className={styles.authorItalic}>—Steve Barbaro</span>
        <br/>
        <br/>
        How might the partakers of cynically sensible deeds be freed in the very
        course of their yielding? “The virtue of ecstasy robs reality of its
-       synthetic robes,” Vi Khi Nao answers by way of her first book, <cite>The
-       Vanishing Point of Desire</cite> (2011). And in this novel-in-roomily-white-
-       spaced-prose, the bleh ritual of a two-person interview in a conference
+       synthetic robes,” Vi Khi Nao answers by way of her first book, <cite> The
+       Vanishing Point of Desire </cite> (2011). And in this novel-in-roomily-white-spaced-prose,
+       the bleh ritual of a two-person interview in a conference
        room is rejigged, via the 1st person narrator’s eros-engined mid-interview
        reveries, to encapsulate Charles de Gaulle Airport, of all earthly backdrops.
        <br/>
@@ -52,7 +52,7 @@ export default function EvenFlesh() {
        including “the torment or equanimity they must feel having to exist with one another.”
        <br/>
        <br/>
-       In the seven-some years since <cite>The Vanishing Point of Desire</cite>,
+       In the seven-some years since <cite> The Vanishing Point of Desire</cite>,
        Vi Khi Nao has persisted to chart a singularly protean course through seven-plus
        books of poems and prose, with the latter’s component pieces often mixing the lyric,
        the essayistic, and the fictive. But even prior to the cross-genre autonomy there
@@ -63,12 +63,12 @@ export default function EvenFlesh() {
        To focus however for now on Nao’s earlier oeuvre—and to again monitor the way that
        Nao subverts contemporary life’s sundry supposedly urgent ceremonies—it is worth
        pondering the consistency of space in Nao’s punchily dexterous, poem-speckled
-       fiction collection, <cite>Oh, God, Your Babies Are So Delicious!</cite> (2015,
+       fiction collection, <cite> Oh, God, Your Babies Are So Delicious! </cite> (2015,
        Per Second Press).
        <br/>
        <br/>
        A profanely diffuse cosmos wherein the wedding of piety and conventionality
-       births brisk breeds of everyday extremity, <cite>Oh, God, Your Babies Are So Delicious!</cite>
+       births brisk breeds of everyday extremity, <cite> Oh, God, Your Babies Are So Delicious! </cite>
        conjures characters who are as self-subvertingly proverbial as a home invasion
        carried out by the home’s very constituents. There is a daughter who sabotages
        an impending family dinner by resolutely vanishing. There is a shopper intent
@@ -86,8 +86,8 @@ export default function EvenFlesh() {
        the atavistic sensorial invasion she feels upon hearing her mother screaming
        after being pierced by a sewing needle. “Sonic flesh has none,” Nao writes,
        apropos of the yelling, “only pores.” So it is that where the meeting of cold
-       utility and sensory shiftiness in <cite>The Vanishing Point of Desire</cite> is generally
-       liberating, the world of <cite>Oh, God, Your Babies Are So Delicious!</cite> renders
+       utility and sensory shiftiness in <cite> The Vanishing Point of Desire </cite> is generally
+       liberating, the world of <cite> Oh, God, Your Babies Are So Delicious! </cite> renders
        the ears and eyes and their ilk into such profoundly vulnerable sieves
         that bodily harm seems an intrinsic facet of a more general doom.
         <br/>
@@ -104,7 +104,7 @@ export default function EvenFlesh() {
         <br/>
         <br/>
        This sense of everyday brutality is pushed to yet further organically profane
-       extremes by the way that <cite>Oh, God, Your Babies Are So Delicious!</cite>
+       extremes by the way that <cite> Oh, God, Your Babies Are So Delicious! </cite>
        often imbues inanimate objects—modern machines, frequently—with sentience
        and even desire. One story is dramatically hinged upon a living breathing
        (even boredom-capable) Xerox machine, say, self-conscious of its own obligatory
@@ -135,7 +135,87 @@ export default function EvenFlesh() {
         <br/>
         <br/>
         <pre className={styles.standardProseSectionBreak}>{` —  —  —  —  —  — `}</pre>
-
+        <br/>
+        <br/>
+        Building imaginative worlds whose radiance both stems from and rivals that of our
+        own, Vi Khi Nao has alas not filled some or other literary niche so much as embodied
+        an itinerancy whose elasticity is so generative as to subsume genre. And if Nao’s
+        practicality-jabbing fits of fancy run the risk of verging into aloofness—the
+        contradictory-seeming boast by the vehement Realist painter Gustave Courbet that
+        he even makes stones think comes to mind—Nao’s recent work serves as model rebuttal.
+        <br/>
+        <br/>
+        See for instance the errantly erudite fiction collection <cite> A Brief Alphabet of
+        Torture </cite> (2017, FC2), where Nao’s uncompromising shifts of register prove not
+        at all ill-fitted for prodding topics such as the war in Syria, suicide bombers,
+        and the state-sponsored torture invoked by the title-story. Or otherwise peruse Nao’s
+        cooly perilous collection of poems, <cite> The Old Philosopher </cite> (2016, Nightboat), where
+        faith in realism itself seems disturbingly compatible with subservience.
+        <br/>
+        <br/>
+        Nao’s most recent collection, <cite>Sheep Machine</cite> (2018, Black Sun Lit) is meanwhile
+        her second consecutive book—the first being <cite>Umbilical Hospital</cite> (2017, 1913 Press)—prodding
+        Leslie Thornton’s <cite>Sheep Machine</cite>. The latter is a two minute and fifty-two-second-long
+        film in which the visual plane is bifurcated into two circular images—the one on
+        the left consisting of sheep grazing beneath cables cars in the Alps; and the one
+        on the right comprised of a digitally doctored kaleidoscopic dance. And where
+        <cite> Umbilical Hospital </cite> embodies a more traditionally free-verse formatted (if topically
+        diffuse) mode of ekphrasis, Nao’s <cite>Sheep Machine</cite> is at once more open and more
+        precise in the way it sacrifices overt lyric form for a prose-poem structure
+        while also literally reproducing—with each title devoted to one of the movie’s
+        particular seconds, and with the seconds moving in a linear fashion—the majority
+        of the frame-by-frame content.
+        <br/>
+        <br/>
+        As far as debased pragmatism goes, the wool-bestowing, milk- and meat-granting
+        sheep stands as but one of Nao’s oeuvre’s many keen metaphors for the brute reduction
+        of organic complexity. A long-time Vi Khi Nao fan of a pessimistic persuasion might
+        even fear the preset dialogic mechanics of ekphrasis to be too circumscribing, capable
+        of reducing Nao’s incomparable range something terrestrial. Yet Nao’s supra-book
+        fixations prove anything but reductive, what with the way that the poems of
+        <cite> Sheep Machine </cite> tunnel through Thornton’s own <cite>Sheep Machine </cite>
+        until centripetal attention somehow breeds centrifugal musings and “the quotidian
+        terrain begins to terrify the lens of perception.”
+        <br/>
+        <br/>
+        As duly frenetic as any setting in Nao’s oeuvre, the landscape of <cite> Sheep Machine </cite>
+        both hosts and causes action, comments and is commented upon. “Even the grass, calm in a sea
+        of sunlight,” the 1:07 mark has it, “can over exhaust our imagination when we don’t prepare
+        for the worst.” And does it really come as a surprise that the-mountainside-backdrop-
+        according-to-Nao (especially the vegetation through which the sheep graze) is rendered
+        as if in competition with the cable cars? In fact, where the wheat is forced, for instance,
+        “to overcompensate for its lateral, provincial demeanor with a more ballistic flair,”
+        pathos is induced with supra-temporal exuberance, fostering an atmosphere of conjecture
+        that is somehow both pre- and hyper-modern.
+        <br/>
+        <br/>
+        Amidst the film-turned-poem’s unfolding however, one thing is certain: the narrator’s voice
+        roves with weirdly centering élan, at times weirdly personal—musing upon a nephew’s mid-bathroom-
+        habit facial expressions, say—and at times studied, such as when Dr. Bhrigupati Singh is
+        invoked to provide geo-philosophical contextualization. But if even, as Nao writes at the 1:30
+        mark, “the small organic gestures of life emulate the various efforts of modernization,” is
+        not the natural world fated for uniform exploitation?
+        <br/>
+        <br/>
+        “Most sheep devote their lives to be de-skinned, to be de-wooled,” Nao states as the
+        movie-mirroring poem-sequence approaches its own conclusion. “What is the purpose of a human’s
+        existence? If not to be breathing on earth, our soul deskinned, de-wooled for ontological
+        certainties?” But as the film is all the while being ekphrastically peeled and peeled, it
+        becomes apparent that the systematic nature of Nao’s own ordering principle would, with
+        signature dexterity, subsume the very sort of interspecies power that the narrative is
+        all the while critiquing.
+        <br/>
+        <br/>
+        The wont to simply break down a motion picture into its constituent parts—to return,
+        that is to say, a kinesis-dense, stock unit of modern life such as a film to its frame-by-frame
+        units of stasis—is itself a fruitfully intrusive stratagem granted extra vigor via the assiduousness
+        of Nao’s attention. But if the overarching reality of early 21st century modernity is such
+        that protracted attention itself seems exotic if not luxurious, <cite>Sheep Machine</cite>’s dispersed
+        thematics also seem a subversion of the entrapment-conducive fixity of prolonged attention.
+        Perhaps the reality ensnaring us—an overarching reality of our own making, legitimized anew
+        each day by the cynical acts to which we accede and accede—is ripe for an ameliorating
+        flux via our own rejigged will. “I have nothing to say to things,” Vi Khi Nao insists,
+        elucidating a frontier in an otherwise brink-breaching oeuvre, “that appear to be not changing.”
        </article>
 
 
