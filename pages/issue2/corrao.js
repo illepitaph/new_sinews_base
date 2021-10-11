5426004c3fcdf83f261242d19852f4e718bc64ee
Steve Barbaro <steve.barbaro@gmail.com>
2021-10-10 22:14:39 -0500

    finishes issue2's corrao contribution

--- pages/issue2/corrao.js
+++ pages/issue2/corrao.js
@@ -27,6 +27,8 @@ export default function Corrao() {
        <LeftGrayTitle title="the archaeology of virtual spaces"/>
        <RightBlackTitle title="... mike ... corrao ..."/>
        <StandardProse>
+       <br/>
+       <br/>
        The subject of my investigation is not the organism that you thought it was.
        <br/>
        <br/>
@@ -55,31 +57,386 @@ export default function Corrao() {
        <br/>
        <br/>
        </StandardProse>
-<StandardLineation
-content={` (it does not need to be this way. it is not necessary that we experience space as it asks
-  us to. that we move through labyrinthine trenches. acquiring certain keys and passcodes
+      <StandardLineation    content={`
+     (it does not need to be this way. it is not necessary that we experience space as it asks
+us to. that we move through labyrinthine trenches. acquiring certain keys and passcodes
 
-         a series of multi-colored dreams. landscape of the body. as it is projected onto this
-      abstract mechanism
+     a series of multi-colored dreams. landscape of the body. as it is projected onto this
+abstract mechanism
 
-         we can refuse. stand further away. remove ourselves. or disregard what we have been
-      asked to do. the constraints we have been asked to adhere to)
+     we can refuse. stand further away. remove ourselves. or disregard what we have been
+asked to do. the constraints we have been asked to adhere to)
 
-      CERTAIN TOOLS ARE GATHERED AND TESTED / MADE USEFUL
+CERTAIN TOOLS ARE GATHERED AND TESTED / MADE USEFUL
 
-      MANIPULATIONS OF THE TEMPORAL / VISION SPANNING THE ARCHIVE
+MANIPULATIONS OF THE TEMPORAL / VISION SPANNING THE ARCHIVE
 
-      REPLICATED GLITCHES TO EVADE BOUNDARY WALLS
+REPLICATED GLITCHES TO EVADE BOUNDARY WALLS
 
-      UN-CORPOREALIZATION / I CANNOT FEEL / I CANNOT BE FELT
+UN-CORPOREALIZATION / I CANNOT FEEL / I CANNOT BE FELT
 
-      COMPRESSION AND DECOMPRESSION OF THE IMAGE ARTIFACT
+COMPRESSION AND DECOMPRESSION OF THE IMAGE ARTIFACT
 
-      INTRUSION UPON HIDDEN CODE LANGUAGES / PROPHETIC SCRIPT
+INTRUSION UPON HIDDEN CODE LANGUAGES / PROPHETIC SCRIPT
 
-      MODIFICATION OF THE VIRTUAL BODY / BECOMING-SPECTRE
+MODIFICATION OF THE VIRTUAL BODY / BECOMING-SPECTRE
 
          `}/>
+      <StandardProse>
+      It is not without reason that I suggest this new kind of excavation. Assisted
+      by digital tools—a time machine only insofar as a glossary denotes what has passed.
+      Where the researcher may examine each iteration of the subject through its duration.
+      <br/>
+      <br/>
+      Collaging the archive into its broader width. The entire body of the webpage. In
+      its enigmatic youth and its plastic present. Where one becomes several. A multiplicity
+      opening its coat to reveal the row of teeth / field of anuses.
+      <br/>
+      <br/>
+      This revealing gives potentiality to an archaeology of virtual space(s). Where
+      the researcher may navigate these labyrinthine temporalities. Documenting the various
+      and ever-changing perspectives of the user. Detailing the mutations in their interface
+      and its complexity.
+      <br/>
+      <br/>
+      The events that have led to its current functionality and aestheticization. What is
+      pretty and what is active. The disparity between what is decadent and what is utilitarian.
+      Sites that began in primitive fashion. Text transposed onto screen. That have slowly
+      mutated into extravagant gestures. Text obscured by its stationary.
+      <br/>
+      <br/>
+      I want you to know that this is not a search for the remnants of an organism. It is not
+      the archaeology of your predecessors. It is the deciphering of dead and obscured text.
+      The locales produced through the mutation of language into construction materials.
+      <br/>
+      <br/>
+      Where the text has been made navigable by geographical means. Only to then be neglected—for
+      whatever legitimate or illegitimate reasons. It is the desire of the research to find and
+      document these dilapidated sites / architectures / interfaces.
+      <br/>
+      <br/>
+      </StandardProse>
+      <StandardLineation    content={`
+     (in the enigmatic structure. which the researcher has tasked themself with occupying.
+the labyrinth appears impenetrable. with each nuanced pattern complexifying further
+
+     but certain tools are utilized. tools which exploit the shortcomings of the game's frame-
+rate. giving the virtual body an inhuman speed
+
+     allowing them to move so fast as to render them briefly incorporeal
+
+     each surface reveals a becoming-membrane. the virtual body passing through this
+matrix. and establishing itself outside of the boundary.
+
+     in the non-space that encompasses the labyrinth. landscape of unfinished platforms and
+deceptive collision)
+
+         `}/>
+         <StandardProse>
+         This perspective that you have taken up is incomplete. It is the first-hand
+         vision of a half-formed avatar. Limited by the scope of its eyes—the lack
+         of peripherals in any direction. When lunging through the boundary you briefly
+         lose the ability to see. Your skeptical eye blinks and you are in another dimension.
+         <br/>
+         <br/>
+         And if this is the case, then what good did the intricacies of your movement
+         do you? If your techniques are haphazard and sensitive. Your praxis is
+         irreplicable. It demands labor and apprenticeship.
+         <br/>
+         <br/>
+         The means by which you project yourself are inadequate. This is what I mean
+         to say. Your virtual body must be built from more robust materials. It cannot
+         be derived from the same linguistic components as the text. You should not
+         look like the space that you have chosen to inhabitat. It will reveal your biases.
+         <br/>
+         <br/>
+         In the construction of this new virtual form, certain questions must be raised.
+         Primarily what differences exist between this avatar and your self. What shortcomings
+         this uncovers in your corporeal form. How this new vessel can undermine these shortcomings
+         and make you a better researcher than you are now. Than you otherwise could be.
+         <br/>
+         <br/>
+         The archaeology of virtual spaces demands a certain nostalgia—as I’m sure most historical
+         professions do. In my excavation of the low poly labyrinth I am guided by faint memories
+         of its original arrangement. My fondness for these patterns. My current love for how the
+         surfaces have remained unchanged since our first encounter.
+         <br/>
+         <br/>
+         I DO REALIZE THE ESCHERIAN NATURE OF THESE ENIGMATIC STRUCTURES
+         <br/>
+         <br/>
+         THE WAYS IN WHICH I AM BEING MANIPULATED BY UNSEEN MECHANISMS
+         <br/>
+         <br/>
+         BUT I HAVE CERTAIN TOOLS / METHODS
+         <br/>
+         <br/>
+         IN MY ESCAPE FROM THE BOUNDARIES OF THIS VIRTUAL SPACE
+         <br/>
+         <br/>
+         / I HAVE FOUND THE SWITCHES THAT ACTIVATE MY IMPRISONMENT
+         <br/>
+         <br/>
+         I HAVE TESTED THEIR VARIOUS OUTCOMES / REARRANGEMENTS
+         <br/>
+         <br/>
+         IN THE NON-SPACE WHICH SURROUNDS THE LABYRINTH I AM DEITY
+         <br/>
+         <br/>
+         THE RESEARCHER IS GIFTED AN UNCOMMON CONTROL OVER THEIR SUBJECT
+         <br/>
+         <br/>
+         AN OCCULT ACADEMIC IS ENCOURAGED BY THEIR SUCCESS
+         <br/>
+         <br/>
+         / TO CONTINUE ENGAGING / WITH AN OTHERWORLDLY PRAXIS
+         <br/>
+         <br/>
+         The contents of this archaeology are vast and seemingly endless. Not only
+         in the variety of its branches—game-based, web-based, of the interface,
+         internal, etc—but in the multitudes which can potentially spawn from these
+         broader entrances.
+         <br/>
+         <br/>
+         The virtual spaces of the game are not compatible with the non-spaces that
+         surround it, or the webpages which exist themselves in a distinct channel
+         of their own.
+         <br/>
+         <br/>
+         Each new iteration altering the medium into something beyond the limitations
+         of its nomenclature. A return to the transition from one to several. The
+         silhouette of a desirable body laid across the expanding field.
+         <br/>
+         <br/>
+         </StandardProse>
+         <StandardLineation    content={`
+                        ::archaeology of server-projected phantoms
+
+                        ::archaeology of impossible architectures
+
+                        ::archaeology of low poly surfaces
+
+                        ::archaeology of the digital rhizome
+
+                        ::archaeology of neglect and decay
+
+                        ::archaeology of what disappears from the internet
+
+                        ::archaeology of restored sludge and video
+
+                        ::archaeology of extramatrical anomalies
+
+                        ::archaeology of defunct forums and file links
+
+                        ::archaeology of primitive coding languages
+
+                        ::archaeology of virtual space
+
+                        ::archaeology of digital badlands
+
+                        ::archaeology of boundary glitches and mods
+
+                        ::archaeology of ominous non-spaces
+
+                        ::archaeology of falsified floor plans and concealed blueprints
+
+                        ::archaeology of in-game histories and artifacts
+
+                        ::archaeology of compression
+
+                        ::archaeology of the interface
+
+            `}/>
+            <StandardProse>
+            Primitive archaeologists have already begun documenting the anomalies
+            spawning in the non-space surrounding the virtual structure. With the
+            use of simplistic tools—mods and glitch exploits. But their activities
+            are touristic. A desire to visit enigmatic landmarks. The documentation
+            itself only occurring as a means to monetize this vacation.
+            <br/>
+            <br/>
+            But it is a record nonetheless. It is findable. Its shortcomings lie
+            elsewhere. In the project of the researcher. The user-tourist is not
+            an investigator. They do not study and inspect the artifact / anomaly.
+            And because of this, its documentation remains shallow. We are only
+             given a record of its aesthetic qualities. Not of its purpose or context.
+            <br/>
+            <br/>
+            (but you think very highly of yourself. and you say that you will not
+            do the same. after the construction of the improved virtual body
+            <br/>
+            <br/>
+            / you will document these anomalies only to delay their obfuscation.
+            to maintain their presence in history
+            <br/>
+            <br/>
+            but what importance does this have? with the ever-expanding depth of
+            this field. of history itself. how do you expect to maintain records
+            of each simulated dimension
+            <br/>
+            <br/>
+            do you think so highly of yourself? do you call yourself an archaeologist?
+            an archivist? is this only a hobby? your reaching towards meaning
+            <br/>
+            <br/>
+            one with a history of archiving other histories. but even this will
+            bring new questions to light. what will contain the history of this
+            place? can you bring yourself to trust
+            <br/>
+            <br/>
+            / these virtual spaces. enough at least to hand it all your findings
+            and conclusions)
+            <br/>
+            <br/>
+            But this non-space is not only a zone of its own anomalies, but an
+            opportunity to freely maneuver through the virtual structure it
+            surrounds—it is here that we should say that the ‘virtual structure’
+            is not specific, but archetypal. In the void, certain qualities become
+            visible.
+            <br/>
+            <br/>
+            Malfunctions and dilapidations in the walls and floor reveal themselves.
+            Each pocket of empty space is denoted by flickering light and texture.
+            The back end of surfaces are exposed to the vacuum / atmosphere. The
+            sloppy ways in which the invisible is assembled.
+            <br/>
+            <br/>
+            Walls that intersect and stretch beyond the connecting floors and ceilings.
+            The apparatus as a whole floating in this voidal dimension. Outside of the
+            boundaries, the user is not subject to the facades of the developer.
+            The aesthetic prominence of the interior is subverted by the ugliness of
+            its assembly.
+            <br/>
+            <br/>
+            In an archaeology of virtual spaces, the user must move beyond the interior.
+            Utilizing non-space to more fully understand what it envelopes / what it is
+            hiding. This is not a field of surface studies—although they are contained
+            within it.
+            <br/>
+            <br/>
+            What is then required is a vessel capable of more reliably moving between
+            interior and exterior / space and non-space. But there are difficulties in
+            this. Similar to those experienced by the corporeal engineer. The builder
+            of the space suit.
+            <br/>
+            <br/>
+            But here our dilemma is not the shift between atmosphere and the lack thereof,
+            it is the jump from facade to reality. In non-space, the fraudulent physics of
+            the virtual is enhanced. Viewing the interior as a place of carefully regulated
+            conditions, the exterior is a zone of neglect. It is not treated with the same
+            polish as its counterpart.
+            <br/>
+            <br/>
+            </StandardProse>
+            <StandardLineation    content={`
+          (this new body must be more complete than your own. it cannot merely be a projection
+    of your self
+
+          flesh is poor building materials. even in a dimension without organic decay you are still
+    fragile. and quick to fall apart
+
+          instead there need be a utilization of the medium. exploiting the un-corporealized body
+    and its spectral tendencies
+
+          not body but body-esque. kind to your physical inclinations but transparent nonetheless.
+    in the expanded field i fall through)
+
+               `}/>
+            <StandardProse>
+            IN THE EXPANDED FIELD I FALL THROUGH / AND REORIENT MYSELF
+            <br/>
+            <br/>
+            TO THE UN-CORPOREALIZED VESSEL
+            <br/>
+            <br/>
+            DRESSED IN LOW POLY SURFACES / FACSIMILE OF SOMEONE
+            <br/>
+            <br/>
+            WHAT ARE YOU ACCELERATING TOWARDS?
+            <br/>
+            <br/>
+            THE DEATH OF THIS PHYSICAL PLACE / THE BIRTH OF
+            <br/>
+            <br/>
+            / AN ARCHITECTURAL LANGUAGE / ROOM OF TEXT
+            <br/>
+            <br/>
+            DO YOU RECOGNIZE ME? IN THIS UNFAMILIAR ZONE?
+            <br/>
+            <br/>
+            AT THE DAWN OF THIS REWORKING / SHIFT IN LOCOMOTION
+            <br/>
+            <br/>
+            I CRAWL FORWARD / INTO THE NON-SPACE OF MY DISCOVERY
+            <br/>
+            <br/>
+            It is in this body-esque position that we are exposed to the erotic
+            qualities of the virtual space. Not only in its relationship to the directly
+            pornographic—the millions of videos and images poorly stored on various
+            platforms—but in the disparities between the user and their representation.
+            This representation, as we have spoken of already, not a direct copy, but
+            instead an improvement of its original. So said original may fully utilize
+            its mobility in the virtual.
+            <br/>
+            <br/>
+            In these differences a certain othering is bound to happen. The user
+            bears witness to their lack. The details of their shortcomings are
+            subverted by a better body. Which they are then given the power to occupy.
+            <br/>
+            <br/>
+            The body-esque seduces the subject by subverting the lack that they see
+            within themselves. The physicality of the subject loses its fleshy qualities.
+            It becomes plastic and unreal. Liken to the fantasies of cinematic and pornographic
+            art. The user a participant of this masturbatory simulation.
+            <br/>
+            <br/>
+            In our movement towards a refined praxis, a new dilemma arises. That of preoccupation.
+            <br/>
+            <br/>
+           </StandardProse>
+           <StandardLineation    content={`
+                          ::defunct root of the rhizome
+
+                          ::user-to-arbor
+
+                          ::river of filtered data
+
+                          ::becoming-lack
+
+                          ::seducer body-esque
+
+              `}/>
+            <StandardProse>
+            The subject of my investigation is not the organism that you thought
+            it was. It is an enigmatic structure. With the qualities of an organism.
+            In so much as it is bound to sentient change. To mutation and cyclical rebirths.
+            <br/>
+            <br/>
+            At the base of the interface I witness a cinematic series of dreams. The shift
+            from sitemap to labyrinthine floor plans. The excavation of the digital badlands
+            performed simultaneously as reader and actor. Eyes scrolling across chains of text.
+            Words highlighted as the entrances to other chains—other sets of data.
+            <br/>
+            <br/>
+            The user performs a summary of their existence. Creating shorthand variations
+            of their identity. Turning one to several. The shift then is from user to arbor.
+            These performances becoming the origin point of infinite branches.
+            <br/>
+            <br/>
+            Each made with the intent of invigorating the arbor, but with the outcome of
+            severing from its trunk. Growing their own branches and rootways. Which will
+            then—in a manner evolutionary to the shortcomings of the arbor—disguise their
+            origin point. Turning arbor into rhizome. Constructing their own labyrinthine
+            interiors.
+            <br/>
+            <br/>
+            The user becoming host to a primitive virtual space of their own unintentional
+            design.
+            <br/>
+            <br/>
+            <br/>
+            </StandardProse>
+
 
               <Footer/>
                     </body>
