a1891dfcfe98bccd9d24791cff143b8aa9f04886
Steve Barbaro <steve.barbaro@gmail.com>
2021-10-20 20:15:23 -0500

    further updates carousel component

--- components/carousel.js
+++ components/carousel.js
@@ -8,18 +8,22 @@ export default function Carousel() {
 
 const [currImg, setCurrImg] = useState(0);
   function prevImage() {
+    if (currImg > 0) {
     setCurrImg(currImg - 1);
+   }
   }
 
   function nextImage() {
+    if (currImg < 6) {
     setCurrImg(currImg + 1);
+   }
   }
 
   return (
     <div className={styles.carousel}>
-    <div className={styles.prev} onClick={prevImage}>&#10094;</div>
+    <div className={styles.prev} onClick={prevImage}>&#10094;&#10094;&#10094;</div>
+    <div className={styles.next} onClick={nextImage}>&#10095;&#10095;&#10095;</div>
     <Image src={`/images/Zamora${currImg}.png`} width={800} height={800} alt="Selections from 'Quotient'" className={styles.imagery}/>
-    <div className={styles.next} onClick={nextImage}>&#10095;</div>
     </div>
   )
 }
