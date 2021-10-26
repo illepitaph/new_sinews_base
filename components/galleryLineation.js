6261b2383ec7ca173c4f25350d839ed3b1091f62
Steve Barbaro <steve.barbaro@gmail.com>
2021-10-25 21:26:12 -0500

    stages initial component for ultimate correction of carousel gallery data

--- components/galleryLineation.js
+++ components/galleryLineation.js
@@ -4,6 +4,8 @@ import styles from '../styles/GalleryLineation.module.css'
 
 export default function GalleryLineation(props) {
   return (
+    <div className={styles.centeringContainer}>
     <pre className={styles.galleryLineation}>{props.children}</pre>
+    </div>
   );
 }
