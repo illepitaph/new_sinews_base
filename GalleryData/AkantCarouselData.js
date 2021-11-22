120b4aa7dc4bfcaf19bb309c4ffcffb6db25504b
Steve Barbaro <steve.barbaro@gmail.com>
2021-11-21 19:48:06 -0600

    instantiates + all but finishes akant's issue3 contribution

---
+++ GalleryData/AkantCarouselData.js
@@ -0,0 +1,29 @@
+import { AkantRawText1 } from "../galleryText/akantGalleryText.js"
+import { AkantRawText2 } from "../galleryText/akantGalleryText.js"
+import { AkantRawText3 } from "../galleryText/akantGalleryText.js"
+import { AkantRawText4 } from "../galleryText/akantGalleryText.js"
+import { AkantRawText5 } from "../galleryText/akantGalleryText.js"
+import { AkantRawText6 } from "../galleryText/akantGalleryText.js"
+import { AkantRawText7 } from "../galleryText/akantGalleryText.js"
+import { AkantRawText8 } from "../galleryText/akantGalleryText.js"
+import { AkantRawText9 } from "../galleryText/akantGalleryText.js"
+import { AkantRawText10 } from "../galleryText/akantGalleryText.js"
+import { AkantRawText11 } from "../galleryText/akantGalleryText.js"
+import { AkantRawText12 } from "../galleryText/akantGalleryText.js"
+import { AkantRawText13 } from "../galleryText/akantGalleryText.js"
+
+export const akantText = [
+  {title : "Akant 1", textName:AkantRawText1},
+  {title : "Akant 2", textName:AkantRawText2},
+  {title : "Akant 3", textName:AkantRawText3},
+  {title : "Akant 4", textName:AkantRawText4},
+  {title : "Akant 5", textName:AkantRawText5},
+  {title : "Akant 6", textName:AkantRawText6},
+  {title : "Akant 7", textName:AkantRawText7},
+  {title : "Akant 8", textName:AkantRawText8},
+  {title : "Akant 9", textName:AkantRawText9},
+  {title : "Akant 10", textName:AkantRawText10},
+  {title : "Akant 11", textName:AkantRawText11},
+  {title : "Akant 12", textName:AkantRawText12},
+  {title : "Akant 13", textName:AkantRawText13}
+]
---
+++ galleryText/akantGalleryText.js
@@ -0,0 +1,229 @@
+import RightGrayTitle from '../components/rightGrayTitle.js'
+import LeftBlackTitle from '../components/leftBlackTitle.js'
+import GalleryLineation from '../components/galleryLineation.js'
+import styles from '../styles/Akant.module.css'
+
+export function AkantRawText1(){
+  return   (
+          <>
+          <LeftBlackTitle><span className={styles.blackShadow}><h3>_<span
+          className={styles.redShadow}>_</span>_     s<span className={styles.redShadow}>a</span>ra<br/><span
+          className={styles.redShadow}> d</span> e <span
+          className={styles.redShadow}>n</span> <span
+          className={styles.redShadow}>i</span> z <br/>akan<span
+          className={styles.redShadow}>t</span> _<span className={styles.redShadow}>_</span>_
+          </h3></span></LeftBlackTitle>
+          </>
+    )
+}
+
+export function AkantRawText2(){
+  return   (
+          <>
+          <RightGrayTitle><h3><span className={styles.redShadow}>h o w</span></h3></RightGrayTitle>
+          <h3 className={styles.bigT}>T</h3><RightGrayTitle><h3><span
+          className={styles.redShadow}>lang ed</span></h3></RightGrayTitle>
+          </>
+    )
+}
+
+export function AkantRawText3(){
+  return   (
+          <>
+          <GalleryLineation><pre>
+{`T spoke languages, T langed. T worked for centuries
+in high and stately governments. Breathed in fishponds,
+lived as chairs. Moved into a big house
+all together. T stole their religion, T
+langed. Took over from the East, tapped his wrist
+inside his brain.
+
+
+T were forbidden to see the priest’s
+daughter. T never learned
+to pray.            `}<i>Now the form and not the substance of the script</i>{`
+                    `}<i>regarding T has changed</i>{`.
+
+
+But as I said, T langed.
+            `}</pre></GalleryLineation>
+          </>
+    )
+}
+
+export function AkantRawText4(){
+  return   (
+          <>
+          <GalleryLineation><pre>
+{`T-Reason for the tree ::
+           Oh ! The city crumbled and we lost everything.
+           Bitches point back and say `}<i>T</i>{`.
+           See ? `}<i>T is where I’m from</i>{`.
+           I come from the seat of `}<i>the empire</i>{`
+           `}<i>that ruled the world</i>{`.
+
+  And I’m very well placed in that hierarchy.`}</pre></GalleryLineation>
+          </>
+    )
+}
+
+export function AkantRawText5(){
+  return   (
+          <>
+          <GalleryLineation><pre>
+{`             T runs ahead and lies
+             against the television now.
+
+             We watch T-Magnificent, attending him with honor.`}</pre></GalleryLineation>
+          </>
+    )
+}
+
+export function AkantRawText6(){
+  return   (
+          <>
+          <GalleryLineation><pre>
+{`My first son T was a writer and an archivist.
+He would simply never tire of the past.
+
+What a love of documents in the desire to become !
+
+`}<i>Who is T</i>{` ? He’d always ask –
+and shove a finger on a photo of T-imself.
+
+
+And so I ask you.`} <i>Who is T</i>{` ?
+
+
+
+Later on, T changed his lang sauce, man.
+T-totaled along the normal human
+trajectory :: from poet to novelist, from diplomat
+to topless whore.`}</pre></GalleryLineation>
+          </>
+    )
+}
+
+export function AkantRawText7(){
+  return   (
+          <>
+           <GalleryLineation><pre>
+{``}<i>Two Ts walk into a bar</i>.{`
+An and Han.
+They tell a story about T’s father, the Khan
+Later on in the car, we dead-end fight.
+
+What were all of T’s names before ?`}</pre></GalleryLineation>
+          </>
+    )
+}
+
+export function AkantRawText8(){
+  return   (
+          <>
+          <GalleryLineation><pre>
+{`T-ilhan means a music; means a song.
+My name is equal to a pure solemn vow.
+T-eyhan means a tragedy performed against
+new soil, and T-ohan is the act of being born
+against the sun. Whereas T –
+T just loved photocopy. Everything you had
+he would make photocopy.`}</pre></GalleryLineation>
+          </>
+    )
+}
+
+export function AkantRawText9(){
+  return   (
+          <>
+          <GalleryLineation><pre>
+{`As for me ? I feel very strongly. I have the weight
+of a centuries old man. I get it. I have a thousand pangs
+for your own damn history. Still, I come back
+with my charm
+       and my quiet capability. Eagerly I show to others
+that I’m no backward alien. There’s no
+scary magic coming from my absent hand.`}</pre></GalleryLineation>
+          </>
+    )
+}
+
+export function AkantRawText10(){
+  return   (
+          <>
+           <GalleryLineation><pre>
+{``}<i>Two Ts walk into a bar</i>.{`
+Why is it so funny ?
+Are you a terrorist, or a tourist ?
+What is the relationship between sacrifice, and purpose ?
+
+That’s the question I was thinking about
+six hundred years ago.`}</pre></GalleryLineation>
+          </>
+    )
+}
+
+export function AkantRawText11(){
+  return   (
+          <>
+          <GalleryLineation><pre>
+{`By the way, there’s an enormous painting
+of T the Mag in Congress.
+
+In a certain sense, I’ve been so well-absorbed.
+
+By the way, I’ve decided that I want to be buried.
+
+                 `}<i>It is time to crawl down</i>{`
+                 `}<i>from the ceiling now</i>.{`
+`}</pre></GalleryLineation>
+          </>
+    )
+}
+
+export function AkantRawText12(){
+  return   (
+          <>
+          <GalleryLineation><pre>
+{`I paced around this T some time.
+I have no wife today, out of hospitality.
+Allow me to escort you as I disappear
+behind T’s mask.
+
+
+T pulls out a lovely picture
+of a Sheraton from the sky.
+
+
+T is very beautiful, as well as
+a nice Sheraton.
+
+
+He stands `}<i>yabancı</i>{` out in front.`}</pre></GalleryLineation>
+          </>
+    )
+}
+
+export function AkantRawText13(){
+  return   (
+          <>
+          <GalleryLineation><pre>
+{`In the end, we’re all together, rolling up a scarf.
+
+
+In the end, I see how every shiny shitty thing
+is related to T, if not by culture, then by marriage.
+
+
+
+                       Tonight, I will Dracula-pray
+                       for a more meaningful translation
+                       of T’s life.
+
+
+
+By the way. I still cry at JFK
+when they say me: `}<i>welcome home</i>{`.`}</pre></GalleryLineation>
+          </>
+    )
+}
