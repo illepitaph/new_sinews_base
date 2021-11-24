b5aac674f0fcf749db2f9a9c8943a37ff108f3b1
Steve Barbaro <steve.barbaro@gmail.com>
2021-11-23 21:25:25 -0600

    more issue3 gette contributiongit add .

--- pages/issue3/gette.js
+++ pages/issue3/gette.js
@@ -36,36 +36,38 @@ export default function Gette() {
         <div className={styles.container}><Image src="/../public/images/Gette1.jpg" width={793} height={800}
         layout={`responsive`}/></div>
 
+          <div className={styles.poemContainer}>
         <p className={styles.getteLineation}>When I write the lines it comes out in a child’s voice</p>
-  <p className={styles.getteLineation}>And the child’s voice sounds like the mouth was stuffed with something—confetti,
-  cotton, dust, sock, apple, duct tape, Stoffer’s, cranberries, fists, locusts, toilet paper, unsharpened pencils, pogs,
-  kinetic sand, dry leaves, soil (wet or caked), feathers (obviously), mattress springs, tulle and tinsel, polymers and
-  lead, Jupiter’s seventh moon, the cartographic remains of a failed vacation, its fold-up road atlas, corner of a
-  month-old large supreme pizza box from Little Ceasar’s, chalk dust from sidewalk hopscotch, three neon orange
-  tyrannosaurus rex arranged in a tea party pattern on the edge of the neighbor’s roadside garden, 3 copies of Poetry
-  magazine (still in plastic), a tiny ceramic cat that bears no resemblance to the real cat you miss and its attempt
-  to fill that fur hole, one locust (real) hanging from a magnolia branch whose leaves occasion to a sunset color
-  at the edges, and are plucked in the moment of attention drifted from the locust to the leaves, coffee coasters that
-  might be bathroom tiles, the overwhelm of material detail,</p>
-  <p className={styles.getteLineation}>the shape of innocuous objects that try to fill that fur hole, fur, fur, and
-  the murmurs of others...</p>
-  <p className={styles.getteLineation}>The voice I think I am writing in is much more sophisticated, raspy even, the one
-  that has succumb to the chemical world & inhales its sentencing to produce a sound too low to hear but can be felt as
-  nausea, the innocuous appliance is a killer (this sound is a matter of time)</p>
-  <p className={styles.getteLineation}>Curlie-cues of throaty resignation that come out in-audible and supplicating so
-  that the air confuses it for gratitude</p>
+  <p className={styles.getteLineation}>And the child’s voice sounds like the mouth was stuffed with something— confetti,
+  cotton, dust, sock, apple, duct tape, Stoffer’s, cranberries, fists, locusts, toilet paper, unsharpened pencils,
+  pogs, kinetic sand, dry leaves, soil (wet or caked), feathers (obviously), mattress springs, tulle and tinsel,
+  polymers and lead, Jupiter’s seventh moon, the cartographic remains of a failed vacation, its fold-up road atlas,
+  corner of a month-old large supreme pizza box from Little Caesar’s, chalk dust from sidewalk hopscotch, three
+  neon orange tyrannosaurus rex arranged in a tea party pattern on the edge of the neighbor’s roadside garden, 3
+  copies of Poetry magazine (still in plastic), a tiny ceramic cat that bears no resemblance to the real cat you
+  miss and its attempt to fill that fur hole, one locust (real) hanging from a magnolia branch whose leaves
+  occasion to a sunset color at the edges, and are plucked in the moment of attention drifted from the locust to
+  the leaves, coffee coasters that might be bathroom tiles, the overwhelm of material detail, the shape of innocuous
+  objects that try to fill that fur hole, fur, fur, and the murmurs of others...</p>
+  <p className={styles.getteLineation}>The voice I think I am writing in is much more sophisticated, raspy even, the
+  one that has succumb to the chemical world & inhales its sentencing to produce a sound too low to hear but can be
+  felt as nausea, the innocuous appliance is a killer (this sound is a matter of time)</p>
+  <p className={styles.getteLineation}>Curlie-cues of throaty resignation that come out in-audible and supplicating
+  so that the air confuses it for gratitude</p>
   <p className={styles.getteLineation}>But I am not grateful, I think</p>
   <p className={styles.getteLineation}>it as ambient as air and innocuous as air that seems breathable enough</p>
-  <p className={styles.getteLineation}>Or nice enough, like bubbles and desk fans blowing directly into your eyeballs
-  on hot afternoons, balloons and butterflies, whatever</p>
+  <p className={styles.getteLineation}>Or nice enough, like bubbles and desk fans blowing directly into your eyeballs on
+  hot afternoons, balloons and butterflies, whatever</p>
   <p className={styles.getteLineation}>can’t remember the kicker so it goes like (                      )</p>
   <p className={styles.getteLineation}>Need to spit</p>
   <p className={styles.getteLineation}>As a dream without memory is a waste of time</p>
   <p className={styles.getteLineation}>Permanent dream permanent dream</p>
+  </div>
 
       <div className={styles.container}><Image src="/../public/images/Gette2.jpg" width={400} height={500}
       layout={`responsive`}/></div>
 
+  <div className={styles.poemContainer}>
   <p className={styles.getteLineation}>
 When I wake up I watch Marguerite Duras’ <span className={styles.grayShadow}><cite>India Song</cite></span> and I write
 down one line that seems to have hit reviewers at the time it goes “lepers burst like sacks of dust”</p>
@@ -87,15 +89,17 @@ hurtled toward but easefully ignored</p>
 sweatpants and the pink shirt with a cute pink bra</p>
 <p className={styles.getteLineation}>Hello to all my fans! you say, I love you, I love my fans...</p>
 <p className={styles.getteLineation}>In <span className={styles.grayShadow}><cite>India Song</cite></span>, the camera pans over her
-red wig, her jewelry, the piano and an incessant gesture of incense —too-long shots of the Palais Rothschild painted
+red wig, her jewelry, the piano and an incessant gesture of incense—too-long shots of the Palais Rothschild painted
 everywhere in a sick green, and I wonder if it is Paris Green, the color of Napoleon’s wallpaper that killed him by
 slow inhalation in his sleep</p>
+</div>
 
       <div className={styles.container}><Image src="/../public/images/Gette3.jpg" width={100} height={100}
       layout={`responsive`}/></div>
 
+  <div className={styles.poemContainer}>
   <p className={styles.getteLineation}>I think of this in the context of household toxins and what it means to keep
-  a house virus-free and to what extent a surface is shared if its reality isn’t— the line between dream and not,
+  a house virus-free and to what extent a surface is shared if its reality isn’t—the line between dream and not,
   virus and loved ones, sickness and health, Napoleon withering by breathing green wallpaper or Napoleon figure of
   colonial power, the evil of stillness or the evil of movement</p>
   <p className={styles.getteLineation}>When he dreamt did he dream of winning the Napoleonic wars and rising through
@@ -114,10 +118,12 @@ slow inhalation in his sleep</p>
   <p className={styles.getteLineation}>The house bursts like a sack of grain but it dreams in imperial time</p>
   <p className={styles.getteLineation}>This is the message of the film it seems, we surround the afflicted with posthumous
   gossip</p>
+  </div>
 
       <div className={styles.container}><Image src="/../public/images/Gette4.jpg" width={100} height={100}
       layout={`responsive`}/></div>
 
+    <div className={styles.poemContainer}>
     <p className={styles.getteLineation}>No one speaks they just loll around performing dull movements to this song while
     people gossip</p>
     <p className={styles.getteLineation}>At the beginning of the film it seems the voices are the ghosts, but then we
@@ -125,9 +131,100 @@ slow inhalation in his sleep</p>
     <p className={styles.getteLineation}>While the figures move in time slow as an image</p>
     <p className={styles.getteLineation}>Anne-Marie, the “promiscuous wife of the French ambassador in India” is dead</p>
     <p className={styles.getteLineation}>She grew bored with the Empire and decided to die</p>
+    </div>
 
       <div className={styles.container}><Image src="/../public/images/Gette5.jpg" width={100} height={100}
       layout={`responsive`}/></div>
+
+    <div className={styles.poemContainer}>
+    <p className={styles.getteLineation}>In the film she moves through time as a gelatinous membrane</p>
+    <p className={styles.getteLineation}>Mendicant because the beggar is described as her mirror Mendicant because no one moves
+    that slow without desire for another time or timing</p>
+    <p className={styles.getteLineation}>She can hold herself in the membrane</p>
+    <p className={styles.getteLineation}>She can puncture it or herself with mendicant gestures that follow the banality
+    of daily tasks and their absurd questions: “Does this dish belong on this shelf or that shelf?” “To turn the laundry
+    machine on, do you press in the button or turn it?” “Is this towel used for the dishes or the body?” “How do I use
+    the lawn mower?” “How much to pay the lawn mower guy?” “How many paper towels do we need?” “What brand of butter
+    do you buy?” “Can disposable gloves be used twice?” “Do you happen to have any batteries?” “Is the milk old?” “Are
+    the beans old?” “Is the Diet Dr. Pepper old?” “Where is the soap for the floor?” “Where is the soap for
+    the dishes?” “Where is the soap for the body?”</p>
+    <p className={styles.getteLineation}>If soap is what kills the lipid layers among lepers is it possible to live
+    in a bubble?</p>
+    <p className={styles.getteLineation}>Is it possible to wear soap gloves or to leave some soap on your hands so that if
+    you share a surface that has the virus the leftover soap will fight it off?</p>
+    <p className={styles.getteLineation}>If I drink bleach will I be cured of my dust?</p>
+    <p className={styles.getteLineation}>Maybe it’s not a vaccine we need maybe it’s more bubbles, bubble
+    parties for everyone</p>
+    <p className={styles.getteLineation}>You daydream a scenario in which the Great Pacific Garbage Patch might be
+    fashioned to create plastic bubbles for everyone to move around in like hazmat suits at a grocery store</p>
+    <p className={styles.getteLineation}>Napoleonic ghosts figure the distance between this daydream and a vaccine,
+    the Paris Green of malaria prevention</p>
+    </div>
+
+        <div className={styles.container}><Image src="/../public/images/Gette6.jpg" width={100} height={100}
+        layout={`responsive`}/></div>
+        <div className={styles.container}><Image src="/../public/images/Gette7.jpg" width={100} height={100}
+        layout={`responsive`}/></div>
+        <div className={styles.container}><Image src="/../public/images/Gette8.jpg" width={100} height={100}
+        layout={`responsive`}/></div>
+
+     <div className={styles.poemContainer}>
+     <p className={styles.getteLineation}>She is exhausted again, or dehydrated, and her child’s voice creeps out
+      of the body imbued with killer paint colors</p>
+      <p className={styles.getteLineation}>She thinks how grateful she is for such technicolor dreams</p>
+      <p className={styles.getteLineation}>Everyone wants this experience! She thinks, They are jealous of me! The
+      penetrating dreams that confuse reality with whatever else there is</p>
+      <p className={styles.getteLineation}>The desire to belong to one or the other without jealousy</p>
+      <p className={styles.getteLineation}>When asleep to remain asleep</p>
+      <p className={styles.getteLineation}>When awake to remain awake</p>
+      <p className={styles.getteLineation}>The dream in which someone recommends a little-known book by Lacan
+      called <span className={styles.grayShadow}><cite>Mechanics</cite></span>, in which psychoanalysis is
+      applied to robots and puppets</p>
+      <p className={styles.getteLineation}>There is an accompanying film called <span
+      className={styles.grayShadow}><cite>Imagination</cite></span></p>
+      <p className={styles.getteLineation}>But that is not real</p>
+      <p className={styles.getteLineation}>What is real is long choreography through greening
+      walls on the brink of decay <span className={styles.grayShadow}>[1]</span></p>
+      <p className={styles.getteLineation}>All poets burst like sacks of grain</p>
+      <p className={styles.getteLineation}>Wasting everyone’s foodstuffs with their holes</p>
+      <p className={styles.getteLineation}>A long choreographed circulation of variables with unseeable consequence until!</p>
+      <p className={styles.getteLineation}>A long choreography of holes,</p>
+      <p className={styles.getteLineation}>A long choreography of fear of your holes, their leaking, the fur you shed
+      and its parasites or lice and the what is it you are passing on as you pass through the park or the grocery store</p>
+      <p className={styles.getteLineation}>Or before that, toxicities airborne and sprayed over the still ponds to kill
+      the mosquitoes before they can bite humans, a larvaecide</p>
+      <p className={styles.getteLineation}>Insecticide sprayed over the cotton fields</p>
+      <p className={styles.getteLineation}>Insecticide leaking into a grain elevator full of rats that are already
+      full of Hanta virus</p>
+      <p className={styles.getteLineation}>Sometimes kids jump into the top of the grain elevator and fall through
+      then suffocate under so much grain</p>
+      <p className={styles.getteLineation}>Have you ever thought about that?</p>
+      <p className={styles.getteLineation}>What does your sandwich taste like knowing there was a dead kid and hanta
+      virus rats in the sack of grain that was a bumper crop because the government okayed a chemical yield?</p>
+      <p className={styles.getteLineation}>“Don’t be such a Debbie Downer!” says a mom, and a child’s voice leaks out
+      with toxic breath droplets like a split note from the raspy tone that gets all of these lovers and their ennui
+      baggage, who flick their cigarettes on the Paris Green floor before green becomes the cure for malaria and
+      the slow death of everyone else, or perhaps just the aristocracy, who had a penchant for green wallpaper and
+      chandeliers</p>
+      <p className={styles.getteLineation}>& whose cures for the plague included eating crushed diamonds instead
+      of spoonfuls of sugar, in which case oh well</p>
+      <p className={styles.getteLineation}>But there are also the wallpaper people in the factory making productive
+      green swirls on the daily for a market that just really likes green</p>
+      <p className={styles.getteLineation}>& people drinking unfiltered water from the marsh</p>
+      <p className={styles.getteLineation}>& people eating sandwiches</p>
+      <p className={styles.getteLineation}>& not-people soaking in the greens of not-technicolor dreams but ordinary
+      grass that is not even mantle-deep</p>
+      <p className={styles.getteLineation}>& the ordinary grass is an absurd question asked during the performance of
+      banal tasks, combing the fur and the stains from one’s jeans which are a beautiful blue because of coal tar,
+      nowadays</p>
+      <p className={styles.getteLineation}>Even indigo is out of fashion</p>
+      <p className={styles.getteLineation}>In a long choreography of real leaks, teeth on the brink of decay</p>
+      <p className={styles.getteLineation}>Skin with such huge pores they might as well be holes</p>
+      <p className={styles.getteLineation}>Swollen up with garbage—ordinary primitive accumulation</p>
+      </div>
+
+        <div className={styles.container}><Image src="/../public/images/Gette9.jpg" width={100} height={100}
+        layout={`responsive`}/></div>
 
                      <Footer/>
                            </body>
