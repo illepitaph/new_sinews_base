4aee269c6c5fb384dbd35f255bb368e5f4013940
Steve Barbaro <steve.barbaro@gmail.com>
2021-12-08 22:20:34 -0600

    all but completes issue4's palacios contribution

--- pages/issue4/palacios.js
+++ pages/issue4/palacios.js
@@ -26,6 +26,9 @@ export default function Palacios() {
 
        <CenterBlackTitle>poems from <br/><br/>_<i>lunar</i>_<i>hilton</i>_<i>elegy</i></CenterBlackTitle>
 
+       <br/>
+       <br/>
+
        <CenterGrayTitle><h3 className={styles.title}>... <span
        className={styles.inverseShadow}>g</span> a <span
        className={styles.inverseShadow}>b </span><span
@@ -43,277 +46,130 @@ export default function Palacios() {
 
         <br/><br/>
 
-<pre className={styles.foam}>{`A wildflower head with a nowness I am. The ism of absence.
-Lookit here, lookee there. Where they bristle—throng together
-in pink Samhain.
-
-
-Loose boots, la dolce vita, the melding
-smother.
-
-
-Heh. I for one am baffled. It’s a cosmic
-joke. A denouement.
-
-
-
-
-Maybe I’m conceited, or
-
-entitled even, when I resent your
-
-radiant speculations that cannot stop
-
-the brainlike thesaurus of the dollar.
-
-
-This is the value, the doll’s house, the
-virus and the cameraman are
-
-lining themselves up with the dioramas
-
-and choreographing the fate of this myth.
-I’ll not shame thy lustrous raiment. Enter this way
-
-With words as sleek as blades. Breathe
-your bass over the
-codeine cadence
-
-
-
-
-So electric, yet, through the stems we swim in
-inflexion. So who's no touch can even?
-
-Surprise the move of heart by glass wolves on bone branches.
-
-This is how we mix like species before
-Release
-
-No itty bitty red pill. No itty bitty blue
-pill.
-
-
-
-Waterfalls of vitamin C. The colors are calibrated for sunsets. Why is its presence a transgression?
-You saw how we were beginning, you do what you do, but I could not bear to imagine your own
-enema of red ants… You want to see me become ekphrasis, the jewel that blooms in indigo.
-
-
-My violence has its own kind of tingle. Unrecognize the gas-orange flounce crumpling over time.
-A comet that might have been. A supernova that might have been. The slow tip of the exponential
-funnel with incinerated lips. The vaporising slag face of unprincipled gullibility. The ghost
-anthracite.
-
-
-
-
-
-Birds do not need men
-
-
-
-to lie on the roof of the world.
-
-
-
-If I were a flower,
-
-
-
-my blooming would appear there by itself,
-
-
-
-intowards fractal dahlias
-
-
-
-with the sun high in the sky
-
-
-
-like a road I did not yet know.
-
-
-
-I ask the reason all facetiousnesses omen an absence. An occluded heave. Our forever crippled,
-our forever suffering. Unwittingly cruel. Uninformed. Brazen. The opposite of the noble instinct.
-The unceasing desire. To love. To feel. But unendurably. Indentured. The sense of loss. To hunger.
-As we starve. Exhilarating. So alluring.
-
-                      Then the lust. Then the high.
-
-                      Then the new race. To the stars.
-
-                      By night.
-
-                      While the world burns.
-
-                      It feasts.
-
-
-
-
-
-Was it the wedding's fault?! Why such a funeral?
-
-
-
-Your breast with its haywire scarves, a bird like a cherub with my handicapped face so sweet, but
-only with the whimbrels are there such truths that stain the dark cupids’ teeth—oh why don’t I
-embrace them?
-
-
-
-Had they become part of that terrible beauty
-
-In the querelle of confused sweetgrass
-
-the promise of blithely dried out glinting this night in ways as faint as the path’s aromas
-
-and I think, not again
-
-for nothing could hold back my exile’s stride. As a
-summer woman I'll be iced with azure silk
-
-feathering benzedrine with zenith signs,
-
-with such slaggard parsimony
-
-I’ll nail myself to the saguaro.
-
-
-
-As a sort of skullware lodged in the mons
-
-cut by whole-beaded rondels
-
-& transversed to flay, registrations
-lined to the neuter
-
-of thy mightiest creation, my need still propels me toward verdancy! Acidification! Licks of
-Neptune! Aye my exclamation point. Would you be my ally? Come at me thrushes!
-
-Ahhh!—hypnotized by thy energy. Oh, that energy! Oh, can't breathe!—blood running like eels
-from my nostrils! Please, play the music! Please!—Dancing, as
-
-foam from this
-sweet hour
-
-
-
-of crashing—
-
-
-
-patrician towers—
-
-I'm so happy to hear the weeping string quartet of estuaries echo
-
-
-
-&
-
-
-
-twin lavender syphillises dripping with shameless nectar
-
-from these terminal moons.
-
-
-
-—and what then shall we all do? No. I shall keep that thought for myself, pencil-pusher. Guidance
-of the drowning on the gunmetal billows. Should you disagree, agitating foreskin of thy head-
-covering, I shall carve it off with an engraving device; clock-puncher, fluxus pervert.
-
-
-
-I want to tinkle in the ficus lyrata.
-
-Make this space sweat its tardigrade-blown brains
-
-across the raft-levelling transduction
-
-of epiphanic bloom
-
-in a megadose of motility
-
-one might expect from a lesser albinism
-
-to squirt before the flood of fake teeth
-
-from titmouse to face paint
-
-I've been picking plumerias
-
-millipedes from the moist litterfall
-
-in mutant roots and network decays in
-
-as extreme a helium voice with coriander fire
-
-metaphorically
-
-being drowned under the maestro’s tongue /
-
-it/ himself/ them/ himself/ it, the rhythm
-of radically maladjusted oscilloscopes
-
-taking a novation in
-
-the drip, drip, drip of sand
-
-in murder, in celibacy, in penitence, in fruitfulness & contortion.
-
-
-
-
-Such is the exposition of man, every beating heart. Betrayal, dispassion, departure. No softer, no
-quieter, no more peaceful, for thy fervent tendril of embrace is shewn. A trust, a shroud, the vista
-of a violent threnody. To fury out of bondage out of love.
-
-Amur chokecherry, fleabane, carpathian bells, and mullein.
-
-In the light they might rise their throats wide, ruminating of something
-monstrous in the thunderheads.
-
-Nothing is impossible.
-
-Because the world is wet.
-
-
-
-Quail sounds behind the face, behind me.
-
-
-
-That I impale my eyes on the circuit of paradise when with diaphanous gloop
-smoke and toxic flora of a compendium of infections
-hold to sacrifice the direction
-& splice in the bull, maneuvering to
-cultivate
-some hegemonical metonym.
-0 to circus
-could put a gun in the muzzle of a fascist star
-and so they fade back to their mothers they have fled and gone out to seize
-the dead and distant nights
-frozen in the hosts of film
-on Colony of shoosh and over
-the saints of war
-inhere themselves under the panics of aesthetic demolition
-as brides in the voltaic flesh of putrid water
-the sun burns me under its chariot of flowers.
-
-The sun burns.
-
-
-
-
-Please, play the music! By God!—Dancing, as foam from this sweet hour
-
-of crashing.`}</pre>
+<h3 className={styles.poemTitle}>City / Gaze / Back</h3>
+<br/>
+<pre className={styles.poem}>{`I look like what I am,
+Storefront rememberer,
+A mildewed stack of dead arcana with no currency
+To lie and claim you read,
+The station every aged out abductee
+Claims to have risen to, then cut
+And run from
+Feeling for
+Old drums of future war
+In structures
+Powerless,
+Riddled by the Zippo-lighted scrawls:
+`}<i>HIT A CRIP<br/>
+IN THE LIP<br/>
+WHAT DRIP</i>{`.
+What crawls?
+Throw towels on it,
+Skewer it with push knives,
+Void the clip,
+Throw the joint in panic at the unsmotherable
+Dust pneumonia wincing, lock your door,
+List your house...
+I’m just putting shit at this point in my calendar.
+The city gazes back now`}</pre>
 <br/><br/><br/><br/>
+<h3 className={styles.poemTitle}>Walking Away</h3>
+<br/>
+<pre className={styles.poem}>{`I am waiting for my turn,
+My problems, offers come
+Rubberbanded on the sidewalk
+
+
+In the morning.
+Good morning,
+Starshine, Earth says.
+
+
+Hello, future as you woulda had it:
+Sunglass geometry,
+Wet textiles,
+
+
+Knobs to modulate
+A circuit dusted off or reconstructed.
+Try as you might
+
+
+To make a block
+Of loanmarts
+Perk up in this skillet.
+
+
+I become a muse for Helmut Newton
+There to be commuted to
+Just saucer crash distance`}</pre>
+<br/><br/><br/><br/>
+<h3 className={styles.poemTitle}>Overlay</h3>
+<br/>
+<pre className={styles.poem}>{`jailed on the six-car trains with otherpeople moving stuck
+pedaling a swan boat on La Ronde
+around the instant islands
+I can buzz around exempt behind a gilded age
+movie pimp’s mustache from being coerced
+to humanize unpaid
+
+
+holding one baby drug-sized Ziploc bag of evidence—
+hairs—
+that say a retribution myth branded almost to the bone makes
+viable offspring with the hoariest of showbiz maxims
+on our lips reflexively, a cave-mind callback
+audible as tiny mad calliope music in the sobbed suppressed
+lullabies to flesh in its collapsing conditions
+
+
+the meat of all of us mauled by park security’s dogs
+
+
+eventually a ghost pirate captain gets dragged out from his throne
+to reassure the seasick
+`}<i>all you need is faith to hear the diesels</i>{`
+overlay & transfiguration
+heavy
+hearing your own voice air from the speaker in his gut
+
+
+because my smoke-poisoned praying to be taken up
+a mothership is shale fed into
+a hopper that dispenses
+horoscope fortunes on the waves
+of excavators swinging like burlesque
+automatons`}</pre>
+<br/><br/><br/><br/>
+<h3 className={styles.poemTitle}>Colony Inside an Iron Mask</h3>
+<br/>
+<pre className={styles.poem}>{`enclaved like a shower and arcade within a rest-
+         stop    papal states
+                    shadowplay on sheets laid over dead pure forms
+
+
+`}<i>Tec out the dresser</i>{` out the cubicles H&R Block occupies
+         in         Walmart
+                     prisoner at sea
+
+
+I tend to flood my first enchanted footsteps with collateral blood
+           build   violently the white electric rail through remembering
+                      skulls
+
+
+whose war now to vibrate any further out young
+          star     file my dungeonfreak holiday as existential
+                     thrashing
+
+
+                     I’m confessing it was game
+
+                     all to germinate
+                                          a ruin`}</pre>
+<br/>
+<br/>
+<br/>
 
                      <Footer/>
                            </body>
