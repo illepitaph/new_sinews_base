30c1098f474988b5b83a5c183ea53492f715c34e
Steve Barbaro <steve.barbaro@gmail.com>
2021-12-27 14:06:17 -0600

    works deep into issue5's nao + alexander collaboration

--- pages/issue5/terrarium.js
+++ pages/issue5/terrarium.js
@@ -32,18 +32,22 @@ export default function Terrarium() {
        className={styles.inverseShadow}>m</span>y <br/><span
        className={styles.titleShadow}>bo </span>dy<br/><br/><span
        className={styles.inverseShadow}>w</span>a<span
-       className={styles.inverseShadow}>s</span><br/> a <br/><span
+       className={styles.inverseShadow}> s</span><br/> a <br/><span
        className={styles.inverseShadow}>g </span>l a <span
        className={styles.titleShadow}>ss</span><br/><br/><span className={styles.titleShadow}> t <span
-       className={styles.inverseShadow}>e </span>r</span>r<span
-       className={styles.inverseShadow}>a</span><span className={styles.titleShadow}>r </span>i<span
+       className={styles.inverseShadow}>e </span>r</span>r <span
+       className={styles.inverseShadow}>a</span><span className={styles.titleShadow}> r </span>i<span
        className={styles.inverseShadow}>u </span>m</h3></RightGrayTitle>
 
        <br/>
 
         <br/>
 
-        <CenterGrayTitle>:: vi ::<br/>:: khi nao + jess ::<br/>:: alexander ::</CenterGrayTitle>
+        <CenterGrayTitle>:: <span
+        className={styles.titleShadow}>vi</span> ::<br/>:: khi <span
+        className={styles.titleShadow}>nao</span> <span
+        className={styles.inverseShadow}>+</span> jess ::<br/>:: <span
+        className={styles.titleShadow}>alexander</span> ::</CenterGrayTitle>
 
         <br/>
         <br/>
@@ -83,12 +87,12 @@ shaped       sleeves    and      flower
 print.      `}</span><span className={styles.alternateVoiceOne}>{`You  wore a  special  skin-toned
 bra    to    cover    up      the scar bisecting
 your      chest.    At    our    apartment,      I
-folded you like`}</span>{`the stem of a flower
+folded  you like`}</span>{`the stem of a flower
 onto the bed.      `}<span
 className={styles.alternateVoiceOne}>{` Your neck smelled
 salty &  floral.`}</span><span
 className={styles.alternateVoiceTwo}>{`        You rested your left
-hand     between    your     shoulder
+hand     between    your      shoulder
 and right  breast           `}</span><span
 className={styles.alternateVoiceOne}>{`&  caught  your
 breath.  You   did  this   the  first   night  we
@@ -98,7 +102,171 @@ turned to   you without moving.`}</pre>
 
 <br/><br/><br/><br/>
 
+<pre className={styles.left}><span className={styles.alternateVoiceTwo}>{`My   travel time home   `}</span><span
+className={styles.alternateVoiceOne}>{`  from Boulder
+back     to     Denver`}</span><span className={styles.alternateVoiceTwo}>{`    has         greatly
+diminished`}</span><span className={styles.alternateVoiceOne}>{`,    in   hardship,     now   that
+you`}</span><span className={styles.alternateVoiceTwo}>{`    drive  to  campus  and collect
+  me.`}</span><span className={styles.alternateVoiceOne}>{` Thanks  to  your   tenderness,   my
+teaching    days are   no   longer  as  long
+nor     as      oxygen-deprived.    Teaching
+behind  a  mask  is  a little       dangerous.
+Even     my   student,    Sam,`}</span>{`      stops
+breathing   when  reading  out
+loud`}<span className={styles.alternateVoiceOne}>{`.   I  woke   early for   an    interview,
+and`}</span>{`  oval spots of light traveled
+up and down  my face   like  a
+diurnal     flashlight.`}<span className={styles.alternateVoiceTwo}>{`      My  face
+burst with luminosity like      bubble
+wrap.`}</span><span className={styles.alternateVoiceOne}>{`    Most    Fridays, you    are   busy
+with  Zoom meetings.`}</span>{`      Your voice
+echoes in our    loft-ish  abode
+like    a  gramophone`}<span className={styles.alternateVoiceOne}>{`.    You   are
+serious    and   friendly  and  I   fantasize;
+I’d    like   to   do   something`}</span>{`      sultry
+andscandalous`}<span className={styles.alternateVoiceOne}>{`behindyourlaptop`}</span>{`to
+make you     smile.`}<span className={styles.alternateVoiceOne}>{`    The  thawed
+out    frozen    pork    I        sauteed in red
+sauce    and salt   had a         hemoglobin
+       aftertaste,       and even the cabbage`}</span><br/>{`contained    the iron relics   of
+something    murdered. There
+was nothing natural about the
+palette transaction.`}<br/></pre>
 
+<pre className={styles.left}><span className={styles.alternateVoiceOne}>{`Last  night,`}</span><span
+className={styles.alternateVoiceTwo}>{`    my  body  hung  like  a
+glass terrarium;`}</span><span className={styles.alternateVoiceOne}>{`  I felt  `}</span>{`   the soil of
+your  fingers  beneath  me,   &
+  the   water  of  my     orgasm
+floating  in  between`}<span className={styles.alternateVoiceOne}>{`  each breath.
+Everything in me was`}</span>{`     suspended
+— between   the elevation   of
+your    tenderness     and   the
+day’s vast longing.`}</pre>
+
+<br/><br/><br/><br/>
+
+<pre className={styles.right}>{`Your    office   air   was   cool   and
+came      through      the        open
+window. `}<span className={styles.alternateVoiceOne}>{` We   applied   for  jobs  in  a  glass
+building   two stories  up  from   the  world  and
+ate lunch outside`}</span>{`   and I was  tired and
+weary of the  college boys — who
+barked   like   guard  dogs  at   the
+girls  in  sunglasses  & bikini  tops.`}<br/><span
+className={styles.alternateVoiceOne}>{`When  we  walked   across  the  green  campus
+lawn`}</span>{`  & under the  canopy of yellow
+  leaves,    I   watched   your  profile
+against the lush trees, &  students
+crowding  the corners of the bright
+afternoon,          feeding          their
+hangovers   with    angus beef    &
+grease,`}<span className={styles.alternateVoiceOne}>{`   and I    asked what you did for   fun
+in  college  and  you  said  you  read   poetry  in
+your  room.  And    now  in  your office  you  tell
+me  you’d  like `}</span><span className={styles.alternateVoiceTwo}>{` to  go  back  to  school,  in
+Switzerland.                                            `}</span></pre>
+
+<br/><br/><br/><br/>
+
+<pre className={styles.left}><span className={styles.alternateVoiceOne}>{`Even  though  I  took  only`}</span><span
+className={styles.alternateVoiceTwo}>{`    one  sip of `}</span><br/>{`the  Bread  and   Butter   wine`}<br/><span
+className={styles.alternateVoiceOne}>{`you    bought,`}</span><span
+className={styles.alternateVoiceOne}>{`   I  woke    feeling  as  if    I
+had    a   hangover.`}</span>{`       We   fought
+intensely last night`}<span
+className={styles.alternateVoiceOne}>{`             with me
+saying many    fucks.`}</span>{`   In the middle
+of  the  fight,`}<span
+className={styles.alternateVoiceOne}>{`   you       climbed out of
+bed.`}</span>{`   I  could  hear   you in the
+darkness, `}<span
+className={styles.alternateVoiceOne}>{`    putting on a   t-shirt and
+pants.`}</span>{`     I could hear the sonic,
+crisp  audio  of  you  throwing
+things        randomly        and
+mindlessly into a bag   and   I
+could  hear  the  teeth  of  the
+zipper  grinding  each  other's
+molars  as  you  zipped    one
+bag up and began another.`}<span
+className={styles.alternateVoiceOne}>{`    i
+suspected    that   you   were   preparing
+for a  night  stay  at  some  random hotel.
+My  heart      quickened  and  I           felt
+defeated. `}</span><span
+className={styles.alternateVoiceTwo}>{`            I began to prepare
+myself, my   mind   mainly,   for   a
+desolate   night,   my   first     night
+in   Denver,   alone   without    you.
+I     thought      how     quickly      it
+escalated  —  one    moment   you
+were    in    my    arms    and     the
+next,   you   were   like       a young
+soldier              who had just joined
+the military,           sitting       on the
+edge   of    the    bed,    waiting   to
+say   his   farewell       and   I   was
+that     pregnant     wife     with     a
+three      month     fetus     in     my
+womb, wondering           when you
+would   return.`}</span>{`     The argument
+appeared      surreal     as    a
+       squid fight in the rain with
+sharp steak knives.`}</pre>
+
+<br/><br/><br/><br/>
+
+<pre className={styles.right}>{`But your face and chest were blue
+in  the  glowing   moon  and   your
+tone softened  to the    tone  I love
+and know`}<span
+className={styles.alternateVoiceOne}>{` and   so   I   did    not  want to go.`}</span></pre>
+
+<br/><br/><br/><br/>
+
+<pre className={styles.left}>{`The dark clouds of our bodies
+took  a  sharp  turn.   Sunlight
+began filtering through`}<span
+className={styles.alternateVoiceOne}>{`    and we
+fell    asleep   into   a   quiet,    resignated
+embrace.`}</span></pre>
+
+<br/><br/><br/><br/>
+
+<pre className={styles.left}><span
+className={styles.alternateVoiceOne}>{`In   the   afternoon`}</span>{`     we walked to
+the      Corner     for     shrimp
+bowls.`}<span
+className={styles.alternateVoiceOne}>{`  We  waited  and      waited  for
+our  food  to  arrive.  In the car ride  back
+to Denver, `}</span>{` I read your  notes on
+`}<i>Paradise Lost</i>{`.     Belial,    the
+lusty  fallen  angel,  preferred
+to exist than to not.`}<span
+className={styles.alternateVoiceOne}>{`   He said `}</span>{`  if
+they`}<span
+className={styles.alternateVoiceOne}>{`    (the   fallen   angels,   including
+Satan)`}</span><span
+className={styles.alternateVoiceTwo}>{`   irritate  God too  much, he
+might   obliterate  them.`}</span><span
+className={styles.alternateVoiceOne}>{`   The  most
+egregious     thing     they     could      do
+(revenge-wise)  was`}</span>{`    to   “disturb”
+and     “alarm”     GOD,`}<span
+className={styles.alternateVoiceOne}>{`        but`}</span><br/><span
+className={styles.alternateVoiceTwo}>{`otherwise,    God   is   God.`}</span><span
+className={styles.alternateVoiceOne}>{`   Belial
+suggested   that`}</span>{`   "familiarity  with
+the   horror     and   darkness
+would`}<span
+className={styles.alternateVoiceTwo}>{`    lessen the pain of  Hell.”`}</span><br/><span
+className={styles.alternateVoiceOne}>{`You   drifted  us   out   of  the   mountain
+sky,  with  the  sunlight  behind  us,  and
+I   was   thinking,    Belial   is   so   naive.
+Satan’s   daughter,   Sin,  was  born  out
+of  his  head   precocial — meaning   full
+grown.`}</span></pre>
 
                      <Footer/>
                            </body>
