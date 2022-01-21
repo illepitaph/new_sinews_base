47c5ed2454b708d367cea4ef73309c24f127b0bc
Steve Barbaro <steve.barbaro@gmail.com>
2022-01-20 18:47:08 -0600

    refines footer component stylings

--- components/footer.js
+++ components/footer.js
@@ -5,7 +5,8 @@ import styles from '../styles/Footer.module.css'
 export default function Footer() {
   return (
     <footer className={styles.socialLinkFooter}>
-    <a className={styles.socialLinkTag} href="https://www.twitter.com/new_sinews">twitter</a>
+    <a className={styles.socialLinkTag} href="https://www.twitter.com/new_sinews">tW<span
+    className={styles.blueText}>e</span>e<span className={styles.blueText}>T</span>eR</a>
       </footer>
   )
 }
