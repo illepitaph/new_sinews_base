d499a63be9329f3f7e7392c430be5557c51b8934
Steve Barbaro <steve.barbaro@gmail.com>
2022-02-05 15:01:59 -0600

    spruces up issue3's ENTER THE DUSK poem

--- components/footer.js
+++ components/footer.js
@@ -4,7 +4,8 @@ export default function Footer() {
   return (
     <footer className={styles.socialLinkFooter}>
     <a className={styles.socialLinkTag} href="https://www.twitter.com/new_sinews">tW<span
-    className={styles.blueText}>e</span>e<span className={styles.blueText}>T</span>eR</a>
+    className={styles.mintText}>e</span>e<span className={styles.mintText}>T</span>e<span
+    className={styles.mintText}>R</span></a>
       </footer>
   )
 }
