ebe4ab70672ebfff1c2fa28835747fbd2e48d2ab
Steve Barbaro <steve.barbaro@gmail.com>
2022-02-15 22:53:43 -0600

    updates header + footer component stylings a bit

--- components/footer.js
+++ components/footer.js
@@ -4,8 +4,8 @@ export default function Footer() {
   return (
     <footer className={styles.socialLinkFooter}>
     <a className={styles.socialLinkTag} href="https://www.twitter.com/new_sinews">tW<span
-    className={styles.mintText}>e</span>e<span className={styles.mintText}>T</span>e<span
-    className={styles.mintText}>R</span></a>
+    className={styles.reddishText}>e</span>e<span className={styles.reddishText}>T</span>e<span
+    className={styles.reddishText}>R</span></a>
       </footer>
   )
 }
