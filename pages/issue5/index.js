e33bf6d41cd93ea6a439a7a7e877db3ada0f0e7f
Steve Barbaro <steve.barbaro@gmail.com>
2022-02-18 17:29:47 -0600

    instantiates issue5 cover + spruces up issue5 moss contribution

---
+++ pages/issue5/index.js
@@ -0,0 +1,67 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import styles from '../../styles/Issue5.module.css'
+Issue5.title = "new_sinews issue5"
+
+export default function Issue5() {
+  return (
+    <>
+      <Head/>
+
+      <div className={styles.fullPageBackground}>
+
+    <div className={styles.titleAndSubtitle}>
+      <div className={styles.pageTitle}>n <i>e </i>w __   <span
+      className={styles.inverseTitleColor}>  s <i>i </i> n</span><br/>  __  e<i> w </i> s</div>
+      <div className={styles.issueTitle}>i<br/> s<br/> s<br/> u<br/> e <br/><br/>      <span
+      className={styles.inverseSubtitleColor}>t<br/>h<br/>r<br/>e<br/>e</span></div>
+        </div>
+
+       <div className={styles.workStructure}>
+
+      <div className={styles.issueContents}>
+        <div className={styles.workColumn}>
+         <Link href="/issue3/dusk"><div className={styles.workLink}>vi khi nao_retrieve the dusk</div></Link>
+        </div>
+        <div className={styles.workColumn}>
+        <Link href="/issue3/stough"><div className={styles.workLink}>cary stough_balance sheet</div></Link>
+        </div>
+        <div className={styles.workColumn}>
+          <Link href="/issue3/akant"><div className={styles.workLink}>sara deniz akant_how t langed</div></Link>
+        </div>
+        <div className={styles.workColumn}>
+        <Link href="/issue3/alvarez"><div className={styles.workLink}>steven alvarez_data blood // zero progress
+          silhouette</div></Link>
+        </div>
+        <div className={styles.workColumn}>
+          <Link href="/issue3/gette"><div className={styles.workLink}>megan jeanne gette_summoning green, a leprosy of the
+          heart</div></Link>
+        </div>
+        <div className={styles.workColumn}>
+          <Link href="/issue3/krumrie"><div className={styles.workLink}>kelly krumrie_excerpts from ‘no measure’</div></Link>
+        </div>
+        <div className={styles.workColumn}>
+          <Link href="/issue3/dasgupta"><div className={styles.workLink}>shome dasgupta_remember nothing</div></Link>
+        </div>
+        <div className={styles.workColumn}>
+          <Link href="/issue3/ovid"><div className={styles.workLink}>mike corrao_ovidian dynamics</div></Link>
+        </div>
+        <div className={styles.workColumn}>
+          <div className={styles.workLink}><Link href="/issue3/bios">~~~ contri butor_ _ _b  i  os ~~~</Link></div>
+        <div className={styles.siteShortcuts}>
+              <button className={styles.linkButton}><a
+              className={styles.innerSiteLinkTag} href="https://newnewsinews.com">new sinews</a></button>
+              <button className={styles.linkButton}><a
+              className={styles.innerSiteLinkTag} href="https://newnewsinews.com/about">about</a></button>
+              <button className={styles.linkButton}>
+                <a className={styles.twitterLinkTag} href="https://twitter.com/new_sinews">tweeter</a>
+              </button>
+              </div>
+          </div>
+        </div>
+      </div>
+</div>
+</>
+
+  )
+}
