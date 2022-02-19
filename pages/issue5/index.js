fe0f773c2046e8f60104f934b85b0b24986a51ba
Steve Barbaro <steve.barbaro@gmail.com>
2022-02-19 16:45:49 -0600

    gets yet further into issue5 cover

--- pages/issue5/index.js
+++ pages/issue5/index.js
@@ -11,38 +11,42 @@ export default function Issue5() {
         <div className={styles.bodyContainer}>
 
       <div className={styles.pageBackgroundOne}>
+       <div className={styles.workStructure}>
        <div className={styles.workColumn}>
-        <Link href="/issue5/shiota"><div className={styles.workLink}>julia shiota_in the beginning, she was the sun</div></Link>
+        <Link href="/issue5/shiota"><div className={styles.workLink}>julia shiota_<i>in the beginning, she was the sun</i></div></Link>
        </div>
        <div className={styles.workColumn}>
-       <Link href="/issue5/terrarium"><div className={styles.workLink}>vi khi nao + jessica alexander_when my body was a glass terrarium</div></Link>
+       <Link href="/issue5/terrarium"><div className={styles.workLink}>vi khi nao + jessica alexander_<i>when my body was a glass terrarium</i></div></Link>
        </div>
        <div className={styles.workColumn}>
-         <Link href="/issue5/theater"><div className={styles.workLink}>mike corrao_theater of materials</div></Link>
+         <Link href="/issue5/theater"><div className={styles.workLink}>mike corrao_<i>theater of materials</i></div></Link>
        </div>
        <div className={styles.workColumn}>
-       <Link href="/issue5/moss"><div className={styles.workLink}>samuel moss_the veldt light</div></Link>
+       <Link href="/issue5/moss"><div className={styles.workLink}>samuel moss_<i>the veldt light</i></div></Link>
        </div>
        <div className={styles.workColumn}>
-         <Link href="/issue5/feldman"><div className={styles.workLink}>jacqueline feldman_passel: eight stories</div></Link>
+         <Link href="/issue5/feldman"><div className={styles.workLink}>jacqueline feldman_<i>passel: eight stories</i></div></Link>
+       </div>
        </div>
        </div>
 
         <div className={styles.pageBackgroundTwo}>
-        <div className={styles.workColumn}>
+        <div className={styles.workStructureTwo}>
+        <div className={styles.workColumnTwo}>
           <Link href="/issue5/yrclimate"><div className={styles.workLink}>steven alvarez_yr climate</div></Link>
         </div>
-        <div className={styles.workColumn}>
+        <div className={styles.workColumnTwo}>
           <Link href="/issue5/stripmall"><div className={styles.workLink}>alvin lu_proposal for the construction of an ethnic strip mall</div></Link>
         </div>
-        <div className={styles.workColumn}>
+        <div className={styles.workColumnTwo}>
           <Link href="/issue5/rainey"><div className={styles.workLink}>delia rainey_somewhere better than this place</div></Link>
         </div>
-        <div className={styles.workColumn}>
+        <div className={styles.workColumnTwo}>
           <Link href="/issue5/bisagni"><div className={styles.workLink}>paul bisagni_nice time formula + jumbo marvelous domesticity
           jilt + by dint of little else</div></Link>
-          <div className={styles.workColumn}>
+          <div className={styles.workColumnTwo}>
             <div className={styles.workLink}><Link href="/issue5/bios">~~~ contri butor_ _ _b  i  os ~~~</Link></div>
+        </div>
         </div>
         </div>
           </div>
