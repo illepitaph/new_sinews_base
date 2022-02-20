4d3404ae0c7ccd787dbe43fddbcf45789090a793
Steve Barbaro <steve.barbaro@gmail.com>
2022-02-19 19:51:33 -0600

    adds button work to issue5 cover

--- pages/issue5/index.js
+++ pages/issue5/index.js
@@ -16,25 +16,25 @@ export default function Issue5() {
         <Link href="/issue5/shiota"><div className={styles.workLink}>julia shiota_<i>in the beginning, she was the sun</i></div></Link>
        </div>
        <div className={styles.workColumn}>
-       <Link href="/issue5/terrarium"><div className={styles.workLink}>vi khi nao + jessica alexander_<i>when my body was a glass terrarium</i></div></Link>
+          <Link href="/issue5/theater"><div className={styles.workLink}>mike corrao_<i>theater of materials</i></div></Link>
        </div>
        <div className={styles.workColumn}>
-         <Link href="/issue5/theater"><div className={styles.workLink}>mike corrao_<i>theater of materials</i></div></Link>
+          <Link href="/issue5/terrarium"><div className={styles.workLink}>vi khi nao + jessica alexander_<i>when my body was a glass terrarium</i></div></Link>
        </div>
        <div className={styles.workColumn}>
        <Link href="/issue5/moss"><div className={styles.workLink}>samuel moss_<i>the veldt light</i></div></Link>
        </div>
        <div className={styles.workColumn}>
-         <Link href="/issue5/feldman"><div className={styles.workLink}>jacqueline feldman_<i>passel: eight stories</i></div></Link>
+         <Link href="/issue5/yrclimate"><div className={styles.workLink}>steven alvarez_<i>yr climate</i></div></Link>
        </div>
-       <Link href="/"><button className={styles.linkButtonOne}>new sinews</button></Link>
+       <Link href="/"><button className={styles.linkButtonOne}>&#128065; new sinews &#128065;</button></Link>
        </div>
        </div>
 
         <div className={styles.pageBackgroundTwo}>
         <div className={styles.workStructureTwo}>
         <div className={styles.workColumnTwo}>
-          <Link href="/issue5/yrclimate"><div className={styles.workLink}>steven alvarez_<i>yr climate</i></div></Link>
+          <Link href="/issue5/feldman"><div className={styles.workLink}>jacqueline feldman_<i>passel: eight stories</i></div></Link>
         </div>
         <div className={styles.workColumnTwo}>
           <Link href="/issue5/stripmall"><div className={styles.workLink}>alvin lu_<i>proposal for the construction of an ethnic strip mall</i></div></Link>
@@ -49,8 +49,7 @@ export default function Issue5() {
             <div className={styles.workLink}><Link href="/issue5/bios">~~~ contri butor_ _ _b  i  os ~~~</Link></div>
         </div>
         <div className={styles.linkButtonBox}>
-        <Link href="/"><button className={styles.linkButtonTwo}>about</button></Link>
-          <Link href="/"><button className={styles.linkButtonTwo}>tweeter</button></Link>
+        <Link href="/about"><button className={styles.linkButtonTwo}>&#128065; about &#128065;</button></Link>
           </div>
         </div>
         </div>
@@ -62,6 +61,7 @@ export default function Issue5() {
         className={styles.inverseTitleColor}>+  s <i>i </i> n<br/>  + </span>e<i> w </i> s</div>
         <div className={styles.issueTitle}>i<br/> s<br/> s<br/> u<br/> e <br/><br/>      <span
         className={styles.inverseSubtitleColor}>f<br/>i<br/>v<br/>e</span></div>
+          <a href="https://www.twitter.com/new_sinews" className={styles.linkButtonThree}>&#128065; tweeter &#128065;</a>
           </div>
       </div>
       </div>
