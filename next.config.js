43799f68d2e82b8470b5d6de55a309c76f6d8af2
Steve Barbaro <steve.barbaro@gmail.com>
2022-02-28 20:07:37 -0600

    updates issue1 rewrite links

--- next.config.js
+++ next.config.js
@@ -3,174 +3,45 @@ module.exports = {
     return [
 
       {
-        source: "/issue1",
-        destination: "issue1/index",
+        source: '/gridley',
+        destination: '/issue1/gridley',
       },
 
       {
-        source: "/issue1bios",
-        destination: "issue1/bios",
+        source: '/blackwell',
+        destination: '/issue1/blackwell',
       },
 
       {
-        source: "/gridley",
-        destination: "issue1/gridley",
+        source: '/romanosky',
+        destination: '/issue1/romanosky',
       },
 
       {
-        source: "/blackwell",
-        destination: "issue1/blackwell",
+        source: '/eisen-martin',
+        destination: '/issue1/eisen-martin',
       },
 
       {
-        source: "/romanosky",
-        destination: "issue1/romanosky",
+        source: '/nao',
+        destination: '/issue1/nao',
       },
 
       {
-        source: "/eisen-martin",
-        destination: "issue1/eisen-martin",
+        source: '/evenflesh',
+        destination: '/issue1/evenflesh',
       },
 
       {
-        source: "/evenflesh",
-        destination: "issue1/evenflesh",
+        source: '/issue1bios',
+        destination: '/issue1/bios',
       },
 
       {
-        source: "/issue2",
-        destination: "issue2/index",
+        source: '/issue1',
+        destination: '/issue1/index',
       },
 
-      {
-        source: "/issue2bios",
-        destination: "issue2/bios",
-      },
-
-      {
-        source: "/briante",
-        destination: "issue2/briante",
-      },
-
-      {
-        source: "/corrao",
-        destination: "issue2/corrao",
-      },
-
-      {
-        source: "/zamora",
-        destination: "issue2/zamora",
-      },
-
-      {
-        source: "/taylor",
-        destination: "issue2/taylor",
-      },
-
-      {
-        source: "/porte",
-        destination: "issue2/porte",
-      },
-
-      {
-        source: "/lewis",
-        destination: "issue2/lewis",
-      },
-
-      {
-        source: "/lakghomi",
-        destination: "issue2/lakghomi",
-      },
-
-      {
-        source: "/leyeaste",
-        destination: "issue2/leyeaste",
-      },
-
-      {
-        source: "/strauss",
-        destination: "issue2/strauss",
-      },
-
-      {
-        source: "/salutingahex",
-        destination: "issue2/salutingahex",
-      },
-
-      {
-        source: "/shimoda",
-        destination: "issue2/shimoda",
-      },
-
-      {
-        source: "/shimoda1",
-        destination: "issue2/shimoda1",
-      },
-
-      {
-        source: "/shimoda2",
-        destination: "issue2/shimoda2",
-      },
-
-      {
-        source: "/shimoda3",
-        destination: "issue2/shimoda3",
-      },
-
-      {
-        source: "/shimoda4",
-        destination: "issue2/shimoda4",
-      },
-
-      {
-        source: "/issue3",
-        destination: "issue3/index",
-      },
-
-      {
-        source: "/issue3bios",
-        destination: "issue3/bios",
-      },
-
-      {
-        source: "/dusk",
-        destination: "issue3/dusk",
-      },
-
-      {
-        source: "/stough",
-        destination: "issue3/stough",
-      },
-
-      {
-        source: "/akant",
-        destination: "issue3/akant",
-      },
-
-      {
-        source: "/alvarez",
-        destination: "issue3/alvarez",
-      },
-
-      {
-        source: "/gette",
-        destination: "issue3/gette",
-      },
-
-      {
-        source: "/krumrie",
-        destination: "issue3/krumrie",
-      },
-
-      {
-        source: "/dasgupta",
-        destination: "issue3/dasgupta",
-      },
-
-      {
-        source: "/corraoovidan",
-        destination: "issue3/corraoovidian",
-      },
     ]
-  }
+  },
 }
