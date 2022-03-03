fecad6bd753a2d30b34713c6f55341db580d68ec
Steve Barbaro <steve.barbaro@gmail.com>
2022-03-02 20:58:13 -0600

    instantiates left-right keyboard arrows for issue1 nao gallery

--- pages/issue1/nao.js
+++ pages/issue1/nao.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import Head from 'next/head'
 import Header from '../../components/header.js'
 import Footer from '../../components/footer.js'
@@ -7,6 +7,7 @@ import { text } from '../../GalleryData/TwoEyesCarouselData'
 Nao.title = "vi_khi_nao ~ two_eyes_in_one_eye: a_delirium — new_sinews";
 
 export default function Nao() {
+
   const [textArrayIndex, setTextArrayIndex] = useState(0);
     function prevText() {
        if (textArrayIndex > 0) {
@@ -23,6 +24,30 @@ export default function Nao() {
       setTextArrayIndex(0);
     }
 }
+
+   function handleKeyDownLeft(e) {
+  if (e.keyCode === 37 && textArrayIndex > 0) {
+ setTextArrayIndex(textArrayIndex - 1);
+} else {
+ setTextArrayIndex(text.length - 1);
+}
+}
+
+    function handleKeyDownRight(e) {
+      if (e.keyCode === 39 && textArrayIndex < 24) {
+      setTextArrayIndex(textArrayIndex + 1);
+    } else {
+      setTextArrayIndex(0);
+    }
+}
+
+useEffect (() => {
+  window.addEventListener("keydown", handleKeyDownLeft, handleKeyDownRight);
+
+return () => {
+  window.removeEventListener("keydown", handleKeyDownLeft, handleKeyDownRight);
+}
+})
 
 const RawText = text[textArrayIndex]["textName"];
 
