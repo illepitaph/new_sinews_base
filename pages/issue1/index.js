eee8960ee364f5a6bd64af9aa4b9cc8797de985c
Steve Barbaro <stevebarbaro@Steves-MacBook-Pro.local>
2022-03-06 20:13:30 -0600

    adds passhref to all issue1 cover links

--- pages/issue1/index.js
+++ pages/issue1/index.js
@@ -17,36 +17,36 @@ export default function Issue1() {
     <div className={styles.issueContents}>
       <div className={styles.issue}>issue_1</div>
       <div className={styles.col1}>
-        <Link href="/issue1/gridley"><div className={styles.workLink}>sarah gridley_poems from _insofar_</div></Link>
+        <Link href="/issue1/gridley" passHref><div className={styles.workLink}>sarah gridley_poems from _insofar_</div></Link>
       </div>
       <div className={styles.col1}>
-        <Link href="/issue1/blackwell"><div className={styles.workLink}>gabriel blackwell_scenery + bedtime</div></Link>
+        <Link href="/issue1/blackwell" passHref><div className={styles.workLink}>gabriel blackwell_scenery + bedtime</div></Link>
       </div>
       <div className={styles.col1}>
-        <Link href="/issue1/romanosky"><div className={styles.workLink}>christa romanosky_three poems</div></Link>
+        <Link href="/issue1/romanosky" passHref><div className={styles.workLink}>christa romanosky_three poems</div></Link>
       </div>
       <div className={styles.col2}>
-      <Link href="/issue1/eisen-martin"><div className={styles.workLink}>tongo eisen-martin_two poems</div></Link>
+      <Link href="/issue1/eisen-martin" passHref><div className={styles.workLink}>tongo eisen-martin_two poems</div></Link>
       </div>
       <div className={styles.col2}>
-        <Link href="/issue1/nao"><div className={styles.workLink}>vi khi nao_2 eyes in 1 eye: a delirium</div></Link>
+        <Link href="/issue1/nao" passHref><div className={styles.workLink}>vi khi nao_2 eyes in 1 eye: a delirium</div></Link>
       </div>
       <div className={styles.col2}>
-        <Link href="/issue1/evenflesh"><div className={styles.workLink} href=""
+        <Link href="/issue1/evenflesh" passHref><div className={styles.workLink} href=""
         >even flesh seems a formality<br/>or: vi khi nao's organic
           oscillations
         </div></Link>
-        <Link href="/issue1/bios"><div className={styles.workLink} href="">~~~b  i  os ~~~</div></Link>
+        <Link href="/issue1/bios" passHref><div className={styles.workLink} href="">~~~b  i  os ~~~</div></Link>
       </div>
 
       <div className={styles.siteShortcuts}>
             <button className={styles.linkButton}><div
-            className={styles.innerSiteLinkTag}><Link href="/">new sinews</Link></div></button>
+            className={styles.innerSiteLinkTag}><Link href="/" passHref>new sinews</Link></div></button>
             <button className={styles.linkButton}><div
-            className={styles.innerSiteLinkTag}><Link href="/about">about</Link></div></button>
+            className={styles.innerSiteLinkTag}><Link href="/about" passHref>about</Link></div></button>
             <button className={styles.linkButton}>
               <div className={styles.twitterLinkTag}><Link
-              href="https://twitter.com/new_sinews">tweeter</Link></div></button>
+              href="https://twitter.com/new_sinews" passHref>tweeter</Link></div></button>
         </div>
     </div>
  </div>
