46b53d4379c43c0408c2deac9d6e8cad3248dea9
Steve Barbaro <stevebarbaro@Steves-MacBook-Pro.local>
2022-03-07 10:34:58 -0600

    fixes issue3 vercel issues

--- pages/issue3/index.js
+++ pages/issue3/index.js
@@ -21,36 +21,36 @@ export default function Issue3() {
 
       <div className={styles.issueContents}>
         <div className={styles.workColumn}>
-         <Link href="/issue3/dusk"><div className={styles.workLink}>vi khi nao_retrieve the dusk</div></Link>
+         <Link href="/issue3/dusk" passHref><div className={styles.workLink}>vi khi nao_retrieve the dusk</div></Link>
         </div>
         <div className={styles.workColumn}>
-        <Link href="/issue3/stough"><div className={styles.workLink}>cary stough_balance sheet</div></Link>
+        <Link href="/issue3/stough" passHref><div className={styles.workLink}>cary stough_balance sheet</div></Link>
         </div>
         <div className={styles.workColumn}>
-          <Link href="/issue3/akant"><div className={styles.workLink}>sara deniz akant_how t langed</div></Link>
+          <Link href="/issue3/akant" passHref><div className={styles.workLink}>sara deniz akant_how t langed</div></Link>
         </div>
         <div className={styles.workColumn}>
-        <Link href="/issue3/alvarez"><div className={styles.workLink}>steven alvarez_data blood // zero progress
+        <Link href="/issue3/alvarez" passHref><div className={styles.workLink}>steven alvarez_data blood // zero progress
           silhouette</div></Link>
         </div>
         <div className={styles.workColumn}>
-          <Link href="/issue3/gette"><div className={styles.workLink}>megan jeanne gette_summoning green, a leprosy of the
+          <Link href="/issue3/gette" passHref><div className={styles.workLink}>megan jeanne gette_summoning green, a leprosy of the
           heart</div></Link>
         </div>
         <div className={styles.workColumn}>
-          <Link href="/issue3/krumrie"><div className={styles.workLink}>kelly krumrie_excerpts from ‘no measure’</div></Link>
+          <Link href="/issue3/krumrie" passHref><div className={styles.workLink}>kelly krumrie_excerpts from ‘no measure’</div></Link>
         </div>
         <div className={styles.workColumn}>
-          <Link href="/issue3/dasgupta"><div className={styles.workLink}>shome dasgupta_remember nothing</div></Link>
+          <Link href="/issue3/dasgupta" passHref><div className={styles.workLink}>shome dasgupta_remember nothing</div></Link>
         </div>
         <div className={styles.workColumn}>
-          <Link href="/issue3/corraoovidian"><div className={styles.workLink}>mike corrao_ovidian dynamics</div></Link>
+          <Link href="/issue3/corraoovidian" passHref><div className={styles.workLink}>mike corrao_ovidian dynamics</div></Link>
         </div>
         <div className={styles.workColumn}>
-          <div className={styles.workLink}><Link href="/issue3/bios">~~~ contri butor_ _ _b  i  os ~~~</Link></div>
+          <div className={styles.workLink}><Link href="/issue3/bios" passHref>~~~ contri butor_ _ _b  i  os ~~~</Link></div>
         <div className={styles.siteShortcuts}>
-              <button className={styles.linkButton}><Link href="/">new sinews</Link></button>
-              <button className={styles.linkButton}><Link href="/about">about</Link></button>
+              <button className={styles.linkButton}><Link href="/" passHref>new sinews</Link></button>
+              <button className={styles.linkButton}><Link href="/about" passHref>about</Link></button>
               <button className={styles.linkButton}>
                 <a className={styles.twitterLinkTag} href="https://twitter.com/new_sinews">tweeter</a>
               </button>
