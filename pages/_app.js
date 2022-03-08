2d3bb3686beb68137e8b868be41800fbf2471701
Steve Barbaro <stevebarbaro@Steves-MacBook-Pro.local>
2022-03-08 12:03:08 -0600

    adds lang=en to app.js file

--- pages/_app.js
+++ pages/_app.js
@@ -10,8 +10,7 @@ function MyApp({ Component, pageProps}) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <meta charSet="utf-8" />
     <link rel="icon" href="/images/favicon.ico" />
-    <Html lang="en">
-
+    <Html lang="en"></Html>
   </Head>
   <Component {...pageProps} />
   </>
