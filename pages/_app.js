1e2cf1501f9c32062d31eda4f44a07fe522ef809
Steve Barbaro <stevebarbaro@Steves-MacBook-Pro.local>
2022-03-09 13:13:50 -0600

    commences refinement of app.js component

--- pages/_app.js
+++ pages/_app.js
@@ -11,11 +11,11 @@ function MyApp({ Component, pageProps}) {
     <meta name="description" content="new sinews ~ late empire new writing ~ digital hub of poems & fiction"/>
     <meta name="author" content="Steve Barbaro"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <meta charSet="utf-8" />
-    <link rel="icon" href="/images/favicon.ico"/>
+    <meta charSet="utf-8"/>
+    <link rel="icon" href="/images/favicon.ico" type="image/x-icon"/>
     <meta name="twitter:card" content="summary_large_image"/>
     <meta name="twitter:site" content={Component.site}/>
-    <meta name="twitter:title" content={Component.pieceName}>
+    <meta name="twitter:title" content={Component.pieceName}/>
     <meta name="twitter:image" content={Component.image} key="ogtitle"/>
     <meta name="twitter:description" content={Component.description}/>
     <meta name="twitter:image" content={Component.image} key="ogtitle"/>
