e87e2fb094e603db827600612bd1f9a2668bfa0e
Steve Barbaro <stevebarbaro@Steves-MacBook-Pro.local>
2022-03-09 12:57:55 -0600

    commences refinement of app.js component

---
+++ cards.js
@@ -0,0 +1,25 @@
+export default {
+
+    originalTitle: "new_sinews",
+
+    currentURL: "https://newnewsinews.com",
+
+    originalImage: "https://i.ibb.co/NtDBtQd/image-3.jpg",
+
+    author: {
+
+    name: "Steve Barbaro",
+
+   },
+
+    originalDescription: "new sinews ~ late empire new writing ~ digital hub of poems & fiction",
+
+    social: {
+
+      twitter: "@new_sinews",
+
+   },
+
+   siteName: "new_sinews",
+
+ };
