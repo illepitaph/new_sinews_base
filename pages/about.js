734449110d92cc01dd088d8840c6ba7f65d091bc
Steve Barbaro <stevebarbaro@Steves-MacBook-Pro.local>
2022-03-10 14:16:41 -0600

    updates about page twitter card info

--- pages/about.js
+++ pages/about.js
@@ -12,7 +12,7 @@ import ArchaicEntity from '../public/images/ArchaicEntity.jpg'
 About.title = "about — new_sinews";
 About.pieceName = "new_sinews about page"
 About.description = 'new_sinews ~ late empire new writing ~ submissions info + imagistic mosaistry'
-About.image = "https://static1.squarespace.com/static/56ffc729d210b8e9fdfebb18/t/622a59e9f261534c88ac23f4/1646942697487/l.jpg"
+About.image = "https://static1.squarespace.com/static/5b78c5275ffd20862324ad8e/t/622a4c8d8249442d34b49b5d/1646939277961/ArchNewSinew.jpg"
 
 export default function About() {
 
