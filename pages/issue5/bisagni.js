dd7b95da6ffd428b4420c7c485bfd51c6cf45a13
Steve Barbaro <stevebarbaro@Steves-MacBook-Pro.local>
2022-03-10 09:57:01 -0600

    completes issue5 twitter card info

--- pages/issue5/bisagni.js
+++ pages/issue5/bisagni.js
@@ -7,7 +7,7 @@ import styles from '../../styles/Bisagni.module.css'
 Bisagni.title = "paul_bisagni ~ nice_time_formula + jumbo_marvelous_domesticity_jilt + by_dint_of_little_else — new_sinews"
 Bisagni.pieceName = "paul_bisagni ~ nice_time_formula + jumbo_marvelous_domesticity_jilt + by_dint_of_little_else — new_sinews"
 Bisagni.description = "_Paul Bisagni_poetry_NICE TIME FORMULA+JUMBO MARVELOUS DOMESTICITY JILT+BY DINT OF LITTLE ELSE_new_sinews_issue5_"
-Bisagni.image = "../../images/Gette1.jpg"
+Bisagni.image = "../../public/images/Gette1.jpg"
 
 export default function Bisagni() {
   return (
