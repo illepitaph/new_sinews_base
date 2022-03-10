397f5c66e5894fac57dff442db045f27833dfb0c
Steve Barbaro <stevebarbaro@Steves-MacBook-Pro.local>
2022-03-10 14:41:18 -0600

    attempts to fix issue5 twitter card info

--- pages/issue5/rainey.js
+++ pages/issue5/rainey.js
@@ -8,7 +8,7 @@ import styles from '../../styles/Rainey.module.css'
 Rainey.title = "delia_rainey ~ somewhere_better_than_this_place — new_sinews"
 Rainey.pieceName = "delia_rainey ~ somewhere_better_than_this_place — new_sinews"
 Rainey.description = "_Delia Rainey_nonfiction_SOMEWHERE BETTER THAN THIS PLACE_new_sinews_issue5_"
-Rainey.image = "https://static1.squarespace.com/static/5b78c5275ffd20862324ad8e/t/622a4c8d8249442d34b49b5d/1646939277961/ArchNewSinew.jpg"
+Rainey.image = "https://static1.squarespace.com/static/56ffc729d210b8e9fdfebb18/t/622a609328438342104875f1/1646944404052/R.jpg"
 
 
 export default function Rainey() {
