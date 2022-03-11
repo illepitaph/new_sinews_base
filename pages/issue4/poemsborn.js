b7ce8d86fb0a51c92bbe678818bfd42ae50d79e0
Steve Barbaro <stevebarbaro@Steves-MacBook-Pro.local>
2022-03-10 23:01:28 -0600

    adds issue4 contribution twitter card info

--- pages/issue4/poemsborn.js
+++ pages/issue4/poemsborn.js
@@ -8,6 +8,7 @@ import styles from '../../styles/PoemsBorn.module.css'
 PoemsBorn.title = "vi_khi_nao ~ poems_born_from_basic_words — new_sinews"
 PoemsBorn.pieceName = "vi_khi_nao ~ poems_born_from_basic_words — new_sinews"
 PoemsBorn.description = "_Vi Khi Nao_poetry_POEMS BORN FROM BASIC WORDS_new_sinews_issue4_"
+PoemsBron.image = "https://static1.squarespace.com/static/56ffc729d210b8e9fdfebb18/t/622ad785109eab3d9d4ad99b/1646974853724/PoemsBornBackground.jpg"
 
 export default function PoemsBorn() {
   return (
