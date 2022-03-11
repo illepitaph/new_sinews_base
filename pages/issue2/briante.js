a33f8f54d04ae1ee30636b2ffe38ea44957fa0f3
Steve Barbaro <stevebarbaro@Steves-MacBook-Pro.local>
2022-03-11 11:54:11 -0600

    adds issue3 twitter card metadata

--- pages/issue2/briante.js
+++ pages/issue2/briante.js
@@ -9,6 +9,7 @@ import Footer from '../../components/footer.js'
 Briante.title = "susan_briante ~ two_poems — new_sinews"
 Briante.pieceName = "susan_briante ~ two_poems — new_sinews"
 Briante.description = "_Susan Briante_2 poems_MAY DAY+GALLERY WALLS new_sinews _issue2_"
+Briante.image = "https://static1.squarespace.com/static/56ffc729d210b8e9fdfebb18/t/622b8c863dd4104e80db89a9/1647021190385/BrianteBackground.jpg"
 
 export default function Briante() {
   return (
