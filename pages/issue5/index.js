c397239247617e0ecf951149cc2d9c25f4e3f844
Steve Barbaro <steve.barbaro@gmail.com>
2022-03-24 13:09:41 -0500

    updates issue5 metadata

--- pages/issue5/index.js
+++ pages/issue5/index.js
@@ -4,7 +4,7 @@ import styles from '../../styles/Issue5.module.css'
 Issue5.title = "new_sinews issue5"
 Issue5.pieceName = "new_sinews issue5"
 Issue5.description = "_newsinews issue5_Julia Shiota+Mike Corrao+Vi Khi Nao+Jessica Alexander+Samuel Moss+Steven Alvarez+Jaqueline Feldman+Alvin Lu+Delia Rainey+Paul Bisagni"
-Issue5.image = "https://static1.squarespace.com/static/5b78c5275ffd20862324ad8e/t/623351da83ab6a58cc676da0/1647530459276/1.jpg"
+Issue5.image = "https://www.newnewsinews.com/images/Issue5Cover.jpg"
 
 export default function Issue5() {
   return (
