f4d1e14d77f091e08b26852462dc1a2fddd43c86
Steve Barbaro <steve.barbaro@gmail.com>
2022-04-11 16:15:27 -0500

    fixes potential about page change's opacity stutter

--- galleryText/imageBoxGalleryText.js
+++ galleryText/imageBoxGalleryText.js
@@ -3,40 +3,40 @@ import styles from '../styles/ImageBox.module.css'
 export function ImageBox1() {
   return (
     <div className={styles.jpegBox1}>
-        <img src="images/SiracusaWall.jpg" alt="new_sinews children-playing-in-front-of-a-wall icon"></img>
-        <img src="images/Bubbles.jpg" alt="new_sinews bubbles-on-plastic icon"></img>
-        <img src="images/Face.jpg" alt="new_sinews deity icon"></img>
-        <img src="images/Security.jpg" alt="new_sinews securitycam icon"></img>
-        <img src="images/ArchaicEntity.jpg" alt="new_sinews fossil icon"></img>
-        <img src="images/SiracusaWall.jpg" alt="new_sinews children-playing-in-front-of-a-wall icon"></img>
-        <img src="images/Security.jpg" alt="new_sinews securitycam icon"></img>
-        <img src="images/Face.jpg" alt="new_sinews deity icon"></img>
-        <img src="images/Security.jpg" alt="new_sinews securitycam icon"></img>
-        <img src="images/ArchaicEntity.jpg" alt="new_sinews fossil icon"></img>
-        <img src="images/Bubbles.jpg" alt="new_sinews bubbles-on-plastic icon"></img>
-    <img src="images/Bubbles.jpg" alt="new_sinews bubbles-on-plastic icon"></img>
-        <img src="images/Face.jpg" alt="new_sinews deity icon"></img>
-     <img src="images/SiracusaWall.jpg" alt="new_sinews children-playing-in-front-of-a-wall icon"></img>
-        <img src="images/ArchaicEntity.jpg" alt="new_sinews fossil icon"></img>
-        <img src="images/Security.jpg" alt="new_sinews securitycam icon"></img>
-        <img src="images/ArchaicEntity.jpg" alt="new_sinews fossil icon"></img>
-        <img src="images/SiracusaWall.jpg" alt="new_sinews children-playing-in-front-of-a-wall icon"></img>
-    <img src="images/ArchaicEntity.jpg" alt="new_sinews fossil icon"></img>
-        <img src="images/SiracusaWall.jpg" alt="new_sinews children-playing-in-front-of-a-wall icon"></img>
-        <img src="images/Security.jpg" alt="new_sinews securitycam icon"></img>
-        <img src="images/Face.jpg" alt="new_sinews deity icon"></img>
-        <img src="images/Security.jpg" alt="new_sinews securitycam icon"></img>
-        <img src="images/SiracusaWall.jpg" alt="new_sinews children-playing-in-front-of-a-wall icon"></img>
-        <img src="images/Bubbles.jpg" alt="new_sinews bubbles-on-plastic icon"></img>
-        <img src="images/Face.jpg" alt="new_sinews deity icon"></img>
-        <img src="images/Security.jpg" alt="new_sinews securitycam icon"></img>
-        <img src="images/ArchaicEntity.jpg" alt="new_sinews fossil icon"></img>
-        <img src="images/Security.jpg" alt="new_sinews securitycam icon"></img>
-        <img src="images/ArchaicEntity.jpg" alt="new_sinews fossil icon"></img>
-        <img src="images/SiracusaWall.jpg" alt="new_sinews children-playing-in-front-of-a-wall icon"></img>
-        <img src="images/Face.jpg" alt="new_sinews deity icon"></img>
-        <img src="images/ArchaicEntity.jpg" alt="new_sinews fossil icon"></img>
-        <img src="images/Security.jpg" alt="new_sinews securitycam icon"></img>
+      <img src="images/SiracusaWall.jpg" alt="new_sinews children-playing-in-front-of-a-wall icon"></img>
+      <img src="images/Bubbles.jpg" alt="new_sinews bubbles-on-plastic icon"></img>
+      <img src="images/Face.jpg" alt="new_sinews deity icon"></img>
+      <img src="images/Security.jpg" alt="new_sinews securitycam icon"></img>
+      <img src="images/ArchaicEntity.jpg" alt="new_sinews fossil icon"></img>
+      <img src="images/SiracusaWall.jpg" alt="new_sinews children-playing-in-front-of-a-wall icon"></img>
+      <img src="images/Security.jpg" alt="new_sinews securitycam icon"></img>
+      <img src="images/Face.jpg" alt="new_sinews deity icon"></img>
+      <img src="images/Security.jpg" alt="new_sinews securitycam icon"></img>
+      <img src="images/ArchaicEntity.jpg" alt="new_sinews fossil icon"></img>
+      <img src="images/Bubbles.jpg" alt="new_sinews bubbles-on-plastic icon"></img>
+      <img src="images/Bubbles.jpg" alt="new_sinews bubbles-on-plastic icon"></img>
+      <img src="images/Face.jpg" alt="new_sinews deity icon"></img>
+      <img src="images/SiracusaWall.jpg" alt="new_sinews children-playing-in-front-of-a-wall icon"></img>
+      <img src="images/ArchaicEntity.jpg" alt="new_sinews fossil icon"></img>
+      <img src="images/Security.jpg" alt="new_sinews securitycam icon"></img>
+      <img src="images/ArchaicEntity.jpg" alt="new_sinews fossil icon"></img>
+      <img src="images/SiracusaWall.jpg" alt="new_sinews children-playing-in-front-of-a-wall icon"></img>
+      <img src="images/ArchaicEntity.jpg" alt="new_sinews fossil icon"></img>
+      <img src="images/SiracusaWall.jpg" alt="new_sinews children-playing-in-front-of-a-wall icon"></img>
+      <img src="images/Security.jpg" alt="new_sinews securitycam icon"></img>
+      <img src="images/Face.jpg" alt="new_sinews deity icon"></img>
+      <img src="images/Security.jpg" alt="new_sinews securitycam icon"></img>
+      <img src="images/SiracusaWall.jpg" alt="new_sinews children-playing-in-front-of-a-wall icon"></img>
+      <img src="images/Bubbles.jpg" alt="new_sinews bubbles-on-plastic icon"></img>
+      <img src="images/Face.jpg" alt="new_sinews deity icon"></img>
+      <img src="images/Security.jpg" alt="new_sinews securitycam icon"></img>
+      <img src="images/ArchaicEntity.jpg" alt="new_sinews fossil icon"></img>
+      <img src="images/Security.jpg" alt="new_sinews securitycam icon"></img>
+      <img src="images/ArchaicEntity.jpg" alt="new_sinews fossil icon"></img>
+      <img src="images/SiracusaWall.jpg" alt="new_sinews children-playing-in-front-of-a-wall icon"></img>
+      <img src="images/Face.jpg" alt="new_sinews deity icon"></img>
+      <img src="images/ArchaicEntity.jpg" alt="new_sinews fossil icon"></img>
+      <img src="images/Security.jpg" alt="new_sinews securitycam icon"></img>
 
     </div>
   )
@@ -45,31 +45,31 @@ export function ImageBox1() {
 export function ImageBox2() {
   return (
     <div className={styles.jpegBox1}>
-            <img src="images/Bubbles.jpg" alt="new_sinews bubbles-on-plastic icon"></img>
-        <img src="images/Face.jpg" alt="new_sinews deity icon"></img>
-     <img src="images/SiracusaWall.jpg" alt="new_sinews children-playing-in-front-of-a-wall icon"></img>
-        <img src="images/ArchaicEntity.jpg" alt="new_sinews fossil icon"></img>
-        <img src="images/Security.jpg" alt="new_sinews securitycam icon"></img>
-        <img src="images/ArchaicEntity.jpg" alt="new_sinews fossil icon"></img>
-        <img src="images/SiracusaWall.jpg" alt="new_sinews children-playing-in-front-of-a-wall icon"></img>
-    <img src="images/ArchaicEntity.jpg" alt="new_sinews fossil icon"></img>
-        <img src="images/SiracusaWall.jpg" alt="new_sinews children-playing-in-front-of-a-wall icon"></img>
-        <img src="images/Security.jpg" alt="new_sinews securitycam icon"></img>
-        <img src="images/Face.jpg" alt="new_sinews deity icon"></img>
-        <img src="images/Security.jpg" alt="new_sinews securitycam icon"></img>
-        <img src="images/SiracusaWall.jpg" alt="new_sinews children-playing-in-front-of-a-wall icon"></img>
-        <img src="images/Bubbles.jpg" alt="new_sinews bubbles-on-plastic icon"></img>
-        <img src="images/Face.jpg" alt="new_sinews deity icon"></img>
-        <img src="images/Security.jpg" alt="new_sinews securitycam icon"></img>
-        <img src="images/ArchaicEntity.jpg" alt="new_sinews fossil icon"></img>
-        <img src="images/Security.jpg" alt="new_sinews securitycam icon"></img>
-        <img src="images/ArchaicEntity.jpg" alt="new_sinews fossil icon"></img>
-        <img src="images/SiracusaWall.jpg" alt="new_sinews children-playing-in-front-of-a-wall icon"></img>
-        <img src="images/Face.jpg" alt="new_sinews deity icon"></img>
-        <img src="images/ArchaicEntity.jpg" alt="new_sinews fossil icon"></img>
-        <img src="images/Security.jpg" alt="new_sinews securitycam icon"></img>
-        <img src="images/SiracusaWall.jpg" alt="new_sinews children-playing-in-front-of-a-wall icon"></img>
-        <img src="images/Bubbles.jpg" alt="new_sinews bubbles-on-plastic icon"></img>
+      <img src="images/Bubbles.jpg" alt="new_sinews bubbles-on-plastic icon"></img>
+      <img src="images/Face.jpg" alt="new_sinews deity icon"></img>
+      <img src="images/SiracusaWall.jpg" alt="new_sinews children-playing-in-front-of-a-wall icon"></img>
+      <img src="images/ArchaicEntity.jpg" alt="new_sinews fossil icon"></img>
+      <img src="images/Security.jpg" alt="new_sinews securitycam icon"></img>
+      <img src="images/ArchaicEntity.jpg" alt="new_sinews fossil icon"></img>
+      <img src="images/SiracusaWall.jpg" alt="new_sinews children-playing-in-front-of-a-wall icon"></img>
+      <img src="images/ArchaicEntity.jpg" alt="new_sinews fossil icon"></img>
+      <img src="images/SiracusaWall.jpg" alt="new_sinews children-playing-in-front-of-a-wall icon"></img>
+      <img src="images/Security.jpg" alt="new_sinews securitycam icon"></img>
+      <img src="images/Face.jpg" alt="new_sinews deity icon"></img>
+      <img src="images/Security.jpg" alt="new_sinews securitycam icon"></img>
+      <img src="images/SiracusaWall.jpg" alt="new_sinews children-playing-in-front-of-a-wall icon"></img>
+      <img src="images/Bubbles.jpg" alt="new_sinews bubbles-on-plastic icon"></img>
+      <img src="images/Face.jpg" alt="new_sinews deity icon"></img>
+      <img src="images/Security.jpg" alt="new_sinews securitycam icon"></img>
+      <img src="images/ArchaicEntity.jpg" alt="new_sinews fossil icon"></img>
+      <img src="images/Security.jpg" alt="new_sinews securitycam icon"></img>
+      <img src="images/ArchaicEntity.jpg" alt="new_sinews fossil icon"></img>
+      <img src="images/SiracusaWall.jpg" alt="new_sinews children-playing-in-front-of-a-wall icon"></img>
+      <img src="images/Face.jpg" alt="new_sinews deity icon"></img>
+      <img src="images/ArchaicEntity.jpg" alt="new_sinews fossil icon"></img>
+      <img src="images/Security.jpg" alt="new_sinews securitycam icon"></img>
+      <img src="images/SiracusaWall.jpg" alt="new_sinews children-playing-in-front-of-a-wall icon"></img>
+      <img src="images/Bubbles.jpg" alt="new_sinews bubbles-on-plastic icon"></img>
       <img src="images/Face.jpg" alt="new_sinews deity icon"></img>
       <img src="images/Security.jpg" alt="new_sinews securitycam icon"></img>
       <img src="images/ArchaicEntity.jpg" alt="new_sinews fossil icon"></img>
