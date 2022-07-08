24d269c051392e7db8e4c93d64abe8b38be20b23
Steve Barbaro <steve.barbaro@gmail.com>
2022-07-07 21:21:29 -0500

    gets pretty deep into stough issue6 contribution

--- pages/issue6/target.js
+++ pages/issue6/target.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Header from '../../components/header.js'
 import RightGrayTitle from '../../components/rightGrayTitle.js'
-import LeftBlackTitle from '../../components/leftBlackTitle.js'
+import RightBlackTitle from '../../components/rightBlackTitle.js'
 import StandardProse from '../../components/standardProse.js'
 import Footer from '../../components/footer.js'
 import styles from '../../styles/Stough.module.css'
@@ -26,9 +26,12 @@ export default function Target() {
       className={styles.inverse}> <span
       className={styles.t}>t</span></span></span></RightGrayTitle>
 
+      <br/>
 
-      <LeftBlackTitle><span className={styles.bareHeader}>... cary ...<br/>... stough ...</span>
-      </LeftBlackTitle>
+      <RightBlackTitle><span className={styles.bareHeader}>... cary ...<br/>... stough ...</span>
+      </RightBlackTitle>
+
+      <br/>
 
       <StandardProse>
       <br/>
@@ -43,7 +46,278 @@ export default function Target() {
       <br/>
       From the driving position, once again I’m adjusting the shade. Recyclical, which is watchful, to say prideful, of my
       hurtling-towards. My gift of my death. What that is my giving. Towards loving. Towards loving my life.
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <span className={styles.sectionBreakSymbol}>~</span>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      After visiting home, or perhaps a little prior, a steady off-ness had built in me a fortress until I awoke one
+      morning to N.’s startled face in my own. An $850 trip to the emergency room and I’m reminded that panic only
+      recedes by great effortlessness, where the story begins, where the will automates the rush of action
+      inconclusively, though operative.
+      <br/>
+      <br/>
+      <br/>
+      Every day a new tremble/ing.
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <span className={styles.sectionBreakSymbol}>~</span>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      I’m seeing seeing. I’m writing writing. I’m reading reading. I’m hearing hearing.
+      <br/>
+      <br/>
+      <br/>
+      Like the old fashioned telephone.
+      <br/>
+      <br/>
+      <br/>
+      “A doubling where there is too much feedback.” A doubling that isn't circle, but an unbreaking dynastic Line.
+      <br/>
+      <br/>
+      <br/>
+      The sensory. What feedbacks the senses senses a route backwards toward a second, personalogical recognition where
+      everything is a cathedral built by wisps of past effort. Like the old fashioned telephone, we can’t surmise
+      its mechanical properties other than the prosthetic use once crossed with the human-divinity plane, where <i>the</i> human is immortal.
+      <br/>
+      <br/>
+      <br/>
+      For example
+      <br/>
+      <br/>
+      <br/>
+      In Charlestown, I held her hand through the streets I was seeing. I was seeing seeing the streets as if the streets
+      were a castle built by refractions in the door frame. Even the Monument seemed like something I’d placed there, poking
+      my own eye (not the atmosphere).
+      <br/>
+      <br/>
+      <br/>
+      I was hearing hearing the people streaming aboard Old Ironsides. I held my face in the mist near the old courts. I felt
+      hers. My legs promptly gave in to a pain I didn’t register as my own.
+      <br/>
+      <br/>
+      <br/>
+      Was I breathing this beating?
+      <br/>
+      <br/>
+      <br/>
+      “Certainty is nothing other than the objective essence itself, that is to say, the way in which we become aware of the
+      formal essence is certainty itself.” Spinoza, <span
+      className={styles.grayShadow}><i>Treatise on the Emendation of the Intellect</i></span>. So simple, and yet, so difficult
+      to internalize, to remember to deploy—conceptually—when I’m digging digging. Instead of, in the place of. I’m in the
+      place of wherein the place-of desists. Certain works of philosophy would take the mind’s inability to focus on
+      the objective essence as evidence of it being a symptom of some other unity, thus abandoning it. Spinoza, on the
+      other hand, is the one who knows that it is precisely the infiltrative presence of these other “unities” that keeps
+      us from focusing on this ever-unwanting task at hand, human freedom.
+      <br/>
+      <br/>
+      <br/>
+      Oscillation inaugurates—sets going—not the recognition of oneself, but the howl. “If you want to drive a person
+      crazy…” Alan Watts wants a bowl of.
+      <br/>
+      <br/>
+      <br/>
+      The howl. We listen to the music of a certain part of our youth we desire to never leave. This is what is meant
+      by a ‘black hole.’ As Keith Reniere used to ask his followers, “What do you lose if you were to stop living
+      the way you live now?”
+      <br/>
+      <br/>
+      <br/>
+      What have you got to lose in forgetting? What has one //
+      <br/>
+      <br/>
+      <br/>
+      got to lose in forgetting?
+      <br/>
+      <br/>
+      <br/>
+      “I first of all realized that if I abandoned the old ways and embarked on a new way of life, I should
+      be abandoning a good that was by its very nature uncertain—as we can clearly gather from what has been said—in favor
+      of one that was uncertain not of its own nature (for I was seeking a permanent good) but only in respect of
+      attainment.” Spinoza again, with a clarity of spirit one like me could only achieve under the wracked bodily
+      strain of nasal steroids—all else sullying, a wet napkin placed over the iron.
+      <br/>
+      <br/>
+      <br/>
+      I worry worrying. I see myself seeing worrying. Fearish and trembling, sun through the white blinds, from
+      the white wall a reflection of tan on the bedsheets I kept kicking. My bloating, the keep. The most important
+      thing to consider now now under consideration, unable to ‘let go.’ Become-Kleist. The puppet coming over
+      the master like shadows of an air raid. We must, first of all, recover belief in this world.
+      <br/>
+      <br/>
+      <br/>
+      “[I]t may be that believing in this world, in this life, becomes our most difficult task, or the task of
+      a mode of existence still to be discovered on our plane of immanence today.” Deleuze and Guattari—or, here, speaking
+      from the double-voice, Deleuze—speaks of an empiricist conversion of the work of philosophy, from
+      Hume to James/Peirce, which would require the belief in belief, or calling belief what has customarily been
+      described as the process by which something is folded into the body, prehension, demiurge of the platelets. Can
+      you hear them howling?
+      <br/>
+      <br/>
+      <br/>
+      There isn’t anything to be <i>recovered</i> by believing in “this world.” This is of no reactionary order. Rather, what
+      is believed is believed in the utility of leading to other belief: of what <i>can</i> be believed. Can we adjust
+      the goal posts of what may comprise the <i>new</i> so as to crack open belief (always <i>once again</i>), is
+      the question asked by those in whom I place my intellectual trust, and those to whom my writing—in the spiritual
+      sense—is dedicated.
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <span className={styles.sectionBreakSymbol}>~</span>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <i>I love you more every day</i>.
+      <br/>
+      <br/>
+      <br/>
+      The bad trip came at a time at which I thought I was becoming one with this person, N., who I loved. A ficus tree
+      turned into a dark mobile of rain-inhered dying. To be breathing or seeing was death-defying, and I wasn’t
+      sure <i>I</i> knew how to do either anymore. It was as if there were fishing lines attached to each muscle in
+      my face, reacting to what I felt were the exultations of my own actions recited back to me in hers. Black
+      hole. Bad hole. That was that.
+      <br/>
+      <br/>
+      <br/>
+      With certain drugs, there are no second chances. Or easy ones. I suppose we could have induced ourselves to expel
+      the moulded shrooms. She did vomit, later, after nearly choking on a piece of bread.
+      <br/>
+      <br/>
+      <br/>
+      I was introduced to Lord of the Dance. The day had been planned to be a day of working on the internet. We’d
+      driven to Southie to pick up a printer/scanner that didn’t print. Later, its sleek modern design features would
+      to us look like the idiosyncratic mouth of Evil, and we treated it so.
+      <br/>
+      <br/>
+      <br/>
+      The rain had stuck us inside like two pairs of sandals. I had no inclination to have sex. This was the first time
+      I’d felt attacked by routine. We just sat on the couch, occasionally becoming looped. In each other’s orbits was
+      a profligate wasting-away of one’s presentness, and not even the music winding through the apartment could bring
+      us any closer to the bliss we’d encountered down the Cape on some beach of which we no longer even remember
+      the name: eating a whole bag of chips, nearly pissing to keep warm, but rolling through the atmosphere with
+      a serpent’s muscle, hearts real. A woman came up to the shack in which we were holed only to put a letter
+      in the empty mailbox. A few hours later, the same woman arrived to retrieve it.
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <span className={styles.sectionBreakSymbol}>~</span>
+      <br/>
+      <br/>
+      <br/>
+      <span className={styles.boldHeader}>7.16.21</span>
+      <br/>
+      <br/>
+      <br/>
+      Aim sight. From Berryville on down, one sees into a past that’s stuck around. These mid-Cambrian hills referred to
+      as mountains, valleys so far below sea-level your ears pop just ascending a stair. N. begs to take a picture in
+      front of the vintage Coca Cola mural. Rolled deep into tourist country. Just before crossing the Line, N. gets
+      out to piss behind an empty church. I snap a picture unsure of whether CVS will develop graven images such as
+      this, her ass popping out from beneath her blouse in the familiar but always shocking bone-shade bone-shape. We’re
+      headed to Eureka, land of so many ancestors, aesthetic and real. Touted as “my favorite place on planet Earth,” and
+      I stand by that.
+      <br/>
+      <br/>
+      <br/>
+      AKA: Little Switzerland. Two Dutch Dames Chocolate Shoppe. History of Klan activities, history of Wiccan
+      activities, history of Osage displacement, history of World War I. In Carroll County, the old places like
+      this. A movie set sans camera crew. Few know of it, though those who do, assured of its dark, dank mystery, acclaim it.
+      <br/>
+      <br/>
+      <br/>
+      I’d put “Eureka Springs” on the agenda as an open-ended prompting, for exploration. I’d already filled N. in on
+      most of the lore—historical, that which precedes me, and my own—such as the haunted Crescent Hotel, the Bourbon St.
+      Inn, where Stanford stayed initially when he blew into town white-suited. Any mention of the poet brings to mind
+      lines from C.D. and the image of how I suspect .22 holes in a chest would look. As if nothing entered the body
+      at all. Negative pimples erupting inward. They say, it’s the force of its trajectory, as well as the swiftness
+      with which lead keel-hauls a biological system, that kills the idiot who uses such a small caliber weapon.
+      <br/>
+      <br/>
+      <br/>
+      We sweat into our shorts, the dinky shop-lined streets where both my parents and my mentors (C.D. and Forrest) undoubtedly
+      strolled, post-coitus in new marriage bliss. A honeymoon destination. The first city in Arkansas to issue marriage
+      licenses to “queers.” An LGBTQ destination solely based on its being the center around which migration falls, a town
+      no one really wants to visit for more than a day, unless you’re in love with each other or God. In the magic shop, the
+      kid asks us if either of us know how to skateboard. In the gallery, the man asks us if we know the work of Thomas
+      Kinkade. Of-fucking-course we do, we think.
+      <br/>
+      <br/>
+      <br/>
+      N. and I stay for 6 hours, though in more love than ever. Leaving not before visiting E. Fay Jones’ Thorncrown
+      Chapel, a hidden gem, literally among the pines. <i>Where the sun never shines, and you shiver when the cold wind blows</i>.
+      <br/>
+      <br/>
+      <br/>
+      Incidentally, this is the location of C.D. and Forrest’s vows. Poets as speech-actors. Reading the pamphlet in pew I
+      recognize the fact that the two must have been one of the first couples wedded here, perhaps just a year after its opening.
+      <br/>
+      <br/>
+      <br/>
+      E. Fay Jones cites the inspiration of his mentor, Frank Lloyd Wright, as well as Anglo-Saxon architecture. And it’s true, one
+      isn’t sure whether they’ve entered the set of Beowulf or Deep Space Nine. A “thin place.” Eric Weiner of the NYT: “locales
+      where the distance between heaven and earth collapses and we’re able to catch glimpses of the divine, or the transcendent
+      or, as I like to think of it, the Infinite Whatever.” A proper deterritorialization. One alights as the site of what had
+      previously been an arbitrary caesura. Is this not the history of the world? Acclamation of glory at the slightest
+      indication of the subject’s waning. Architecture: a frozen multitude.
+      <br/>
+      <br/>
+      <br/>
+      Being <i>in></i> or <i>among</i> a thin place remembers me to the importance thinkers like Deleuze and Foucault (as Nietzsche
+      and Spinoza before them) on <i>comportment</i>, the way one carries one’s meat around. Especially in this century this
+      strikes me as immediately vital, this century which no longer believes the body other than discursively drawn. Thought
+      to have eliminated the mind-body dualism it (our chatter) has made everything mind. Deleuze and Foucault are in some
+      sense blameworthy, though through misreadings of their work, undoubtedly. No doubt still to blame. Though comportment (as
+      in, repetition is a comportment, from the first page of <span
+      className={styles.grayShadow}><i>Difference and Repetition</i></span>). A position: that’s how it’s felt
+      with N. I’m held by a love that has nothing of a beginning or end. Truly as if I came in at the middle, clambering
+      through the orchestra pit and onto the stage, the audience full in its emptiness.
+      <br/>
+      <br/>
+      <br/>
+      On the long drive home (as always, I mistakenly turn onto 13 instead of continuing through Branson West to 160, resulting
+      in a much longer trek through the old country, where the old people lived their deaths) mom calls to tell us dinner
+      won’t be ready. “I have no energy,” she says, “I’ve been in the ER with Granny all day.” Last night, apparently, Granny
+      downed an entire bottle of Lovenox, an anticoagulant. At her age, I assume, there’s not much blood to thin beneath
+      such brittle skin. A thin place. Frantic, I grip the wheel. “She’s alright. The doctor says she can leave
+      tomorrow. Nothing happened.” Nothing happened. One more thing: “When I told her this is why she needs
+      someone to watch over her—because she took all of her pills by mistake—she tells me ‘Maybe I did it
+      on purpose, did you ever think of that?’”
+      <br/>
+      <br/>
+      <br/>
+      “Maybe I don’t want to live anymore.” Not ‘maybe I want to die,’ an exhaustion of what’s possible. Maybe I
+      don’t want to live anymore, maybe life doesn’t want me. This proliferation of dead cells. I carry around
+      this unconscious, this sack of real globlets.
+      <br/>
+      <br/>
+      <br/>
+      Love nox. Bring on the night-love.
       </StandardProse>
+      <Footer/>
     </>
   )
 }
