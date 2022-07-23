fb37d06fb619ea725c5cce2f4ff9a4c2969700b0
Steve Barbaro <steve.barbaro@gmail.com>
2022-07-23 16:24:59 -0500

    gets deep into issue 6 shimoda contribution

--- pages/blank/mfnz.js
+++ pages/blank/mfnz.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Header from '../../components/header.js'
 import RightGrayTitle from '../../components/rightGrayTitle.js'
 import LeftGrayTitle from '../../components/leftGrayTitle.js'
-
+import RightBlackTitle from '../../components/rightBlackTitle.js'
 import Image from 'next/image'
 import Footer from '../../components/footer.js'
 import styles from '../../styles/Mfnz.module.css'
@@ -41,6 +41,329 @@ export default function Marfa() {
       className={styles.silverShadowUpper}>n </span><span
       className={styles.redShadow}>d </span><span
       className={styles.inverse}>_</span></span></LeftGrayTitle>
+
+      <br/><br/>
+
+      <RightBlackTitle><span className={styles.bareHeader}>... ...  brandon<br/>... ... shimoda</span>
+      </RightBlackTitle>
+
+      <pre className={styles.poem}>{`poems
+corpsify
+
+us—prematurely
+
+drain us
+refill us
+
+wit h
+ancient humor`}</pre>
+
+<p className={styles.asteriskRed}>+</p>
+
+<pre className={styles.poem}>{`A poem is a form of exhaustion—
+boundless The body
+keeps giving. The spirit
+
+—mispronounced`}</pre>
+
+<p className={styles.asteriskInverse}>+</p>
+
+<pre className={styles.poem}>{`shariden   an oven
+left to cool
+for a century`}</pre>
+
+<p className={styles.asteriskSilver}>+</p>
+
+<pre className={styles.poem}>{`Atrocities   distant lands
+
+Then
+standing in front of skulls
+
+drawn into
+opaque darkness
+
+behind the bone,
+
+standing there, I was contributing
+too
+
+the pile—
+the tower, the temple of idols
+
+to Mary?`}</pre>
+
+<p className={styles.asteriskRed}>+</p>
+
+<pre className={styles.poem}>{`There is a white square—
+I do not see, but feel
+with my throat—
+
+silent
+at odds with
+the small, amnesiac world
+
+I’ve created
+with my decisions
+
+desires   errors
+lack of remembering.
+
+The white square hovers
+parallel to the ground,
+
+The sky seen from
+an open grave—`}</pre>
+
+<p className={styles.asteriskInverse}>+</p>
+
+<pre className={styles.poem}>{`The skulls were parental.
+rejected a chorus`}</pre>
+
+<p className={styles.asteriskSilver}>+</p>
+
+<pre className={styles.poem}>{`Did mushrooms grow
+after rain? Did flowers?
+
+Yes, one mushroom	cont.`}</pre>
+
+<p className={styles.asteriskRed}>+</p>
+
+<pre className={styles.poem}>{`The longer I play this game
+
+with military paper
+
+shouting
+through the wall
+
+the more or less
+ground
+
+beef   I become
+
+that eating
+will resolve
+
+looking long across
+the high desert grasslands`}</pre>
+
+<p className={styles.asteriskInverse}>+</p>
+
+<pre className={styles.poem}>{`aliens situate ama
+
+as people   human people, hungry
+
+I slept on top of a tower
+and watched a horse’s Bouncing penis
+
+I sketched the light fixtures in a church
+turned into a light source`}</pre>
+
+<p className={styles.asteriskSilver}>+</p>
+
+<pre className={styles.poem}>{`Who dreams of burning the pagoda down.
+
+Where it touches the sky
+twin eyes   red mask
+
+I sit below
+a tree
+
+The tree
+cures my headaches
+
+slender arms   victims
+
+listen
+to my headache   refrains
+
+I don’t want to feel
+my reflection   leap out
+of the pond
+
+and land on my back (my neck)
+
+the fish
+of my reflection
+
+soothe me   small fish
+
+hanging from the pagoda’s eaves
+
+if you cannot climb it—
+you have to bring
+it down
+
+to you
+
+you
+don’t have a broom
+and are not an old lady`}</pre>
+
+<p className={styles.asteriskRed}>+</p>
+
+<pre className={styles.poem}>{`Surely there are people
+you forgot—
+
+when you saw them,
+wasn’t?`}</pre>
+
+<p className={styles.asteriskInverse}>+</p>
+
+<pre className={styles.poem}>{`occasionally I see the woman
+frozen as a horse
+
+like the guardian of her megalithic sentence—`}</pre>
+
+<p className={styles.asteriskSilver}>+</p>
+
+<pre className={styles.poem}>{`corpse poem
+
+Google Bks
+`}</pre><p className={styles.specialArrow}>↓</p><pre className={styles.poem}>{`
+
+Is it true?`}</pre>
+
+<p className={styles.asteriskRed}>+</p>
+
+<pre className={styles.poem}>{`Where there is an inexplicable rock
+the earth
+rests beneath
+
+it
+is left
+
+to fend for itself
+
+how it chooses
+to populate
+
+itself
+vegetation`}</pre>
+
+<p className={styles.asteriskInverse}>+</p>
+
+<pre className={styles.poem}>{`The swamp hen
+
+edges
+
+the sub-tropical gully
+
+like a drone
+
+who wants to suck
+
+13lack cows
+
+dirty sheep
+
+the prestige`}</pre>
+
+<p className={styles.asteriskSilver}>+</p>
+
+<pre className={styles.poem}>{``}</pre><span className={styles.altarText}>I approach the altar.</span><pre className={styles.poem}>{`
+
+Fathers   always clutching,
+
+wearing white   burying
+their face
+in their father’s armed; were
+
+loved   deserved to be
+visited   Apparently
+
+`}</pre><span className={styles.altarText}>I approach the altar.</span><pre className={styles.poem}>{`
+dirt. walls
+are cool. The sun
+cannot penetrate. tails
+hanging from timber
+beams   A paean
+to the missing
+
+dead; the only thing that was
+expected, was unexpected.
+
+What place, where were we going?
+I don’t see art, but old men
+given everything
+they asked for,
+including everything they did not ask for.
+
+Paint children blue
+Paint children
+sitting in
+
+dirt, clutching their knees.
+
+makeshift, beautiful?
+She told us
+where the dirt came from,
+
+I forget (the mountain),
+
+`}</pre><span className={styles.altarText}>I approach the altar</span><pre className={styles.poem}>{`The sun
+Shines through cracks
+in the wall
+On faces of strangers   Materializing
+
+cracks   the faces
+of strangers
+
+stops
+are shining,
+holes
+in the wall
+
+lizards, spiders,
+mice,
+
+strangers
+assuage their depression ...
+
+`}</pre><span className={styles.altarText}>I approach the altar.</span><pre className={styles.poem}>{`The strait is behind me
+I approach the mirror
+If I can see The strait
+I am Invisible,
+will be fortified
+
+Bridges
+inside me
+
+faces hang over
+the rail
+
+“in solitude, asylum, or restraint”
+
+`}</pre><span className={styles.altarText}>I approach the altar.</span><pre className={styles.poem}>{`The strait
+in The back of my head
+
+implores
+the mirror
+to be Stronger,
+
+for the reflection
+To decide?
+
+The strait is a commitment
+Lunar specialties
+
+`}</pre><span className={styles.altarText}>I approach the altar</span><pre className={styles.poemSpecial}>{`   not sound`}</pre>
+<pre className={styles.poem}>{`infects the way
+I have been
+thinking?
+
+I see the sun in a piece of clay
+I see yellow in black stone
+
+I approach the altar
+a woman rubbing trees Between
+her hands
+In a worshipful attitude
+
+to translate The mild asphyxiation –
+
+vice`}</pre>
+
+
 
       <Footer/>
     </>
