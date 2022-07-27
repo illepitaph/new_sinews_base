3a194e0bb3a2e86bb1f63f5aaad4034101222bf1
Steve Barbaro <steve.barbaro@gmail.com>
2022-07-26 20:49:59 -0500

    instantiates issue6 beausang titles

--- pages/blank/beausang.js
+++ pages/blank/beausang.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Header from '../../components/header.js'
-import RightGrayTitle from '../../components/rightGrayTitle.js'
-import LeftBlackTitle from '../../components/leftBlackTitle.js'
+import LeftGrayTitle from '../../components/leftGrayTitle.js'
+import RightBlackTitle from '../../components/rightBlackTitle.js'
 import StandardProse from '../../components/standardProse.js'
 import Image from 'next/image'
 import Footer from '../../components/footer.js'
@@ -16,6 +16,33 @@ export default function Eighty() {
     <>
       <Head/>
         <Header headerBackground="beausangBackground"/>
+        <LeftGrayTitle><span
+        className={styles.inverse}> _ </span><span
+        className={styles.silverShadow}>n</span><span
+        className={styles.grayShadow}> i</span> <span
+        className={styles.inverseUpper}> <i>n</i></span><span
+        className={styles.silverShadow}> e</span><br/><br/><br/><br/><span
+        className={styles.leftMarginPusher}> <span
+        className={styles.inverseUpper}> t</span><span
+        className={styles.grayShadow}> <i>e</i></span><span
+        className={styles.silverShadow}> <i>e</i></span><span
+        className={styles.inverseUpper}> n</span><span
+        className={styles.silverShadow}> _ </span></span><br/><br/><br/><br/><br/><span
+        className={styles.leftMarginPusherTwo}> <span
+        className={styles.grayShadow}>e </span><span
+        className={styles.inverse}> _ </span><span
+        className={styles.silverShadow}>i</span><span
+        className={styles.grayShadow}> <i>g</i></span><span
+        className={styles.silverShadow}> _ </span><span
+        className={styles.inverseUpper}> <i>h</i></span><span
+        className={styles.grayShadowUpper}> t</span><span
+        className={styles.silverShadow}> y</span><span
+        className={styles.grayShadow}> _ </span></span></LeftGrayTitle>
+
+        <br/>
+
+        <RightBlackTitle><span className={styles.bareHeader}>chris ... ...<br/>beausang ... ...</span>
+        </RightBlackTitle>
     </>
   )
 }
