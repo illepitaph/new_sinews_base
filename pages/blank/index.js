da7be949f25fbca2f5a92f959c390bf576d3e1e8
Steve Barbaro <steve.barbaro@gmail.com>
2022-08-30 16:48:58 -0500

    gets yet deeper into issue6 cover

--- pages/blank/index.js
+++ pages/blank/index.js
@@ -13,7 +13,50 @@ export default function Issue6() {
 
         <div className={styles.bodyContainer}>
               <div className={styles.pageTitle}>n<br/><i>e</i><br/>w<br/>s<br/>i<br/>n<br/><i>e</i><br/>w<br/>s</div></div>
-              <div className={styles.issueTitle}>issue_6</div>
+
+              <div className={styles.issueContentsOne}>
+                <div className={styles.workColumn}>
+                 <Link href="/issue3/dusk" passHref><div className={styles.workLink}>kirsten_ihns ::<br/>all properties of the<br/>house burning ++ in which <br/>you are appearing as its<br/>shape ++ </div></Link>
+                </div>
+                <div className={styles.workColumn}>
+                <Link href="/issue3/stough" passHref><div className={styles.workLink}>jacqueline_feldman ::<br/>casting<br/>off</div></Link>
+                </div>
+                <div className={styles.workColumn}>
+                  <Link href="/issue3/akant" passHref><div className={styles.workLink}>brandon_shimoda ::<br/>marfa new<br/>zealand</div></Link>
+                </div>
+                <div className={styles.workColumn}>
+                <Link href="/issue3/alvarez" passHref><div className={styles.workLink}>chris_beausang ::<br/>nineteen<br/>eighty</div></Link>
+                </div>
+                <div className={styles.workColumn}>
+                  <Link href="/issue3/gette" passHref><div className={styles.workLink}>brendan_griffin ::<br/>wolly a wal-<br/>nut talks another-<br/>where</div></Link>
+                </div>
+                </div>
+                  <div className={styles.issueContentsTwo}>
+                <div className={styles.workColumn}>
+                  <Link href="/issue3/krumrie" passHref><div className={styles.workLink}>megan_jeanne_gette ::<br/>wolly a walnut<br/>talks anotherwhere</div></Link>
+                </div>
+                <div className={styles.workColumn}>
+                  <Link href="/issue3/dasgupta" passHref><div className={styles.workLink}>vi_khi_nao ::<br/>the suicidal<br/>rain ++ the bleached<br/>cauliflower of my<br/>adversary ++</div></Link>
+                </div>
+                <div className={styles.workColumn}>
+                  <Link href="/issue3/corraoovidian" passHref><div className={styles.workLink}>cary_stough ::<br/>acquired target</div></Link>
+                </div>
+                <div className={styles.workColumn}>
+                  <Link href="/issue3/corraoovidian" passHref><div className={styles.workLink}>xiao_yue_shan ::<br/>the dictionary<br/>of desire</div></Link>
+                </div>
+                <br/>
+                <div className={styles.workColumn}>
+                  <div className={styles.workLink}><Link href="/issue3/bios" passHref>~~~ contri butor_ _ _b  i  os ~~~</Link></div>
+                  </div>
+                  <div className={styles.siteShortcuts}>
+                        <button className={styles.linkButton}><Link href="/" passHref>new sinews</Link></button>
+                        <button className={styles.linkButton}><Link href="/about" passHref>about</Link></button>
+                        <button className={styles.linkButton}>
+                          <a className={styles.twitterLinkTag} href="https://twitter.com/new_sinews" target="_blank" rel="noreferrer">tweeter</a>
+                        </button>
+                        </div>
+                </div>
+
 
 </>
 
