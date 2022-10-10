b991ed332b71f94c6802aef99f0e3cdf0945bf14
Steve Barbaro <steve.barbaro@gmail.com>
2022-10-09 21:49:03 -0500

    fixes issue6 ihns meta image

--- pages/issue6/ihns.js
+++ pages/issue6/ihns.js
@@ -7,7 +7,7 @@ import styles from '../../styles/Ihns.module.css'
 Ihns.title = "kirsten_ihns ~ 6_poems — new_sinews"
 Ihns.pieceName = "kirsten_ihns ~ all_properties_of_the_house_burning ++ in_which_you_are_appearing_as_its_shape ++ — new_sinews"
 Ihns.description = "_Kirsten Ihns_poetry_6 POEMS_issue6_"
-Ihns.image = "https://www.newnewsinews.com/images/NumbersBackground.jpg"
+Ihns.image = "https://www.newnewsinews.com/images/IhnsBackground.jpg"
 
 
 
