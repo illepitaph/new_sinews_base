d83e80fe66ee01401a090e8106385ef13d25cf13
Steve Barbaro <steve.barbaro@gmail.com>
2022-11-30 11:51:49 -0600

    refines issue1 eisen-martin code formatting

--- pages/issue1/eisen-martin.js
+++ pages/issue1/eisen-martin.js
@@ -1,56 +1,101 @@
-import Head from 'next/head'
-import Header from '../../components/header.js'
-import CenterGrayTitle from '../../components/centerGrayTitle.js'
-import CenterBlackTitle from '../../components/centerBlackTitle.js'
-import Footer from '../../components/footer.js'
-import styles from '../../styles/EisenMartin.module.css'
-Eisen_Martin.title = "tongo_eisen-martin ~ two_poems — new_sinews"
-Eisen_Martin.pieceName = "tongo_eisen-martin ~ two_poems — new_sinews"
-Eisen_Martin.description = "Tongo Eisen-Martin_2 Poems_FISH WITH AMBITION TO BECOME THE RIVER+TWO NIGHTS ON 16TH STREET_new_sinews _issue1_"
-Eisen_Martin.image = "https://www.newnewsinews.com/images/EisenMartinBackground.jpg"
+import Head from "next/head";
+import Header from "../../components/header.js";
+import CenterGrayTitle from "../../components/centerGrayTitle.js";
+import CenterBlackTitle from "../../components/centerBlackTitle.js";
+import Footer from "../../components/footer.js";
+import styles from "../../styles/EisenMartin.module.css";
+Eisen_Martin.title = "tongo_eisen-martin ~ two_poems — new_sinews";
+Eisen_Martin.pieceName = "tongo_eisen-martin ~ two_poems — new_sinews";
+Eisen_Martin.description =
+  "Tongo Eisen-Martin_2 Poems_FISH WITH AMBITION TO BECOME THE RIVER+TWO NIGHTS ON 16TH STREET_new_sinews _issue1_";
+Eisen_Martin.image =
+  "https://www.newnewsinews.com/images/EisenMartinBackground.jpg";
 
 export default function Eisen_Martin() {
   return (
-   <>
-      <Head/>
+    <>
+      <Head />
 
+      <Header headerBackground="eisenBackground" />
 
+      <CenterGrayTitle>
+        {" "}
+        :: tongo :: <br /> :: eisen :: <br /> :: martin ::{" "}
+      </CenterGrayTitle>
+      <CenterBlackTitle>
+        {" "}
+        ... ...{" "}
+        <i>
+          fish with
+          <br />
+          ambition to become
+          <br />
+          the river
+        </i>{" "}
+        ... ...{" "}
+      </CenterBlackTitle>
 
-      <Header headerBackground="eisenBackground"/>
-
-<CenterGrayTitle> :: tongo :: <br/> :: eisen :: <br/> :: martin :: </CenterGrayTitle>
-<CenterBlackTitle> ... ... <i>fish with<br/>ambition to become<br/>the river</i> ... ... </CenterBlackTitle>
-
-<div className={styles.poem}>
-<p className={styles.leftJustified}>I looked in my bank account</p>
-<p className={styles.leftJustified}>It said, &quot;you have five toilets to your name.&quot;<br/>
-                          It said, &quot;don’t just sit there. Return fire.&quot;</p>
-<p className={styles.leftJustified}>Talking head says, “go to sleep children. You will all be police tomorrow.”</p>
-<p className={styles.leftJustified}>We say: No. We will be the poor.</p>
-<p className={styles.leftJustified}>Talking head says, “ok scumbags, I talked to you like children; now you will be dogs”</p>
-<p className={styles.leftJustified}>“Market Street” is the best two-word joke I have ever heard in San Francisco</p>
-<p className={styles.leftJustified}>“Now I have to teach myself how to pass out sketches and throw pencils at pigeons when I retire”</p>
-<p className={styles.leftJustified}>“I don’t have any friends who would reprimand me anyway”</p> 
-<pre className={styles.standardLineation}>{`“Retire to the ocean and walk backwards into the surf 
+      <div className={styles.poem}>
+        <p className={styles.leftJustified}>I looked in my bank account</p>
+        <p className={styles.leftJustified}>
+          It said, &quot;you have five toilets to your name.&quot;
+          <br />
+          It said, &quot;don’t just sit there. Return fire.&quot;
+        </p>
+        <p className={styles.leftJustified}>
+          Talking head says, “go to sleep children. You will all be police
+          tomorrow.”
+        </p>
+        <p className={styles.leftJustified}>We say: No. We will be the poor.</p>
+        <p className={styles.leftJustified}>
+          Talking head says, “ok scumbags, I talked to you like children; now
+          you will be dogs”
+        </p>
+        <p className={styles.leftJustified}>
+          “Market Street” is the best two-word joke I have ever heard in San
+          Francisco
+        </p>
+        <p className={styles.leftJustified}>
+          “Now I have to teach myself how to pass out sketches and throw pencils
+          at pigeons when I retire”
+        </p>
+        <p className={styles.leftJustified}>
+          “I don’t have any friends who would reprimand me anyway”
+        </p>
+         
+        <pre
+          className={styles.standardLineation}
+        >{`“Retire to the ocean and walk backwards into the surf 
               pretending to fight off the weirdly dressed hordes”
 
                                              —and I used to joke about being underwater.`}</pre>
-
-<p className={styles.leftJustified}>Laughed at the people stealing my cigarettes<br/>Laughed at the people pretending to tie my shoes</p>
-
-<p className={styles.leftJustified}>“now I have to put charcoal under the classroom and do impossible things with tap water”</p>
-
-<p className={styles.leftJustified}>“leave the window open so that the most respectful rats will come in and white enlightenment will have something to ponder.”</p>
-
-<p className={styles.leftJustified}>—oh what machines may come</p>
-
-<p className={styles.leftJustified}>“I should have never quit heroin,” we all say (all of the time)</p>
-
-<p className={styles.leftJustified}>we started early.<br/>twenty wagons appeared in a dream I had about the 7th grade.</p>
-
-<p className={styles.leftJustified}>Pathology needs a god to represent it to children and child-like states</p>
-
-<pre className={styles.polyphony}>{`Eat your people today
+        <p className={styles.leftJustified}>
+          Laughed at the people stealing my cigarettes
+          <br />
+          Laughed at the people pretending to tie my shoes
+        </p>
+        <p className={styles.leftJustified}>
+          “now I have to put charcoal under the classroom and do impossible
+          things with tap water”
+        </p>
+        <p className={styles.leftJustified}>
+          “leave the window open so that the most respectful rats will come in
+          and white enlightenment will have something to ponder.”
+        </p>
+        <p className={styles.leftJustified}>—oh what machines may come</p>
+        <p className={styles.leftJustified}>
+          “I should have never quit heroin,” we all say (all of the time)
+        </p>
+        <p className={styles.leftJustified}>
+          we started early.
+          <br />
+          twenty wagons appeared in a dream I had about the 7th grade.
+        </p>
+        <p className={styles.leftJustified}>
+          Pathology needs a god to represent it to children and child-like
+          states
+        </p>
+        <pre className={styles.polyphony}>{`Eat your people today
 
 The top of the tornado starts under our shoe soles
 And touches down on prison farms
@@ -58,8 +103,7 @@ Bibles offer no explanation
 But one cool preacher has quite a laugh
 
 All looked up at our beautiful shoes`}</pre>
-
-<pre className={styles.rightJustified}>{`
+        <pre className={styles.rightJustified}>{`
 “You still keep that knife in the car.”
 —the reality is
 that I am awake
@@ -73,8 +117,7 @@ that I am built
 to protect religion,
 but not all of the time
 `}</pre>
-
-<pre className={styles.centerJustified}>{`
+        <pre className={styles.centerJustified}>{`
 “I was watching her on my hands”
 “even my imagination just sat there”
 
@@ -83,8 +126,7 @@ but not all of the time
 —the choir is done with you
 done with all of us
 `}</pre>
-
-<pre className={styles.polyphony}>{`
+        <pre className={styles.polyphony}>{`
 “We call those projects ‘The Jar’”
 the private said to himself
 while working on his birthday
@@ -97,8 +139,7 @@ We hid the beds behind the trash”
 
 —the rest of the crash course came in the morning
 `}</pre>
-
-<pre className={styles.centerJustified}>{`
+        <pre className={styles.centerJustified}>{`
 “like you’ve never seen this card trick before
 the one where your grandfather appears
 and throws a couple of people into a fire”
@@ -117,16 +158,14 @@ now the whole freak show is on the back of my hand
 
 including capitalists looking up at the sky
 `}</pre>
-
-<pre className={styles.polyphony}>{`
+        <pre className={styles.polyphony}>{`
 what fascists are determined to do
 does not scare most people
 
 it makes most people bored,
 but we want our grandparents to survive…
 `}</pre>
-
-<pre className={styles.rightJustified}>{`
+        <pre className={styles.rightJustified}>{`
 Dear grandfather,
 
 I met some bluesmen today.
@@ -135,9 +174,7 @@ I declared, “your lady is with you,
 but all these chairs in here
 are mine.”
 `}</pre>
-
-
-<pre className={styles.standardLineation}>{`
+        <pre className={styles.standardLineation}>{`
 Before I opened the door to the street
             I knocked
                         And the world let me in
@@ -147,21 +184,30 @@ And there were miles and miles
 Of chalk outlines and window curtains
 Littered across the earth’s city
 `}</pre>
-
-<pre className={styles.rightJustified}>{`
+        <pre className={styles.rightJustified}>{`
 Dear Bluesmen,
 
 Now I am deader than you.
 Please take your seats.
 —the title of our song
 `}</pre>
-</div>
+      </div>
 
-<CenterBlackTitle> ... ... <i>two<br/>nights on 16th<br/>street</i> ... ... </CenterBlackTitle>
+      <CenterBlackTitle>
+        {" "}
+        ... ...{" "}
+        <i>
+          two
+          <br />
+          nights on 16th
+          <br />
+          street
+        </i>{" "}
+        ... ...{" "}
+      </CenterBlackTitle>
 
-<div className={styles.poem}>
-
-<pre className={styles.standardLineation}>{`
+      <div className={styles.poem}>
+        <pre className={styles.standardLineation}>{`
 First, I must apologize to the souls of the house
 
               Wearing the cheek bones of the mask only
@@ -178,7 +224,7 @@ Back of the head of the mask now
 Stuck with one of those cultures that believes I chose this family
 `}</pre>
 
-<pre className={styles.rightJustified}>{`
+        <pre className={styles.rightJustified}>{`
 I am not creative
 Just the silliest of the revolutionaries
 
@@ -189,7 +235,7 @@ About hating your enemies
 A little more realistically
 `}</pre>
 
-<pre className={styles.standardLineation}>{`
+        <pre className={styles.standardLineation}>{`
 Our eyesight returning to red // Notepad swimming to red // Bracing for the medicine’s recoil
 
 I need my left hand back
@@ -200,7 +246,7 @@ Watching the universe’s last metronome
            Some have the nerve to call a “homeless teenager”
 `}</pre>
 
-<pre className={styles.rightJustified}>{`
+        <pre className={styles.rightJustified}>{`
 Just wait…
 These religions will start resigning in a decade or two
 
@@ -210,14 +256,17 @@ Some transactionally
 
 In a cotton gothic society
 `}</pre>
-<p className={styles.leftJustified}>Class betrayal gone glassless // I mean ironically // my window started fogging over too</p>
+        <p className={styles.leftJustified}>
+          Class betrayal gone glassless // I mean ironically // my window
+          started fogging over too
+        </p>
 
-<pre className={styles.rightJustified}>{`
+        <pre className={styles.rightJustified}>{`
 I remember childhood
 Remember the word “Childhood” being a beginning
 `}</pre>
 
-<pre className={styles.standardLineation}>{`
+        <pre className={styles.standardLineation}>{`
 A poet loses his mind, you know, like the room has weather
 Or first-girlfriend gravity
 Poem gravity
@@ -230,13 +279,13 @@ Is that the madness
 Wants me forever
 `}</pre>
 
-<pre className={styles.rightJustified}>{`
+        <pre className={styles.rightJustified}>{`
 Bright lights behind hyena eyes,
 I mean, if I had to fall in love today
 My grandchildren would be doomed
 `}</pre>
 
-<pre className={styles.standardLineation}>{`
+        <pre className={styles.standardLineation}>{`
 A pair of apartments
             Defining both my family
             And political composure
@@ -250,19 +299,16 @@ Euphoria
 Cliché
 `}</pre>
 
-<pre className={styles.rightJustified}>{`
+        <pre className={styles.rightJustified}>{`
 Sharing a dirty deli sandwich with my friend
 A cosmic tool counted on by scared people
 
 Psychology of the mask now
 
 Teeth of the mask again`}</pre>
+      </div>
 
-</div>
-
-   <Footer/>
-
-      </>
-
-  )
-  }
+      <Footer />
+    </>
+  );
+}
