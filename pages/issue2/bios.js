49f3c34c8b6860dbf3f5bc75ce75ba16cf6ade66
Steve Barbaro <steve.barbaro@gmail.com>
2022-12-07 08:58:20 -0600

    fixes issue bios page broken link

--- pages/issue2/bios.js
+++ pages/issue2/bios.js
@@ -244,7 +244,7 @@ export default function Issue2Bios() {
           className={styles.biosExternalLink}
           target="_blank"
           rel="noreferrer"
-          href="http://www.uapress.ua.edu/product/Brief-Alphabet-of-Torture,6637.aspx"
+          href="https://www.uapress.ua.edu/9781573660617/a-brief-alphabet-of-torture"
         >
           {" "}
           <cite>
