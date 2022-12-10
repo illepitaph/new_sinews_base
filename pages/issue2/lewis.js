dc099ba95aaadf31c612947138bc6722df787d25
Steve Barbaro <steve.barbaro@gmail.com>
2022-12-10 16:15:22 -0600

    fixes issue2 lewis code formatting

--- pages/issue2/lewis.js
+++ pages/issue2/lewis.js
@@ -1,75 +1,75 @@
-import Head from 'next/head'
-import Header from '../../components/header.js'
-import Footer from '../../components/footer.js'
-import styles from '../../styles/Lewis.module.css'
-import React, {useState, useEffect} from 'react'
-import { lewisText } from '../../GalleryData/LewisCarouselData'
-Lewis.title = "susan_lewis ~ two poems — new_sinews"
-Lewis.pieceName = "susan_lewis ~ two poems — new_sinews"
-Lewis.description = "_Susan Lewis_2 poems_THE AGGREGATE OF FLOWN FUR+SIMPLE PLEASURES_new_sinews _issue2_"
-Lewis.image = "https://www.newnewsinews.com/images/LewisBackground.jpg"
-
+import Head from "next/head";
+import Header from "../../components/header.js";
+import Footer from "../../components/footer.js";
+import styles from "../../styles/Lewis.module.css";
+import React, { useState, useEffect } from "react";
+import { lewisText } from "../../GalleryData/LewisCarouselData";
+Lewis.title = "susan_lewis ~ two poems — new_sinews";
+Lewis.pieceName = "susan_lewis ~ two poems — new_sinews";
+Lewis.description =
+  "_Susan Lewis_2 poems_THE AGGREGATE OF FLOWN FUR+SIMPLE PLEASURES_new_sinews _issue2_";
+Lewis.image = "https://www.newnewsinews.com/images/LewisBackground.jpg";
 
 export default function Lewis() {
   const [textArrayIndex, setTextArrayIndex] = useState(0);
-    function prevText() {
-       if (textArrayIndex > 0) {
+  function prevText() {
+    if (textArrayIndex > 0) {
       setTextArrayIndex(textArrayIndex - 1);
     } else {
       setTextArrayIndex(lewisText.length - 1);
     }
-}
-    function nextText() {
-      if (textArrayIndex < 12) {
+  }
+  function nextText() {
+    if (textArrayIndex < 12) {
       setTextArrayIndex(textArrayIndex + 1);
     } else {
       setTextArrayIndex(0);
     }
-}
+  }
 
-useEffect (() => {
+  useEffect(() => {
+    function handleKeyDown(e) {
+      if (e.keyCode === 37 && textArrayIndex > 0) {
+        setTextArrayIndex(textArrayIndex - 1);
+      } else if (e.keyCode === 37 && textArrayIndex === 0) {
+        setTextArrayIndex(lewisText.length - 1);
+      } else if (e.keyCode === 39 && textArrayIndex < 12) {
+        setTextArrayIndex(textArrayIndex + 1);
+      } else if (e.keyCode === 39 && textArrayIndex === 12) {
+        setTextArrayIndex(0);
+      }
+    }
 
-function handleKeyDown(e) {
-   if (e.keyCode === 37 && textArrayIndex > 0) {
-setTextArrayIndex(textArrayIndex - 1);
-} else if (e.keyCode === 37 && textArrayIndex === 0)  {
- setTextArrayIndex(lewisText.length - 1);
-}  else if (e.keyCode === 39 && textArrayIndex < 12) {
-  setTextArrayIndex(textArrayIndex + 1);
-} else if (e.keyCode === 39 && textArrayIndex === 12) {
-  setTextArrayIndex(0);
- }
-}
+    window.addEventListener("keydown", handleKeyDown);
 
-  window.addEventListener("keydown", handleKeyDown);
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [textArrayIndex]);
 
-return () => {
-  window.removeEventListener("keydown", handleKeyDown);
-};
-}, [textArrayIndex]);
+  const RawText = lewisText[textArrayIndex]["textName"];
 
-
-const RawText = lewisText[textArrayIndex]["textName"];
-
-return (
+  return (
     <div>
-        <Head/>
+      <Head />
 
-        <Header headerBackground="lewisBackground"/>
+      <Header headerBackground="lewisBackground" />
 
-        <div className={styles.container}>
-
+      <div className={styles.container}>
         <div className={styles.carousel}>
-        <div className={styles.prev} onClick={prevText}>&#10094;&#10094;&#10094;</div>
-        <div className={styles.next} onClick={nextText}>&#10095;&#10095;&#10095;</div>
-        <div><RawText/></div>
+          <div className={styles.prev} onClick={prevText}>
+            &#10094;&#10094;&#10094;
+          </div>
+          <div className={styles.next} onClick={nextText}>
+            &#10095;&#10095;&#10095;
+          </div>
+          <div>
+            <RawText />
+          </div>
         </div>
+      </div>
 
-         </div>
-
-        <Footer/>
-
-              </div>
-
-                )
-              }
+      <Footer />
+    </div>
+  );
+}
