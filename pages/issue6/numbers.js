e988dc002591eb3733ae68d146c7f66e98b99eab
Steve Barbaro <steve.barbaro@gmail.com>
2022-12-11 15:09:49 -0600

    fixes issue6 nao error + refines said same entry's code formatting

--- pages/issue6/numbers.js
+++ pages/issue6/numbers.js
@@ -1,72 +1,96 @@
-import Head from 'next/head'
-import Header from '../../components/header.js'
-import CenterGrayTitle from '../../components/centerGrayTitle.js'
-import RightBlackTitle from '../../components/rightBlackTitle.js'
-import Footer from '../../components/footer.js'
-import styles from '../../styles/Numbers.module.css'
-Numbers.title = "vi_khi_nao ~ 3 poems — new_sinews"
-Numbers.pieceName = "vi_khi_nao ~ the_suicidal_rain ++ the_bleached_cauliflower_of_my_adversary ++ — new_sinews"
-Numbers.description = "_Vi Khi Nao_poetry_3 POEMS_issue6_"
-Numbers.image = "https://www.newnewsinews.com/images/NumbersBackground.jpg"
-
-
+import Head from "next/head";
+import Header from "../../components/header.js";
+import CenterGrayTitle from "../../components/centerGrayTitle.js";
+import RightBlackTitle from "../../components/rightBlackTitle.js";
+import Footer from "../../components/footer.js";
+import styles from "../../styles/Numbers.module.css";
+Numbers.title = "vi_khi_nao ~ 3 poems — new_sinews";
+Numbers.pieceName =
+  "vi_khi_nao ~ the_suicidal_rain ++ the_bleached_cauliflower_of_my_adversary ++ — new_sinews";
+Numbers.description = "_Vi Khi Nao_poetry_3 POEMS_issue6_";
+Numbers.image = "https://www.newnewsinews.com/images/NumbersBackground.jpg";
 
 export default function Numbers() {
   return (
     <>
-      <Head/>
-      <Header headerBackground="numbersBackground"/>
-      <CenterGrayTitle><span className={styles.faintShadow}><span
-      className={styles.extraSpaceOne}> v <i>i</i> <span
-      className={styles.alternateTitleColor}>+</span></span><br/><span
-      className={styles.extraSpaceTwo}><span
-      className={styles.alternateTitleColor}>_</span> k <span
-      className={styles.alternateTitleColor}><i>h</i></span> i <span
-      className={styles.alternateTitleColor}>_</span></span><br/><span
-      className={styles.extraSpaceThree}><span
-      className={styles.alternateTitleColor}> +</span> <span className={styles.uppercaseFaint}>n</span> <i>a</i> <span
-      className={styles.alternateTitleColor}>o</span> </span></span></CenterGrayTitle>
-      <br/>
-      <RightBlackTitle><span
-      className={styles.titleShadow}><i>MY DUSKY SOUL SOILED IN GLOOMY N<span
-      className={styles.uppercaseFaint}>A</span>RR<span className={styles.uppercaseFaint}>A</span>TIVE WHILE I <span
-      className={styles.uppercaseFaint}>A</span>TE THE BLE<span className={styles.uppercaseFaint}>A</span>CHED C<span
-      className={styles.uppercaseFaint}>A</span>ULIFLOWER OF MY <span className={styles.uppercaseFaint}>A</span>DVERS<span
-      className={styles.uppercaseFaint}>A</span>RY, MY SERFDOM, MY INF<span
-      className={styles.uppercaseFaint}>A</span>NT, MY DRE<span className={styles.uppercaseFaint}>A</span>MSC<span
-      className={styles.uppercaseFaint}>A</span>PE</i></span></RightBlackTitle>
-      <br/>
-      <br/>
+      <Head />
+      <Header headerBackground="numbersBackground" />
+      <CenterGrayTitle>
+        <span className={styles.faintShadow}>
+          <span className={styles.extraSpaceOne}>
+            {" "}
+            v <i>i</i> <span className={styles.alternateTitleColor}>+</span>
+          </span>
+          <br />
+          <span className={styles.extraSpaceTwo}>
+            <span className={styles.alternateTitleColor}>_</span> k{" "}
+            <span className={styles.alternateTitleColor}>
+              <i>h</i>
+            </span>{" "}
+            i <span className={styles.alternateTitleColor}>_</span>
+          </span>
+          <br />
+          <span className={styles.extraSpaceThree}>
+            <span className={styles.alternateTitleColor}> +</span>{" "}
+            <span className={styles.uppercaseFaint}>n</span> <i>a</i>{" "}
+            <span className={styles.alternateTitleColor}>o</span>{" "}
+          </span>
+        </span>
+      </CenterGrayTitle>
+      <br />
+      <RightBlackTitle>
+        <span className={styles.titleShadow}>
+          <i>
+            MY DUSKY SOUL SOILED IN GLOOMY N
+            <span className={styles.uppercaseFaint}>A</span>RR
+            <span className={styles.uppercaseFaint}>A</span>TIVE WHILE I{" "}
+            <span className={styles.uppercaseFaint}>A</span>TE THE BLE
+            <span className={styles.uppercaseFaint}>A</span>CHED C
+            <span className={styles.uppercaseFaint}>A</span>ULIFLOWER OF MY{" "}
+            <span className={styles.uppercaseFaint}>A</span>DVERS
+            <span className={styles.uppercaseFaint}>A</span>RY, MY SERFDOM, MY
+            INF<span className={styles.uppercaseFaint}>A</span>NT, MY DRE
+            <span className={styles.uppercaseFaint}>A</span>MSC
+            <span className={styles.uppercaseFaint}>A</span>PE
+          </i>
+        </span>
+      </RightBlackTitle>
+      <br />
+      <br />
       <pre className={styles.poem}>{`1. Back arched, moon falling
 2. Whisper into my arboreal gloom, hovering
 3. Clusters of canine, holy Harriet Tubman
 4. My new boxer—a boat, its arrow of velocity in me
 5. Cloud wizards, shivers of white
 6. Gathering sound while my silhouette survives
-6. Pregnant, I vomit slavery while my son collects marijuana into his mouth
-7. He flings high, my form reserves its own personal echo in its own cowbell
-8. If the cloud could behave well in a Zombie film, his performance/acting has such resemblance
-9. To the winter months; curtains and scythe mimic each other’s tongues.
-10. In the distance, my memory of independence
-11. Meanwhile, he farted black clouds while my child followed me into a well as I held the face of bondage across from this reform
-12. Legs above estate, acres of sinecure `}</pre>
-      <br/>
-      <br/>
-  <RightBlackTitle><span
-  className={styles.titleShadow}><i>THE SUICID<span
-  className={styles.uppercaseFaint}>A</span>L R<span
-  className={styles.uppercaseFaint}>A</span>IN CLIMBS THE L<span
-  className={styles.uppercaseFaint}>A</span>MPPOSTS, DESCENDS THE <span
-  className={styles.uppercaseFaint}>A</span>SPH<span
-  className={styles.uppercaseFaint}>A</span>LT, <span
-  className={styles.uppercaseFaint}>A</span>ND SOMEHOW IT W<span
-  className={styles.uppercaseFaint}>A</span>S <span
-  className={styles.uppercaseFaint}>A</span>LL OF MY F<span
-  className={styles.uppercaseFaint}>A</span>ULT, IT W<span
-  className={styles.uppercaseFaint}>A</span>S <span
-  className={styles.uppercaseFaint}>A</span>LL MY F<span
-  className={styles.uppercaseFaint}>A</span>ULT</i></span></RightBlackTitle>
-  <pre className={styles.poem}>{`1. 	The suicidal rain
+7. Pregnant, I vomit slavery while my son collects marijuana into his mouth
+8. He flings high, my form reserves its own personal echo in its own cowbell
+9. If the cloud could behave well in a Zombie film, his performance/acting has such resemblance
+10. To the winter months; curtains and scythe mimic each other’s tongues.
+11. In the distance, my memory of independence
+12. Meanwhile, he farted black clouds while my child followed me into a well as I held the face of bondage across from this reform
+13. Legs above estate, acres of sinecure `}</pre>
+      <br />
+      <br />
+      <RightBlackTitle>
+        <span className={styles.titleShadow}>
+          <i>
+            THE SUICID<span className={styles.uppercaseFaint}>A</span>L R
+            <span className={styles.uppercaseFaint}>A</span>IN CLIMBS THE L
+            <span className={styles.uppercaseFaint}>A</span>MPPOSTS, DESCENDS
+            THE <span className={styles.uppercaseFaint}>A</span>SPH
+            <span className={styles.uppercaseFaint}>A</span>LT,{" "}
+            <span className={styles.uppercaseFaint}>A</span>ND SOMEHOW IT W
+            <span className={styles.uppercaseFaint}>A</span>S{" "}
+            <span className={styles.uppercaseFaint}>A</span>LL OF MY F
+            <span className={styles.uppercaseFaint}>A</span>ULT, IT W
+            <span className={styles.uppercaseFaint}>A</span>S{" "}
+            <span className={styles.uppercaseFaint}>A</span>LL MY F
+            <span className={styles.uppercaseFaint}>A</span>ULT
+          </i>
+        </span>
+      </RightBlackTitle>
+      <pre className={styles.poem}>{`1. 	The suicidal rain
 2.	Is the refrain in my outer coast
 3.	It’s the sea
 4.	It’s the rooster in a corncob
@@ -89,16 +113,20 @@ export default function Numbers() {
 21.	Within this suicidal infinity
 22.	Within this dumfounded disharmony
 23.	Oh, this suicidal rain`}</pre>
-<RightBlackTitle><span
-className={styles.titleShadow}><i>MY YELLOW HE<span
-className={styles.uppercaseFaint}>A</span>RT WITHERS, C<span
-className={styles.uppercaseFaint}>A</span>T<span
-className={styles.uppercaseFaint}>A</span>LOGUING ITS LEMONY GR<span
-className={styles.uppercaseFaint}>A</span>NITE WHILE YOU  UNC<span
-className={styles.uppercaseFaint}>A</span>NNED MY TOM<span
-className={styles.uppercaseFaint}>A</span>TO S<span
-className={styles.uppercaseFaint}>A</span>UCE</i></span></RightBlackTitle>
-<pre className={styles.poem}>{`1. My midnight: soft   
+      <RightBlackTitle>
+        <span className={styles.titleShadow}>
+          <i>
+            MY YELLOW HE<span className={styles.uppercaseFaint}>A</span>RT
+            WITHERS, C<span className={styles.uppercaseFaint}>A</span>T
+            <span className={styles.uppercaseFaint}>A</span>LOGUING ITS LEMONY
+            GR<span className={styles.uppercaseFaint}>A</span>NITE WHILE YOU UNC
+            <span className={styles.uppercaseFaint}>A</span>NNED MY TOM
+            <span className={styles.uppercaseFaint}>A</span>TO S
+            <span className={styles.uppercaseFaint}>A</span>UCE
+          </i>
+        </span>
+      </RightBlackTitle>
+      <pre className={styles.poem}>{`1. My midnight: soft   
 2. You’re bread. You’re diameter.   
 3. You disquieted my opaque reflection
 4. Like a shuttlecock: predator,
@@ -120,7 +148,7 @@ className={styles.uppercaseFaint}>A</span>UCE</i></span></RightBlackTitle>
 20. And, incarcerated
 21. And, as a prefix
 22. Respond by crying`}</pre>
-<Footer/>
+      <Footer />
     </>
-  )
+  );
 }
