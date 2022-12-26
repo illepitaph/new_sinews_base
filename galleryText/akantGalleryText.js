7bfc9e9e05ffbd90c1c759b719fa2c95954f5c79
Steve Barbaro <steve.barbaro@gmail.com>
2022-12-26 16:04:49 -0600

    refines issue3 akant code formatting

--- galleryText/akantGalleryText.js
+++ galleryText/akantGalleryText.js
@@ -1,37 +1,46 @@
-import RightGrayTitle from '../components/rightGrayTitle.js'
-import LeftBlackTitle from '../components/leftBlackTitle.js'
-import GalleryLineation from '../components/galleryLineation.js'
-import styles from '../styles/Akant.module.css'
+import RightGrayTitle from "../components/rightGrayTitle.js";
+import LeftBlackTitle from "../components/leftBlackTitle.js";
+import GalleryLineation from "../components/galleryLineation.js";
+import styles from "../styles/Akant.module.css";
 
-export function AkantRawText1(){
-  return   (
-          <>
-          <LeftBlackTitle><span className={styles.blackShadow}>_<span
-          className={styles.redShadow}>_</span>_     s<span className={styles.redShadow}>a</span>ra<br/><span
-          className={styles.redShadow}> d</span> e <span
-          className={styles.redShadow}>n</span> <span
-          className={styles.redShadow}>i</span> z <br/>akan<span
-          className={styles.redShadow}>t</span> _<span className={styles.redShadow}>_</span>_
-          </span></LeftBlackTitle>
-          </>
-    )
+export function AkantRawText1() {
+  return (
+    <>
+      <LeftBlackTitle>
+        <span className={styles.blackShadow}>
+          _<span className={styles.redShadow}>_</span>_ s
+          <span className={styles.redShadow}>a</span>ra
+          <br />
+          <span className={styles.redShadow}> d</span> e{" "}
+          <span className={styles.redShadow}>n</span>{" "}
+          <span className={styles.redShadow}>i</span> z <br />
+          akan<span className={styles.redShadow}>t</span> _
+          <span className={styles.redShadow}>_</span>_
+        </span>
+      </LeftBlackTitle>
+    </>
+  );
 }
 
-export function AkantRawText2(){
-  return   (
-          <>
-          <RightGrayTitle><span className={styles.redShadow}>h o w</span></RightGrayTitle>
-          <h3 className={styles.bigT}>T</h3><RightGrayTitle><span
-          className={styles.redShadow}>lang ed</span></RightGrayTitle>
-          </>
-    )
+export function AkantRawText2() {
+  return (
+    <>
+      <RightGrayTitle>
+        <span className={styles.redShadow}>h o w</span>
+      </RightGrayTitle>
+      <h3 className={styles.bigT}>T</h3>
+      <RightGrayTitle>
+        <span className={styles.redShadow}>lang ed</span>
+      </RightGrayTitle>
+    </>
+  );
 }
 
-export function AkantRawText3(){
-  return   (
-          <>
-          <GalleryLineation>
-{`T spoke languages, T langed. T worked for centuries
+export function AkantRawText3() {
+  return (
+    <>
+      <GalleryLineation>
+        {`T spoke languages, T langed. T worked for centuries
 in high and stately governments. Breathed in fishponds,
 lived as chairs. Moved into a big house
 all together. T stole their religion, T
@@ -41,151 +50,184 @@ inside his brain.
 
 T were forbidden to see the priest’s
 daughter. T never learned
-to pray.           `}<i>Now the form and not the substance of the script</i>{`
-                       `}<i>regarding T has changed</i>{`.
+to pray.           `}
+        <i>Now the form and not the substance of the script</i>
+        {`
+                       `}
+        <i>regarding T has changed</i>
+        {`.
 
 
 But as I said, T langed.
-            `}</GalleryLineation>
-          </>
-    )
+            `}
+      </GalleryLineation>
+    </>
+  );
 }
 
-export function AkantRawText4(){
-  return   (
-          <>
-          <GalleryLineation>
-{`T-Reason for the tree ::
+export function AkantRawText4() {
+  return (
+    <>
+      <GalleryLineation>
+        {`T-Reason for the tree ::
           Oh ! The city crumbled and we lost everything.
-          Bitches point back and say `}<i>T</i>{`.
-          See ? `}<i>T is where I’m from</i>{`.
-          I come from the seat of `}<i>the empire</i>{`
-          `}<i>that ruled the world</i>{`.
+          Bitches point back and say `}
+        <i>T</i>
+        {`.
+          See ? `}
+        <i>T is where I’m from</i>
+        {`.
+          I come from the seat of `}
+        <i>the empire</i>
+        {`
+          `}
+        <i>that ruled the world</i>
+        {`.
 
-  And I’m very well placed in that hierarchy.`}</GalleryLineation>
-          </>
-    )
+  And I’m very well placed in that hierarchy.`}
+      </GalleryLineation>
+    </>
+  );
 }
 
-export function AkantRawText5(){
-  return   (
-          <>
-          <GalleryLineation>
-{`             T runs ahead and lies
+export function AkantRawText5() {
+  return (
+    <>
+      <GalleryLineation>
+        {`             T runs ahead and lies
              against the television now.
 
-             We watch T-Magnificent, attending him with honor.`}</GalleryLineation>
-          </>
-    )
+             We watch T-Magnificent, attending him with honor.`}
+      </GalleryLineation>
+    </>
+  );
 }
 
-export function AkantRawText6(){
-  return   (
-          <>
-          <GalleryLineation>
-{`My first son T was a writer and an archivist.
+export function AkantRawText6() {
+  return (
+    <>
+      <GalleryLineation>
+        {`My first son T was a writer and an archivist.
 He would simply never tire of the past.
 
 What a love of documents in the desire to become !
 
-`}<i>Who is T</i>{` ? He’d always ask –
+`}
+        <i>Who is T</i>
+        {` ? He’d always ask –
 and shove a finger on a photo of T-imself.
 
 
-And so I ask you.`} <i>Who is T</i>{` ?
+And so I ask you.`}{" "}
+        <i>Who is T</i>
+        {` ?
 
 
 
 Later on, T changed his lang sauce, man.
 T-totaled along the normal human
 trajectory :: from poet to novelist, from diplomat
-to topless whore.`}</GalleryLineation>
-          </>
-    )
+to topless whore.`}
+      </GalleryLineation>
+    </>
+  );
 }
 
-export function AkantRawText7(){
-  return   (
-          <>
-           <GalleryLineation>
-{``}<i>Two Ts walk into a bar</i>.{`
+export function AkantRawText7() {
+  return (
+    <>
+      <GalleryLineation>
+        {``}
+        <i>Two Ts walk into a bar</i>.
+        {`
 An and Han.
 They tell a story about T’s father, the Khan
 Later on in the car, we dead-end fight.
 
-What were all of T’s names before ?`}</GalleryLineation>
-          </>
-    )
+What were all of T’s names before ?`}
+      </GalleryLineation>
+    </>
+  );
 }
 
-export function AkantRawText8(){
-  return   (
-          <>
-          <GalleryLineation>
-{`T-ilhan means a music; means a song.
+export function AkantRawText8() {
+  return (
+    <>
+      <GalleryLineation>
+        {`T-ilhan means a music; means a song.
 My name is equal to a pure solemn vow.
 T-eyhan means a tragedy performed against
 new soil, and T-ohan is the act of being born
 against the sun. Whereas T –
 T just loved photocopy. Everything you had
-he would make photocopy.`}</GalleryLineation>
-          </>
-    )
+he would make photocopy.`}
+      </GalleryLineation>
+    </>
+  );
 }
 
-export function AkantRawText9(){
-  return   (
-          <>
-          <GalleryLineation>
-{`As for me ? I feel very strongly. I have the weight
+export function AkantRawText9() {
+  return (
+    <>
+      <GalleryLineation>
+        {`As for me ? I feel very strongly. I have the weight
 of a centuries old man. I get it. I have a thousand pangs
 for your own damn history. Still, I come back
 with my charm
            and my quiet capability. Eagerly I show to others
 that I’m no backward alien. There’s no
-scary magic coming from my absent hand.`}</GalleryLineation>
-          </>
-    )
+scary magic coming from my absent hand.`}
+      </GalleryLineation>
+    </>
+  );
 }
 
-export function AkantRawText10(){
-  return   (
-          <>
-           <GalleryLineation>
-{``}<i>Two Ts walk into a bar</i>.{`
+export function AkantRawText10() {
+  return (
+    <>
+      <GalleryLineation>
+        {``}
+        <i>Two Ts walk into a bar</i>.
+        {`
 Why is it so funny ?
 Are you a terrorist, or a tourist ?
 What is the relationship between sacrifice, and purpose ?
 
 That’s the question I was thinking about
-six hundred years ago.`}</GalleryLineation>
-          </>
-    )
+six hundred years ago.`}
+      </GalleryLineation>
+    </>
+  );
 }
 
-export function AkantRawText11(){
-  return   (
-          <>
-          <GalleryLineation>
-{`By the way, there’s an enormous painting
+export function AkantRawText11() {
+  return (
+    <>
+      <GalleryLineation>
+        {`By the way, there’s an enormous painting
 of T the Mag in Congress.
 
 In a certain sense, I’ve been so well-absorbed.
 
 By the way, I’ve decided that I want to be buried.
 
-                         `}<i>It is time to crawl down</i>{`
-                         `}<i>from the ceiling now</i>.{`
-`}</GalleryLineation>
-          </>
-    )
+                         `}
+        <i>It is time to crawl down</i>
+        {`
+                         `}
+        <i>from the ceiling now</i>.
+        {`
+`}
+      </GalleryLineation>
+    </>
+  );
 }
 
-export function AkantRawText12(){
-  return   (
-          <>
-          <GalleryLineation>
-{`I paced around this T some time.
+export function AkantRawText12() {
+  return (
+    <>
+      <GalleryLineation>
+        {`I paced around this T some time.
 I have no wife today, out of hospitality.
 Allow me to escort you as I disappear
 behind T’s mask.
@@ -199,16 +241,19 @@ T is very beautiful, as well as
 a nice Sheraton.
 
 
-He stands `}<i>yabancı</i>{` out in front.`}</GalleryLineation>
-          </>
-    )
+He stands `}
+        <i>yabancı</i>
+        {` out in front.`}
+      </GalleryLineation>
+    </>
+  );
 }
 
-export function AkantRawText13(){
-  return   (
-          <>
-          <GalleryLineation>
-{`In the end, we’re all together, rolling up a scarf.
+export function AkantRawText13() {
+  return (
+    <>
+      <GalleryLineation>
+        {`In the end, we’re all together, rolling up a scarf.
 
 
 In the end, I see how every shiny shitty thing
@@ -223,7 +268,10 @@ is related to T, if not by culture, then by marriage.
 
 
 By the way. I still cry at JFK
-when they say me: `}<i>welcome home</i>{`.`}</GalleryLineation>
-          </>
-    )
+when they say me: `}
+        <i>welcome home</i>
+        {`.`}
+      </GalleryLineation>
+    </>
+  );
 }
--- pages/issue3/akant.js
+++ pages/issue3/akant.js
@@ -1,80 +1,83 @@
-import React, {useState, useEffect} from 'react'
-import Head from 'next/head'
-import Header from '../../components/header.js'
-import Footer from '../../components/footer.js'
-import styles from '../../styles/Akant.module.css'
-import { akantText } from '../../GalleryData/AkantCarouselData.js'
-Akant.title = "sara_deniz_akant ~ how_t_langed — new_sinews"
-Akant.pieceName = "sara_deniz_akant ~ how_t_langed — new_sinews"
-Akant.description = "_Sara Deniz Akant_poetry_HOW T LANGED_new_sinews_issue3_"
-Akant.image = "https://www.newnewsinews.com/images/AkantBackground.jpg"
+import React, { useState, useEffect } from "react";
+import Head from "next/head";
+import Header from "../../components/header.js";
+import Footer from "../../components/footer.js";
+import styles from "../../styles/Akant.module.css";
+import { akantText } from "../../GalleryData/AkantCarouselData.js";
+Akant.title = "sara_deniz_akant ~ how_t_langed — new_sinews";
+Akant.pieceName = "sara_deniz_akant ~ how_t_langed — new_sinews";
+Akant.description = "_Sara Deniz Akant_poetry_HOW T LANGED_new_sinews_issue3_";
+Akant.image = "https://www.newnewsinews.com/images/AkantBackground.jpg";
 
 export default function Akant() {
   const [textArrayIndex, setTextArrayIndex] = useState(0);
-    function prevText() {
-       if (textArrayIndex > 0) {
+  function prevText() {
+    if (textArrayIndex > 0) {
       setTextArrayIndex(textArrayIndex - 1);
     } else {
       setTextArrayIndex(akantText.length - 1);
     }
-}
+  }
 
-    function nextText() {
-      if (textArrayIndex < 12) {
+  function nextText() {
+    if (textArrayIndex < 12) {
       setTextArrayIndex(textArrayIndex + 1);
     } else {
       setTextArrayIndex(0);
     }
-}
+  }
 
-useEffect (() => {
+  useEffect(() => {
+    function handleKeyDown(e) {
+      if (e.keyCode === 37 && textArrayIndex > 0) {
+        setTextArrayIndex(textArrayIndex - 1);
+      } else if (e.keyCode === 37 && textArrayIndex === 0) {
+        setTextArrayIndex(akantText.length - 1);
+      } else if (e.keyCode === 39 && textArrayIndex < 12) {
+        setTextArrayIndex(textArrayIndex + 1);
+      } else if (e.keyCode === 39 && textArrayIndex === 12) {
+        setTextArrayIndex(0);
+      }
+    }
 
-function handleKeyDown(e) {
-   if (e.keyCode === 37 && textArrayIndex > 0) {
-setTextArrayIndex(textArrayIndex - 1);
-} else if (e.keyCode === 37 && textArrayIndex === 0)  {
- setTextArrayIndex(akantText.length - 1);
-}  else if (e.keyCode === 39 && textArrayIndex < 12) {
-  setTextArrayIndex(textArrayIndex + 1);
-} else if (e.keyCode === 39 && textArrayIndex === 12) {
-  setTextArrayIndex(0);
- }
-}
+    window.addEventListener("keydown", handleKeyDown);
 
-  window.addEventListener("keydown", handleKeyDown);
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [textArrayIndex]);
 
-return () => {
-  window.removeEventListener("keydown", handleKeyDown);
-};
-}, [textArrayIndex]);
-
-
-const RawText = akantText[textArrayIndex]["textName"];
-
+  const RawText = akantText[textArrayIndex]["textName"];
 
   return (
-      <>
-          <Head/>
+    <>
+      <Head />
 
+      <Header headerBackground="akantBackground" />
 
-          <Header headerBackground="akantBackground"/>
-
-          <div className={styles.container}>
-
-          <div className={styles.carousel}>
-          <div className={styles.prev} onClick={prevText}>&#x29CF;<br/>&#x29CF;<br/>&#x29CF;</div>
-          <div className={styles.next} onClick={nextText}>&#x29D0;<br/>&#x29D0;<br/>&#x29D0;</div>
-          <div><RawText/></div>
+      <div className={styles.container}>
+        <div className={styles.carousel}>
+          <div className={styles.prev} onClick={prevText}>
+            &#x29CF;
+            <br />
+            &#x29CF;
+            <br />
+            &#x29CF;
           </div>
+          <div className={styles.next} onClick={nextText}>
+            &#x29D0;
+            <br />
+            &#x29D0;
+            <br />
+            &#x29D0;
+          </div>
+          <div>
+            <RawText />
+          </div>
+        </div>
+      </div>
 
-           </div>
-
-
-
-
-              <Footer/>
-
-                    </>
-
-                      )
-                    }
+      <Footer />
+    </>
+  );
+}
