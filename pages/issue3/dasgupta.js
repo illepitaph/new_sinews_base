5c1637629ed3f9dabd990c38fea90c299c72d32c
Steve Barbaro <steve.barbaro@gmail.com>
2022-12-29 18:46:08 -0600

    refines issue3 dasgupta code formatting

--- pages/issue3/dasgupta.js
+++ pages/issue3/dasgupta.js
@@ -1,145 +1,171 @@
-import Head from 'next/head'
-import Header from '../../components/header.js'
-import RightBlackTitle from '../../components/rightBlackTitle.js'
-import LeftGrayTitle from '../../components/leftGrayTitle.js'
-import StandardProse from '../../components/standardProse.js'
-import Footer from '../../components/footer.js'
-import styles from '../../styles/Dasgupta.module.css'
-Dasgupta.title = "shome_dasgupta ~ remember_nothing — new_sinews"
-Dasgupta.pieceName = "shome_dasgupta ~ remember_nothing — new_sinews"
-Dasgupta.description = "_Shome Dasgupta_fiction_REMEMBER NOTHING_new_sinews_issue3_"
-Dasgupta.image = "https://www.newnewsinews.com/images/DasguptaBackground.jpg"
+import Head from "next/head";
+import Header from "../../components/header.js";
+import RightBlackTitle from "../../components/rightBlackTitle.js";
+import LeftGrayTitle from "../../components/leftGrayTitle.js";
+import StandardProse from "../../components/standardProse.js";
+import Footer from "../../components/footer.js";
+import styles from "../../styles/Dasgupta.module.css";
+Dasgupta.title = "shome_dasgupta ~ remember_nothing — new_sinews";
+Dasgupta.pieceName = "shome_dasgupta ~ remember_nothing — new_sinews";
+Dasgupta.description =
+  "_Shome Dasgupta_fiction_REMEMBER NOTHING_new_sinews_issue3_";
+Dasgupta.image = "https://www.newnewsinews.com/images/DasguptaBackground.jpg";
 
 export default function Dasgupta() {
   return (
-      <>
-          <Head/>
+    <>
+      <Head />
 
-       <Header headerBackground="dasguptaBackground"/>
+      <Header headerBackground="dasguptaBackground" />
 
-      <LeftGrayTitle>r<br/><span
-      className={styles.inverseTitleShadow}>e</span><br/>m<br/><span
-      className={styles.titleShadow}>e</span><br/>m<br/>b<br/><span
-      className={styles.inverseTitleShadowUppercase}>e</span><br/><span
-      className={styles.titleShadowUppercase}>r</span><br/><br/><br/>n<span
-      className={styles.inverseTitleShadowUppercase}>o</span><span
-      className={styles.titleShadowUppercase}>t</span>hi<span
-      className={styles.inverseTitleShadowUppercase}>n</span><span
-      className={styles.titleShadow}>g</span></LeftGrayTitle>
-      <RightBlackTitle> ... shome<br/>dasgupta ... </RightBlackTitle>
+      <LeftGrayTitle>
+        r<br />
+        <span className={styles.inverseTitleShadow}>e</span>
+        <br />m<br />
+        <span className={styles.titleShadow}>e</span>
+        <br />m<br />b<br />
+        <span className={styles.inverseTitleShadowUppercase}>e</span>
+        <br />
+        <span className={styles.titleShadowUppercase}>r</span>
+        <br />
+        <br />
+        <br />n<span className={styles.inverseTitleShadowUppercase}>o</span>
+        <span className={styles.titleShadowUppercase}>t</span>hi
+        <span className={styles.inverseTitleShadowUppercase}>n</span>
+        <span className={styles.titleShadow}>g</span>
+      </LeftGrayTitle>
+      <RightBlackTitle>
+        {" "}
+        ... shome
+        <br />
+        dasgupta ...{" "}
+      </RightBlackTitle>
       <StandardProse>
-      The way the night looked, it looked like a massive hole was drilled into the sky.
-      And the sun was gone. And the moon was covered. No stars. He looked up into the darkness
-      and saw nothing. And nothing was fine. That was all he ever wanted. To see nothing. He
-      used to close his eyes to see nothing, but that night, he didn’t have to.
-      <br/>
-      <br/>
-      It was on that night when he decided to give it all away. All of his possessions. His house,
-      his car, his TV, everything. He stared into the ocean of blackness and realized that this was
-      what he wanted.
-      <br/>
-      <br/>
-      “I want nothing.”
-      <br/>
-      <br/>
-      The next day, he put up his house for sale including all that was inside. He put up his car
-      for sale. He quit his job.
-      <br/>
-      <br/>
-      “What are you going to do?” his friend asked, a childhood friend and now former colleague.
-      <br/>
-      <br/>
-      “Nothing.”
-      <br/>
-      <br/>
-      Once all that he had owned was sold, he lived on the streets as a homeless man. He had a homeless
-      friend and they talked about nothing, and that was all he ever wanted.
-      <br/>
-      <br/>
-      One day his friend and former colleague—a friend since childhood—came up to him and gave him some money.
-      <br/>
-      <br/>
-      “I miss you,” his friend said. “Come back.”
-      <br/>
-      <br/>
-      He didn’t respond, and after a minute or so, his friend walked away. He watched him walk and thought
-      about nothing.
-      <br/>
-      <br/>
-      His homeless companion asked him,” Who was that.”
-      <br/>
-      <br/>
-      “He’s nothing,” he said.
-      <br/>
-      <br/>
-      That night the rain came down, large cold drops falling upon his body. He didn’t seek cover. He sat
-      there in the rain and thought about how much he loved the rain, he thought about how much he loved her
-      and nothing.
-      <br/>
-      <br/>
-      “This is nothing.”
-      <br/>
-      <br/>
-      It was a thunderstorm, and the night was loud. There was lightning. And across from where he sat, drenched
-      and happy about nothing, he saw a building catch on fire after being struck by lightning.
-      <br/>
-      <br/>
-      The building was ablaze, pure fire. It was the building where his wife used to work. She no longer exists.
-      She was nothing.
-      <br/>
-      <br/>
-      He stared at the building covered in light, the raindrops fell, the thunder was there, and there was the
-      lightning. He sat there. He sat there and remembered. He remembered when his wife was alive. He remembered
-      the touch of her skin, her voice. He remembered her kindness and compassion. He remembered her smile and
-      the way she slept. He remembered. He remembered her last day.
-      <br/>
-      <br/>
-      The building burned. He sat there and watched it before it was washed out. He went to sleep. The next day
-      was bright and fresh. The air was cool. He woke up and looked at the remains of the building.
-      <br/>
-      <br/>
-      “It’s nothing now,” he said. “Nothing is nothing. And nothing is fine.”
-      <br/>
-      <br/>
-      His homeless friend, who had sought shelter during the storm, came back and asked him what happened.
-      <br/>
-      <br/>
-      “Nothing,” he said.
-      <br/>
-      <br/>
-      He closed his eyes and let the shiny day press upon his face.
-      <br/>
-      <br/>
-      His friend, his childhood friend and former colleague, came back to give him some money—he did this daily.
-      <br/>
-      <br/>
-      “I’m sorry,” his friend said.
-      <br/>
-      <br/>
-      “It’s nothing.”
-      <br/>
-      <br/>
-      As he watched his friend walk away, he thought about everything. And everything was the past, and everything
-      was just memories. And his memories was everything, and it was his wife.
-      <br/>
-      <br/>
-      “I miss you,” he said.
-      <br/>
-      <br/>
-      “What’s that?” his homeless friend asked.
-      <br/>
-      <br/>
-      “Nothing.”
-      <br/>
-      <br/>
-      Before he moved to another place, it would be his last move, he had one last memory of her. He remembered.
-      They were sitting on the grass of their front yard. It was midnight and it was raining, and the night was
-      pure and dark and starless. There was no sky. And they talked about everything and nothing. It was the night
-      before she died. And it was his last memory before going. And it was everything and nothing. And he smiled
-      before going.
+        The way the night looked, it looked like a massive hole was drilled into
+        the sky. And the sun was gone. And the moon was covered. No stars. He
+        looked up into the darkness and saw nothing. And nothing was fine. That
+        was all he ever wanted. To see nothing. He used to close his eyes to see
+        nothing, but that night, he didn’t have to.
+        <br />
+        <br />
+        It was on that night when he decided to give it all away. All of his
+        possessions. His house, his car, his TV, everything. He stared into the
+        ocean of blackness and realized that this was what he wanted.
+        <br />
+        <br />
+        “I want nothing.”
+        <br />
+        <br />
+        The next day, he put up his house for sale including all that was
+        inside. He put up his car for sale. He quit his job.
+        <br />
+        <br />
+        “What are you going to do?” his friend asked, a childhood friend and now
+        former colleague.
+        <br />
+        <br />
+        “Nothing.”
+        <br />
+        <br />
+        Once all that he had owned was sold, he lived on the streets as a
+        homeless man. He had a homeless friend and they talked about nothing,
+        and that was all he ever wanted.
+        <br />
+        <br />
+        One day his friend and former colleague—a friend since childhood—came up
+        to him and gave him some money.
+        <br />
+        <br />
+        “I miss you,” his friend said. “Come back.”
+        <br />
+        <br />
+        He didn’t respond, and after a minute or so, his friend walked away. He
+        watched him walk and thought about nothing.
+        <br />
+        <br />
+        His homeless companion asked him,” Who was that.”
+        <br />
+        <br />
+        “He’s nothing,” he said.
+        <br />
+        <br />
+        That night the rain came down, large cold drops falling upon his body.
+        He didn’t seek cover. He sat there in the rain and thought about how
+        much he loved the rain, he thought about how much he loved her and
+        nothing.
+        <br />
+        <br />
+        “This is nothing.”
+        <br />
+        <br />
+        It was a thunderstorm, and the night was loud. There was lightning. And
+        across from where he sat, drenched and happy about nothing, he saw a
+        building catch on fire after being struck by lightning.
+        <br />
+        <br />
+        The building was ablaze, pure fire. It was the building where his wife
+        used to work. She no longer exists. She was nothing.
+        <br />
+        <br />
+        He stared at the building covered in light, the raindrops fell, the
+        thunder was there, and there was the lightning. He sat there. He sat
+        there and remembered. He remembered when his wife was alive. He
+        remembered the touch of her skin, her voice. He remembered her kindness
+        and compassion. He remembered her smile and the way she slept. He
+        remembered. He remembered her last day.
+        <br />
+        <br />
+        The building burned. He sat there and watched it before it was washed
+        out. He went to sleep. The next day was bright and fresh. The air was
+        cool. He woke up and looked at the remains of the building.
+        <br />
+        <br />
+        “It’s nothing now,” he said. “Nothing is nothing. And nothing is fine.”
+        <br />
+        <br />
+        His homeless friend, who had sought shelter during the storm, came back
+        and asked him what happened.
+        <br />
+        <br />
+        “Nothing,” he said.
+        <br />
+        <br />
+        He closed his eyes and let the shiny day press upon his face.
+        <br />
+        <br />
+        His friend, his childhood friend and former colleague, came back to give
+        him some money—he did this daily.
+        <br />
+        <br />
+        “I’m sorry,” his friend said.
+        <br />
+        <br />
+        “It’s nothing.”
+        <br />
+        <br />
+        As he watched his friend walk away, he thought about everything. And
+        everything was the past, and everything was just memories. And his
+        memories was everything, and it was his wife.
+        <br />
+        <br />
+        “I miss you,” he said.
+        <br />
+        <br />
+        “What’s that?” his homeless friend asked.
+        <br />
+        <br />
+        “Nothing.”
+        <br />
+        <br />
+        Before he moved to another place, it would be his last move, he had one
+        last memory of her. He remembered. They were sitting on the grass of
+        their front yard. It was midnight and it was raining, and the night was
+        pure and dark and starless. There was no sky. And they talked about
+        everything and nothing. It was the night before she died. And it was his
+        last memory before going. And it was everything and nothing. And he
+        smiled before going.
       </StandardProse>
-       <Footer/>
-
-         </>
-     )
-   }
+      <Footer />
+    </>
+  );
+}
