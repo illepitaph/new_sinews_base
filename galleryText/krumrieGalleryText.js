7f25796fa2c374d49576ee85dd20c0c9f2bd550f
Steve Barbaro <steve.barbaro@gmail.com>
2023-01-03 20:22:54 -0600

    fixes issue3 krumrie code formatting

--- galleryText/krumrieGalleryText.js
+++ galleryText/krumrieGalleryText.js
@@ -1,36 +1,53 @@
-import CenterGrayTitle from '../components/centerGrayTitle.js'
-import CenterBlackTitle from '../components/centerBlackTitle.js'
-import GalleryLineation from '../components/galleryLineation.js'
-import styles from '../styles/Krumrie.module.css'
+import CenterGrayTitle from "../components/centerGrayTitle.js";
+import CenterBlackTitle from "../components/centerBlackTitle.js";
+import GalleryLineation from "../components/galleryLineation.js";
+import styles from "../styles/Krumrie.module.css";
 
-export function KrumrieRawText1(){
-  return   (
-          <>
-          <CenterGrayTitle><span className={styles.blackShadow}>
-          <i>k <span
-          className={styles.alternateShadow}> e</span> l<span
-          className={styles.alternateShadow}> l</span> y</i><br/>   k <span
-          className={styles.alternateShadow}> r</span> u<span
-          className={styles.alternateShadow}> m</span> r<span
-          className={styles.alternateShadow}> i</span> e</span></CenterGrayTitle>
-          </>
-    )
+export function KrumrieRawText1() {
+  return (
+    <>
+      <CenterGrayTitle>
+        <span className={styles.blackShadow}>
+          <i>
+            k <span className={styles.alternateShadow}> e</span> l
+            <span className={styles.alternateShadow}> l</span> y
+          </i>
+          <br /> k <span className={styles.alternateShadow}> r</span> u
+          <span className={styles.alternateShadow}> m</span> r
+          <span className={styles.alternateShadow}> i</span> e
+        </span>
+      </CenterGrayTitle>
+    </>
+  );
 }
 
-export function KrumrieRawText2(){
-  return   (
-          <>
-          <CenterGrayTitle>selections<br/>from<br/><br/><br/><i><span className={styles.blackShadow}>_no_<br/>
-          _measure_</span></i></CenterGrayTitle>
-          </>
-    )
+export function KrumrieRawText2() {
+  return (
+    <>
+      <CenterGrayTitle>
+        selections
+        <br />
+        from
+        <br />
+        <br />
+        <br />
+        <i>
+          <span className={styles.blackShadow}>
+            _no_
+            <br />
+            _measure_
+          </span>
+        </i>
+      </CenterGrayTitle>
+    </>
+  );
 }
 
-export function KrumrieRawText3(){
-  return   (
-          <>
-          <GalleryLineation>
-          {`An   expanse.  The  terrestrial world is
+export function KrumrieRawText3() {
+  return (
+    <>
+      <GalleryLineation>
+        {`An   expanse.  The  terrestrial world is
 made    of    surfaces.    The   desert’s
 surface    drifts    sand.  Sand-colored,
 dark   boulders   and  brush,  shadows
@@ -48,28 +65,33 @@ hard cracking, dead roots.
 Uplifts.   Roads   at   the   edges    and
 darkened courses, thinned trees along
 the road’s edges,  sand beneath those,
-sand on the roads.`}</GalleryLineation>
-          </>
-    )
+sand on the roads.`}
+      </GalleryLineation>
+    </>
+  );
 }
 
-export function KrumrieRawText4(){
-  return   (
-          <>
-          <GalleryLineation>
-          {`A crystal is an example of an ordered system. I touch the basin in a stone’s face.
+export function KrumrieRawText4() {
+  return (
+    <>
+      <GalleryLineation>
+        {`A crystal is an example of an ordered system. I touch the basin in a stone’s face.
 
 These perceptive contrasts are like tiny mirrors.
 
-See, the grass keeps the dunes in place.`}</GalleryLineation>
-          </>
-    )
+See, the grass keeps the dunes in place.`}
+      </GalleryLineation>
+    </>
+  );
 }
 
-export function KrumrieRawText5(){
-  return   (
-          <>
-            <GalleryLineation><span className={styles.reducedLineation}>{`            Each location is parallel. Here. When I walk here the canyon opens. Its sides. A river. The canyon
+export function KrumrieRawText5() {
+  return (
+    <>
+      <GalleryLineation>
+        <span
+          className={styles.reducedLineation}
+        >{`            Each location is parallel. Here. When I walk here the canyon opens. Its sides. A river. The canyon
             slopes to desert. Accuracy is an ideal limit. I walk here. See, you call me. An expanse. Dead grass.
             The dunes’ edges soften. They change location. The sand is here, and here. The river planes the
             canyon’s fallen rock. I walk here, over dunes. Accurate. Their edges soft radial, to the center of
@@ -86,29 +108,32 @@ export function KrumrieRawText5(){
 
      I walk toward—the desert—ground moves. The canyon empties—here—the field—at the center—of
           the desert—
-     your eyes behind—layers of glass—see me here—I walk—the river’s cold in shadow.`}</span></GalleryLineation>
-          </>
-    )
+     your eyes behind—layers of glass—see me here—I walk—the river’s cold in shadow.`}</span>
+      </GalleryLineation>
+    </>
+  );
 }
 
-export function KrumrieRawText6(){
-  return   (
-          <>
-          <GalleryLineation>
-          {`What location do you recognize? Here.
+export function KrumrieRawText6() {
+  return (
+    <>
+      <GalleryLineation>
+        {`What location do you recognize? Here.
 
 To interpret separate senses, to split them like an iris, snap the root in
 sections. I shake out loose matter. I signal I’m coming.
 
-We have forms. We think we know them.`}</GalleryLineation>
-          </>
-    )
+We have forms. We think we know them.`}
+      </GalleryLineation>
+    </>
+  );
 }
 
-export function KrumrieRawText7(){
-  return   (
-          <>
-          <GalleryLineation><span className={styles.reducedLineation}>
+export function KrumrieRawText7() {
+  return (
+    <>
+      <GalleryLineation>
+        <span className={styles.reducedLineation}>
           {`Documentation proceeds by accumulation. One thing, another, the most complete. The image of
 what you see the most complete.
 
@@ -130,16 +155,18 @@ I see myself in the field, in the rows. On the map, the row, me crouched over th
 grains with my fingers, dropping samples into a container, pulling my cart to the next coordinate.
 You press my body so I move. Rotate the telescope. The conveyor along the quarry’s edge, trucks,
 the sweepers clearing the road of sand. I brush sand from my arm. You brush sand from my arm.
-`}</span></GalleryLineation>
-          </>
-    )
+`}
+        </span>
+      </GalleryLineation>
+    </>
+  );
 }
 
-export function KrumrieRawText8(){
-  return   (
-          <>
-          <GalleryLineation>
-          {`Underground may solve the temperature for root systems spreading lateral,
+export function KrumrieRawText8() {
+  return (
+    <>
+      <GalleryLineation>
+        {`Underground may solve the temperature for root systems spreading lateral,
 and  downward,   where  the  moisture’s  locked  from  the  grains’  latticed
 covering. This is posted at the entrance. Our hands aren’t touching. How do
 the roots respond to footsteps, overcast?
@@ -147,7 +174,8 @@ the roots respond to footsteps, overcast?
 If I turn just right—crystalline.
 
 Glass has interior dynamism.  I mean, the pattern comes later. I mean, when
-I’m looking at it.`}</GalleryLineation>
-          </>
-    )
+I’m looking at it.`}
+      </GalleryLineation>
+    </>
+  );
 }
--- pages/issue3/krumrie.js
+++ pages/issue3/krumrie.js
@@ -1,79 +1,80 @@
-import React, {useState, useEffect} from 'react'
-import Head from 'next/head'
-import Header from '../../components/header.js'
-import Footer from '../../components/footer.js'
-import styles from '../../styles/Krumrie.module.css'
-import { krumrieText } from '../../GalleryData/KrumrieCarouselData.js'
-Krumrie.title = "kelly_krumrie ~ selections_from_'no_measure' — new_sinews"
-Krumrie.pieceName = "kelly_krumrie ~ selections_from_'no_measure' — new_sinews"
-Krumrie.description = "_Kelly Krumrie_poetry_SELECTIONS FROM NO MEASURE_new_sinews_issue3"
-Krumrie.image = "hhttps://www.newnewsinews.com/images/KrumrieBackground.jpg"
+import React, { useState, useEffect } from "react";
+import Head from "next/head";
+import Header from "../../components/header.js";
+import Footer from "../../components/footer.js";
+import styles from "../../styles/Krumrie.module.css";
+import { krumrieText } from "../../GalleryData/KrumrieCarouselData.js";
+Krumrie.title = "kelly_krumrie ~ selections_from_'no_measure' — new_sinews";
+Krumrie.pieceName = "kelly_krumrie ~ selections_from_'no_measure' — new_sinews";
+Krumrie.description =
+  "_Kelly Krumrie_poetry_SELECTIONS FROM NO MEASURE_new_sinews_issue3";
+Krumrie.image = "hhttps://www.newnewsinews.com/images/KrumrieBackground.jpg";
 
 export default function Krumrie() {
   const [textArrayIndex, setTextArrayIndex] = useState(0);
-    function prevText() {
-       if (textArrayIndex > 0) {
+  function prevText() {
+    if (textArrayIndex > 0) {
       setTextArrayIndex(textArrayIndex - 1);
     } else {
       setTextArrayIndex(krumrieText.length - 1);
     }
-}
+  }
 
-    function nextText() {
-      if (textArrayIndex < 7) {
+  function nextText() {
+    if (textArrayIndex < 7) {
       setTextArrayIndex(textArrayIndex + 1);
     } else {
       setTextArrayIndex(0);
     }
-}
+  }
 
-useEffect (() => {
+  useEffect(() => {
+    function handleKeyDown(e) {
+      if (e.keyCode === 37 && textArrayIndex > 0) {
+        setTextArrayIndex(textArrayIndex - 1);
+      } else if (e.keyCode === 37 && textArrayIndex === 0) {
+        setTextArrayIndex(krumrieText.length - 1);
+      } else if (e.keyCode === 39 && textArrayIndex < 7) {
+        setTextArrayIndex(textArrayIndex + 1);
+      } else if (e.keyCode === 39 && textArrayIndex === 7) {
+        setTextArrayIndex(0);
+      }
+    }
 
-function handleKeyDown(e) {
-   if (e.keyCode === 37 && textArrayIndex > 0) {
-setTextArrayIndex(textArrayIndex - 1);
-} else if (e.keyCode === 37 && textArrayIndex === 0)  {
- setTextArrayIndex(krumrieText.length - 1);
-}  else if (e.keyCode === 39 && textArrayIndex < 7) {
-  setTextArrayIndex(textArrayIndex + 1);
-} else if (e.keyCode === 39 && textArrayIndex === 7) {
-  setTextArrayIndex(0);
- }
-}
+    window.addEventListener("keydown", handleKeyDown);
 
-  window.addEventListener("keydown", handleKeyDown);
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [textArrayIndex]);
 
-return () => {
-  window.removeEventListener("keydown", handleKeyDown);
-};
-}, [textArrayIndex]);
-
-
-const RawText = krumrieText[textArrayIndex]["textName"];
-
+  const RawText = krumrieText[textArrayIndex]["textName"];
 
   return (
-      <>
-          <Head/>
+    <>
+      <Head />
 
+      <Header headerBackground="krumrieBackground" />
 
-          <Header headerBackground="krumrieBackground"/>
-
-          <div className={styles.container}>
-
-          <div className={styles.carousel}>
-          <div className={styles.prev} onClick={prevText}>&#8678; &#8723;<br/>&#177; &#8678;</div>
-          <div className={styles.next} onClick={nextText}>&#177; &#8680;<br/>&#8680; &#8723;</div>
-          <div><RawText/></div>
+      <div className={styles.container}>
+        <div className={styles.carousel}>
+          <div className={styles.prev} onClick={prevText}>
+            &#8678; &#8723;
+            <br />
+            &#177; &#8678;
           </div>
+          <div className={styles.next} onClick={nextText}>
+            &#177; &#8680;
+            <br />
+            &#8680; &#8723;
+          </div>
+          <div>
+            <RawText />
+          </div>
+        </div>
+      </div>
 
-           </div>
-
-
-
-              <Footer/>
-
-                    </>
-
-                      )
-                    }
+      <Footer />
+    </>
+  );
+}
