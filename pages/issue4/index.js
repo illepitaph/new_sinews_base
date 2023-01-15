35dfe3051937b3cc16273677e4500b9420c73ec0
Steve Barbaro <steve.barbaro@gmail.com>
2023-01-15 13:46:53 -0600

    refines issue4 cover code formatting

--- pages/issue4/index.js
+++ pages/issue4/index.js
@@ -1,84 +1,140 @@
-import Head from 'next/head'
-import Link from 'next/link'
-import styles from '../../styles/Issue4.module.css'
-Issue4.title = "new_sinews issue4"
-Issue4.pieceName = "new_sinews issue4"
-Issue4.description = "_newsinews_issue4_Brandon Shimoda+K-Ming Chang+Evan Isoline+Christa Romanosky+Gabriel Palacios+Allison Hummel+Mike Corrao+Vi Khi Nao+Grant Maierhofer_"
-Issue4.image = "https://www.newnewsinews.com/images/Issue4Cover.jpg"
+import Head from "next/head";
+import Link from "next/link";
+import styles from "../../styles/Issue4.module.css";
+Issue4.title = "new_sinews issue4";
+Issue4.pieceName = "new_sinews issue4";
+Issue4.description =
+  "_newsinews_issue4_Brandon Shimoda+K-Ming Chang+Evan Isoline+Christa Romanosky+Gabriel Palacios+Allison Hummel+Mike Corrao+Vi Khi Nao+Grant Maierhofer_";
+Issue4.image = "https://www.newnewsinews.com/images/Issue4Cover.jpg";
 
 export default function Issue4() {
   return (
     <>
-      <Head/>
+      <Head />
 
-        <div className={styles.pageStructure}>
-          <div className={styles.issueContents}>
-        <div className={styles.workColumn}>
-            <Link href="/issue4/hour" passHref><div className={styles.workLink}>... brandon shimoda + the hour of the rat ...</div></Link>
-        </div>
-        <div className={styles.workColumn}>
-          <Link href="/issue4/guanyin" passHref><div className={styles.workLink}>... k-ming chang + guanyin ...</div></Link>
-        </div>
-        <div className={styles.workColumn}>
-        <Link href="/issue4/isoline" passHref><div className={styles.workLink}>... evan isoline + as foam from
-          this sweet hour ...</div></Link>
-        </div>
-        <div className={styles.workColumn}>
-          <Link href="/issue4/howfar" passHref><div className={styles.workLink}>... christa romanosky + how far you can
-          get on an empty tank of gas ...</div></Link>
-        </div>
-        <div className={styles.workColumn}>
-          <Link href="/issue4/palacios" passHref><div className={styles.workLink}>... gabriel palacios + poems from lunar hilton
-          elegy ...</div></Link>
-        </div>
-        <div className={styles.workColumn}>
-          <Link href="/issue4/hummel" passHref><div className={styles.workLink}>... allison hummel + the fires are back // i look at
-          sauce // untitled // comfort like the ...</div></Link>
-        </div>
-        <div className={styles.workColumn}>
-          <Link href="/issue4/textspinality" passHref><div className={styles.workLink}>... mike corrao + text spinality ...</div></Link>
-        </div>
-        <div className={styles.workColumn}>
-        <Link href="/issue4/poemsborn" passHref><div className={styles.workLink}>... vi khi nao + poems born from basic
-          words ...</div></Link>
-        </div>
-        <div className={styles.workColumn}>
-          <Link href="/issue4/maierhofer" passHref><div className={styles.workLink}>... grant maierhofer + in the time when my memory
-          fixated on the death of princess diana ...</div></Link>
-        </div>
-        <div className={styles.workColumn}>
-            <div className={styles.workLink}><Link href="/issue4/bios" passHref>~~~ contri butor_ _ _b  i  os ~~~</Link></div>
+      <div className={styles.pageStructure}>
+        <div className={styles.issueContents}>
+          <div className={styles.workColumn}>
+            <Link href="/issue4/hour" passHref>
+              <div className={styles.workLink}>
+                ... brandon shimoda + the hour of the rat ...
+              </div>
+            </Link>
           </div>
+          <div className={styles.workColumn}>
+            <Link href="/issue4/guanyin" passHref>
+              <div className={styles.workLink}>
+                ... k-ming chang + guanyin ...
+              </div>
+            </Link>
           </div>
-
-             <div className={styles.issueDecor}>
-             <div className={styles.titleAndSubtitle}>
-               _ n e w + <br/>s i n e w s _<br/><br/><span className={styles.subtitle}>issue _ 4</span></div>
-             <div className={styles.backgroundBox}></div>
-             <div className={styles.backgroundBox}></div>
-             <div className={styles.backgroundBox}></div>
-             <div className={styles.backgroundBox}></div>
-             <div className={styles.backgroundBox}></div>
-             <div className={styles.backgroundBox}></div>
-             <div className={styles.backgroundBox}></div>
-             <div className={styles.backgroundBox}></div>
-             <div className={styles.backgroundBox}></div>
-             <div className={styles.backgroundBox}></div>
-             <div className={styles.backgroundBox}></div>
-             <div className={styles.backgroundBox}></div>
-             <div className={styles.siteShortcuts}>
-                   <button className={styles.linkButton}><div
-                   className={styles.innerSiteLinkTag}><Link href="/" passHref>new sinews</Link></div></button>
-                   <button className={styles.linkButton}><div
-                   className={styles.innerSiteLinkTag}><Link href="/about" passHref>about</Link></div></button>
-                   <button className={styles.linkButton}>
-                     <a className={styles.twitterLinkTag} href="https://twitter.com/new_sinews"
-                     target="_blank" rel="noreferrer">tweeter</a>
-                     </button>
-                   </div>
-             </div>
+          <div className={styles.workColumn}>
+            <Link href="/issue4/isoline" passHref>
+              <div className={styles.workLink}>
+                ... evan isoline + as foam from this sweet hour ...
+              </div>
+            </Link>
+          </div>
+          <div className={styles.workColumn}>
+            <Link href="/issue4/howfar" passHref>
+              <div className={styles.workLink}>
+                ... christa romanosky + how far you can get on an empty tank of
+                gas ...
+              </div>
+            </Link>
+          </div>
+          <div className={styles.workColumn}>
+            <Link href="/issue4/palacios" passHref>
+              <div className={styles.workLink}>
+                ... gabriel palacios + poems from lunar hilton elegy ...
+              </div>
+            </Link>
+          </div>
+          <div className={styles.workColumn}>
+            <Link href="/issue4/hummel" passHref>
+              <div className={styles.workLink}>
+                ... allison hummel + the fires are back // i look at sauce //
+                untitled // comfort like the ...
+              </div>
+            </Link>
+          </div>
+          <div className={styles.workColumn}>
+            <Link href="/issue4/textspinality" passHref>
+              <div className={styles.workLink}>
+                ... mike corrao + text spinality ...
+              </div>
+            </Link>
+          </div>
+          <div className={styles.workColumn}>
+            <Link href="/issue4/poemsborn" passHref>
+              <div className={styles.workLink}>
+                ... vi khi nao + poems born from basic words ...
+              </div>
+            </Link>
+          </div>
+          <div className={styles.workColumn}>
+            <Link href="/issue4/maierhofer" passHref>
+              <div className={styles.workLink}>
+                ... grant maierhofer + in the time when my memory fixated on the
+                death of princess diana ...
+              </div>
+            </Link>
+          </div>
+          <div className={styles.workColumn}>
+            <div className={styles.workLink}>
+              <Link href="/issue4/bios" passHref>
+                ~~~ contri butor_ _ _b i os ~~~
+              </Link>
+            </div>
+          </div>
         </div>
 
-        </>
-      )
-    }
+        <div className={styles.issueDecor}>
+          <div className={styles.titleAndSubtitle}>
+            _ n e w + <br />s i n e w s _<br />
+            <br />
+            <span className={styles.subtitle}>issue _ 4</span>
+          </div>
+          <div className={styles.backgroundBox}></div>
+          <div className={styles.backgroundBox}></div>
+          <div className={styles.backgroundBox}></div>
+          <div className={styles.backgroundBox}></div>
+          <div className={styles.backgroundBox}></div>
+          <div className={styles.backgroundBox}></div>
+          <div className={styles.backgroundBox}></div>
+          <div className={styles.backgroundBox}></div>
+          <div className={styles.backgroundBox}></div>
+          <div className={styles.backgroundBox}></div>
+          <div className={styles.backgroundBox}></div>
+          <div className={styles.backgroundBox}></div>
+          <div className={styles.siteShortcuts}>
+            <button className={styles.linkButton}>
+              <div className={styles.innerSiteLinkTag}>
+                <Link href="/" passHref>
+                  new sinews
+                </Link>
+              </div>
+            </button>
+            <button className={styles.linkButton}>
+              <div className={styles.innerSiteLinkTag}>
+                <Link href="/about" passHref>
+                  about
+                </Link>
+              </div>
+            </button>
+            <button className={styles.linkButton}>
+              <a
+                className={styles.twitterLinkTag}
+                href="https://twitter.com/new_sinews"
+                target="_blank"
+                rel="noreferrer"
+              >
+                tweeter
+              </a>
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
