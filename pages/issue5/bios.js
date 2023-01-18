3f41b8ee496921118a9e5db7b958e1b99047b022
Steve Barbaro <steve.barbaro@gmail.com>
2023-01-18 12:22:50 -0600

    fixes issue5 bios page broken link

--- pages/issue5/bios.js
+++ pages/issue5/bios.js
@@ -162,7 +162,7 @@ export default function Issue5Bios() {
           className={styles.biosExternalLink}
           target="_blank"
           rel="noreferrer"
-          href="http://www.uapress.ua.edu/product/Brief-Alphabet-of-Torture,6637.aspx"
+          href="https://www.uapress.ua.edu/9781573660617/a-brief-alphabet-of-torture"
         >
           <cite>
             <strong> A Brief Alphabet of Torture </strong>
