1cc6951f04fd4d95ce4ccad3365a9ad0d43eb9df
Steve Barbaro <steve.barbaro@gmail.com>
2023-01-24 13:48:49 -0600

    fixes issue5 corrao code formatting

--- galleryText/theaterGalleryText.js
+++ galleryText/theaterGalleryText.js
@@ -1,105 +1,183 @@
-import styles from '../styles/Theater.module.css'
+import styles from "../styles/Theater.module.css";
 
-export function TheaterRawText1(){
-  return   (
-          <>
-          <span className={styles.load}><img className={styles.img} src="../images/Theaterofmaterials1.jpg" alt="Theater of Materials by Mike Corrao ~ Image 1"></img></span>
-          </>
-    )
+export function TheaterRawText1() {
+  return (
+    <>
+      <span className={styles.load}>
+        <img
+          className={styles.img}
+          src="../images/Theaterofmaterials1.jpg"
+          alt="Theater of Materials by Mike Corrao ~ Image 1"
+        ></img>
+      </span>
+    </>
+  );
 }
 
-export function TheaterRawText2(){
-  return   (
-          <>
-          <span className={styles.load}><img className={styles.img} src="../images/Theaterofmaterials2.jpg" alt="Theater of Materials by Mike Corrao ~ Image 2"></img></span>
-          </>
-    )
+export function TheaterRawText2() {
+  return (
+    <>
+      <span className={styles.load}>
+        <img
+          className={styles.img}
+          src="../images/Theaterofmaterials2.jpg"
+          alt="Theater of Materials by Mike Corrao ~ Image 2"
+        ></img>
+      </span>
+    </>
+  );
 }
 
-export function TheaterRawText3(){
-  return   (
-           <>
-           <span className={styles.load}><img className={styles.img} src="../images/Theaterofmaterials3.jpg" alt="Theater of Materials by Mike Corrao ~ Image 3"></img></span>
-           </>
-    )
+export function TheaterRawText3() {
+  return (
+    <>
+      <span className={styles.load}>
+        <img
+          className={styles.img}
+          src="../images/Theaterofmaterials3.jpg"
+          alt="Theater of Materials by Mike Corrao ~ Image 3"
+        ></img>
+      </span>
+    </>
+  );
 }
 
-export function TheaterRawText4(){
-  return   (
-            <>
-            <span className={styles.load}><img className={styles.img} src="../images/Theaterofmaterials4.jpg" alt="Theater of Materials by Mike Corrao ~ Image 4"></img></span>
-            </>
-    )
+export function TheaterRawText4() {
+  return (
+    <>
+      <span className={styles.load}>
+        <img
+          className={styles.img}
+          src="../images/Theaterofmaterials4.jpg"
+          alt="Theater of Materials by Mike Corrao ~ Image 4"
+        ></img>
+      </span>
+    </>
+  );
 }
 
-export function TheaterRawText5(){
-  return   (
-            <>
-            <span className={styles.load}><img className={styles.img} src="../images/Theaterofmaterials5.jpg" alt="Theater of Materials by Mike Corrao ~ Image 5"></img></span>
-            </>
-    )
+export function TheaterRawText5() {
+  return (
+    <>
+      <span className={styles.load}>
+        <img
+          className={styles.img}
+          src="../images/Theaterofmaterials5.jpg"
+          alt="Theater of Materials by Mike Corrao ~ Image 5"
+        ></img>
+      </span>
+    </>
+  );
 }
 
-export function TheaterRawText6(){
-  return   (
-            <>
-            <span className={styles.load}><img className={styles.img} src="../images/Theaterofmaterials6.jpg" alt="Theater of Materials by Mike Corrao ~ Image 6"></img></span>
-            </>
-    )
+export function TheaterRawText6() {
+  return (
+    <>
+      <span className={styles.load}>
+        <img
+          className={styles.img}
+          src="../images/Theaterofmaterials6.jpg"
+          alt="Theater of Materials by Mike Corrao ~ Image 6"
+        ></img>
+      </span>
+    </>
+  );
 }
 
-export function TheaterRawText7(){
-  return   (
-            <>
-            <span className={styles.load}><img className={styles.img} src="../images/Theaterofmaterials7.jpg" alt="Theater of Materials by Mike Corrao ~ Image 7"></img></span>
-            </>
-    )
+export function TheaterRawText7() {
+  return (
+    <>
+      <span className={styles.load}>
+        <img
+          className={styles.img}
+          src="../images/Theaterofmaterials7.jpg"
+          alt="Theater of Materials by Mike Corrao ~ Image 7"
+        ></img>
+      </span>
+    </>
+  );
 }
 
-export function TheaterRawText8(){
-  return   (
-            <>
-            <span className={styles.load}><img className={styles.img} src="../images/Theaterofmaterials8.jpg" alt="Theater of Materials by Mike Corrao ~ Image 8"></img></span>
-            </>
-    )
+export function TheaterRawText8() {
+  return (
+    <>
+      <span className={styles.load}>
+        <img
+          className={styles.img}
+          src="../images/Theaterofmaterials8.jpg"
+          alt="Theater of Materials by Mike Corrao ~ Image 8"
+        ></img>
+      </span>
+    </>
+  );
 }
 
-export function TheaterRawText9(){
-  return   (
-            <>
-            <span className={styles.load}><img className={styles.img} src="../images/Theaterofmaterials9.jpg" alt="Theater of Materials by Mike Corrao ~ Image 9"></img></span>
-            </>
-    )
+export function TheaterRawText9() {
+  return (
+    <>
+      <span className={styles.load}>
+        <img
+          className={styles.img}
+          src="../images/Theaterofmaterials9.jpg"
+          alt="Theater of Materials by Mike Corrao ~ Image 9"
+        ></img>
+      </span>
+    </>
+  );
 }
 
-export function TheaterRawText10(){
-  return   (
-            <>
-            <span className={styles.load}><img className={styles.img} src="../images/Theaterofmaterials10.jpg" alt="Theater of Materials by Mike Corrao ~ Image 10"></img></span>
-            </>
-    )
+export function TheaterRawText10() {
+  return (
+    <>
+      <span className={styles.load}>
+        <img
+          className={styles.img}
+          src="../images/Theaterofmaterials10.jpg"
+          alt="Theater of Materials by Mike Corrao ~ Image 10"
+        ></img>
+      </span>
+    </>
+  );
 }
 
-export function TheaterRawText11(){
-  return   (
-            <>
-            <span className={styles.load}><img className={styles.img} src="../images/Theaterofmaterials11.jpg" alt="Theater of Materials by Mike Corrao ~ Image 11"></img></span>
-            </>
-    )
+export function TheaterRawText11() {
+  return (
+    <>
+      <span className={styles.load}>
+        <img
+          className={styles.img}
+          src="../images/Theaterofmaterials11.jpg"
+          alt="Theater of Materials by Mike Corrao ~ Image 11"
+        ></img>
+      </span>
+    </>
+  );
 }
 
-export function TheaterRawText12(){
-  return   (
-            <>
-            <span className={styles.load}><img className={styles.img} src="../images/Theaterofmaterials12.jpg" alt="Theater of Materials by Mike Corrao ~ Image 12"></img></span>
-            </>
-    )
+export function TheaterRawText12() {
+  return (
+    <>
+      <span className={styles.load}>
+        <img
+          className={styles.img}
+          src="../images/Theaterofmaterials12.jpg"
+          alt="Theater of Materials by Mike Corrao ~ Image 12"
+        ></img>
+      </span>
+    </>
+  );
 }
 
-export function TheaterRawText13(){
-  return   (
-            <>
-            <span className={styles.load}><img className={styles.img} src="../images/Theaterofmaterials13.jpg" alt="Theater of Materials by Mike Corrao ~ Image 14"></img></span>
-            </>
-    )
+export function TheaterRawText13() {
+  return (
+    <>
+      <span className={styles.load}>
+        <img
+          className={styles.img}
+          src="../images/Theaterofmaterials13.jpg"
+          alt="Theater of Materials by Mike Corrao ~ Image 14"
+        ></img>
+      </span>
+    </>
+  );
 }
--- pages/issue5/theater.js
+++ pages/issue5/theater.js
@@ -1,77 +1,79 @@
-import React, {useState, useEffect} from 'react'
-import Head from 'next/head'
-import Header from '../../components/header.js'
-import Footer from '../../components/footer.js'
-import styles from '../../styles/Theater.module.css'
-import { theaterText } from '../../GalleryData/TheaterCarouselData.js'
-TheaterOfMaterials.title = "mike_corrao ~ theater_of_materials — new_sinews"
-TheaterOfMaterials.pieceName = "mike_corrao ~ theater_of_materials — new_sinews"
-TheaterOfMaterials.description = "_Mike_Corrao_fiction_THEATEROFMATERIALS_new_sinews_issue5_"
-TheaterOfMaterials.image = "https://www.newnewsinews.com/images/TheaterHeader.jpg"
+import React, { useState, useEffect } from "react";
+import Head from "next/head";
+import Header from "../../components/header.js";
+import Footer from "../../components/footer.js";
+import styles from "../../styles/Theater.module.css";
+import { theaterText } from "../../GalleryData/TheaterCarouselData.js";
+TheaterOfMaterials.title = "mike_corrao ~ theater_of_materials — new_sinews";
+TheaterOfMaterials.pieceName =
+  "mike_corrao ~ theater_of_materials — new_sinews";
+TheaterOfMaterials.description =
+  "_Mike_Corrao_fiction_THEATEROFMATERIALS_new_sinews_issue5_";
+TheaterOfMaterials.image =
+  "https://www.newnewsinews.com/images/TheaterHeader.jpg";
 
 export default function TheaterOfMaterials() {
   const [textArrayIndex, setTextArrayIndex] = useState(0);
-    function prevText() {
-       if (textArrayIndex > 0) {
+  function prevText() {
+    if (textArrayIndex > 0) {
       setTextArrayIndex(textArrayIndex - 1);
     } else {
       setTextArrayIndex(theaterText.length - 1);
     }
-}
+  }
 
-    function nextText() {
-      if (textArrayIndex < 12) {
+  function nextText() {
+    if (textArrayIndex < 12) {
       setTextArrayIndex(textArrayIndex + 1);
     } else {
       setTextArrayIndex(0);
     }
-}
+  }
 
-useEffect (() => {
+  useEffect(() => {
+    function handleKeyDown(e) {
+      if (e.keyCode === 37 && textArrayIndex > 0) {
+        setTextArrayIndex(textArrayIndex - 1);
+      } else if (e.keyCode === 37 && textArrayIndex === 0) {
+        setTextArrayIndex(theaterText.length - 1);
+      } else if (e.keyCode === 39 && textArrayIndex < 12) {
+        setTextArrayIndex(textArrayIndex + 1);
+      } else if (e.keyCode === 39 && textArrayIndex === 12) {
+        setTextArrayIndex(0);
+      }
+    }
 
-function handleKeyDown(e) {
-   if (e.keyCode === 37 && textArrayIndex > 0) {
-setTextArrayIndex(textArrayIndex - 1);
-} else if (e.keyCode === 37 && textArrayIndex === 0)  {
- setTextArrayIndex(theaterText.length - 1);
-}  else if (e.keyCode === 39 && textArrayIndex < 12) {
-  setTextArrayIndex(textArrayIndex + 1);
-} else if (e.keyCode === 39 && textArrayIndex === 12) {
-  setTextArrayIndex(0);
- }
-}
+    window.addEventListener("keydown", handleKeyDown);
 
-  window.addEventListener("keydown", handleKeyDown);
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [textArrayIndex]);
 
-return () => {
-  window.removeEventListener("keydown", handleKeyDown);
-};
-}, [textArrayIndex]);
-
-
-const RawText = theaterText[textArrayIndex]["textName"];
-
+  const RawText = theaterText[textArrayIndex]["textName"];
 
   return (
-      <>
-          <Head/>
+    <>
+      <Head />
 
+      <Header headerBackground="theaterBackground" />
 
-          <Header headerBackground="theaterBackground"/>
-
-          <div className={styles.container}>
-
-          <div className={styles.carousel}>
-          <div className={styles.prev} onClick={prevText}>&#8678; back <br/> &#8604; &#10710; &#8604;<br/> back &#8678;</div>
-          <div className={styles.next} onClick={nextText}>&#8680; forth <br/> &#10547;  &#10710; &#10547; <br/> forth &#8680;</div>
-          <div><RawText/></div>
+      <div className={styles.container}>
+        <div className={styles.carousel}>
+          <div className={styles.prev} onClick={prevText}>
+            &#8678; back <br /> &#8604; &#10710; &#8604;
+            <br /> back &#8678;
           </div>
+          <div className={styles.next} onClick={nextText}>
+            &#8680; forth <br /> &#10547; &#10710; &#10547; <br /> forth &#8680;
+          </div>
+          <div>
+            <RawText />
+          </div>
+        </div>
+      </div>
 
-           </div>
-
-              <Footer/>
-
-                    </>
-
-                      )
-                    }
+      <Footer />
+    </>
+  );
+}
