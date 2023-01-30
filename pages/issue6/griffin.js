19ff1b51ed30ff6088375ef26fb61cc85b1dab64
Steve Barbaro <steve.barbaro@gmail.com>
2023-01-29 19:38:04 -0600

    fixes issue6 walnut code formatting

--- pages/issue6/griffin.js
+++ pages/issue6/griffin.js
@@ -1,86 +1,115 @@
-import Head from 'next/head'
-import Header from '../../components/header.js'
-import CenterGrayTitle from '../../components/centerGrayTitle.js'
-import RightGrayTitle from '../../components/rightGrayTitle.js'
-import LeftGrayTitle from '../../components/leftGrayTitle.js'
-import RightBlackTitle from '../../components/rightBlackTitle.js'
-import Footer from '../../components/footer.js'
-import styles from '../../styles/Griffin.module.css'
-Griffin.title = "brandan_griffin ~ wolly_a_walnut_talks_anotherwhere — new_sinews"
-Griffin.pieceName = "brandan_griffin ~ wolly_a_walnut_talks_anotherwhere — new_sinews"
-Griffin.description = "_BRANDAN GRIFFIN_poetry_WALLY A WALNUT TALKS ANOTHERWHERE_new_sinews_issue6_"
-Griffin.image = "https://www.newnewsinews.com/images/GriffinBackground.jpg"
+import Head from "next/head";
+import Header from "../../components/header.js";
+import CenterGrayTitle from "../../components/centerGrayTitle.js";
+import RightGrayTitle from "../../components/rightGrayTitle.js";
+import LeftGrayTitle from "../../components/leftGrayTitle.js";
+import RightBlackTitle from "../../components/rightBlackTitle.js";
+import Footer from "../../components/footer.js";
+import styles from "../../styles/Griffin.module.css";
+Griffin.title =
+  "brandan_griffin ~ wolly_a_walnut_talks_anotherwhere — new_sinews";
+Griffin.pieceName =
+  "brandan_griffin ~ wolly_a_walnut_talks_anotherwhere — new_sinews";
+Griffin.description =
+  "_BRANDAN GRIFFIN_poetry_WALLY A WALNUT TALKS ANOTHERWHERE_new_sinews_issue6_";
+Griffin.image = "https://www.newnewsinews.com/images/GriffinBackground.jpg";
 
 export default function Griffin() {
   return (
     <>
-    <Header headerBackground="griffinBackground"/>
+      <Header headerBackground="griffinBackground" />
 
-    <br/>
-    <br/>
-    <br/>
-    <br/>
+      <br />
+      <br />
+      <br />
+      <br />
 
-    <CenterGrayTitle><span
-    className={styles.inverseShadow}>w </span><span
-    className={styles.titleShadow}>o</span> <span
-    className={styles.mintAlternate}>ll</span><span
-    className={styles.inverseShadowAlternate}> y</span> <br/><br/>
-    </CenterGrayTitle>
+      <CenterGrayTitle>
+        <span className={styles.inverseShadow}>w </span>
+        <span className={styles.titleShadow}>o</span>{" "}
+        <span className={styles.mintAlternate}>ll</span>
+        <span className={styles.inverseShadowAlternate}> y</span> <br />
+        <br />
+      </CenterGrayTitle>
 
+      <RightGrayTitle>
+        <span className={styles.inverseShadow}>a</span>
+        <br />
+        <br />
+        <span className={styles.titleShadow}>w</span>
+        <span className={styles.inverseShadow}>a</span>
+        <span className={styles.mintAlternate}>l </span>
+        <span className={styles.titleShadow}>
+          <i>n</i>
+        </span>
+        <span className={styles.inverseShadow}> u</span>
+        <span className={styles.mintAlternate}>t</span>
+      </RightGrayTitle>
 
-    <RightGrayTitle><span className={styles.inverseShadow}>a</span><br/><br/><span
-    className={styles.titleShadow}>w</span><span
-    className={styles.inverseShadow}>a</span><span
-    className={styles.mintAlternate}>l </span><span
-    className={styles.titleShadow}><i>n</i></span><span
-    className={styles.inverseShadow}> u</span><span
-    className={styles.mintAlternate}>t</span></RightGrayTitle>
+      <br />
+      <br />
 
-    <br/>
-    <br/>
+      <CenterGrayTitle>
+        <span className={styles.inverseShadowAlternate}>t</span>
+        <span className={styles.inverseShadow}>
+          a<span className={styles.mintAlternate}>l</span>{" "}
+        </span>
+        <span className={styles.titleShadow}>k s</span>
+      </CenterGrayTitle>
 
-    <CenterGrayTitle><span
-    className={styles.inverseShadowAlternate}>t</span><span
-    className={styles.inverseShadow}>a<span
-    className={styles.mintAlternate}>l</span> </span><span
-    className={styles.titleShadow}>k s</span></CenterGrayTitle>
+      <br />
+      <br />
 
-    <br/>
-    <br/>
+      <LeftGrayTitle>
+        <span className={styles.inverseShadow}>a </span>
+        <span className={styles.titleShadow}>n </span>
+        <span className={styles.inverseShadow}>o </span>
+        <span className={styles.mintAlternate}>t </span>
+        <span className={styles.titleShadow}>
+          <i>h</i>{" "}
+        </span>
+        <span className={styles.inverseShadowAlternate}>
+          e <span className={styles.inverseShadow}>r </span>
+        </span>
+        <span className={styles.mintAlternate}>
+          wh<span className={styles.inverseShadowAlternate}>er</span>e
+        </span>
+      </LeftGrayTitle>
 
-    <LeftGrayTitle><span className={styles.inverseShadow}>a </span><span
-    className={styles.titleShadow}>n </span><span
-    className={styles.inverseShadow}>o </span><span
-    className={styles.mintAlternate}>t </span><span
-    className={styles.titleShadow}><i>h</i> </span><span
-    className={styles.inverseShadowAlternate}>e <span
-    className={styles.inverseShadow}>r </span></span><span
-    className={styles.mintAlternate}>wh<span
-    className={styles.inverseShadowAlternate}>er</span>e</span></LeftGrayTitle>
+      <br />
+      <br />
 
-    <br/>
-    <br/>
+      <RightBlackTitle>
+        <span className={styles.bareHeader}>... brandan ... griffin ...</span>
+      </RightBlackTitle>
 
-    <RightBlackTitle><span className={styles.bareHeader}>... brandan ... griffin ...</span>
-    </RightBlackTitle>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
 
-    <br/>
-    <br/>
-    <br/>
-    <br/>
-    <br/>
-    <br/>
-    <br/>
-    <br/>
+      <div className={styles.poemTitleContainer}>
+        <i>the nut&apos;s on public transit, then park</i>
+      </div>
 
-    <div className={styles.poemTitleContainer}>
-    <i>the nut&apos;s on public transit, then park</i>
-    </div>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
 
-    <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-
-    <pre className={styles.poem}>{`                                                                        ....
+      <pre
+        className={styles.poem}
+      >{`                                                                        ....
                                                                         ....
                                                                           ....
                                                                              ....
@@ -722,7 +751,7 @@ export default function Griffin() {
 
 `}</pre>
 
-    <Footer/>
+      <Footer />
     </>
-  )
+  );
 }
