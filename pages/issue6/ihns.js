997a3a1c37bff2344640e62c029da312533e8706
Steve Barbaro <steve.barbaro@gmail.com>
2023-01-30 13:05:46 -0600

    fixes issue6 ihns code formatting

--- pages/issue6/ihns.js
+++ pages/issue6/ihns.js
@@ -1,52 +1,73 @@
-import Head from 'next/head'
-import Header from '../../components/header.js'
-import RightGrayTitle from '../../components/rightGrayTitle.js'
-import CenterBlackTitle from '../../components/centerBlackTitle.js'
-import Footer from '../../components/footer.js'
-import styles from '../../styles/Ihns.module.css'
-Ihns.title = "kirsten_ihns ~ 6_poems — new_sinews"
-Ihns.pieceName = "kirsten_ihns ~ all_properties_of_the_house_burning ++ in_which_you_are_appearing_as_its_shape ++ — new_sinews"
-Ihns.description = "_Kirsten Ihns_poetry_6 POEMS_issue6_"
-Ihns.image = "https://www.newnewsinews.com/images/IhnsBackground.jpg"
-
-
+import Head from "next/head";
+import Header from "../../components/header.js";
+import RightGrayTitle from "../../components/rightGrayTitle.js";
+import CenterBlackTitle from "../../components/centerBlackTitle.js";
+import Footer from "../../components/footer.js";
+import styles from "../../styles/Ihns.module.css";
+Ihns.title = "kirsten_ihns ~ 6_poems — new_sinews";
+Ihns.pieceName =
+  "kirsten_ihns ~ all_properties_of_the_house_burning ++ in_which_you_are_appearing_as_its_shape ++ — new_sinews";
+Ihns.description = "_Kirsten Ihns_poetry_6 POEMS_issue6_";
+Ihns.image = "https://www.newnewsinews.com/images/IhnsBackground.jpg";
 
 export default function Ihns() {
   return (
     <>
-      <Head/>
-      <Header headerBackground="ihnsBackground"/>
+      <Head />
+      <Header headerBackground="ihnsBackground" />
 
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
 
-      <CenterBlackTitle><span className={styles.titleShadow}><i>wulv // hickory // the heaven of knitwear // art
-      <br/><br/><br/>class // survival of the smile of the cheshire <br/><br/><br/>cat // the simplest case of participation</i></span></CenterBlackTitle>
+      <CenterBlackTitle>
+        <span className={styles.titleShadow}>
+          <i>
+            wulv // hickory // the heaven of knitwear // art
+            <br />
+            <br />
+            <br />
+            class // survival of the smile of the cheshire <br />
+            <br />
+            <br />
+            cat // the simplest case of participation
+          </i>
+        </span>
+      </CenterBlackTitle>
 
-      <br/>
-      <br/>
-      <br/>
+      <br />
+      <br />
+      <br />
 
-      <RightGrayTitle><span className={styles.inverseNameShadow}>k<span
-      className={styles.mintAlternate}>i </span><span
-      className={styles.nameShadow}>r</span>-<br/><br/>s<span
-      className={styles.mintAlternateUppercase}>t</span><i>e</i> <span
-      className={styles.nameShadow}><i>n</i></span></span><br/><br/><span
-      className={styles.mintAlternate}>i </span><span
-      className={styles.inverseNameShadow}>h </span><span
-      className={styles.nameShadow}><i>n </i></span><span
-      className={styles.inverseNameShadow}>s</span></RightGrayTitle>
+      <RightGrayTitle>
+        <span className={styles.inverseNameShadow}>
+          k<span className={styles.mintAlternate}>i </span>
+          <span className={styles.nameShadow}>r</span>-<br />
+          <br />s<span className={styles.mintAlternateUppercase}>t</span>
+          <i>e</i>{" "}
+          <span className={styles.nameShadow}>
+            <i>n</i>
+          </span>
+        </span>
+        <br />
+        <br />
+        <span className={styles.mintAlternate}>i </span>
+        <span className={styles.inverseNameShadow}>h </span>
+        <span className={styles.nameShadow}>
+          <i>n </i>
+        </span>
+        <span className={styles.inverseNameShadow}>s</span>
+      </RightGrayTitle>
 
-      <br/>
-      <br/>
-      <br/>
+      <br />
+      <br />
+      <br />
 
       <h3 className={styles.poemTitle}>wulv</h3>
 
-      <br/>
+      <br />
       <pre className={styles.poem}>{`EVANGEL	BLISSED ANGEL
 CRY ON SLEEP
 WET PEP
@@ -66,21 +87,35 @@ SHE TELLS ME MY BODY IS A SPIRITUAL LESSON
 INNER IS A DIMENSION HE SAYS LOOKING DOWN
 IT IS LEFT TO NO ONE
 IT’S THE WHOLE PICTURE`}</pre>
-      <br/><br/><br/><br/><br/><br/><br/><br/>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
 
       <h3 className={styles.poemTitle}>hickory</h3>
 
-      <br/>
+      <br />
       <pre className={styles.poem}>{`you appear to me through the episode
            in which you are appearing as its shape
            when i am seeing it the tree has two faces
            the last one is contact
            what is that of`}</pre>
-      <br/><br/><br/><br/><br/><br/><br/><br/>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
 
       <h3 className={styles.poemTitle}>the heaven of knitwear</h3>
 
-      <br/>
+      <br />
       <pre className={styles.poem}>{`a perfect, stupid tulip, dewdrop yellow
                        hello my friend i do not confuse you
 
@@ -119,21 +154,37 @@ the sanctuary is there by itself so on hand
                       have to go back in myself and require something in there
           the future of knitwear
           the heaven`}</pre>
-      <br/><br/><br/><br/><br/><br/><br/><br/>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
 
       <h3 className={styles.poemTitle}>art class</h3>
 
-      <br/>
+      <br />
       <pre className={styles.poem}>{`We want to eat teacher
 He gets on the ground
 When we make the child trap
 He looks even more
 Edible on the ground`}</pre>
-      <br/><br/><br/><br/><br/><br/><br/><br/>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
 
-      <h3 className={styles.poemTitle}>survival of the smile of the cheshire cat</h3>
+      <h3 className={styles.poemTitle}>
+        survival of the smile of the cheshire cat
+      </h3>
 
-      <br/>
+      <br />
       <pre className={styles.poem}>{`half whose mind is gearing in a wheel
 
 “I have turned a cake into something else” (Pierre Hermé)
@@ -157,22 +208,35 @@ feel through it like a sour stone
 fell
 
 the facts are constantly about me`}</pre>
-      <br/><br/><br/><br/><br/><br/><br/><br/>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
 
       <h3 className={styles.poemTitle}>the simplest case of participation</h3>
 
-      <br/>
-      <pre className={styles.poem}>{`Karma, the huge dog, takes a shit in the grass
+      <br />
+      <pre
+        className={styles.poem}
+      >{`Karma, the huge dog, takes a shit in the grass
 Karma is in fact like a big sheep
 I had my eye right on her
 Karma come, come on, hey, hey miss, go play
 She jogs in a line toward the light pole`}</pre>
-      <br/><br/><br/><br/><br/><br/><br/><br/>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
 
-
-
-
-      <Footer/>
+      <Footer />
     </>
-  )
+  );
 }
