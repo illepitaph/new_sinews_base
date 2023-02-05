3d106c40a63a55e731bbc7f8b834be9ea01322f6
Steve Barbaro <steve.barbaro@gmail.com>
2023-02-04 22:15:32 -0600

    commences design of issue7 rival graveyards

---
+++ pages/issue7/graveyards.js
@@ -0,0 +1,247 @@
+import Head from "next/head";
+import Header from "../../components/header.js";
+import CenterGrayTitle from "../../components/centerGrayTitle.js";
+import RightGrayTitle from "../../components/rightGrayTitle.js";
+import Footer from "../../components/footer.js";
+import styles from "../../styles/RivalGraveyards.module.css";
+RivalGraveyards.title = "gabriel palacios ~ rival graveyards — new_sinews";
+RivalGraveyards.pieceName = "gabriel palacios ~ rival graveyards — new_sinews";
+RivalGraveyards.description =
+  "_Gabriel Palacios_poetry_RIVAL GRAVEYARDS_issue7_";
+RivalGraveyards.image =
+  "https://www.newnewsinews.com/images/IhnsBackground.jpg";
+
+export default function RivalGraveyards() {
+  return (
+    <>
+      <Head />
+      <Header headerBackground="ihnsBackground" />
+
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+
+      <CenterGrayTitle>
+        <span className={styles.nameShadow}>R</span>
+        <br />
+        <br />
+        <span className={styles.inverseNameShadow}>I</span>
+        <br />
+        <br />
+        <span className={styles.nameShadow}>V</span>
+        <br />
+        <br />
+        <span className={styles.inverseNameShadow}>A</span>
+        <br />
+        <br />
+        <span className={styles.blackAlternateUppercase}>L</span>
+        <br />
+        <br />
+        <br />G<br />R<br />A<br />V<br />E<br />Y<br />A<br />R<br />D<br />S
+      </CenterGrayTitle>
+
+      <br />
+      <br />
+      <br />
+
+      <RightGrayTitle>
+        <span className={styles.inverseNameShadow}>
+          k<span className={styles.mintAlternate}>i </span>
+          <span className={styles.nameShadow}>r</span>-<br />
+          <br />s<span className={styles.mintAlternateUppercase}>t</span>
+          <i>e</i>{" "}
+          <span className={styles.nameShadow}>
+            <i>n</i>
+          </span>
+        </span>
+        <br />
+        <br />
+        <span className={styles.mintAlternate}>i </span>
+        <span className={styles.inverseNameShadow}>h </span>
+        <span className={styles.nameShadow}>
+          <i>n </i>
+        </span>
+        <span className={styles.inverseNameShadow}>s</span>
+      </RightGrayTitle>
+
+      <br />
+      <br />
+      <br />
+
+      <h3 className={styles.poemTitle}>wulv</h3>
+
+      <br />
+      <pre className={styles.poem}>{`EVANGEL	BLISSED ANGEL
+CRY ON SLEEP
+WET PEP
+VAULT OF TRUCKS
+& FERM
+
+SITTER ANGEL	RIVET DIVOT
+MR SNEAKERS
+ROYAL OF NOISES
+THESE PEOPLE ARE SCHOLARS I AM NO ONE
+
+SAD CAT SCREAMING WHO! THE MOOD IS ON ME!
+
+GORGEOUSNESS OF MERE IMAGE DRAWN DOWN DARKNESS
+IT BLEEDS IT FALLS IT RECOVERS
+SHE TELLS ME MY BODY IS A SPIRITUAL LESSON
+INNER IS A DIMENSION HE SAYS LOOKING DOWN
+IT IS LEFT TO NO ONE
+IT’S THE WHOLE PICTURE`}</pre>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+
+      <h3 className={styles.poemTitle}>hickory</h3>
+
+      <br />
+      <pre className={styles.poem}>{`you appear to me through the episode
+           in which you are appearing as its shape
+           when i am seeing it the tree has two faces
+           the last one is contact
+           what is that of`}</pre>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+
+      <h3 className={styles.poemTitle}>the heaven of knitwear</h3>
+
+      <br />
+      <pre className={styles.poem}>{`a perfect, stupid tulip, dewdrop yellow
+                       hello my friend i do not confuse you
+
+           you come shaking the light from somewhere else
+  off
+       it’s a shower
+                                redemptive, misgrained, without hesitation
+                       visitor to the plasma vault, always again, not the thing
+
+           (1)
+
+                                  how do we align the incredible mirror
+                      simply bearing that it is read
+              here is the house, burning
+
+                               all properties of the house burning, the
+                      water flowing out of it, the firemen…
+
+            well, what is the thing that can bear?
+
+            bear things like “red”
+
+
+                                  the form is the house being what it is as a house
+                       what makes it an eye is seeing
+           the nothing of a specific content
+the sanctuary is there by itself so on hand
+                      it was nice it
+
+                      red round solid day
+                      red gram fella thing
+
+          a gel
+
+                                 at all times you decide what belongs together
+                      have to go back in myself and require something in there
+          the future of knitwear
+          the heaven`}</pre>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+
+      <h3 className={styles.poemTitle}>art class</h3>
+
+      <br />
+      <pre className={styles.poem}>{`We want to eat teacher
+He gets on the ground
+When we make the child trap
+He looks even more
+Edible on the ground`}</pre>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+
+      <h3 className={styles.poemTitle}>
+        survival of the smile of the cheshire cat
+      </h3>
+
+      <br />
+      <pre className={styles.poem}>{`half whose mind is gearing in a wheel
+
+“I have turned a cake into something else” (Pierre Hermé)
+
+so a smile was there on her
+
+dry white face
+
+the slow air in an
+
+air roll
+
+slowly something was there obtaining permission
+
+what was the permission?
+
+it was obtained
+
+feel through it like a sour stone
+
+fell
+
+the facts are constantly about me`}</pre>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+
+      <h3 className={styles.poemTitle}>the simplest case of participation</h3>
+
+      <br />
+      <pre
+        className={styles.poem}
+      >{`Karma, the huge dog, takes a shit in the grass
+Karma is in fact like a big sheep
+I had my eye right on her
+Karma come, come on, hey, hey miss, go play
+She jogs in a line toward the light pole`}</pre>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+
+      <Footer />
+    </>
+  );
+}
