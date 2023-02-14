940a97955405f7001e3022a8c358914fbdf49a78
Steve Barbaro <steve.barbaro@gmail.com>
2023-02-14 14:32:59 -0600

    adds text to issue7 personae contribution

--- pages/secret/personae.js
+++ pages/secret/personae.js
@@ -56,10 +56,267 @@ export default function Personae() {
         <span className={styles.ellipses}>...</span>shiota{" "}
       </div>
       <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
       <StandardProse>
-        Commitment?! She just wanted someone who’d get the joke, find it as
-        funny as she did. Jumping up on the bed, throwing pillows around—that’s
-        it! That’s the whole joke! You have to keep living!
+        The reflection in the antique mirror trembles, a manic energy blurring
+        the lines of her face almost imperceptibly on the polished surface.
+        <br />
+        <br />
+        But she can see it.
+        <br />
+        <br />
+        Head tilts downward, eyes pan over the contents of the glass-topped
+        vanity before her: rouge, combs and brushes, scattered lipsticks, and a
+        squat black pot of kohl. What here could protect against the dissolution
+        of her face, what could fix her features in place to stop that blurring
+        trembling erasure she sees beginning in the mirror?
+        <br />
+        <br />
+        Her hand reaches for the kohl and she presses her finger into the solid
+        mass, swirling and swirling until it forms a thick paste warmed by her
+        skin. She considers the slick black that now coats her fingers.
+        <br />
+        <br />
+        The mirror shows the kohl-blackened hand reaching up and a sudden flash
+        of teeth as she begins rubbing, darkening her teeth with broader and
+        broader strokes. Soon she moves beyond just teeth, dipping fingers back
+        into the kohl to smear more across the border of her mouth, tasting waxy
+        nothing and feeling the fine sand-crunch of grit. The face that regards
+        her in the mirror is exaggerated now, mouth elongated and teeth lost to
+        the kohl.
+        <br />
+        <br />
+        The reflection's eyes shift to her hair.
+        <br />
+        <br />
+        The kohl-stained hand now pulls a honeyed-gold teak comb through long
+        black hair. If she presses down, firmly, teeth digging into the pale
+        white of her scalp, she can stop the trembling, she is certain of it.
+        <br />
+        <br />
+        How many times did she need to run the comb through her hair?
+        <br />
+        <br />
+        <i>One hundred strokes</i>, her mother's voice comes to her now,{" "}
+        <i>One hundred strokes each night</i>, <i>my daughter</i>,{" "}
+        <i>and your hair will be so beautiful</i>.
+        <br />
+        <br />
+        The comb flashes briefly in the mirror, hovering above the point of her
+        widow's peak before it bites down again, catching bright flashes of red
+        that cake each tooth for a split second before disappearing further into
+        a black cascade. She long lost count of how many times she has dragged
+        the comb through her hair. And still her hand keeps its mechanical
+        movement up, down, through.
+        <br />
+        <br />
+        <i>Yes</i>, <i>mother</i>,{" "}
+        <i>but how many times before I am beautiful</i>?
+        <br />
+        <br />
+        As her eyes fix on her image in the mirror, she sees the hair clump,
+        thick coils beginning to curl upwards in disarray no matter how hard she
+        presses down. The comb no longer moves fluidly and she has to wrench it
+        out several times. She takes no notice of the droplets of red that
+        spread across the stark white of her nightgown. She continues combing.
+        <br />
+        <br />
+        <i>How many times</i>?
+        <br />
+        <br />
+        The comb suddenly catches, sharp teeth caught on a wet matted knot,
+        slipping out of her hand. Without missing a beat her hand compulsively
+        continues its path, fingers digging in and moving around the comb now
+        embedded in the slick mass that seems to grow with each pass. She
+        reaches up with her other hand, clawing through her hair with
+        increasingly jerky motions. As her hands rake through the full length of
+        her hair, they came away with handfuls of glistening red-black coils.
+        <br />
+        <br />
+        In one sudden move she gathers her hair in both hands, wringing it
+        firmly. A thick rivulet of blood drips from the ends, pooling onto the
+        tiled floor.
+        <br />
+        <br />"<i>And</i>, <i>cut</i>!"
+        <br />
+        <br />
+        Suiko instantly relaxed, holding out her red-black dripping hands to the
+        assistant that dutifully appeared at her side with a towel. Off to the
+        side the director gives her a preliminary thumbs up, the bright stage
+        lights glinting off thick tortoise-shell glasses while the rest of the
+        crew bustled around the set.
+        <br />
+        <br />
+        Amid the activity, Suiko suddenly smelled a thick, acrid tang in the
+        air, like the pressure of the sky bearing down just before a storm
+        breaks.
+        <br />
+        <br />
+        She instinctively glanced around.
+        <br />
+        <br />
+        From behind the production crew and the large camera rig situated
+        several feet from her mark she saw a single glassy eye glinting within a
+        sheet of long, scraggly hair.
+        <br />
+        <br />
+        <br />
+        <br />
+        <p className={styles.sectionBreak}>#</p>
+        <br />
+        <br />
+        <br />
+        <br />
+        Suiko had been skeptical about taking the role when her agent approached
+        her about a reimagining of the Yotsuya kaidan some boutique American
+        studio was putting together.
+        <br />
+        <br />
+        "The casting director told me they're making it a point to cast Japanese
+        speaking actors for all the roles and it seems like they've managed to
+        fill out most of the cast. All they need is someone to play Oiwa." He
+        looked meaningfully here at Suiko, "There's been a few influential
+        movies with older women protagonists in the U.S., so they're attempting
+        to age Oiwa up for this and they think you would be perfect."
+        <br />
+        <br />
+        Back in her trailer, Suiko sat at her own vanity and considered her face
+        in the mirror, now devoid of the thick paste of makeup, stage blood, and
+        smeared kohl from the scene.
+        <br />
+        <br />
+        It had been several decades since Suiko had decided to try her hand at
+        aging mortally. She had known that it would be risky for a being like
+        her to be in the public eye for so long, even if she kept her true
+        nature hidden. Yet, Suiko found the prospect of such a dedicated
+        performance utterly irresistible—a tanuki living as a naturally aging
+        human pretending to be other humans on film and stage.
+        <br />
+        <br />
+        <i>Method acting taken to an entirely new level</i>, she had thought
+        grandly.
+        <br />
+        <br />
+        Her current face had settled with the graceful weight of six human
+        decades, the slight pouching here and there, the softness of jowls and
+        cheeks, the series of horizontal creases that folded when she lifted her
+        eyebrows, all meticulously and lovingly crafted from years of
+        painstaking practice. The deep, cheerful crinkling of skin around her
+        eyes radiating out towards her temples when she smiled was her favorite
+        touch.
+        <br />
+        <br />
+        As she was pulling up her hair into a bun, the scent of murky brackish
+        water flooded the trailer and she repressed a groan. <i>Not again</i>.
+        <br />
+        <br />
+        A hunched figure materialized in one corner of the trailer, a ragged
+        kimono hung on her frail frame and pooled at her feet. A sheet of long
+        black hair hanging limply over part of her face, which tilted slightly
+        from a neck that hung at an unnatural angle. Suiko turned to face the
+        apparition.
+        <br />
+        <br />
+        "Well?" She asked, trying to keep her tone even.
+        <br />
+        <br />
+        The figure adjusted itself to cast its gaze toward Suiko and let out a
+        bubbling, choked laugh.
+        <br />
+        <br />
+        "I have seen better." It said, its voice reverberating within the small
+        confines of Suiko's trailer, the sound bouncing off the walls like a
+        stone clattering to the bottom of a well.
+        <br />
+        <br />
+        "Can you lower your voice please, Oiwa." Suiko said with a wince,
+        reflexively reaching up to her ears. She forgot how grating it was to
+        speak with the dead.
+        <br />
+        <br />
+        The ghost let out a gurgling sigh, but pitched her voice lower.
+        <br />
+        <br />
+        "Your performance today was adequate." Oiwa said, "Certainly there has
+        been improvement over the last several weeks. Though why the
+        playwright—"
+        <br />
+        <br />
+        "—screenwriter—"
+        <br />
+        <br />
+        "Why the <i>writer</i> insists on forcing the angle of madness is beyond
+        me. I find it quite tedious that my supposed madness always remains the
+        central focus for most of these stories. Even now, when I am to be
+        portrayed as an older woman, the madness remains when the events that
+        have ostensibly caused that madness would have occurred nearly forty
+        years before. Is the presumption that I have not found my own peace even
+        into old age?"
+        <br />
+        <br />
+        Suiko was used to Oiwa's pontifications by this point and knew it was
+        best to simply let things run its course. Suiko herself had been alive
+        long before the murders that occurred in Yotsuya Ward, watching the
+        legend morph first hand from the Yotsuya zotan, a book presented as
+        something supposedly written by an official from within the ward, to the
+        plays and rakugo performances that had made Oiwa's story so famous. It
+        was a far more robust canon than most of the stories she had come across
+        for those of her own kind. A bulk of those stories depicted tanuki as
+        almost exclusively male with huge scrotums, for heaven's sake, and Oiwa
+        had grievances against complex character depictions in kabuki plays and
+        movies? How so many of the ghosts flitting around felt so entitled to
+        their stories long after they had passed was beyond her.
+        <br />
+        <br />
+        "I don't understand why you fuss about all this," Suiko finally cut in,
+        "You're one of the most well-known ghosts and you've inspired so many
+        other stories. There are even shrines erected in your name throughout
+        Tokyo! Surely that must count for something?"
+        <br />
+        <br />
+        The ghost hung silently in the clammy air for a moment, thin tendrils of
+        mist coalescing faintly around her.
+        <br />
+        <br />
+        "These stories do not feel quite right," Oiwa said, her voice now the
+        gentler trickle of a creek, "I believe it is because of the humans
+        themselves who were trying to portray me—how could they possibly get it
+        right? I had hope that a tanuki actress would be able to do something
+        different."
+        <br />
+        <br />
+        "And I am, aren't I? You have to admit the bit with the kohl on the face
+        and the fight against the dissolution of being was pretty good."
+        <br />
+        <br />
+        The ghost offered a spasming grimace that Suiko decided to interpret as
+        a smile.
+        <br />
+        <br />
+        "Yes, you do manage to capture something of what I imagine, even if it
+        appears to be accidental at times."
+        <br />
+        <br />
+        "Well, maybe we could achieve your artistic vision more quickly if you
+        haunted the director, or any member of the crew, rather than me." Suiko
+        said with some irritation.
+        <br />
+        <br />
+        Oiwa's laugh bubbled up one last time as she dissolved fully back into
+        the air.
+        <br />
+        <br />
+        <br />
+        <br />
+        <p className={styles.sectionBreak}>#</p>
+        <br />
+        <br />
+        <br />
+        <br />
       </StandardProse>
 
       <Footer />
