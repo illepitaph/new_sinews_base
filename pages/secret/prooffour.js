f75e36304c1e9be7a2910e6bc7778c913a8ef456
Steve Barbaro <steve.barbaro@gmail.com>
2023-03-05 20:43:08 -0600

    instantiates issue7 prooffour page + concomitant image

---
+++ pages/secret/prooffour.js
@@ -0,0 +1,46 @@
+import Head from "next/head";
+import Image from "next/image";
+import Header from "../../components/header.js";
+import Footer from "../../components/footer.js";
+import StandardProse from "../../components/standardProse.js";
+import styles from "../../styles/Proof.module.css";
+import ProofDeduction from "../../public/images/ProofDeduction.jpg";
+ProofFour.title = "kelly_krumrie ~ proof — new_sinews";
+ProofFour.pieceName = "kelly_krumrie ~ proof — new_sinews";
+ProofFour.description = "_Kelly Krumrie_poetry_PROOF_new_sinews_issue7_";
+ProofFour.image = "https://www.newnewsinews.com/images/ProofBackground.jpg";
+
+export default function ProofFour() {
+  return (
+    <>
+      <Head />
+
+      <Header headerBackground="proofBackground" />
+      <div className={styles.containerContainer}>
+        <div className={styles.containerContainerContainer}>
+          <div className={styles.container}>
+            <StandardProse>
+              <Image
+                src={ProofDeduction}
+                width={70}
+                height={40}
+                layout={`responsive`}
+                alt="Still from THE SHAPING OF DEDUCTION IN GREEK MATHEMATICS"
+              />
+              <br />
+              <br />
+              <br />
+              Netz, Reviel.{" "}
+              <span className={styles.proofText}>
+                The Shaping of Deduction in Greek Mathematics
+              </span>
+              . Cambridge UP, 2003.
+            </StandardProse>
+          </div>
+        </div>
+      </div>
+
+      <Footer />
+    </>
+  );
+}
