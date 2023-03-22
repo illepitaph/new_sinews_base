ce28547a4db318c6ac1c1106bb50f2eceac5afd1
Steve Barbaro <steve.barbaro@gmail.com>
2023-03-21 19:14:22 -0500

    refines issue7 automation font

--- pages/secret/needle.js
+++ pages/secret/needle.js
@@ -63,28 +63,30 @@ export default function Needle() {
         </span>
       </LeftGrayTitle>
       <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
       <StandardProse>
-        hurry, tense to steel before the pen touches, tense, tense. constant
-        nude, hurry, there’s anything and anyone there, in the wardrobe of your
-        eyes. my camera is saying go out with dignity. yes there’s armor no
-        won’t sink you don’t be petty about it. you gonna, you gonna?
+        <span className={styles.shadowOne}>
+          hurry, tense to steel before the pen touches, tense, tense. constant
+          nude, hurry, there’s anything and anyone there, in the wardrobe of
+          your eyes. my camera is saying go out with dignity. yes there’s armor
+          no won’t sink you don’t be petty about it. you gonna, you gonna?
+        </span>
         <br />
         <br />
         <br />
@@ -112,17 +114,19 @@ export default function Needle() {
         <br />
         <br />
         <br />
-        no no you got it wrong we have to panic before we absolve anyone or sit
-        and breathe with the broken candy of it all. you have to run for your
-        life as if it wasn’t already over, don’t you think it absurd when you’re
-        on miles and miles of cloud and he’s behind you, the towering angel with
-        cotton in his ears, in blindfolds, how he takes only one step to cover
-        your hundred, do you think he’ll ever need to catch his breath. if death
-        bleeds you as you’re dreaming like a needle stalked through the mind,
-        and you feel another blade slipping down to touch the teeth of your
-        spine, sweep and jig and run along, let it unzip everything, cut in
-        half, the hemispheres hardly prised but giving themselves over, blurting
-        out their contents:
+        <span className={styles.shadowOne}>
+          no no you got it wrong we have to panic before we absolve anyone or
+          sit and breathe with the broken candy of it all. you have to run for
+          your life as if it wasn’t already over, don’t you think it absurd when
+          you’re on miles and miles of cloud and he’s behind you, the towering
+          angel with cotton in his ears, in blindfolds, how he takes only one
+          step to cover your hundred, do you think he’ll ever need to catch his
+          breath. if death bleeds you as you’re dreaming like a needle stalked
+          through the mind, and you feel another blade slipping down to touch
+          the teeth of your spine, sweep and jig and run along, let it unzip
+          everything, cut in half, the hemispheres hardly prised but giving
+          themselves over, blurting out their contents:
+        </span>
         <br />
         <br />
         <br />
