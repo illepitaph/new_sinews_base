c08293bf6057819f247bee06a0d78adbf8fab650
Steve Barbaro <steve.barbaro@gmail.com>
2023-04-26 13:49:50 -0500

    updates issue7 rewrites

--- next.config.js
+++ next.config.js
@@ -1,445 +1,440 @@
 module.exports = {
-
   async rewrites() {
     return [
-
       {
-        source: '/issue1',
-        destination: '/issue1/index',
+        source: "/issue1",
+        destination: "/issue1/index",
       },
 
       {
-        source: '/gridley',
-        destination: '/issue1/gridley',
+        source: "/gridley",
+        destination: "/issue1/gridley",
       },
 
       {
-        source: '/blackwell',
-        destination: '/issue1/blackwell',
+        source: "/blackwell",
+        destination: "/issue1/blackwell",
       },
 
       {
-        source: '/romanosky',
-        destination: '/issue1/romanosky',
+        source: "/romanosky",
+        destination: "/issue1/romanosky",
       },
 
       {
-        source: '/eisen-martin',
-        destination: '/issue1/eisen-martin',
+        source: "/eisen-martin",
+        destination: "/issue1/eisen-martin",
       },
 
       {
-        source: '/nao',
-        destination: '/issue1/nao',
+        source: "/nao",
+        destination: "/issue1/nao",
       },
 
       {
-        source: '/evenflesh',
-        destination: '/issue1/evenflesh',
+        source: "/evenflesh",
+        destination: "/issue1/evenflesh",
       },
 
       {
-        source: '/issue1bios',
-        destination: '/issue1/bios',
+        source: "/issue1bios",
+        destination: "/issue1/bios",
       },
 
       {
-        source: '/issue2',
-        destination: '/issue2/index',
+        source: "/issue2",
+        destination: "/issue2/index",
       },
 
       {
-        source: '/briante',
-        destination: '/issue2/briante',
+        source: "/briante",
+        destination: "/issue2/briante",
       },
 
       {
-        source: '/corrao',
-        destination: '/issue2/corrao',
+        source: "/corrao",
+        destination: "/issue2/corrao",
       },
 
       {
-        source: '/zamora',
-        destination: '/issue2/zamora',
+        source: "/zamora",
+        destination: "/issue2/zamora",
       },
 
       {
-        source: '/taylor',
-        destination: '/issue2/taylor',
+        source: "/taylor",
+        destination: "/issue2/taylor",
       },
 
       {
-        source: '/porte',
-        destination: '/issue2/porte',
+        source: "/porte",
+        destination: "/issue2/porte",
       },
 
       {
-        source: '/lewis',
-        destination: '/issue2/lewis',
+        source: "/lewis",
+        destination: "/issue2/lewis",
       },
 
       {
-        source: '/lakghomi',
-        destination: '/issue2/lakghomi',
+        source: "/lakghomi",
+        destination: "/issue2/lakghomi",
       },
 
       {
-        source: '/leyeaste',
-        destination: '/issue2/leyeaste',
+        source: "/leyeaste",
+        destination: "/issue2/leyeaste",
       },
 
       {
-        source: '/strauss',
-        destination: '/issue2/strauss',
+        source: "/strauss",
+        destination: "/issue2/strauss",
       },
 
       {
-        source: '/salutingahex',
-        destination: '/issue2/salutingahex',
+        source: "/salutingahex",
+        destination: "/issue2/salutingahex",
       },
 
       {
-        source: '/shimoda',
-        destination: '/issue2/shimoda',
+        source: "/shimoda",
+        destination: "/issue2/shimoda",
       },
 
       {
-        source: '/shimoda1',
-        destination: '/issue2/shimoda1',
+        source: "/shimoda1",
+        destination: "/issue2/shimoda1",
       },
 
       {
-        source: '/shimoda2',
-        destination: '/issue2/shimoda2',
+        source: "/shimoda2",
+        destination: "/issue2/shimoda2",
       },
 
       {
-        source: '/shimoda3',
-        destination: '/issue2/shimoda3',
+        source: "/shimoda3",
+        destination: "/issue2/shimoda3",
       },
 
       {
-        source: '/shimoda4',
-        destination: '/issue2/shimoda4',
+        source: "/shimoda4",
+        destination: "/issue2/shimoda4",
       },
 
       {
-        source: '/issue2bios',
-        destination: '/issue2/bios',
+        source: "/issue2bios",
+        destination: "/issue2/bios",
       },
 
       {
-        source: '/issue3',
-        destination: '/issue3/index',
+        source: "/issue3",
+        destination: "/issue3/index",
       },
 
       {
-        source: '/dusk',
-        destination: '/issue3/dusk',
+        source: "/dusk",
+        destination: "/issue3/dusk",
       },
 
       {
-        source: '/stough',
-        destination: '/issue3/stough',
+        source: "/stough",
+        destination: "/issue3/stough",
       },
 
       {
-        source: '/akant',
-        destination: '/issue3/akant',
+        source: "/akant",
+        destination: "/issue3/akant",
       },
 
       {
-        source: '/alvarez',
-        destination: '/issue3/alvarez',
+        source: "/alvarez",
+        destination: "/issue3/alvarez",
       },
 
       {
-        source: '/gette',
-        destination: '/issue3/gette',
+        source: "/gette",
+        destination: "/issue3/gette",
       },
 
       {
-        source: '/krumrie',
-        destination: '/issue3/krumrie',
+        source: "/krumrie",
+        destination: "/issue3/krumrie",
       },
 
       {
-        source: '/dasgupta',
-        destination: '/issue3/dasgupta',
+        source: "/dasgupta",
+        destination: "/issue3/dasgupta",
       },
 
       {
-        source: '/corraoovidian',
-        destination: '/issue3/corraoovidian',
+        source: "/corraoovidian",
+        destination: "/issue3/corraoovidian",
       },
 
       {
-        source: '/issue3bios',
-        destination: '/issue3/bios',
+        source: "/issue3bios",
+        destination: "/issue3/bios",
       },
 
       {
-        source: '/issue4',
-        destination: '/issue4/index',
+        source: "/issue4",
+        destination: "/issue4/index",
       },
 
       {
-        source: '/hour',
-        destination: '/issue4/hour',
+        source: "/hour",
+        destination: "/issue4/hour",
       },
 
       {
-        source: '/guanyin',
-        destination: '/issue4/guanyin',
+        source: "/guanyin",
+        destination: "/issue4/guanyin",
       },
 
       {
-        source: '/isoline',
-        destination: '/issue4/isoline',
+        source: "/isoline",
+        destination: "/issue4/isoline",
       },
 
       {
-        source: '/howfar',
-        destination: '/issue4/howfar',
+        source: "/howfar",
+        destination: "/issue4/howfar",
       },
 
       {
-        source: '/palacios',
-        destination: '/issue4/palacios',
+        source: "/palacios",
+        destination: "/issue4/palacios",
       },
 
       {
-        source: '/hummel',
-        destination: '/issue4/hummel',
+        source: "/hummel",
+        destination: "/issue4/hummel",
       },
 
       {
-        source: '/textspinality',
-        destination: '/issue4/textspinality',
+        source: "/textspinality",
+        destination: "/issue4/textspinality",
       },
 
       {
-        source: '/poemsborn',
-        destination: '/issue4/poemsborn',
+        source: "/poemsborn",
+        destination: "/issue4/poemsborn",
       },
 
       {
-        source: '/maierhofer',
-        destination: '/issue4/maierhofer',
+        source: "/maierhofer",
+        destination: "/issue4/maierhofer",
       },
 
       {
-        source: '/issue4bios',
-        destination: '/issue4/bios',
+        source: "/issue4bios",
+        destination: "/issue4/bios",
       },
 
       {
-        source: '/issue5',
-        destination: '/issue5/index',
+        source: "/issue5",
+        destination: "/issue5/index",
       },
 
       {
-        source: '/shiota',
-        destination: '/issue5/shiota',
+        source: "/shiota",
+        destination: "/issue5/shiota",
       },
 
       {
-        source: '/theater',
-        destination: '/issue5/theater',
+        source: "/theater",
+        destination: "/issue5/theater",
       },
 
       {
-        source: '/terrarium',
-        destination: '/issue5/terrarium',
+        source: "/terrarium",
+        destination: "/issue5/terrarium",
       },
 
       {
-        source: '/moss',
-        destination: '/issue5/moss',
+        source: "/moss",
+        destination: "/issue5/moss",
       },
 
       {
-        source: '/yrclimate',
-        destination: '/issue5/yrclimate',
+        source: "/yrclimate",
+        destination: "/issue5/yrclimate",
       },
 
       {
-        source: '/feldman',
-        destination: '/issue5/feldman',
+        source: "/feldman",
+        destination: "/issue5/feldman",
       },
 
       {
-        source: '/stripmall',
-        destination: '/issue5/stripmall',
+        source: "/stripmall",
+        destination: "/issue5/stripmall",
       },
 
       {
-        source: '/rainey',
-        destination: '/issue5/rainey',
+        source: "/rainey",
+        destination: "/issue5/rainey",
       },
 
       {
-        source: '/bisagni',
-        destination: '/issue5/bisagni',
+        source: "/bisagni",
+        destination: "/issue5/bisagni",
       },
 
       {
-        source: '/issue5bios',
-        destination: '/issue5/bios',
+        source: "/issue5bios",
+        destination: "/issue5/bios",
       },
 
       {
-        source: '/issue6',
-        destination: '/issue6/index',
+        source: "/issue6",
+        destination: "/issue6/index",
       },
 
       {
-        source: '/ihns',
-        destination: '/issue6/ihns',
+        source: "/ihns",
+        destination: "/issue6/ihns",
       },
 
       {
-        source: '/castingoff',
-        destination: '/issue6/castingoff',
+        source: "/castingoff",
+        destination: "/issue6/castingoff",
       },
 
       {
-        source: '/mfnz',
-        destination: '/issue6/mfnz',
+        source: "/mfnz",
+        destination: "/issue6/mfnz",
       },
 
       {
-        source: '/harrier',
-        destination: '/issue6/harrier',
+        source: "/harrier",
+        destination: "/issue6/harrier",
       },
 
       {
-        source: '/griffin',
-        destination: '/issue6/griffin',
+        source: "/griffin",
+        destination: "/issue6/griffin",
       },
 
       {
-        source: '/beausang',
-        destination: '/issue6/beausang',
+        source: "/beausang",
+        destination: "/issue6/beausang",
       },
 
       {
-        source: '/numbers',
-        destination: '/issue6/numbers',
+        source: "/numbers",
+        destination: "/issue6/numbers",
       },
 
       {
-        source: '/ferdinand',
-        destination: '/issue6/ferdinand',
+        source: "/ferdinand",
+        destination: "/issue6/ferdinand",
       },
 
       {
-        source: '/target',
-        destination: '/issue6/target',
+        source: "/target",
+        destination: "/issue6/target",
       },
 
       {
-        source: '/desire',
-        destination: '/issue6/desire',
+        source: "/desire",
+        destination: "/issue6/desire",
       },
 
       {
-        source: '/issue6bios',
-        destination: '/issue6/bios',
+        source: "/issue6bios",
+        destination: "/issue6/bios",
       },
 
       {
-        source: '/issue7',
-        destination: '/issue7/index',
+        source: "/issue7",
+        destination: "/issue7/index",
       },
 
       {
-        source: '/care',
-        destination: '/issue7/care',
+        source: "/care",
+        destination: "/issue7/care",
       },
 
       {
-        source: '/graveyards',
-        destination: '/issue7/graveyards',
+        source: "/graveyards",
+        destination: "/issue7/graveyards",
       },
 
       {
-        source: '/proof',
-        destination: '/issue7/proof',
+        source: "/proof",
+        destination: "/issue7/proof",
       },
 
       {
-        source: '/prooftwo',
-        destination: '/issue7/prooftwo',
+        source: "/prooftwo",
+        destination: "/issue7/prooftwo",
       },
 
       {
-        source: '/proofthree',
-        destination: '/issue7/proofthree',
+        source: "/proofthree",
+        destination: "/issue7/proofthree",
       },
 
       {
-        source: '/prooffour',
-        destination: '/issue7/prooffour',
+        source: "/prooffour",
+        destination: "/issue7/prooffour",
       },
 
       {
-        source: '/prooffive',
-        destination: '/issue7/prooffive',
+        source: "/prooffive",
+        destination: "/issue7/prooffive",
       },
 
       {
-        source: '/proofsix',
-        destination: '/issue7/proofsix',
+        source: "/proofsix",
+        destination: "/issue7/proofsix",
       },
 
       {
-        source: '/proofseven',
-        destination: '/issue7/proofseven',
+        source: "/proofseven",
+        destination: "/issue7/proofseven",
       },
 
       {
-        source: '/proofeight',
-        destination: '/issue7/proofeight',
+        source: "/proofeight",
+        destination: "/issue7/proofeight",
       },
 
       {
-        source: '/automation',
-        destination: '/issue7/automation',
+        source: "/automation",
+        destination: "/issue7/automation",
       },
 
       {
-        source: '/needle',
-        destination: '/issue7/needle',
+        source: "/needle",
+        destination: "/issue7/needle",
       },
 
       {
-        source: '/kaput',
-        destination: '/issue7/kaput',
+        source: "/kaput",
+        destination: "/issue7/kaput",
       },
 
       {
-        source: '/personae',
-        destination: '/issue7/personae',
+        source: "/personae",
+        destination: "/issue7/personae",
       },
 
       {
-        source: '/moore',
-        destination: '/issue7/moore',
+        source: "/moore",
+        destination: "/issue7/moore",
       },
-
 
       {
-        source: '/issue7bios',
-        destination: '/issue7/bios',
+        source: "/issue7bios",
+        destination: "/issue7/bios",
       },
 
       {
-        source: '/home',
-        destination: '/',
+        source: "/home",
+        destination: "/",
       },
-
-
-    ]
+    ];
   },
-}
+};
