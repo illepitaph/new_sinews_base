f392ce089b8d5dd1fc89b32f2d30065f65101afc
Steve Barbaro <steve.barbaro@gmail.com>
2023-08-20 19:32:50 -0500

    updates next config file redirects

--- next.config.js
+++ next.config.js
@@ -1,528 +1,440 @@
 module.exports = {
-  reactStrictMode: true,
-  redirects: async () => {
+  async rewrites() {
     return [
       {
         source: "/issue1",
         destination: "/issue1/index",
-        permanent: true,
       },
 
       {
         source: "/gridley",
         destination: "/issue1/gridley",
-        permanent: true,
       },
 
       {
         source: "/blackwell",
         destination: "/issue1/blackwell",
-        permanent: true,
       },
 
       {
         source: "/romanosky",
         destination: "/issue1/romanosky",
-        permanent: true,
       },
 
       {
         source: "/eisen-martin",
         destination: "/issue1/eisen-martin",
-        permanent: true,
       },
 
       {
         source: "/nao",
         destination: "/issue1/nao",
-        permanent: true,
       },
 
       {
         source: "/evenflesh",
         destination: "/issue1/evenflesh",
-        permanent: true,
       },
 
       {
         source: "/issue1bios",
         destination: "/issue1/bios",
-        permanent: true,
       },
 
       {
         source: "/issue2",
         destination: "/issue2/index",
-        permanent: true,
       },
 
       {
         source: "/briante",
         destination: "/issue2/briante",
-        permanent: true,
       },
 
       {
         source: "/corrao",
         destination: "/issue2/corrao",
-        permanent: true,
       },
 
       {
         source: "/zamora",
         destination: "/issue2/zamora",
-        permanent: true,
       },
 
       {
         source: "/taylor",
         destination: "/issue2/taylor",
-        permanent: true,
       },
 
       {
         source: "/porte",
         destination: "/issue2/porte",
-        permanent: true,
       },
 
       {
         source: "/lewis",
         destination: "/issue2/lewis",
-        permanent: true,
       },
 
       {
         source: "/lakghomi",
         destination: "/issue2/lakghomi",
-        permanent: true,
       },
 
       {
         source: "/leyeaste",
         destination: "/issue2/leyeaste",
-        permanent: true,
       },
 
       {
         source: "/strauss",
         destination: "/issue2/strauss",
-        permanent: true,
       },
 
       {
         source: "/salutingahex",
         destination: "/issue2/salutingahex",
-        permanent: true,
       },
 
       {
         source: "/shimoda",
         destination: "/issue2/shimoda",
-        permanent: true,
       },
 
       {
         source: "/shimoda1",
         destination: "/issue2/shimoda1",
-        permanent: true,
       },
 
       {
         source: "/shimoda2",
         destination: "/issue2/shimoda2",
-        permanent: true,
       },
 
       {
         source: "/shimoda3",
         destination: "/issue2/shimoda3",
-        permanent: true,
       },
 
       {
         source: "/shimoda4",
         destination: "/issue2/shimoda4",
-        permanent: true,
       },
 
       {
         source: "/issue2bios",
         destination: "/issue2/bios",
-        permanent: true,
       },
 
       {
         source: "/issue3",
         destination: "/issue3/index",
-        permanent: true,
       },
 
       {
         source: "/dusk",
         destination: "/issue3/dusk",
-        permanent: true,
       },
 
       {
         source: "/stough",
         destination: "/issue3/stough",
-        permanent: true,
       },
 
       {
         source: "/akant",
         destination: "/issue3/akant",
-        permanent: true,
       },
 
       {
         source: "/alvarez",
         destination: "/issue3/alvarez",
-        permanent: true,
       },
 
       {
         source: "/gette",
         destination: "/issue3/gette",
-        permanent: true,
       },
 
       {
         source: "/krumrie",
         destination: "/issue3/krumrie",
-        permanent: true,
       },
 
       {
         source: "/dasgupta",
         destination: "/issue3/dasgupta",
-        permanent: true,
       },
 
       {
         source: "/corraoovidian",
         destination: "/issue3/corraoovidian",
-        permanent: true,
       },
 
       {
         source: "/issue3bios",
         destination: "/issue3/bios",
-        permanent: true,
       },
 
       {
         source: "/issue4",
         destination: "/issue4/index",
-        permanent: true,
       },
 
       {
         source: "/hour",
         destination: "/issue4/hour",
-        permanent: true,
       },
 
       {
         source: "/guanyin",
         destination: "/issue4/guanyin",
-        permanent: true,
       },
 
       {
         source: "/isoline",
         destination: "/issue4/isoline",
-        permanent: true,
       },
 
       {
         source: "/howfar",
         destination: "/issue4/howfar",
-        permanent: true,
       },
 
       {
         source: "/palacios",
         destination: "/issue4/palacios",
-        permanent: true,
       },
 
       {
         source: "/hummel",
         destination: "/issue4/hummel",
-        permanent: true,
       },
 
       {
         source: "/textspinality",
         destination: "/issue4/textspinality",
-        permanent: true,
       },
 
       {
         source: "/poemsborn",
         destination: "/issue4/poemsborn",
-        permanent: true,
       },
 
       {
         source: "/maierhofer",
         destination: "/issue4/maierhofer",
-        permanent: true,
       },
 
       {
         source: "/issue4bios",
         destination: "/issue4/bios",
-        permanent: true,
       },
 
       {
         source: "/issue5",
         destination: "/issue5/index",
-        permanent: true,
       },
 
       {
         source: "/shiota",
         destination: "/issue5/shiota",
-        permanent: true,
       },
 
       {
         source: "/theater",
         destination: "/issue5/theater",
-        permanent: true,
       },
 
       {
         source: "/terrarium",
         destination: "/issue5/terrarium",
-        permanent: true,
       },
 
       {
         source: "/moss",
         destination: "/issue5/moss",
-        permanent: true,
       },
 
       {
         source: "/yrclimate",
         destination: "/issue5/yrclimate",
-        permanent: true,
       },
 
       {
         source: "/feldman",
         destination: "/issue5/feldman",
-        permanent: true,
       },
 
       {
         source: "/stripmall",
         destination: "/issue5/stripmall",
-        permanent: true,
       },
 
       {
         source: "/rainey",
         destination: "/issue5/rainey",
-        permanent: true,
       },
 
       {
         source: "/bisagni",
         destination: "/issue5/bisagni",
-        permanent: true,
       },
 
       {
         source: "/issue5bios",
         destination: "/issue5/bios",
-        permanent: true,
       },
 
       {
         source: "/issue6",
         destination: "/issue6/index",
-        permanent: true,
       },
 
       {
         source: "/ihns",
         destination: "/issue6/ihns",
-        permanent: true,
       },
 
       {
         source: "/castingoff",
         destination: "/issue6/castingoff",
-        permanent: true,
       },
 
       {
         source: "/mfnz",
         destination: "/issue6/mfnz",
-        permanent: true,
       },
 
       {
         source: "/harrier",
         destination: "/issue6/harrier",
-        permanent: true,
       },
 
       {
         source: "/griffin",
         destination: "/issue6/griffin",
-        permanent: true,
       },
 
       {
         source: "/beausang",
         destination: "/issue6/beausang",
-        permanent: true,
       },
 
       {
         source: "/numbers",
         destination: "/issue6/numbers",
-        permanent: true,
       },
 
       {
         source: "/ferdinand",
         destination: "/issue6/ferdinand",
-        permanent: true,
       },
 
       {
         source: "/target",
         destination: "/issue6/target",
-        permanent: true,
       },
 
       {
         source: "/desire",
         destination: "/issue6/desire",
-        permanent: true,
       },
 
       {
         source: "/issue6bios",
         destination: "/issue6/bios",
-        permanent: true,
       },
 
       {
         source: "/issue7",
         destination: "/issue7/index",
-        permanent: true,
       },
 
       {
         source: "/care",
         destination: "/issue7/care",
-        permanent: true,
       },
 
       {
         source: "/graveyards",
         destination: "/issue7/graveyards",
-        permanent: true,
       },
 
       {
         source: "/proof",
         destination: "/issue7/proof",
-        permanent: true,
       },
 
       {
         source: "/prooftwo",
         destination: "/issue7/prooftwo",
-        permanent: true,
       },
 
       {
         source: "/proofthree",
         destination: "/issue7/proofthree",
-        permanent: true,
       },
 
       {
         source: "/prooffour",
         destination: "/issue7/prooffour",
-        permanent: true,
       },
 
       {
         source: "/prooffive",
         destination: "/issue7/prooffive",
-        permanent: true,
       },
 
       {
         source: "/proofsix",
         destination: "/issue7/proofsix",
-        permanent: true,
       },
 
       {
         source: "/proofseven",
         destination: "/issue7/proofseven",
-        permanent: true,
       },
 
       {
         source: "/proofeight",
         destination: "/issue7/proofeight",
-        permanent: true,
       },
 
       {
         source: "/automation",
         destination: "/issue7/automation",
-        permanent: true,
       },
 
       {
         source: "/needle",
         destination: "/issue7/needle",
-        permanent: true,
       },
 
       {
         source: "/kaput",
         destination: "/issue7/kaput",
-        permanent: true,
       },
 
       {
         source: "/personae",
         destination: "/issue7/personae",
-        permanent: true,
       },
 
       {
         source: "/moore",
         destination: "/issue7/moore",
-        permanent: true,
       },
 
       {
         source: "/issue7bios",
         destination: "/issue7/bios",
-        permanent: true,
       },
 
       {
         source: "/home",
         destination: "/",
-        permanent: true,
       },
-    ]
+    ];
   },
 };
