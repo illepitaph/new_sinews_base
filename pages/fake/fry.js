20a6650470e0ea97031d63c724c6cad0aece0622
Steve Barbaro <steve.barbaro@gmail.com>
2024-01-23 11:50:10 -0600

    instantiates issue8 fry contribution

---
+++ pages/fake/fry.js
@@ -0,0 +1,475 @@
+import Head from "next/head";
+import Header from "../../components/header.js";
+import RightBlackTitle from "../../components/rightBlackTitle.js";
+import LeftBlackTitle from "../../components/leftBlackTitle.js";
+import Footer from "../../components/footer.js";
+import styles from "../../styles/Fry.module.css";
+Fry.title =
+  "logan fry ~ the once-hot-starred skies ++ the once-winkling irises ++ — new_sinews";
+Fry.pieceName =
+  "logan fry ~ the once-hot-starred skies ++ the once-winkling irises ++ — new_sinews";
+Fry.description = "_Logan Fry_poetry_6 POEMS_issue8_";
+Fry.image = "https://www.newnewsinews.com/images/MooreBackground.jpg";
+
+export default function Fry() {
+  return (
+    <>
+      <Head />
+      <Header headerBackground="mooreBackground" />
+
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <RightBlackTitle>
+        <span className={styles.titleShadow}>
+          fo<span className={styles.uppercase}>n</span>
+          <span className={styles.uppercase}>d </span>
+          <br />w<span className={styles.uppercase}>i</span>t
+          <span className={styles.uppercase}>h</span>
+          <br />p <span className={styles.uppercase}>r </span>ope
+          <span className={styles.uppercase}>
+            {" "}
+            <i>r</i>
+          </span>{" "}
+          t y
+          <br />
+          <br />
+          <br />
+        </span>
+      </RightBlackTitle>
+      <LeftBlackTitle>
+        <span className={styles.titleShadow}>
+          nu<span className={styles.uppercase}>t</span>s
+          <span className={styles.uppercase}>h</span>e
+          <span className={styles.uppercase}>
+            <i>ll</i>
+          </span>{" "}
+          ++ b<span className={styles.uppercase}>ea</span>t
+          <span className={styles.uppercase}>i</span>tude // <br />
+          <br /> n<span className={styles.uppercase}>e</span>t of <br />
+          <i>thus</i>
+        </span>
+      </LeftBlackTitle>
+      <RightBlackTitle>
+        <span className={styles.titleShadow}>
+          v
+          <span className={styles.uppercase}>
+            ari <i>e</i> t
+          </span>
+          y wit
+          <span className={styles.uppercase}>h </span>
+          <br />
+          out
+          <br />
+          qua<span className={styles.uppercase}>L</span>i
+          <span className={styles.uppercase}>t</span> <i>i</i> e s
+          <br />
+          <br />
+          <br />
+        </span>
+      </RightBlackTitle>
+      <LeftBlackTitle>
+        <span className={styles.titleShadow}>
+          u<span className={styles.uppercase}> tt </span>
+          <i>e</i>
+          <span className={styles.uppercase}> r</span>
+          <br />
+        </span>
+      </LeftBlackTitle>
+
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+
+      <RightBlackTitle>
+      <span className={styles.nameHeight}>
+        <span className={styles.nameShadowUppercase}> l </span><br/>o<br/>
+        <span className={styles.inverseNameShadow}> g</span><br/>{" "}
+        <span className={styles.nameShadow}>_ </span>
+        <span className={styles.specialCursive}>a</span><br/>
+        <span className={styles.rightMargin}>
+        <span className={styles.inverseNameShadow}> n </span></span>
+        </span>
+      </RightBlackTitle>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <LeftBlackTitle>
+        <span className={styles.nameShadowUppercase}>f</span>
+        <br />
+        <br />
+        <span className={styles.leftMargin}>
+          <span className={styles.inverseNameShadow}>r</span>
+        </span>
+        <br />
+        <br />
+
+        <span className={styles.leftMarginTwo}>
+          {" "}
+          <span className={styles.nameShadowUppercase}> _</span>y
+        <span className={styles.inverseNameShadow}> _</span>
+        <span className={styles.nameShadowUppercase}> _</span>
+        <br/><br/>
+        <span className={styles.nameShadowUppercase}>_</span>
+        <span className={styles.inverseNameShadow}> _</span>
+        <span className={styles.inverseNameShadow}> _</span>
+        <span className={styles.nameShadowUppercase}> _ </span>
+        </span>
+        <br />
+      </LeftBlackTitle>
+
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+
+      <h3 className={styles.poemTitle}>unction</h3>
+      <br />
+      <br />
+      <pre className={styles.poem}>{`Today could be last 
+Night.
+Spoke-dawn,
+Just so.
+Adige’s glass docks, for show,
+Arrive.
+Understandings
+Retreat.
+Little maiden bits and pieces.
+Against
+Dawn, mountains to
+House, beneath cool
+Noon, by the seaside.
+Chapter
+Of a sphere. Spatial enough to
+Hand you and I
+Longing. The hut. Earth’s lien.`}</pre>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+
+      <h3 className={styles.poemTitleTwo}>naxalite flight</h3>
+      <br />
+      <br />
+      <pre className={styles.poem}>{`Were
+The
+Heart
+Not
+R
+U
+N
+On
+Sprung
+R
+H
+Y
+T
+H
+M
+Would
+The
+Heart
+Be
+R
+E
+D
+?
+
+
+Were
+The
+Forest
+Not
+F
+U
+G
+I
+T
+I
+V
+E
+Not
+A
+Verge,
+Would
+The
+Forest
+Hold
+Against
+The
+Police
+?
+The
+State
+?
+Were
+Food
+Not
+Gotten,
+S
+A
+T
+And
+H
+A
+D
+Together
+As
+G
+I
+V
+E
+N
+Would
+F
+O
+O
+D
+Be
+Food
+?
+
+
+The
+Ice
+F
+L
+O
+W
+E
+R
+S
+Burn,
+Is
+Their
+O
+R
+A
+N
+G
+E
+Color
+What
+Burns
+R
+E
+D
+?`}</pre>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+
+      <h3 className={styles.poemTitle}>dead friends</h3>
+      <br />
+      <br />
+      <pre className={styles.poem}>
+        <span className={styles.specialWrap}>
+          {`Rome’s alleys are washed in the blood of Fascist and anti-Fascist graffiti.
+Sentenced names. Fateful names. Helpless, the hands scrawl out fate.
+It is fate the perpetrators run guns for. Dead echoes. Carnate remnants.
+Heaven’s contingencies. Observation cannot allay breath’s prejudice.
+Thrush hides low in the heart’s bush, still, and beating, between thorns
+And dust, under the needle of the moon against the dungeon’s shaft,
+The four shafts, cut into the four walls, so narrow, each admits a thread
+Of moonlight, four needles weaving four threads for starlight’s dress,
+Dunning the floor of the dungeon for cloth-light, air clotted with vision.
+The dungeons are washed in the blood of anti-Fascists, and partisans,
+Of noms de guerre who La Decima tortured and killed, score after score.
+Fascism’s operatives are beholden to its targets and all its brigandries,
+Including love and torture, form its grammar, the lines its tongue trawls,
+On wanton ropes of blood, if sea snails and brass shells hug, noosing,
+By arms, and legs, and necks, asemic populations of breath, and hoisted up
+Corpses, enfleshed in every room by fires and knives mutilations hide,
+In every room no vacancy keeps, to parody, thus unite, a residence on earth.`}
+        </span>
+      </pre>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+
+      <h3 className={styles.poemTitleTwo}>zone</h3>
+      <br />
+      <br />
+      <pre className={styles.poem}>{`Words
+increase,
+they
+cannot
+be
+increased,
+they
+incarnate
+now
+to
+warrant
+due,
+as
+smoke-clouds
+on
+the
+mountain
+ash,
+beleaguer,
+in
+qualities
+as
+if
+snow.`}</pre>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+
+      <h3 className={styles.poemTitle}>apsidal</h3>
+      <br />
+      <br />
+      <pre className={styles.poem}>{`Winter, the fire of clarity
+The once-hot-starred skies draw closer.
+      
+      
+Winter, the feudal desire
+The once-winkling irises fix, their steel.
+      
+      
+Winter, the hot and cold
+Blood’s Midas cannot ressurect breath.`}</pre>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+
+      <h3 className={styles.poemTitleTwo}>poem for walter benjamin</h3>
+
+      <br />
+      <br />
+      <pre className={styles.poem}>{`i.
+      
+Portage, being the gross of all marches.
+The reverse military pageant detaches.
+Security sees the cowed parties off to sea.
+That sift over the waves to civilian death.
+
+
+The light, being the water’s excrement:
+Exile’s unction. Nowhere: agapē’s kiss;
+A cool breeze. Bora act. Then, a wind’s
+Marbled blue, and bone. No act. Zone.
+
+
+Wordlessly the sea keeps the occupation,
+Shores open, the prison gates stand agape.
+Impregnable, the Word hold. Portation
+Marches. Sufferings of movement are air.
+
+
+
+ii.
+
+The light, being the water’s excrement: 
+Shores open, the prison gates stand agape.
+Wordlessly the sea keeps the occupation,
+That sift, over the waves, to civilian death.
+
+
+Security sees the cowed parties off to sea.
+Marbled blue, and bone. No act. Zone.
+Impregnable, the Word hold. Portation:
+Exile’s unction. Nowhere: agapē’s kiss;
+
+
+A cool breeze. Bora act. Then, a wind’s
+Levantine. The forward military pageant
+Attaches. Portage being the gross of all
+Marches. Sufferings of movement are air.
+
+
+
+iii.
+
+I keep and you keep, without
+World through
+Revelations, without
+World through
+Reconciliation,
+The violations untouched.`}</pre>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+
+      <h3 className={styles.poemTitle}>sore</h3>
+      <br />
+      <br />
+      <pre className={styles.poem}>{`Old wound’s isinglass brightens to dying.
+Life’s memories’ squat.
+Justification would-struck. Could’s mask
+Forecastles, (and mars)
+A counting-out-game. Old can of Christ.`}</pre>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+
+      <Footer />
+    </>
+  );
+}
