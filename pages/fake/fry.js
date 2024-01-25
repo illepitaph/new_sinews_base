eebab5391dcddaf58d9e9f2e57372f897bacb187
Steve Barbaro <steve.barbaro@gmail.com>
2024-01-24 20:51:10 -0600

    injects much of issue8 fry page code

--- pages/fake/fry.js
+++ pages/fake/fry.js
@@ -100,13 +100,16 @@ export default function Fry() {
       <br />
 
       <RightBlackTitle>
-      <span className={styles.nameHeight}>
-        <span className={styles.nameShadowUppercase}> l </span><br/>o<br/>
-        <span className={styles.inverseNameShadow}> g</span><br/>{" "}
-        <span className={styles.nameShadow}>_ </span>
-        <span className={styles.specialCursive}>a</span><br/>
-        <span className={styles.rightMargin}>
-        <span className={styles.inverseNameShadow}> n </span></span>
+        <span className={styles.nameHeight}>
+          <span className={styles.nameShadowUppercase}> l </span>
+          <br />o<br />
+          <span className={styles.inverseNameShadow}> g</span>
+          <br /> <span className={styles.nameShadow}>_ </span>
+          <span className={styles.specialCursive}>a</span>
+          <br />
+          <span className={styles.rightMargin}>
+            <span className={styles.inverseNameShadow}> n </span>
+          </span>
         </span>
       </RightBlackTitle>
       <br />
@@ -128,13 +131,14 @@ export default function Fry() {
         <span className={styles.leftMarginTwo}>
           {" "}
           <span className={styles.nameShadowUppercase}> _</span> y
-        <span className={styles.inverseNameShadow}> _</span>
-        <span className={styles.nameShadowUppercase}> _</span>
-        <br/><br/>
-        <span className={styles.nameShadowUppercase}>_</span>
-        <span className={styles.inverseNameShadow}> _</span>
-        <span className={styles.inverseNameShadow}> _</span>
-        <span className={styles.nameShadowUppercase}> _ </span>
+          <span className={styles.inverseNameShadow}> _</span>
+          <span className={styles.nameShadowUppercase}> _</span>
+          <br />
+          <br />
+          <span className={styles.nameShadowUppercase}>_</span>
+          <span className={styles.inverseNameShadow}> _</span>
+          <span className={styles.inverseNameShadow}> _</span>
+          <span className={styles.nameShadowUppercase}> _ </span>
         </span>
         <br />
       </LeftBlackTitle>
@@ -153,26 +157,136 @@ export default function Fry() {
       <br />
       <br />
 
-      <h3 className={styles.poemTitle}>unction</h3>
+      <h3 className={styles.poemTitle}>
+        f<br />o<br />{" "}
+        <span className={styles.uppercase}>
+          <i>n</i>
+        </span>
+        <br />d <br />
+        <br />w <span className={styles.uppercase}>i </span>t <i>h</i> <br />
+        <br />p<br />
+        <span className={styles.uppercase}>r</span>
+        <br />o<br />p<br />
+        <i>e</i>
+        <br /> <span className={styles.uppercase}>r</span>
+        <br />t<br />y
+      </h3>
       <br />
       <br />
-      <pre className={styles.poem}>{`Today could be last 
-Night.
-Spoke-dawn,
-Just so.
-Adige’s glass docks, for show,
-Arrive.
-Understandings
-Retreat.
-Little maiden bits and pieces.
-Against
-Dawn, mountains to
-House, beneath cool
-Noon, by the seaside.
-Chapter
-Of a sphere. Spatial enough to
-Hand you and I
-Longing. The hut. Earth’s lien.`}</pre>
+      <pre className={styles.poem}>{`
+Lewd fortune does its cute attunement,
+It says, Yes please I’ll have blank thanks
+So much. So much land’s a fuck. It does
+The permits, means the soil. Exchanges
+Like a yawn. As a child I feared terribly
+The stuck hinge I learned tetanus made
+Of a jaw. I pictured a farmer in costume
+Big loopy mouth hung well ajar because
+I guess a farm’s where the rust and barb
+Wire are. Have you lately gazed tenderly
+Into an open cut?—was it yours?—best
+For my purposes is if it is another’s cut.
+Better too if the studied calm you look
+With to project the tenderness was not
+Meant to ease their hurt but to cement
+The hurt as real? That your goal is not
+Even to tend their need but to let your
+Need to tend be known, not from some
+Selfish impulse on its own but so their
+Knowing provided for the cut one some
+More steady and profound balm. Some
+Are known to feed on other’s pain as a
+Way to earn the data for a new genre of
+Hurt.
+
+I bet it’s dumb. Gore mauls time open,
+
+Which is pretty dumb when you really
+Think on it, since time’s the unit thru
+Which wealth amasses, passive as a dad.
+The better get
+To do harm so
+Gradually that only each who feels its
+Flood (—in their blood, or guts, skin, or
+The like—) trusts the pain they really
+Feel, that each,
+The theys, feel
+All, individual. The most lauded calling
+Of to-day is
+That of harvesting from the body’s land
+This embodied disembodied body’s pain.
+`}</pre>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <h3 className={styles.poemTitle}>
+        n<i>u</i> <span className={styles.uppercase}>t</span>
+        <br />s h{" "}
+        <span className={styles.uppercase}>
+          e <i>l</i> l
+        </span>
+      </h3>
+      <br />
+      <br />
+      <pre
+        className={styles.poemSpecial}
+      >{`I would begin demands to things, illegibly, before facing up to it, a total thrust of life
+as sensible, a front for warding off. The screw that turns time’s thumb is sheared off.
+And I’m fiddling with the lucite’s lock, on a hinge, a deictic If
+that listens for the dimness of a proof’s blood folded up in the crux of a limb.
+They’ve given me this sweatshirt that my shoulders note the felt of, my back
+taut and neck dropped in to accentuate the drape I’m told is for getting it on trend
+with a family of eleven networked keyboards circa 1990 IBM
+that the seven twigs plunked with dew still on and bent and singed just for this purpose say
+is key to growing marketshare in a plurality of tosses.
+The discourse: a treaty that a trick of light undoes. When I peer off toward what seems to be
+its edge’s lip, I meet in me a whirl of faceslooking aside and smirking intermittently, a grimace I can weave
+a smile from. But do I want to? It’s like, if it were my heart, or not that but a hot lung pulled from me, being
+mindful to take off the sweatshirt first and only slip my head back thru it once the blood has crusted nice
+enough, there may then be a way to. I could regain a fond adherence to the medium.
+It’s not like I have used them much. There’s something tedious in the bulging cells and too-deep redness
+of the tendrils running wildly about, like it was something made in haste
+and with a frankly sloppy lack of tenderness with limit and yet
+pridefully compelled to come in under cost and for it to be, consistent with its aims, done on schedule.
+Some things must be faced head on. So I leaned that way. Beside, I haven’t got a real desire to disobey
+and in fact I trust the objects flanking me earn a conciliation’s sigh I’d guess, being teal, having a real great
+texture that I couldn’t even begin, at first, to imagine—the kindly one, the type of crick I like sharing winks
+with, not of intention but a mindfulness the bends in rivers lack but lift from thin tributaries meant to let time
+broaden them. In the stream I see jokes flitting up like trout for gnats but helplessly, sillily, getting that
+getting it is all part of the lure. Understanding that it isn’t reachable, that’s
+clear. But, too, it’s, well, maybe the of of texture will snag on it yet still. Is it truly so off to see that a texture—
+even a soft one!—has no texture, which of course it can’t—but does—that this texture may impress upon this
+first supple so-thin slice of air? One’s distance to it is an If unfixed, transcribed and mappable
+as when one, forced into the barest tip of noticing, must adjust a hand’s presence to the form
+of compensation. But between these rank periods of, generally minute, adjustments and of fixities it is
+distance that limiting becomes all too knowable a limn for, even if it had not been tallied up as such just yet.
+
+My finest hook has in it a relatively fixed amount of barbs and also at any given moment, number of admirers,
+but if these properties don’t hover in the cognition den I let thoughts int0, they’re done. Not because I won’t
+allow them, it’s just I bar from entrance the unknown. You get it. I could tell fast you liked my thumb’s
+bend, but that’s just not enough, see. Who else may have been in
+it, while attending to this so. That’s it. You see
+the peril. It needs a contract with endlessness,
+a idiot move, infinity, that can’t keep up, not ever.
+My palms are both facing toward, on arms limp and dangling yet not without great care, the fingers all curled
+slightly in as though gripping something limp and loosely, some hot weapon needing a worm’s carelessness
+in order to preserve the static of its being lain as dormant, reacting inversely with its function to tear potential
+in its waiting there where length is curled.
+But there is no weapon my hands grip. Rather it’s a gesture’s metaphor. Well, an illustrative ideation, too, yes.
+Now you can imagine not only the tilt of each my hands but the figure they imported to operate within to lure
+their tilt in with just so, critical as I begin, in telling you, as I did, of the emblem that my totalised body did for
+guiding all this that I’d rather not be doing. I’m stable again. I’m under a palm and lurid in my mind. And you
+can and I hope will make visual the endpoint of the texture of the ripples sent thru 1 by 1 the thin slices of the
+air that are so neatly piled as to feel and to jot down as so minor a result as that which is impressed—I can just
+imagine it! so subtly, by the dint
+of texture’s surfacing that 1 is 1, that one over there, that one I briefly saw far off over there, and my palm will
+shade the that of what I saw, its dust upon your palm, that what it is is smiling there—
+of’s fruit—, if plucked and absently and under what totalises this so, just being here.`}</pre>
       <br />
       <br />
       <br />
@@ -182,145 +296,60 @@ Longing. The hut. Earth’s lien.`}</pre>
       <br />
       <br />
 
-      <h3 className={styles.poemTitleTwo}>naxalite flight</h3>
-      <br />
-      <br />
-      <pre className={styles.poem}>{`Were
-The
-Heart
-Not
-R
-U
-N
-On
-Sprung
-R
-H
-Y
-T
-H
-M
-Would
-The
-Heart
-Be
-R
-E
-D
-?
-
-
-Were
-The
-Forest
-Not
-F
-U
-G
-I
-T
-I
-V
-E
-Not
-A
-Verge,
-Would
-The
-Forest
-Hold
-Against
-The
-Police
-?
-The
-State
-?
-Were
-Food
-Not
-Gotten,
-S
-A
-T
-And
-H
-A
-D
-Together
-As
-G
-I
-V
-E
-N
-Would
-F
-O
-O
-D
-Be
-Food
-?
-
-
-The
-Ice
-F
-L
-O
-W
-E
-R
-S
-Burn,
-Is
-Their
-O
-R
-A
-N
-G
-E
-Color
-What
-Burns
-R
-E
-D
-?`}</pre>
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-
-      <h3 className={styles.poemTitle}>dead friends</h3>
+      <h3 className={styles.poemTitle}>
+        b<br />e<br />
+        <span className={styles.uppercase}>a</span>
+        <br />
+        <i>t</i>
+        <br />
+        <span className={styles.uppercase}>i</span>
+        <br />t<br />u<br />
+        <i>d</i>
+        <br />
+        <span className={styles.uppercase}>e</span>
+      </h3>
       <br />
       <br />
       <pre className={styles.poem}>
-        <span className={styles.specialWrap}>
-          {`Rome’s alleys are washed in the blood of Fascist and anti-Fascist graffiti.
-Sentenced names. Fateful names. Helpless, the hands scrawl out fate.
-It is fate the perpetrators run guns for. Dead echoes. Carnate remnants.
-Heaven’s contingencies. Observation cannot allay breath’s prejudice.
-Thrush hides low in the heart’s bush, still, and beating, between thorns
-And dust, under the needle of the moon against the dungeon’s shaft,
-The four shafts, cut into the four walls, so narrow, each admits a thread
-Of moonlight, four needles weaving four threads for starlight’s dress,
-Dunning the floor of the dungeon for cloth-light, air clotted with vision.
-The dungeons are washed in the blood of anti-Fascists, and partisans,
-Of noms de guerre who La Decima tortured and killed, score after score.
-Fascism’s operatives are beholden to its targets and all its brigandries,
-Including love and torture, form its grammar, the lines its tongue trawls,
-On wanton ropes of blood, if sea snails and brass shells hug, noosing,
-By arms, and legs, and necks, asemic populations of breath, and hoisted up
-Corpses, enfleshed in every room by fires and knives mutilations hide,
-In every room no vacancy keeps, to parody, thus unite, a residence on earth.`}
-        </span>
+        {`Just imagine it, strung from wire
+     `}{" "}
+        <i> (14 gauge)</i>
+        {`
+to level with your sightline. The
+
+ice encasing it engages in a, honestly pathetic, ritual of drips
+imperceptible. Oh, you didn’t
+
+notice? How do you know you
+didn’t? Look now,
+a purse cinched with bent lead pipe asway there,
+
+the rib crease
+of gesture
+
+leaves sift
+for preciousness that,                          is a sulfur
+                                      if employed well,
+ore for
+smiles.
+            Mill that ashes edit.
+Torch of the trash bins.
+Fastened to
+the tin nail bone-hewn and looser in its punch-hole rawer to.
+
+                          I’ve been in gumboots
+                                      sifting a sewer
+                                     from shortcake,
+                                        a horsetooth,
+                                        a fibre noose
+                                         from oil dew,
+                     that if the leather holds
+                     I’ll have a weight to tug
+the thread
+up, parson,
+to my name,
+of Patience,
+for apathetic fortune’s low horn tune.`}
       </pre>
       <br />
       <br />
@@ -331,33 +360,43 @@ In every room no vacancy keeps, to parody, thus unite, a residence on earth.`}
       <br />
       <br />
 
-      <h3 className={styles.poemTitleTwo}>zone</h3>
+      <h3 className={styles.poemTitle}>
+        net
+        <br />
+        of
+        <br />
+        <i>t h u s</i>
+      </h3>
       <br />
       <br />
-      <pre className={styles.poem}>{`Words
-increase,
-they
-cannot
-be
-increased,
-they
-incarnate
-now
-to
-warrant
-due,
-as
-smoke-clouds
-on
-the
-mountain
-ash,
-beleaguer,
-in
-qualities
-as
-if
-snow.`}</pre>
+      <pre className={styles.poem}>{`Curses and sausage.
+Garish and porcine.
+Bloodlines in syrup.
+Lick at the fireapple.
+Forced permutation.
+Letting its trickle in.
+A war is, uh, boring.
+Courteousness, yes.
+Latherers’ fortitude.
+Bricks of shitted fib.
+Yeast goes on living.
+That none becomes.
+Perchance’s injuries.
+This hand is hurting.
+Unfitful permissions.
+Tidying of flametips.
+Luridness evaporates.
+Burnt with a vinegar.
+The morose pathetic.
+Porcelain pried open.
+Form’s a gutted liver.
+Sort out the sewages.
+Knowing the corpse.
+Oiling the sawtooth.
+Bathos’ fun oblivion.
+Tonnage upon lung.
+A knife in the mud.
+That none know of.`}</pre>
       <br />
       <br />
       <br />
