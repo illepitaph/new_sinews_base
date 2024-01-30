79638703f3a418edd402019a6cf691881a4c5302
Steve Barbaro <steve.barbaro@gmail.com>
2024-01-30 13:41:06 -0600

    instantiates issue8 drug submission

---
+++ pages/fake/drug.js
@@ -0,0 +1,270 @@
+import Head from "next/head";
+import Header from "../../components/header.js";
+import RightBlackTitle from "../../components/rightBlackTitle.js";
+import LeftBlackTitle from "../../components/leftBlackTitle.js";
+import Footer from "../../components/footer.js";
+import styles from "../../styles/Drug.module.css";
+Drug.title =
+  "valerie hsiung ~ an If unfixed ++ all kinetic in rectitude ++ — new_sinews";
+Drug.pieceName =
+  "valerie hsiung ~ an If unfixed ++ all kinetic in rectitude ++ — new_sinews";
+Drug.description = "_Valerie Hsiung_poetry_9 POEMS_issue8_";
+Drug.image = "https://www.newnewsinews.com/images/FryBackground.jpg";
+
+export default function Drug() {
+  return (
+    <>
+      <Head />
+      <Header headerBackground="fryBackground" />
+
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <RightBlackTitle>
+        <span className={styles.nameHeight}>
+          <span className={styles.nameShadowUppercase}> v </span>
+          <br />
+          <span className={styles.specialCursive}>a</span>
+          <br />
+          <span className={styles.rightMargin}>
+            <span className={styles.inverseNameShadow}> l </span>
+          </span>
+          <br />
+          <span className={styles.specialCursive}>e</span>
+          <br />
+          <span className={styles.nameShadowUppercase}> r </span>
+          <br />i
+          <br />
+          <span className={styles.rightMargin}>
+            <span className={styles.inverseNameShadow}>_ e </span>
+          </span>
+          <br />
+          <br />
+          <br />
+          <br />
+          <span className={styles.nameShadowUppercase}> h </span>
+          <br />
+          <span className={styles.inverseNameShadow}>s </span>
+          <br />
+          <span className={styles.specialCursive}>i</span> <br />
+          <span className={styles.rightMargin}>
+            <span className={styles.inverseNameShadow}> u </span>
+          </span>
+          <br />n<br />
+          <span className={styles.specialCursive}> g </span>
+          <br />
+          <span className={styles.inverseNameShadow}>&gt; </span>
+          <br />
+          <span className={styles.nameShadowSpecial}>&gt;</span>
+          <br />
+          &gt;
+          <br />
+          <span className={styles.inverseNameShadow}>&gt; </span>
+          <br />
+          <span className={styles.nameShadowSpecial}>&gt;</span>
+          <br />
+          &gt;
+        </span>
+      </RightBlackTitle>
+
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <pre className={styles.poem}>{`
+the second you become a drug
+we’ll have to sterilize you
+won’t hesitate to
+she’s charming that way
+tells us everything she’s thinking
+and then turns the volume off
+for us so that we live inside
+a glass box when really
+there’s a glowing diamond
+inside her chest it’s really hot
+it’s giving off deep earth
+there is no life enough for her
+without the store window
+which is distant beetle to her as China
+`}</pre>
+
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+
+      <pre className={styles.poemTwo}>
+        {`until that age I’ll be needed
+and will have to look for accommodations
+a hawk could knock me off
+a soldier in the background
+cane off with this dilapidation
+I am here to witness color
+partially it’s my choice
+partially out of abundant circulation
+whereas so many close
+undulate with heat packs
+on their routes will take out
+all their change for heat packs
+if we smash up the globe
+cut it up with scissors like its herbs
+for more surface area
+then I must become liquid
+solvent preservative
+in which to be dipped in
+do not approach the woman who says “Love”
+do not extend your hand with a handshake
+she has said love
+at our fingertips lightning
+not whoosh
+not even stakes holding up hundred year vine
+but to climb
+like a spider
+in the emergence of
+this immaculate port o potty
+be Sumerians now`}
+      </pre>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <pre className={styles.poemThree}>{`I sit at the other end
+of the telephone
+there is not a cloud in the sky
+I tell myself to move
+slightly to the left
+on the other end
+of the telephone
+where what they have
+learned of me is not yet
+my sun my cloud
+our science fictions
+the child
+cannot be adulterated
+the child
+cannot be mistaken
+for beginner’s luck
+they will have their breath mints
+they will breath onto the window
+begin the new germ`}</pre>
+      <br />
+      <br />
+     
+
+     
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+
+      <Footer />
+    </>
+  );
+}
