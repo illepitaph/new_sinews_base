0a085040a8c6c4dca5738b266951f955e74fc1d2
Steve Barbaro <steve.barbaro@gmail.com>
2024-01-30 20:14:24 -0600

    adds all poem text to issue8 drug entry

--- pages/fake/drug.js
+++ pages/fake/drug.js
@@ -52,11 +52,12 @@ export default function Drug() {
           <br />
           <span className={styles.specialCursive}>i</span> <br />
           <span className={styles.rightMargin}>
-          <span className={styles.nameShadowUppercase}> _ </span>
+            <span className={styles.nameShadowUppercase}> _ </span>
             <span className={styles.inverseNameShadow}> u </span>
           </span>
           <br />n<br />
           <span className={styles.specialCursive}> g </span>
+          <br />
           <br />
           <span className={styles.inverseNameShadow}>&gt; </span>
           <br />
@@ -253,8 +254,456 @@ they will breath onto the window
 begin the new germ`}</pre>
       <br />
       <br />
-     
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <pre className={styles.poemFour}>{`one hippocratic oath to another
+behold an everyday theory
+to having our arms full
+I don’t like knowing where I came from
+but there it is
+you are like a painting
+I could’ve grown up with
+if I were plantagenet
+and wouldn’t replace the problems I had
+with the problems you would bring me
+the population there was unremarkable
+unaccounted for even by the census`}</pre>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <pre className={styles.poemThree}>{`dextromorphically I’m skipping
+through the chords as index cards are chords
+tritrating newspapers searching for
+an advertisement for perfume
+each one fits exactly the terms
+of my search each one continues
+my search there is a room around you
+Valentina there is a room around you
+exactly the person I was married to
+could be not the person I was married to
+but always is and that is my impunity
+we could cut our sleeping bag in half
+all these sympathizers with jobs
+sharing a big room with bunk beds
+and us in half together
+I appear healthy! I mix powdered cheese
+onto the hot plate! I point to my stomach
+well did you find it?
+no because it turns out
+I am looking for an advertisement
+for perfume that is pre perfume`}</pre>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <pre className={styles.poemTwo}>{`no one’s passed you by
+physically in the hallway
+then they’re metal
+the stairs leading from the street
+to inside the passageway isn’t
+dilated when all of us and me making eggs
+come through to you dilated
+the hand on the plaque
+with words gridlocked
+very correspondences hand you
+of the fair meters goldilocks
+the tree fell but just missed
+the neglect you come through
+the front door staggering visiting your
+earliest vestibular pigment I know your hand
+caught in a machine which is a human
+wielding a primitive tool
+but your body can’t close in on it
+with or without the only t-shirt`}</pre>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <pre className={styles.poemFour}>{`all I ever wanted was to wake up
+be perfect
+be mean
+take my little dog to daycare
+instead my cuticles are ripped
+at the edges so that I see
+how outside of me I fan out
+like a gingko leaf
+which is delicate enough
+for lads to put on them
+but not the same way I jump
+out of prostitution where have I been
+when I have not showered
+when I have not spoken to no one
+for thirteen days`}</pre>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
 
+      <pre className={styles.poemTwo}>
+        {`heavenly heavenly heavenly
+numbers on hand ripped legal pad paper
+a vest in polycotton is asking
+you remember don’t you
+for the musket for the basket
+and for some cream
+I have never been so tempted
+as I think I have been tempted
+to make a concertina the shape of a gasket
+with all of its reproductive value
+fully entrenched how whatever
+grows on one fallen
+will fell another before long
+I bleed onto the stairs
+and then trays rush`}
+      </pre>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+
+      <pre className={styles.poem}>
+        {`there have been times
+when you needed to race through traffic
+run down the street hailing hailing
+perhaps I’m an eruption too
+I certainly am overtaken
+by my idea of an uneventful morning
+the idea of it being all over
+can only remedy that by putting
+construction paper blue tape
+on the walls stepping back
+reaping the walls
+this happens when there’s no more
+blood left you’ve used it all up
+spit out everything 3/8 inch drill bit
+take the man who helped me
+home with me he’s in the squad now
+a spree will suffice
+a spree a spree come with me`}
+      </pre>
 
       <br />
       <br />
