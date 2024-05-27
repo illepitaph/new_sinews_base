37fb85b78c00193b1105e509bfa905c7579872d4
Steve Barbaro <steve.barbaro@gmail.com>
2024-05-26 20:28:30 -0500

    instantiates issue8 nails entry

---
+++ pages/fake/nails.js
@@ -0,0 +1,212 @@
+import Head from "next/head";
+import Header from "../../components/header.js";
+import LeftGrayTitle from "../../components/leftGrayTitle.js";
+import LeftBlackTitle from "../../components/leftBlackTitle.js";
+import StandardProse from "../../components/standardProse.js";
+import Image from "next/image";
+import Footer from "../../components/footer.js";
+import styles from "../../styles/Nails.module.css";
+Nails.title = "cary_stough ~ nails — new_sinews";
+Nails.description = "_Cary Stough_lyric~memoir_NAILS_new_sinews_issue6_";
+Nails.image = "https://www.newnewsinews.com/images/TargetBackground.jpg";
+
+export default function Nails() {
+  return (
+    <>
+      <Head />
+      <Header headerBackground="nailsBackground" />
+      <LeftGrayTitle>
+        <span className={styles.duskShadow}>
+          <span className={styles.duskShadowUppercase}>n</span>
+          <span className={styles.inverse}>
+            {" "}
+            <span className={styles.headerIndent}>a</span>{" "}
+          </span>
+          <span className={styles.headerIndent}>
+            <span className={styles.alternateCursive}>i</span>
+          </span>
+          <span className={styles.inverseUppercase}>
+            {" "}
+            <span className={styles.headerIndentTwo}>l</span>
+          </span>
+          <span className={styles.duskShadowUppercase}>s</span>
+        </span>
+      </LeftGrayTitle>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />
+      <LeftBlackTitle>
+        <span className={styles.nameSpacing}>
+          ... ... c
+          <span className={styles.uppercase}>
+            <span className={styles.nameIndent}>a</span> r
+          </span>{" "}
+          <span className={styles.nameIndent}>y</span> ... ... s
+          <span className={styles.uppercase}>t</span>o u
+          <span className={styles.uppercase}>g</span> h ... ...
+        </span>
+      </LeftBlackTitle>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <StandardProse>
+        Driving past the houses of dead Lexingtoners I paused the book and
+        repeated the phrases:{" "}
+        <span className={styles.grayShadow}>
+          <i>
+            May I be free of danger, May I have mental happiness, May I have
+            physical happiness, May I have ease of well-being.
+          </i>
+        </span>{" "}
+        I name this performance, I call it{" "}
+        <span className={styles.grayShadow}>
+          Swallowing White Buddhism While Driving.
+        </span>{" "}
+        Thought: possibly I’m ready to swallow White Buddhism. Possibly I’m
+        ready. <br />
+        <br />
+        <br />
+        To write.
+        <br />
+        <br />
+        <br />I had convinced myself fairly strongly that I wouldn’t or couldn’t
+        write about the beginning of the plague years because, topographically,
+        rather than happening around me, it was happening <i>with</i> and{" "}
+        <i>on</i> me. (They are still happening with me! These years) With us.
+        <br />
+        <br />
+        <br />
+        We had been told that, due to a forced poverty of sexual activity,
+        aspects of our personalities would emerge that had been kept dormant,
+        lending a renewed aptitude for everyday social life. Learn crafts, the
+        art of something, living perhaps. Going on Zoom dates. Seeing this as
+        yet another axiom in line with self-care capital-extraction, I convinced
+        myself, with regards to myself, that there was nothing left to learn.
+        However,
+        <br />
+        <br />
+        <br />
+        This was what autonomy earned. Scrolling back and forth through my
+        matches.
+        <br />
+        <br />
+        <br />
+        Walking in the Arnold Arboretum with C., then back to her house where I
+        was promptly saluted and sent on my way, despite being promised a cup of
+        green tea. Thinking it had gone well. Walking across Harvard’s
+        campus—from the Radcliffe Quadrangle, through the square to Felipe’s, up
+        Broadway—with L., with whom I was excited to meet again, though{" "}
+        <i>not in the headspace</i> to continue, or make of our time a process.
+        A process I didn&rsquo;t wish to renew. Walking, one morning, across Perry
+        Park for iced coffee at Broadsheet with A. in the middle of the summer.
+        An agreement we’d made immediately, to acknowledge each other’s
+        turn-ons. She told me to take my time with it. With what, presumably.
+        She chipped a nail on my belt.
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <span className={styles.sectionBreakSymbol}>~</span>
+        <br />
+        <br />
+        <br />
+        I’d refused to begin writing this, I told myself, because of time. Since
+        this was happening <i>with</i> me, and seeing as I have (writing this)
+        no indication of when it will end, the only conclusive point I am
+        capable of imagining (having <i>at hand</i>, very literally) is that of
+        my own life. The end of my life. The threshold of a time period is the
+        point past which I can no longer be said to exist in it, except in the
+        memory of those who have loved me, which is to say, those who have
+        allowed my bones to be broken and buried.
+        <br />
+        <br />
+        <br />
+        So that that won’t do any good. You can’t write out a suicide.
+        <br />
+        <br />
+        <br />
+        <span className={styles.grayShadow}>
+          Dear Suicide,
+          <br />
+          <br />
+          <br />
+          Make yourself at home.
+          <br />
+          <br />
+          <br />
+          Sincerely,
+          <br />
+          <br />
+          <br />
+          Riding it out.
+        </span>
+        <br />
+        <br />
+        <br />
+        Not wanting to be honest with myself about my stupidity—not lack of
+        knowledge, but its limit, its <i>founding</i> limit. I hear 2.01 million
+        worldwide and can’t attach such a number to human lives. Rather than
+        investigate this, I forget the number until someone says it out loud
+        again. I feel nothing in regards to loss of life within the expression
+        of a number. Rather, I feel the emotion behind such a number I can’t
+        comprehend. To quantify, in a way, is comparing two objects. Surplus
+        value measured out in rice in a Tik Tok helped. 1.4 million Americans
+        attempted suicide in 2020, I’m told by a graphic. I wasn’t one of them.
+        You cannot count the desire. I feel nothing other than the hope of being
+        one of them at a point in time where I will be able to look back. 48,
+        334 suicided (in the US). 22,000 students enrolled at Harvard, across
+        the three schools. Birth rate of 11.990 in the US. How many is that?
+        Google wouldn’t tell me, or doesn’t know. The truth is it’s all of them.
+        It’s all.
+        <br />
+        <br />
+        <br />
+        <span className={styles.grayShadow}>
+          May I have a model for loss
+          <br />
+          <br />
+          May I have the diagrammatical strength of a number
+          <br />
+          <br />
+          May I have it all
+        </span>
+        <br />
+        <br />
+        <br />
+        And it all, someday, will end. Which is to say, for an us. As it, I
+        would be remiss to mention, has been ending—and may have as well already
+        ended—for some, for those on the outer circle, what we call “bodies,”
+        concealing the guilt of how we’d previously identified them.
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <span className={styles.sectionBreakSymbol}>~</span>
+        <br />
+        <br />
+        <br />
+        “You did this type of breathing,” she said, and exhaled, slowly, twice
+        through her nose, “and so I thought you were asleep.” “I wasn’t
+        asleep,” I said, “I was emotional.”
+        <br/>
+        <br/>
+        <br/>
+
+      </StandardProse>
+      <Footer />
+    </>
+  );
+}
