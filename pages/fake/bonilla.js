1c49bb3b1b5418a498f867fa75920f4f61d6873b
Steve Barbaro <steve.barbaro@gmail.com>
2024-05-30 14:41:47 -0500

    refines issue8 harimate entry

--- pages/fake/bonilla.js
+++ pages/fake/bonilla.js
@@ -55,7 +55,277 @@ export default function Bonilla() {
       <br />
 
       <StandardProse>
-       
+        It was in one of those badly lit, bureaucrat-filled, crime-crowned,
+        cum-reeking joints that offer Calzada Independencia its distinct flavor
+        of decadence where Leobardo’s brother, having suffered celibacy for
+        thirty-one years owing to a middling height and protruding hump,
+        understood at last that his body could be something more than a prison.
+        Penelope was always on his lips, manifesting as a deeply caring and
+        thoughtful figure who asked for little; that is, for her pay and nothing
+        else. Everyone knew, especially Leobardo, that a maternal disposition in
+        one of Ishtar’s daughters cannot obtain if you fail to go beyond
+        protocol. Mere pay yields tried-and-true craftsmanship: some handiwork
+        and a tightening of the cunt. Verily, Penelope made a Pindar of
+        Heriberto, and soon brought out his lavish tendencies, insofar as
+        minimum wage and family loans permitted.
+        <br />
+        <br />
+        Leobardo determined Penelope was a coping mechanism that Heriberto
+        excogitated in an instant of despair, and therefore recurred to
+        relentless derision. Although the case for brotherly love was hard to
+        argue, Leobardo certainly imagined, consistent with his devious loyalty
+        to time-worn masculinity, that only through his aggressive deflation of
+        illusions he would open his brother’s eyes. But words are words,
+        penurious stabs at passing air, and even Leobardo’s hebephrenic humor
+        could not debark in Heriberto’s inner haven, truculent as it now was to
+        all material which brought no memory of viscous hydromancy. Indeed, our
+        erstwhile eunuch acquired the bearings of a satrap, bid adieu to
+        nosocomial self-consciousness, and dared to call Leobardo an equal. The
+        relationship was heating. Words had to be relinquished.
+        <br />
+        <br />
+        Step where you may, there are prerogatives for regulars. No exception in
+        Lipsdick: a minute of intense grinding after your underwear were as wet
+        as a viene viene’s rag, ribald tattle designed to cement your insider
+        status, an improbable hand job among your clueless pals, some coquettish
+        slaps to harmonize with the allotted spanking. It is easy to notice that
+        all these concoctions, however intimate in execution, were simple
+        charades. Leobardo, in his hubristic spell, asked for truth. Who tended
+        to his brother? Correct, correct, the sly Heriberto had anticipated an
+        intervention and so gave a false name. Lipsdick housed no Penelopes.
+        Furthermore, he soliloquized as if she were a soul unbounded by matter,
+        a visitation that tickled his cock with the effectiveness of a pagan
+        goddess. In this <i>civitas diaboli</i>, you would have been fortunate
+        to remember humanity had interests beyond the haptic. Leobardo tried,
+        spent, and folded. Plotinian Penelope eluded his coarse sensibility. How
+        could she not? Why would such a cock-slanted being find within the
+        whorizon the exquisite Phryne, beloved of Apelles, Praxiteles, and
+        Hypereides? Why would the lowly devotee of some nameless satyr detect
+        among the pornae the sophisticated Leontion, Zoilus of Aristotelians?
+        Perhaps a capricious Lais would have made herself known. Perhaps.
+        Against Larentina, however, he had to approach me.
+        <br />
+        <br />I hasten to add that my involvement sprang from a need for
+        dallying, not from any genuine interest in the fraternal tug-of-war. A
+        tyrannical older brother who lords it over an embryonic anchorite teases
+        us with mythic undertones, yes, but the eventual rebellion that is
+        ignited through a growing self-worth surely evokes another trinket for
+        Americana aficionados. Therefore I set aside the speculative leisure in
+        which it is my custom to hibernate. Did not Machiavelli forge his shrewd{" "}
+        <i>Discorsi</i> in the amiable and aimless atmosphere of the Orti
+        Oricellari, where conspirators, poets, and philosophers alike reveled in
+        the hidden harmonies of the vernacular? What of Ficino and his
+        Neoplatonic Florentine Academy, so debonair as to outspread and fill
+        each member’s heart? Or Boucicaut’s Order of the Green Shield with the
+        White Lady, dedicated to something <i>other</i> than the “honor, estate,
+        reputation and praise of all women and damsels of noble lineage”? Let us
+        found our <i>Cours d’Amour</i> whenever possible, and trust that the
+        spoken word will engrave within once and for all the fleeting
+        experience. But we must gather first.
+        <br />
+        <br />
+        Cunning though Heriberto was, he ultimately fixated on traits that
+        betrayed a slender silhouette. On the basis of this seemingly meager
+        datum, four-fifths of Lipsdick was discounted. Your gluttonous civil
+        servant has always had a predilection for continual reminders of his
+        daily feasts: nothing will do short of callipygian damsels with enough
+        rack for the sudden erotomastia. What is more, upstanding members of
+        society that they are, they would not think of stuffing their
+        triple-chinned faces into their steaks; at night, nevertheless, they can
+        stuff them for minutes on end into a handsomely paid ass. And let us
+        keep in mind their rebellious counterparts, those cap-wearing gentlemen
+        who, peanut-shaped though they be, must have tributes to their impetuous
+        manhood throughout the day. These proud bearers of our animal past favor
+        defenseless cries for help and pleading eyes, but they can manage to
+        sleep if a zaftig dancer growls smutty nonsense about their cocks almost
+        puncturing their insides. Lipsdick cared for these abliguritious
+        customers. The stray students who giggled at the sight of nudity and
+        whispered “tits” with bated breaths, the hoodied bachelor who looked
+        askance in fear of finding his equally lonely colleagues, the reluctant
+        drunks who had an innocent time at the karaoke bar yet felt there must
+        be more to a night of fun were all merely tolerated. The five exceptions
+        to contemporary standards of lust were at their disposal. I turned to
+        them.
+        <br />
+        <br />
+        Inés in her inglenook, gratifier of nikhedonia, angled her eyes and
+        opened them out of pot-induced engrossment, for she knew the prevailing
+        addiction to minima and disbelieved of cuntcentration. A grin, a slow
+        dance, and a remote spread. I could see those gentlemen on repeat
+        skeptical of their luck. Here you had a long pair of legs attached to a
+        comatose stunner: the crude dream implied in countless jokes. Inés did
+        not talk, legs swinging and trembling as bait. Inés waited and stuck to
+        her inner life. No, no, she would certainly not allow anomalies. Thus I
+        sat and made no further inference. She slept. Susana, antipode at order,
+        hurried toward my cock, mumbling <i>donkey horse tripod impaler</i>,
+        stripping with violence her outmoded lingerie, baroque pleaser to eye
+        and ear. I had to act the tease. She played with her nipples, licked and
+        bit her lip, stared with consuming ardor. Blistering lust as sedative
+        was the strategy. So again I would not hear a meaningful word. In point
+        of fact, I masturbated out of politeness, but impatience grew. A
+        makeover, she insisted. Noticing the cumulative bitterness, I went with
+        that beloved defilement of porn-glutted teenagers and dog-natured hunks.
+        I was then hurried out. Melanie succumbed to the garishness of her lair.
+        The walls were covered with posters of Rafael Inclán, Alfonso Zayas,
+        Jorge Rivero, Chatanuga, and Tun Tun; two bedside tables had small lamps
+        in the shape of ficheras; the bed aspired to roundness. She was the
+        youngest yet. Perhaps nineteen? Submission, aggression, and now
+        regression. Giddy and grumpy, resolutely inarticulate, she came and went
+        enthralled by the echoes of her screeching laughs and curses. Taunts to
+        toil and triumph. Like in the movies. Cut. Why follow in the footsteps
+        of curmudgeons who link lust and lewd literacy, who sweat for a
+        nostalgic hard-on? Heriberto, as was customary in those crass
+        productions, would have been lampooned and offered a last-minute pity
+        fuck—the origins of a Weininger, not of a bard. Dalia began with a
+        story. In her freelance days, a certain vigorous youth from Providencia
+        (the type who enjoys his Neronian escapades into the famously marginal
+        localities, where he is greeted like the royal rake he feels himself to
+        be and repasts on the dubious choices aroused by romantic delusions of
+        ascent in our perilous social scale) contacted her. Since her clientele
+        lived off pills and handouts, she was delirious. A lifetime of
+        full-blooded fucks sprinkled with many-colored wads passed through her
+        eyes. Her circle had its legends, and Providencia figured prominently as
+        a place of miracles (the rich there were modelled after the sweet,
+        compassionate Luis Fernando de la Vega Montenegro). They met at his
+        apartment, and he stated the terms of their engagement: Mondays she
+        would arrive, make herself at home, enter his room, and fuck him while
+        he pretended to be asleep; Sundays she would pretend to be asleep. Odd
+        but all-around comfy. She then understood the euphemism. On Sundays, he
+        was brusque whenever she moved and quickly stopped her. She once opened
+        her eyes, and he lost his erection. Doubtless our Periander preferred
+        cold ovens. But for her own sake she defaulted to the more charitable
+        interpretation; she defaulted, that is, until the little Catholic
+        upbringing her wastrel parents managed to spit out between bouts of
+        turpentine inhalation possessed her body in primeval spasmodic chills
+        that evolved into a refined and abject guilt, which soon required
+        expiation through violence. Although I am a man of great poetic fancy, I
+        never shun the protocols of reason: the bleak turn that followed struck
+        me as mythmaking of an erotic cast. She apparently got hold of some
+        blood bags through a friend and bathed the would-be embalmer in the
+        liquid of his dreams. She went on and on and on. Was there a moral here?
+        Some sort of warning? I couldn’t tell. But I could tell the red lighting
+        was meant to match the plot. Heriberto would have resented the
+        intimations of any paraphilia. So on Tatiana I set hopes that
+        immediately caught fire. Tatiana, the incandescence of your nondescript
+        professionalism burned through the layers of impersonal method and
+        marked the insularity easily traced in the contours of my being.
+        <br />
+        <br />
+        Yet I am of Pericles’ strain and would convince whoever witnessed my
+        fall of a failure of perception. As occurred presently, I convinced
+        myself. Improvident despite austerity, desperate through lack of
+        revelations, I had to choose an adequate temper for Leobardo’s
+        imagination. Tatiana too bland, Dalia too off, Melanie too wry, Susana
+        too crass. Inés, she of the hazy mood, dislocate, was ideal, for her
+        tenuous existence, hard to delimit, would be lumped by Leobardo in the
+        deviate realities of introversion. Certainly, this spurious distinction,
+        which has served to shackle moody tempers to a caricature that
+        highlights only the worst in themselves, was a favorite of his.
+        <br />
+        <br />
+        I touched upon Inés and her reticence. Let it be known that I am
+        no imp who lives to saddle the wretched with additional
+        weights. No, that is not the course of my instinctual drives. Were
+        it so, doubt not I would follow it with great dignity and flair, and
+        thus I would have seduced Inés in order to thrust three outsiders
+        further to the margins just for the thrill. To be clear, I am
+        an imp who unsettles the stupor-scented, with qualification—I should
+        be unsettled as well, I should feel the moderate shock that compels
+        pause. The swashbuckling immoralist must crash. There is no place
+        for him; he belongs to the interstices. “All luxury ends in
+        slavery: even luxuriation in the purest love to the most
+        sacred being.” I would leave Inés intact and barely involved.
+        <br/>
+        <br/>
+        Three more visits and a rough-hewn reconstruction of the brotherly
+        feud (a simple use of epithets: cunt-crazy Leobardo against
+        spirit-loving Heriberto) exacted enough sympathy for Inés
+        to agree to a benign intervention, which would consist in nothing
+        more than standing on guard while Leobardo assailed her with
+        preposterously doltish questions (why are you fucking him, why
+        do you keep going, do you like it, don’t you feel sorry) and
+        perhaps some commandments (stop fucking my brother, stop
+        answering, stop coddling). Having put all my trust in this
+        infatuating jade, I enlightened Leobardo on the evancalous
+        nature of his brother’s deipnosophist and blessed him on his
+        charette. He did not seem as eager as before. I suppose that
+        of his kaleidoscopic impulses there was only left at this point
+        a combination of saving face and the need for revenge. A natural
+        development in one who suckled on the factual. Not quite
+        the climax I expected for my sleuthing, yet fitting: was it
+        not mostly a tour in which I honored chastity? 
+        <br/>
+        <br/>
+        Now, one’s genius is not easily fooled. Hickey, with constrained
+        heart, saw his father’s tears and heard his earnest words; he even
+        resolved to seek a path hostile to his dissolute inclinations. Do
+        you remember the amount of time he managed? Never more than
+        a week. Then you had him toasting with criminals, crashing
+        a boat in a drunken rapture, and mounting the lover of a supercilious
+        noble. And there was no alternative. The world is a cutthroat mass
+        of possibilities, but all are not open; <i>au contraire</i>, most
+        are adamantine bolted doors. It matters little. We are perfectly
+        aware of our desires, though reluctant. We can cross and
+        instantiate. The silly geese who stand awestruck before
+        the myriad lives that are lived intensely and have no
+        unruly urge to carve their own must be destined for
+        spectatorship. There are crowds upon crowds without soul-searing
+        impulses. Rejoice, then, if your genitalia have plans other
+        than the adjustments called for by your hesitating 
+        mind, which, listen not to the dualists, is no separate
+        substance. Now, one’s genius is persistent. And its enablers
+        are always there. Leobardo was unconvinced, aggrieved. He
+        spoke of masculine intuitions, but really. One’s genius is
+        also perceptive.  	  
+        <br/>
+        <br/>
+        A last postprandial visit, alas! I looked into the neocratic
+        regime and witnessed its subtle yet iron rule. From
+        the glistening, half-corroded central pole, where
+        a dissociated middle-aged blonde vied to outdo
+        the twenty-year-old money-machine that preceded her, down
+        to the cornered comessation of adolescents, where a mildly
+        bored thirty-year-old slammed her cheeks against the birthday
+        boy, I saw it all; for, except the wizened bartender, no
+        person kept aloof of the alluring petrichor that promised
+        discharge. Hyperactive, bloody-eyed traffickers weaseled
+        their way to their sole piss-glazed territories: there, adding
+        a layer of sumptuous accent, rolled in the capacious wallet
+        and its sycophantic cortege; here, smog-showered and
+        sooty-limbed, a beggar stumbled in his stupor against
+        the sweaty bouncer, too much a prey of inaccessible perceptions
+       to ask for alms: hundreds of bodies, kinetic and worn-out, came
+        tumbling in with bulging peduncles, and went tumbling out
+        again with doused languid flowers. Do you know whether this
+        incessant <i>immunditia</i>, <i>macula</i>, <i>turpitudo</i>, <i>ignominia</i> will
+        ever cease its thrust? From violence, onwards to violence! This
+        is humanity stripped to its mercenary core. Are they all
+        not potential foot soldiers prowling the streets in search
+        of the weary and crushed so as to debase them further and gain
+        some short-lived energy? Lipsdick, the very tabernacle that preserves
+        callousness in times of skittish peace. Here one sees a living link
+        to the brutalities which ever trail over our
+        march. Laughable, certainly, that generic chastisement of bygone
+        periods in which battalions pillaged everything in sight and as
+        recreation stuck a linstock through half a village’s ass, or in
+        which a common thug forced Palioly’s Pear into an unsuspecting
+        civilian’s mouth while having no recollection of its key ever
+        existing, or in which tyrants ordered their devoted servants
+        into brazen bulls simply to kick-start the anguished bellows. The
+        permutations of pain increase in sophistication, and we are 
+        unwilling and unable to go back. Ah, but you had me there! Regal
+        in my detachment, scanning the caliginous sublime, erect, meeting
+        filth with dispassion, I let the surplus approach. And
+        then, abrupt, Inés and the anagnorisis in her dazed smile. She
+        knew. The finagler duped. Done. Riot cried aloud. Just as there
+        abound quacks, skinners, wetworkers, burglars, embezzlers, backyard
+        breeders, double-crossers, impersonators, flim-flams, money
+        mules, mochileros, pornographers, poachers, pimps, so too
+        martyrs and saints bloom. Here, again, resounded the basso 
+        continuo of my life: the lost with the lost; the upright with
+        the rest. All zealotry of conversion as transient as our nocturnal
+        emissions.
+
       </StandardProse>
 
       <Footer />
