6119e56ba4c150daf339529a93924498f103ca9e
Steve Barbaro <steve.barbaro@gmail.com>
2024-07-28 15:57:49 -0500

    gets yet deeper into issue videosouls contribution

--- pages/fake/videosouls.js
+++ pages/fake/videosouls.js
@@ -26,7 +26,8 @@ export default function Video() {
         <br />
         <br />
         <span className={styles.rightMargin}>
-        <span className={styles.titleShadow}> i </span></span>
+          <span className={styles.titleShadow}> i </span>
+        </span>
         <br />
         <br />
         <br />
@@ -68,9 +69,10 @@ export default function Video() {
         <br />
         <br />
         <span className={styles.leftMargin}>
-        <span className={styles.titleShadow}>
-          <i> l </i>
-        </span></span>
+          <span className={styles.titleShadow}>
+            <i> l </i>
+          </span>
+        </span>
         <br />
         <br />
         <br />
@@ -115,7 +117,11 @@ export default function Video() {
 
       <StandardProse>
         <p className={styles.grayShadowUpper}>
-          &#123; INT. SANDWICH SHOP — AFTERNOON &#125;
+          <p className={styles.grayShadowUpperTwo}>i. </p>
+          <br />
+          <br />
+          &#123; INT. BROWN CARPETED LIVING ROOM BARE EXCEPT FOR TWO COLOR
+          TV/VCR COMBINATION UNITS MOUNTED ON CINDERBLOCKS - DAY &#125;
         </p>{" "}
         <br />
         <br />
@@ -123,157 +129,73 @@ export default function Video() {
         <br />
         <br />
         <br />
-        <br />
-        <br />
-        <br />
-        Cables are hanging down from the ceiling, sparking ever so often. Debris
-        and dust cover the shop surfaces and floors. On the wall there is a
-        poster of a man grinning ear to ear with his hands strangling a wrap,
-        from which a rainbow of foods is exploding. A dark sludge is splattered
-        across it, dripping onto the ground. WOMAN OF WONDER and QUIRKY
-        NEUROBIOLOGIST are in the middle of this scene, lounging in one of the
-        shop&rsquo;s booths.
-        <br />
-        <br />
-        <p className={styles.compactProse}>
-          <p className={styles.middleText}>WOMAN OF WONDER</p>
-          It is such a relief that we saved our home once again.
-          <br />
-          <br />
-          <p className={styles.middleText}>QUIRKY NEUROBIOLOGIST</p>
-          Indeed! I feared for a moment we wouldn’t—but now we can finally rest!
-        </p>
-        <br />
-        They are eating from two piles of gritty paste. One is light,
-        sand-colored. The other is an ominous dark. The sounds they make while
-        savoring the food are orgasmic.
+        back then waking up your son to walk him downstairs, to the vacant unit,
+        to watch the roosters duel was like a family thing you did. you
+        demonstrated how to spray cologne for him, tuck his shirt in, shine
+        boots, appear impervious to gore. to learn to think that he respects
+        above all else a face off. color value vs. color value of a scarf tied
+        to your lance or feather plucked by ripples of a lady through 240
+        horizontal scan lines. Sally vs. Jessy. 700 Club MTV. young, restless,
+        bold or beautiful. or MORTON DOWNEY JR&rsquo;s canine teeth and demon
+        tongue extracting marrow from his Habsburg-jawboned freak serpent host
+        body MAC TONIGHT. you can only have one of the above cultures. or you
+        can have another culture but this one comes preinstalled. the carpet
+        desert&rsquo;s double window teaches itself history this way. the mute
+        and hungry settlements outside appear to pulse, glimmer, human love
+        obscuring helicopter light. inside us still&rsquo;s a syndicate of
+        hometowns laid adjacent to the factory premises and work camps to
+        reeducate through labor all of this sweet recollection linked by rumored
+        tunnel: one smug groove underneath the skin, too hoary, underloved and
+        powered down to abdicate. please don&rsquo;t disturb this groove. let it
+        fill your abscesses as fog cascades from the machine in all the discos
+        of the dead. that&rsquo;s tomorrow and that is it for us
         <br />
         <br />
         <p className={styles.compactProse}>
-          <p className={styles.middleText}>QUIRKY NEUROBIOLOGIST</p>
-          <i>Wow</i>! I never knew hummus could be this chocolate. You’ll have
-          to give me your recipe—whenever <i>I</i> get in the kitchen, I always
-          manage to <i>burn everything</i>.
-        </p>
-        <br />
-        <i>Womp womp wommmmp</i>.<br />
-        The audience laughs.
-        <br />
-        <br />
-        <p className={styles.compactProse}>
-          <p className={styles.middleText}>WOMAN OF WONDER</p>
-          It is a recipe that has been passed down in my family since the very
-          first human being made hummus.
-          <br />
-          <br />
-          <p className={styles.middleText}>QUIRKY NEUROBIOLOGIST</p>
-          Hey, I thought you were a <i>Themyscirian</i> not a <i>human</i>!
-        </p>
-        <br />
-        The audience laughs.
-        <br />
-        <br />
-        <p className={styles.compactProse}>
-          <p className={styles.middleText}>WOMAN OF WONDER</p>
-          Oh my, you are so silly.
-        </p>
-        <br />
-        She tries to laugh, but her face doesn’t make that expression easily.
-        Instead, she looks like she’s crying.
-        <br />
-        <br />
-        <p className={styles.compactProse}>
-          <p className={styles.middleText}>WOMAN OF WONDER</p>
-          I owe you at least a recipe. We were lucky today, that the enemy
-          didn’t have the means to fight you and your powers.
-          <br />
-          <br />
-          <br />
-          <p className={styles.middleText}>QUIRKY NEUROBIOLOGIST</p>
-          It doesn’t take a <i>neurobiologist</i> to know that they needed food
-          to survive.
-        </p>
-        <br />
-        The audience laughs.
-        <br />
-        <br />
-        <p className={styles.compactProse}>
-          <p className={styles.middleText}>QUIRKY NEUROBIOLOGIST</p>
-          Anyhoo, don’t sell yourself short—I’m sure we could have got ‘em with
-          just your artillery.
-          <br />
-          <br />
-          <br />
-          <p className={styles.middleText}>WOMAN OF WONDER</p>
-          <p className={styles.middleTextTwo}>
-            (adjusting her stars and stripes top)
+          <p className={styles.middleText}>
+            <i>
+              0:03
+              <br />
+              today and we will leave you with a I<br />
+              0:06
+              <br />
+              can&rsquo;t do it we&rsquo;ll do it live okay we&rsquo;ll
+              <br />
+              0:12
+              <br />
+              do it live fuck it we&rsquo;ll do it live I&rsquo;ll dial it
+              <br />
+              0:15
+              <br />
+              in and we&rsquo;ll do it live fuckin&rsquo; thing
+              <br />
+              0:19
+              <br />
+              sucks [five, four, three] that&rsquo;s tomorrow and that
+              <br />
+              0:26
+              <br />
+              is it for us today I&rsquo;m Bill O&rsquo;Reilly
+              <br />
+              0:28
+              <br />
+              thanks again for watching we&rsquo;ll leave
+              <br />
+              0:30
+              <br />
+              you with Sting and a cut off his new
+              <br />
+              0:32
+              <br />
+              album take it away
+              <br />
+              0:36
+            </i>
+            <br />
+            <br />
+            OSCILLATION, FATAL GRINDING OF MACHINERY, ORCHESTRAL STABS
+            <br />
           </p>
-          I had some help. And hey...I was thinking...
-          <br />
-          <br />
-          <br />
-          <p className={styles.middleText}>QUIRKY NEUROBIOLOGIST</p>
-          <p className={styles.middleTextTwo}>
-            (scooting closer to the WOMAN OF WONDER)
-          </p>
-          Yeah?
-        </p>
-        <br />
-        Without saying anything else, WOMAN OF WONDER surges forward to press
-        her mouth against the mouth of QUIRKY NEUROBIOLOGIST. For several
-        moments, they drily rub their mouths against each other. The audience
-        oooohs and aaaaahs. They pull away from each other, completely placid.
-        <br />
-        <br />
-        <p className={styles.compactProse}>
-          <p className={styles.middleText}>QUIRKY NEUROBIOLOGIST</p>
-          Wuh-wuh-wowza! What took you so long?
-        </p>
-        <br />
-        The audience laughs.
-        <br />
-        <br />
-        <p className={styles.compactProse}>
-          <p className={styles.middleText}>WOMAN OF WONDER</p>
-          <p className={styles.middleTextTwo}>(smiling)</p>
-          We had a war to win and a city to save.
-        </p>
-        <br />
-        The camera pans outside the shop.
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <p className={styles.grayShadowUpperTwo}>
-          Everything is gray. It takes a moment for the eye to focus on any one
-          thing. The first thing is the hollowed-out buildings and the piles of
-          rubble around them. Each former apartment, former bedroom, former
-          kitchen, former closet, former bathroom, former doctor&#39;s office,
-          former ICU, former NICU, former emergency room, former classroom,
-          former church, former mosque, former synagogue, former museum, former
-          shop, former restaurant, former records room, former studio, former
-          storeroom, former shelter is screaming out around whatever remains of
-          the building&#39;s bones. The shop the WOMAN OF WONDER and the QUIRKY
-          NEUROBIOLOGIST are in is the building which has received the least
-          damage.
-          <br />
-          <br />
-          Once the eye has adjusted to this, it next notices that the piles of
-          rubble have some moving parts. People. People are shifting through the
-          rubble, digging for each other, scrabbling through the rock to find
-          whatever may remain of their loved ones. They are filling plastic bags
-          with body parts. They are carrying bodies, with no place to put them
-          down, to bury them. They are stumbling. They are coughing. Their
-          cheeks are caving in. The eye closes, looks elsewhere. This land. This
-          land is ruined, no, has been ruined. When the WOMAN OF WONDER and the
-          QUIRKY NEUROBIOLOGIST staked out this land in ACT ONE, it was vibrant.
-          Now, like everything else, it is gray. The eye shuts. The eye opens.
-          The eye shuts. The eye opens.
         </p>
         <br />
         <br />
@@ -297,6 +219,236 @@ export default function Video() {
         <br />
         <br />
         <br />
+        <br />
+        <p className={styles.grayShadowUpper}>
+          <p className={styles.grayShadowUpperTwo}>ii. </p>
+          <br />
+          <br />
+          &#123; INT. A DOWNTOWN SHOP DURING BUSINESS HOURS, EMPTY OF CUSTOMERS
+          - DAY &#125;
+        </p>{" "}
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        it isn&rsquo;t clear what type of merchandise this store specializes in:
+        there&rsquo;s simply no cohesion to the products offered up for sale.
+        austerely displayed are wares like ration buckets, Giorgio perfume,
+        fishing rods, massagers, video rewinders, a 2. Casio SK-1 sampling
+        keyboard, one of each item set out on acrylic shelves latched to aisle
+        walls, as in a catalog showroom. you&rsquo;re eyeballed then ignored by
+        the proprietor, CONSTANTIN THE WET, fifty-five, with stripes of hair
+        glued by sweat sideways across. CONSTANTIN monitors a many-columned wall
+        of televisions stacked three high and here the chatter of satellite and
+        cable political news feeds is recorded onto videotapes. he pitches the
+        cassettes, which are never labeled, in the shambolic cabin of his Bronco
+        at the close of each day.
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <p className={styles.grayShadowUpper}>
+          <p className={styles.grayShadowUpperTwo}>iii. </p>
+          <br />
+          <br />
+          &#123; INT. ENTERTAINMENT WEEKLY #001 MAGAZINE DATED FEBRUARY 16, 1990
+          - DAY &#125;
+        </p>{" "}
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        on the cover: K.D. Lang photographed by Lena Bertucci and Neneh Cherry
+        photographed by Andrew Catlin/LFI. You will find in the debut issue of
+        EW a preview of the midseason replacement tv lineup and in it
+        there&rsquo;s a photo of the cast of Nasty Boys, a new police drama. the
+        nasty boys are Las Vegas narcotics officers. their black police
+        windbreakers, black police ballcaps and defiant mugging actor faces
+        could not have been born outside the echo of NWA&rsquo;s
+        &lsquo;&lsquo;Fuck the Police.&rsquo;&rsquo; also in this issue: a
+        review of <i>Dream Song: The Life of John Berryman</i>. the review and
+        the portrait of Berryman span almost the entire two pages of the
+        centerfold and you could place your bookmark here, at this end time
+        refusal to settle a perplexity no money man can win. my love.
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <p className={styles.grayShadowUpper}>
+          <p className={styles.grayShadowUpperTwo}>iv. </p>
+          <br />
+          <br />
+          &#123; EXT. THE BURN BARREL, YOU KNOW, THE BURN BARREL, OUT BACK. YOU
+          DON&rsquo;T HAVE ONE? WHAT ABOUT A 55 GALLON OIL DRUM OR A HUMAN SIZED
+          TRUCK TOOLBOX WHICH YOU&rsquo;RE NOT TO OPEN UNDER ANY CIRCUMSTANCES?
+          NOT THAT YOU COULD BRING YOURSELF TO - NIGHT &#125;
+        </p>{" "}
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        fire streams up into new wood from the dying. scent it leaves is sugar.
+        see? your life&rsquo;s alright you&rsquo;re carrying a cake box through
+        the intersection. your life&rsquo;s alright you&rsquo;re making heart
+        hands at an electronic music festival contrived by private equity dweebs
+        on a twopenny-halfpenny-grade space colony. the maker of the
+        spaceship&rsquo;s trying to poison us before we can get sucked through
+        loose-screwed panels or before we can grow up and go to work for them
+        and maybe blow a whistle on em. much hair being held for ease of
+        vomiting: a flash onset of gastrointestinal complaint,{" "}
+        <i>shigellosis</i> I heard it was, over the loudspeaker, spread either
+        through tainted food or sexual contact. <i>heart hands</i>. I find the
+        rituals grotesque but I&rsquo;m a child in these airless corridors,
+        these barricaded roads. i don&rsquo;t know where my next meal will come
+        from, and all that, but I know that I must pay for it by means of a
+        <i>leveraged buyout</i>. what are all the fundamental truths again and
+        do you still believe in them because these are the names of my case
+        workers at the juvenile court-contracted call center that sentenced me,
+        then shuttled me to this: Edgar Chayce, Cashier and Manateigh.
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <p className={styles.grayShadowUpper}>
+          <p className={styles.grayShadowUpperTwo}>v. </p>
+          <br />
+          <br />
+          &#123; EXT. A CORNER STOREFRONT WHERE THEY SELL TOBACCO AND A MAGAZINE
+          FOR EVERY INTEREST, UNDERWATER - DAY &#125;
+        </p>{" "}
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        god made me thus: a love so animal it takes my wind out, has to be
+        expelled. useless and invisible, my love burns up over the upper
+        atmosphere. disappearing cuneiform hallucinated in the wake of a creator
+        gone and into other schemes. love underemployed. love laundering money
+        in a summer camp for evil despots&rsquo; kids. 6 AM&rsquo;s a heart
+        hands drill. 7 AM counterprotest chanting at police to fire on the
+        flip-flop-costumed drone. my love she’s like some blue orb glowing up to
+        end our shit from underneath the sea, a light in whom you&rsquo;ll soon
+        believe.
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <p className={styles.grayShadowUpper}>
+          <p className={styles.grayShadowUpperTwo}>vi. </p>
+          <br />
+          <br />
+          &#123; EXT. CLOUDS COME SMOTHER - MORNING &#125;
+        </p>{" "}
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        last thing you remember you were programming a beat last night, or
+        started one when you were high, you fell asleep doing that, at your
+        desk, and when you came to in the cold slobber of day it was there,
+        formed fully, an entrancing four minutes of song and you thought how
+        funny would it be to upload this <i>free content</i> to Spotify and
+        release it under the artist name Michael Jackson Jr. so you search it.
+        someone did that already.
+        <br />
+        <br />
+        you had the idea that the best way to get an idea of who Richard Lewis
+        was would be to watch other comics impersonations of him. because you
+        couldn&rsquo;t really remember. what&rsquo;s the point of being a man
+        inside a tucked in shirt and hair combed anyway. what&rsquo;s the point
+        Jerry Seinfeld.
+        <br />
+        <br />
+        the scientific method was a ploy to dispossess the land mapping
+        continuous infinite values to a diminished set of discrete finite
+        values. now, on the Concerning Mortality Rate of Influencers&rsquo;
+        Children...
       </StandardProse>
 
       <Footer />
