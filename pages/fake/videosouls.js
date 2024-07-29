035d5c1b764deb862c586b4a58af12b57b7cd959
Steve Barbaro <steve.barbaro@gmail.com>
2024-07-28 20:10:58 -0500

    gets yet further into issue8 videosouls entry

--- pages/fake/videosouls.js
+++ pages/fake/videosouls.js
@@ -449,7 +449,343 @@ export default function Video() {
         continuous infinite values to a diminished set of discrete finite
         values. now, on the Concerning Mortality Rate of Influencers&rsquo;
         Children...
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <p className={styles.grayShadowUpper}>
+          <p className={styles.grayShadowUpperTwo}>vii. </p>
+          <br />
+          <br />
+          &#123; EXT. PATENT LEATHER PANTS FLAGGING FROM A TELEPHONE WIRE, CITY
+          RESIDENTIAL BLOCK - NIGHT &#125;
+        </p>{" "}
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <p className={styles.compactProseTwo}>
+          <p className={styles.middleText}>
+            LAUNDROMAT MORGANA
+            <p className={styles.middleTextTwo}>
+              (pulling up to CRISPIN-TODD THE KID, who is passionately typing on
+              his laptop under the street light)
+            </p>
+          </p>
+          CRISPIN! we need to get in the car!
+          <br />
+          DAD needs a ride home!
+          <br />
+          <br />
+          <p className={styles.middleText}>CRISPIN-TODD</p>
+          do we have to leave right now?
+          <br />
+          <br />
+          <p className={styles.middleText}>MORGANA</p>
+          if we expect to find him we do. what, you have something better to do?
+          <br />
+          <br />
+          <p className={styles.middleText}>
+            CRISPIN-TODD
+            <p className={styles.middleTextTwo}>(gestures at the computer)</p>
+          </p>
+          I don&rsquo;t know.
+          <br />
+          <br />
+          <p className={styles.middleText}>MORGANA</p>
+          what are you typing?
+          <br />
+          <br />
+          <p className={styles.middleText}>
+            CRISPIN-TODD
+            <p className={styles.middleTextTwo}>
+              (did not anticipate explaining, at a loss)
+            </p>
+          </p>
+          I&rsquo;m... making a... ranking of the albums the Commodores put out
+          after Lionel Richie left.
+          <br />
+          <br />
+          <p className={styles.middleText}>MORGANA</p>
+          when he went solo?
+          <br />
+          <br />
+          <p className={styles.middleText}>CRISPIN-TODD</p>
+          yeah.
+          <br />
+          <br />
+          <p className={styles.middleText}>MORGANA</p>
+          and they put out music after &lsquo;&lsquo;Night Shift?&rsquo;&rsquo;
+          <br />
+          <br />
+          <p className={styles.middleText}>CRISPIN-TODD</p>
+          a decent amount. but they had all these personnel changes. to some of
+          their fans they weren&rsquo;t the Commodores any more. I think their
+          fans wanted more &lsquo;&lsquo;Night Shifts.&rsquo;&rsquo; where is
+          DAD?
+          <br />
+          <br />
+          <p className={styles.middleText}>MORGANA</p>
+          he&rsquo;s stuck at the Underworld.
+          <br />
+          <br />
+          <p className={styles.middleText}>CRISPIN-TODD</p>
+          the barbecue place?
+          <br />
+          <br />
+          <p className={styles.middleText}>MORGANA</p>
+          no. it&rsquo;s not that anymore. it&rsquo;s a rage room. with the flag
+          that has a tree on it that says AN APPEAL TO HEAVEN.
+          <br />
+          <br />
+          <p className={styles.middleText}>CRISPIN-TODD</p>
+          <p className={styles.middleTextTwo}>(nods)</p>
+          <br />
+          <br />
+          <p className={styles.middleText}>MORGANA</p>
+          I just remember the smiling. they seemed so happy in that video to be
+          making music with each other.
+          <br />
+          <br />
+          <p className={styles.middleText}>CRISPIN-TODD</p>
+          the camaraderie&rsquo;s infectious, but Ronald LaPread who had played
+          the bass since they started the band would leave them right after
+          this. my favorite part of the video is when they&rsquo;re sitting
+          around the table putting on their make up. I like it because
+          it&rsquo;s not a joke. it&rsquo;s just a real part of their lives, as
+          performers, like the way a mime troupe would have to suit up or
+          whatever. they&rsquo;re totally at home with one another. I think that
+          &lsquo;&lsquo;Night Shift&rsquo;&rsquo; becoming a hit must have been
+          such a surprise and a fuck you to Lionel Richie and it just must have
+          made it seem like anything was possible for them. to be written off
+          and come back. to have a dream and it comes true. and then you lose it
+          but you dream
+          <p className={styles.middleText}>
+            MORGANA
+            <p className={styles.middleTextTwo}>
+              (shaking head and smiling, bittersweetly)
+            </p>
+          </p>
+          when I was small I dreamed of having my own phone and my own money and
+          ordering up a giant hero sandwich all for myself that I don&rsquo;t have to
+          share with no one and now that I&rsquo;m old, and I&rsquo;m a mother, I mean, I
+          could do that every day if I wanted to but shoot I&rsquo;ve maybe done it
+          once. hey. since when do you say fuck you in front of your mother?
+          <br />
+          <br />
+          <p className={styles.middleText}>
+            CRISPIN-TODD
+            <p className={styles.middleTextTwo}>
+              (ignoring MORGANA, talking to himself)
+            </p>
+          </p>
+          they get so nasty about Lionel Richie in the Commodores forum...
+        </p>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <p className={styles.grayShadowUpper}>
+          <p className={styles.grayShadowUpperTwo}>viii. </p>
+          <br />
+          <br />
+          &#123; EXT. CITY GARBAGE DUMP - NIGHT. &#125;
+        </p>{" "}
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        two cops are called to reciprocate an old favor, to retrieve a pair of
+        victims from a city trash dump before the light of day. the men
+        responsible for the deaths of these bodies are sergeants from an army
+        base near town. the sergeants were advised by a friendly contact on the
+        police force to bring the bodies to a particular waste processing
+        facility, but in their haste the sergeants misunderstood this
+        instruction and utilized the public dump. the policemen were informed
+        that their cargo would be easy to find but on arriving they don&rsquo;t know
+        what to look for: everywhere, bodies are underfoot. it&rsquo;s now nearly 5
+        AM, the sun will soon be up, workers will arrive. the missing bodies
+        belong to a boy and his mother, whom the pair of sergeants mistook for
+        drug couriers they had meant to rob.
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <p className={styles.grayShadowUpper}>
+          <p className={styles.grayShadowUpperTwo}>ix. </p>
+          <br />
+          <br />
+          &#123; EXT. A NEON SIGN DISPLAYS TWO CURSIVE WORDS IN ALTERNATING RED
+          AND GREEN FLASHES: &lsquo;&lsquo;SEQUELISM,&rsquo;&rsquo; &lsquo;&lsquo;FLESHLESSNESS&rsquo;&rsquo; - BEFORE DAWN &#125;
+        </p>{" "}
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        the caretaker who keeps
+        <br />
+        a bedroom here yet
+        <br />
+        doesn’t owe you her genuine life—
+        <br />
+        that’s your baby son.
+        <br />
+        you die & he delivers to your widow
+        <br />
+        a sympathy card autographed but otherwise unread
+        <br />
+        by him. alone, she lets diffuse
+        <br />
+        the fist that balls up
+        <br />
+        in her spine.
+        <br />
+        & peels out the sentiment in cursive from
+        <br />
+        its envelope:{" "}
+        <i>
+          a negative mind will never give you a positive
+          <br />
+          life
+        </i>
+        , it says.
+        <br />
+        shake your aspergillum at her cackling,
+        <br />
+        the fugitive rite.
+        <br />
+        sprinkle on some unearned grace.
+        <br />
+        streaming through the streets & kissing soldiers
+        <br />
+        on a day
+        <br />
+        we haven’t won.
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <p className={styles.grayShadowUpper}>
+          <p className={styles.grayShadowUpperTwo}>x. </p>
+          <br />
+          <br />
+          &#123; EXT. CITY PLAZA - DUSK &#125;
+        </p>{" "}
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        what comes up through our eyes is massaged memory, a sunset uncorrupted
+        in the coelacanth night, fountains agitated by the lazerswords of little
+        motherfuckers on the plaza of the court. nothing impure along the signal
+        chain of that wax lip colored dusk.
       </StandardProse>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
 
       <Footer />
     </>
