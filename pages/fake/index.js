f1a9d3c19a882a64425ff981babd33c657ab093b
Steve Barbaro <steve.barbaro@gmail.com>
2024-07-31 17:39:26 -0500

    gets yet further into issue8 cover

--- pages/fake/index.js
+++ pages/fake/index.js
@@ -239,8 +239,6 @@ export default function Issue8() {
                 <br />
                 <br />
                 <br />
-                <br />
-                <br />
                 <br />b<br />i<br />o<br />s
               </div>
             </Link>
