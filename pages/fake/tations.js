ef8f8db0137a7ca21463e2eead3bdac117583093
Steve Barbaro <steve.barbaro@gmail.com>
2025-07-19 18:33:02 -0500

    gets yet deeper into issue9 wong contribution

--- pages/fake/tations.js
+++ pages/fake/tations.js
@@ -6,8 +6,10 @@ import StandardProse from "../../components/standardProse.js";
 import Footer from "../../components/footer.js";
 import styles from "../../styles/Tations.module.css";
 Tations.title = "mandy-suzanne_wong ~ fragmen / lamen - tations — new_sinews";
-Tations.pieceName = "mandy-suzanne_wong ~ fragmen / lamen - tations — new_sinews";
-Tations.description = "_Mandy-Suzanne Wong_essay_fragmen / lamen - tations_news_sinews_issue9_";
+Tations.pieceName =
+  "mandy-suzanne_wong ~ fragmen / lamen - tations — new_sinews";
+Tations.description =
+  "_Mandy-Suzanne Wong_essay_fragmen / lamen - tations_news_sinews_issue9_";
 Tations.image = "https://www.newnewsinews.com/images/ActaeonBackground.jpg";
 
 export default function Tations() {
@@ -18,44 +20,58 @@ export default function Tations() {
       <Header headerBackground="actaeonBackground" />
       <LeftGrayTitle>
         <span className={styles.titleShadow}>
-            <span className={styles.inverseShadowUppercase}>f </span>
-             <span className={styles.titleShadowUppercase}><i>r</i></span>
-          <span className={styles.alternateTitleColor}>a</span>
+          <span className={styles.inverseShadowUppercase}>f </span>
+          <span className={styles.titleShadowUppercase}>
+            <i>r </i>
+          </span>
+          <span className={styles.alternateTitleColor}>a </span>
           <span className={styles.inverseShadow}>g </span>
-           <span className={styles.alternateTitleColor}>m </span>
+          <span className={styles.alternateTitleColor}>m </span>
           <span className={styles.skewRight}>
             <span className={styles.inverseShadowUppercase}>e </span>
           </span>
           <span className={styles.alternateTitleColorUppercase}>n</span>
-           <br/>
+          <br />
           <span className={styles.skewRight}>
             <span className={styles.titleShadowUppercase}>l </span>
           </span>
           <span className={styles.inverseShadow}>a </span>
-         <span className={styles.titleShadowUppercase}><i>m </i></span>
-            <span className={styles.alternateTitleColor}>e</span>
-            <span className={styles.inverseShadowUppercase}>n</span>
-            <span className={styles.hyphenBlack}> -</span>
-            <span className={styles.titleShadowUppercase}>t</span>
-             <span className={styles.inverseShadowUppercase}>a </span>
+          <span className={styles.titleShadowUppercase}>
+            <i>m </i>
+          </span>
+          <span className={styles.alternateTitleColor}>e</span>
+          <span className={styles.inverseShadowUppercase}>n</span>
+          <span className={styles.hyphenBlack}> -</span>
+          <span className={styles.titleShadowUppercase}>t</span>
+          <span className={styles.inverseShadowUppercase}>a </span>
           <span className={styles.alternateTitleColor}>t </span>
           <span className={styles.inverseShadow}>i </span>
-           <span className={styles.alternateTitleColor}>o</span>
-            <span className={styles.inverseShadowUppercase}>n </span>
-             <span className={styles.skewRight}>
+          <span className={styles.alternateTitleColor}>o</span>
+          <span className={styles.inverseShadowUppercase}>n </span>
+          <span className={styles.skewRight}>
             <span className={styles.titleShadowUppercase}>s</span>
           </span>
         </span>
       </LeftGrayTitle>
-     <RightBlackTitle>
+      <br />
+      <br />
+      <br />
+      <br />
+      <RightBlackTitle>
         <span className={styles.nameSpacing}>
           ... ... m<span className={styles.uppercase}>an</span>d
           <span className={styles.uppercase}> y</span>-s
           <span className={styles.uppercase}>u</span>za
-           <span className={styles.uppercase}>nn</span>e
-          ... ... w<span className={styles.uppercase}> o </span>n
+          <span className={styles.uppercase}>nn</span>e ... ... w
+          <span className={styles.uppercase}> o </span>n
           <span className={styles.uppercase}> g</span> ... ...
         </span>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
       </RightBlackTitle>
       <br />
       <br />
@@ -68,48 +84,121 @@ export default function Tations() {
           <div className={styles.row}>
             <div className={styles.column}>
               <div className={styles.columnOne}>
+                <span className={styles.titleShadow}>
+                  <span className={styles.inverseShadow}>p </span>
+                  <span className={styles.titleShadowUppercase}>
+                    <i>e </i>
+                  </span>
+                  <span className={styles.alternateTitleColor}>a </span>
+                  <span className={styles.inverseShadowUppercase}>r </span>
+                  <span className={styles.alternateTitleColor}>l </span>
+                </span>
+                <br />
+                <br />
+                <br />
+                <br />
                 God in heaven, he’ll strangle me, no, he’ll lecture me, that’s
                 worse: How can I trust you with anything important if you can’t
                 even . . . I’ll blame the maid. Can’t blame the maid, he knows
-                she doesn’t touch my jewels. There goes one under the—catch it,
-                you stupid girl! Why he told her not to touch. Another one by
-                the curtain, the other curtain, see it? First good gift he ever
+                she doesn’t touch my jewels. <i>There goes one under the</i>—
+                <i>catch it</i>,<i> you stupid girl</i>! Why he told her not to
+                touch. <i>Another one by the curtain</i>,{" "}
+                <i>the other curtain</i>, <i>see it</i>? First good gift he ever
                 gave me, first lecture too, his purity of affection, because,
                 you see, pearls symbolize purity and affection. And heaven.
                 Wealth and prestige of the nation. And maidenhood, God help us.
                 The timelessness of pearls, he said. No, that was at our tenth
-                anniversary when I wore . . . Hang it, there’s one right here
-                under my slipper. Ten years before that, he already knew, he
-                said. That’s why he gave me pearls. Elegance both modest and
-                exotic, he said. And if not the fashion, pearls are always in
-                fashion. That was on our twentieth, and I think it was a quote.
-                For heaven’s sake, how should I know how many? And they’re all
-                matched to perfection, he said, all exactly alike, and how’s
-                that for a miracle of
+                anniversary when I wore . . . <i>Hang it</i>, <i>there</i>’
+                <i>s one right here under my slipper</i>. Ten years before that,
+                he already knew, he said. That’s why he gave me pearls. Elegance
+                both modest and exotic, he said. And if not the fashion, pearls
+                are always in fashion. That was on our twentieth, and I think it
+                was a quote.<i> For heaven</i>’<i>s sake</i>,{" "}
+                <i>how should I know how many</i>? And they’re all matched to
+                perfection, he said, all exactly alike, and how’s that for a
+                miracle of pure, wild nature. <i>Oh</i>, <i>Tilly</i>,{" "}
+                <i>you know that bit about </i>“<i>let no man put asunder</i>”?{" "}
+                <i>That</i>’<i>s what he said when he gave</i> . . .
+                <i>and now look what I </i>. . . Oh, I’m terrible, horrible, how
+                did I ever get so—oh, it doesn’t even look like a neck anymore,
+                it looks like a, like a blob about to explode! I’ve let him
+                down, it’s unforgivable, he hasn’t said a word. But he must’ve
+                liked my neck, all those years ago, to give it such exquisite .
+                . . Tilly’s laughing at me, I know it, she’ll tell everybody how
+                the mistress got out her pearl necklace, went to fasten it
+                around her fat blob of a neck, and then it popped and then the
+                pearls flying rolling scattering every which way, his first good
+                gift to her in fragments. Shattered! He’ll find some kind of
+                metaphor for fragments. Some analogy with shattered, scattered
+                pearls, and that look of his, oh I can’t bear it! <i>Tilly</i>,{" "}
+                <i>I know what to do</i>! <i>Remember we heard about those</i>,{" "}
+                <i>from China or</i>,<i>no</i>, <i>Japan</i>.
+                <i> Some magazine</i>, <i>that man who said he</i>’
+                <i>d put a pearl necklace on every neck</i>,{" "}
+                <i>guess they can train oysters like cows or</i>—
+                <i>cultured pearls</i>, <i>that</i>’<i>s it</i>.{" "}
+                <i>Even my piddly allowance</i> . . . <i>and he</i>’
+                <i>ll never know the difference</i>, <i>right</i>? <i>They</i>’
+                <i>re pearls like any other</i>, <i>right</i>?{" "}
+                <i>But when you need them</i>, <i>they show up on time</i>.{" "}
+                <i>Quick</i>, <i>go order a cab</i>!
               </div>
             </div>
             <div className={styles.column}>
               <div className={styles.columnTwo}>
                 1853. The United States forced the Tokugawa shogunate in Japan
                 to accept unfair trade terms by send­ ing warships to threaten
-                Japan with cannon fire. 1893. Mikimoto Kōkichi, under the guid-
-                ance of marine biologists, forced an oyster to produce a pearl
-                by surgically inserting an irritant (nucleus) into its soft
-                tissues. 1899. Mikimoto’s first boutique opened in Tokyo,
-                introducing customers to his “invention,” cultured pearls. 1907.
-                “The favorites of fortune the world over in all ages have
+                Japan with cannon fire.
+                <br />
+                <br />
+                1893. Mikimoto Kōkichi, under the guid- ance of marine
+                biologists, forced an oyster to produce a pearl by surgically
+                inserting an irritant (nucleus) into its soft tissues.
+                <br />
+                <br />
+                1899. Mikimoto’s first boutique opened in Tokyo, introducing
+                customers to his “invention,” cultured pearls.
+                <br />
+                <br />
+                1907. “The favorites of fortune the world over in all ages have
                 succumbed to the modest beauty of the pearl. Its ascendancy
                 marks not alone the refinement of the individuals with whom it
                 finds favor, but the high status of the nation where it is
-                widely appreciated.” (Cattelle, The Pearl) 1911. “Perhaps it
-                would interest you to know more about this wonderful and
-                precious jewel which could not only reflect the beauties of
-                other gems, but also put power into the hand of the feeble wise
-                man to save a poor girl from a life of misery.” (Mikimoto,
-                Japanese Culture Pearl s) c. 1916. In the US Midwest and South,
-                button manufacture from freshwater-mussel nacre, a
-                multimillion-­ dollar industry, began its rapid decline, having
-                decimated its mussel species of choice. 
+                widely appreciated.” (Cattelle, <i>The Pearl</i>)
+                <br />
+                <br />
+                1911. “Perhaps it would interest you to know more about this
+                wonderful and precious jewel which could not only reflect the
+                beauties of other gems, but also put power into the hand of the
+                feeble wise man to save a poor girl from a life of misery.”
+                (Mikimoto, <i>Japanese Culture Pearls</i>)
+                <br />
+                <br />
+                c. 1916. In the US Midwest and South, button manufacture from
+                freshwater-mussel nacre, a multimillion-­ dollar industry, began
+                its rapid decline, having decimated its mussel species of
+                choice.
+                <br />
+                <br />
+                1927. On a visit to US factories, Mikimoto found that mussels
+                considered inferior for button production made ideal nuclei for
+                cultured pearls. He shipped the mussels to Japan, ground their
+                shells into fragments, and implanted the fragments in Akoya
+                oysters.
+                <br />
+                <br />
+                1930s. “Even as Mikimoto’s cultivation operations relocated
+                pearl oysters to cages and his female workforce out of the water
+                and into shellfish surgical wards, Pearl Island put shellfish
+                ama divers on display in the flesh. It was a new captive
+                environment not [only] for animals but [also] for people.”
+                (Ericson, “Nature’s Helper”)
+                <br />
+                <br />
+                1955. “Furthermore, American women were wearing more of his
+                necklaces than any other women in the world.” (Eunson,{" "}
+                <i>The Pearl King</i>: <i>The Story of the Fabulous Mikimoto</i>
+                )
               </div>
             </div>
           </div>
