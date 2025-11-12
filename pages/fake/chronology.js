f3ed38327cea7530c44260b69aa105f00909eeaf
Steve Barbaro <steve.barbaro@gmail.com>
2025-11-11 19:34:12 -0600

    gets yet deeper into issue9 chronology entry

--- pages/fake/chronology.js
+++ pages/fake/chronology.js
@@ -192,6 +192,381 @@ export default function Chronology() {
         encountering the occasional escaped zoo animal. At night when the
         mutants come out, he barricades his windows and lies curled in his
         bathtub clutching his rifle.
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <span className={styles.specialCursiveThree}>2019</span>By now Los
+        Angeles has become a derelict, neon-soaked metropolis. Vehicles are
+        airborne. People still read magazines. Those who can afford to have
+        abandoned Earth for far-flung colonies. A private corporation has
+        manufactured a fleet of androids to service their human overlords. They
+        have a battery life of four years to limit their ambitions and keep them
+        meek. But a faction of these robots has gone rogue. <i>Father</i>,{" "}
+        <i>fucker</i>, <i>we want more life</i>.
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <span className={styles.specialCursiveThree}>2021</span>The most secure
+        way to transfer large amounts of sensitive data is by downloading the
+        information directly into the brains of so-called mnemonic couriers, who
+        make room for these gigabytes by removing large chunks of their personal
+        memories. Much of the world’s population now suffers from a neurological
+        condition known as NAS, or Nerve Attenuation Syndrome. Also known as the
+        “Black Shakes,” the disease is caused by overexposure to technology; the
+        people who have it seize uncontrollably. Newspapers still exist.
+        Mega-corporations rule society, protected by hired Yakuza. But there is
+        a resistance movement, too, which joins forces with a data courier to
+        thwart Big Pharma executives’ plan to withhold a cure for the plague so
+        they might profit from a costly treatment program.
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <span className={styles.specialCursiveThree}>2024</span>Warming currents
+        in the Atlantic Ocean have produced a new Ice Age that spreads rapidly
+        across the Northern Hemisphere. Superstorms overwhelm Canada and Europe.
+        Tornados break over Los Angeles and hailstorms pummel Tokyo. Manhattan
+        is knee-deep in floodwaters. Would-be climate refugees from the United
+        States must cross frozen tundras to reach Mexico, a journey punctuated
+        by perilous run-ins with animals escaped from local zoos.
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <span className={styles.specialCursiveThree}>2027</span> An inexplicable
+        fertility crisis has befallen the world. Humanity is dying out: Baby
+        Diego, the planet’s youngest person at age 18, is stabbed outside a bar
+        for refusing to sign an autograph. Economies have collapsed. The UK has
+        become a totalitarian police state; vast territories are under military
+        occupation and migrants are targeted for mass deportation. Bomb attacks
+        in urban centers have become so common that it’s all you can do to hold
+        onto your morning coffee as you make your way to the office.
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <span className={styles.specialCursiveThree}>2029</span> A singularity
+        created by a private corporation that was once the world’s biggest
+        supplier of military computer systems has developed a mind of its own
+        and concluded that human beings are a threat to its survival. This
+        neural-networked entity manufactures a nuclear war between the US and
+        Russia to exterminate most of humanity; those who survived are hunted by
+        a squadron of killer robots. But time travel is possible, and the hope
+        is that an intervention into the past can reverse the horrors of the
+        present.
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <span className={styles.specialCursiveThree}>2035</span>Now a global
+        airborne virus has wiped out nearly everyone on Earth except for the one
+        percent of our species that has been driven underground. The survivors
+        once again seize upon the promise of time travel as a strategy to
+        unravel fate, but the results are decidedly mixed—it becomes evident
+        that what has happened in the past can’t be undone, only understood,
+        like a procedural.
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <span className={styles.specialCursiveThree}>2054</span>Public safety is
+        maintained by heavy surveillance and state-of-the-art biometric security
+        systems. What’s more, three wild-eyed, soggy clairvoyants and their
+        predictive algorithms, so to speak, have made it possible to prevent
+        crimes before they even happen. Self-driving cars shoot upwards in
+        vertical highways; screens are activated not by touch but by arm and
+        hand gestures. Three-D advertisements call us by our first names and
+        offer bespoke consumer goods and experiences to fulfill our desires and
+        overcome our feelings of emptiness and inadequacy.
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <span className={styles.specialCursiveThree}>2084</span>There is no need
+        to travel anymore to take a vacation because you can pay a company to
+        implant memories in your brain of trips to thrilling destinations such
+        as Mars, which is now a privatized colony. You’ll save money, not to
+        mention reduce your carbon footprint. This technology, capable of
+        implanting memories so real you won’t be able to distinguish them from
+        your actual experiences, can be abused in the wrong hands, it must be
+        said. But what a great leap forward from sixty years ago when the
+        mediasphere was the only tool we had to plant false information into
+        people’s minds.
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <span className={styles.specialCursiveThree}>2101</span>Humanoids
+        powered by artificial intelligence now service humanity’s need for
+        menial labor, companionship, grief management, and sexual gratification.
+        Obsolete and otherwise damaged robots are relegated to a lawless circus
+        called Flesh Fair where the robots are violently destroyed or tortured
+        for entertainment. Meanwhile, global warming is causing sea levels to
+        rise and rise and it won’t be long before New York City is completely
+        underwater.
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <span className={styles.specialCursiveThree}>2122</span>A crew aboard a
+        cargo ship on a flightpath back to Earth responds to a distress signal
+        emanating from another ship on a distant moon. There are no signs of
+        life aboard the derelict vessel, at least that’s what everyone thinks
+        until a squid-like creature manages to clamp itself on a crew member’s
+        face, incubate inside him, and later burst forth from the man’s chest
+        over dinner. The alien, whose blood burns like acid on contact, proceeds
+        to wreak havoc on the commercial space craft. It is possible to smoke
+        cigarettes in outer space and keep a pet cat.
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <span className={styles.specialCursiveThree}>2154</span>The Earth,
+        ravaged by plague, overpopulation, and resource depletion, is divided
+        into the haves and have-nots: the one percent live on a space colony
+        with swimming pools and high-end fixtures; the rest of humanity
+        struggles to survive in parched, squalid environs. A marvelous
+        invention, Med-Bay, exists, but only the rich have access. It resembles
+        a CT scan machine and is capable of curing disease and healing broken
+        bones if not ennui and depression.
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <span className={styles.specialCursiveThree}>2274</span>Humanity now
+        lives in underground cities topped by geodesic domes. A supercomputer
+        maintains this utopia in part by solving the problem of overpopulation
+        and overcrowding with a simple policy: Once you reach the age of thirty,
+        you are euthanized.
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <span className={styles.specialCursiveThree}>2293</span>Was it a war or
+        an ecological disaster? It’s never made clear, but the result is a
+        society divided between two classes: the Eternals, immortals who live in
+        a gauzy paradise, bored and louche and horrible as late-empire Roman
+        elites; and the the Brutals, barbarians in the hinterlands who worship a
+        mysterious stone god and raise crops for the Eternals without ever
+        having set eyes on them. But it is the Brutals who are in possession of
+        the greatest gift: Death.
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <span className={styles.specialCursiveThree}>2500</span>The polar ice
+        caps have melted; the world is nearly entirely submerged except for a
+        handful of floating man-made islands built from salvaged materials. To
+        adapt to this new world, some have evolved by growing webbed feet and
+        gills; dirt is a valuable trade commodity. Still, cigarettes are
+        plentiful and a particularly notorious gang of pirates smokes them with
+        nihilistic disregard for their health.
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <span className={styles.specialCursiveThree}>4001</span>Humanity has
+        entered what some call the era of full employment of memory. Every one
+        of us, not only a select few, has photographic memory. Nothing is
+        forgotten. This makes us better workers and more efficient at performing
+        certain domestic tasks, but over time, we come to realize that these
+        cognitive powers come at a cost: We’ve lost the delirious melancholy of
+        half-remembering, the only state in which poetry and forgiveness are
+        possible.
       </StandardProse>
       <br />
       <br />
