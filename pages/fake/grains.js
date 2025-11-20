334ca65f3dcaeee25c9c65995c989ab81969bec8
Steve Barbaro <steve.barbaro@gmail.com>
2025-11-19 18:59:13 -0600

    instantiates issue9 grains entry

---
+++ pages/fake/grains.js
@@ -0,0 +1,351 @@
+import Head from "next/head";
+import Header from "../../components/header.js";
+import LeftGrayTitle from "../../components/leftGrayTitle.js";
+import StandardProse from "../../components/standardProse.js";
+import Footer from "../../components/footer.js";
+import styles from "../../styles/Grains.module.css";
+Grains.title = "atsushi ikeda ~ needle stalked — new_sinews";
+Grains.pieceName = "atsushi ikeda ~ needle stalked — new_sinews";
+Grains.description = "_Atsushi Ikeda_fiction_NEEDLE STALKED_new_sinews_issue7_";
+Grains.image = "https://www.newnewsinews.com/images/NeedleBackground.jpg";
+
+export default function Grains() {
+  return (
+    <>
+      <Head />
+
+      <Header headerBackground="needleBackground" />
+      <br />
+      <br />
+      <br />
+      <br />
+      <LeftGrayTitle>
+        <span className={styles.titleShadow}>
+          <span className={styles.inverseShadowUppercase}>&#9744;</span>&#9744;
+          <span className={styles.inverseShadowUppercase}>&#9744;</span> b
+          <span className={styles.inverseShadowUppercase}>r</span>
+          <span className={styles.inverseShadow}>
+            <i>a </i>
+          </span>
+          <span className={styles.inverseShadowUppercase}>n</span>
+          <span className={styles.titleShadowUppercase}>d</span>
+          <span className={styles.inverseShadow}>o</span>
+          <span className={styles.titleShadowUppercase}>n</span>
+          <br />
+          <span className={styles.titleShadowUppercase}>&#9744;</span>
+          <span className={styles.extraMarginOne}></span>
+          <span className={styles.inverseShadow}>&#9744;</span>
+          <span className={styles.titleShadowUppercase}>&#9744;</span>
+          <span className={styles.inverseShadow}>&#9744;</span>
+          <span className={styles.extraMarginOne}></span>
+          <span className={styles.titleShadowUppercase}>&#9744;</span>
+          <span className={styles.inverseShadow}>&#9744;</span>
+          <span className={styles.extraMarginOne}></span>
+          <span className={styles.titleShadowUppercase}>&#9744;</span>
+          <span className={styles.inverseShadow}>&#9744;</span>
+          <span className={styles.titleShadowUppercase}>&#9744;</span>
+          <span className={styles.extraMarginOne}></span>
+          <span className={styles.inverseShadow}>&#9744;</span>
+          <br />
+          <span className={styles.inverseShadow}>s</span>
+          <span className={styles.titleShadowUppercase}>h</span>
+          <span className={styles.inverseShadow}>
+            <i>i</i>{" "}
+          </span>
+          m<span className={styles.inverseShadow}>o</span>
+          <span className={styles.titleShadowUppercase}>d</span>
+          <span className={styles.inverseShadow}>
+            <i>a</i>{" "}
+          </span>
+          <span className={styles.inverseShadowUppercase}>&#9744;</span>&#9744;
+          <span className={styles.inverseShadowUppercase}>&#9744;</span>
+        </span>
+      </LeftGrayTitle>
+      <br />
+      <br />
+      <br />
+      <br />
+      <LeftGrayTitle>
+        <span className={styles.title}>
+          <span className={styles.titleShadow}>
+            <span className={styles.extraMarginTwo}>
+            <span className={styles.inverseShadow}> g </span>
+            <span className={styles.titleShadowUppercase}>r</span>
+            <span className={styles.inverseShadowUppercase}>a</span>
+            <span className={styles.inverseShadow}>
+              <i>i </i>
+            </span>
+            <span className={styles.titleShadowUppercase}>n</span>
+            <span className={styles.inverseShadow}>s </span>
+            </span>
+            <br />
+              <br />
+
+              
+              <span className={styles.inverseShadow}> o</span>
+              <span className={styles.titleShadowUppercase}>f</span>
+               <span className={styles.extraMarginOne}>
+              <span className={styles.titleShadowUppercase}>&#9744;</span>
+              <span className={styles.inverseShadow}>&#9744;</span>
+              <span className={styles.extraMarginOne}></span>
+              <span className={styles.titleShadowUppercase}>&#9744;</span>
+              <span className={styles.inverseShadow}>&#9744;</span>
+              <span className={styles.titleShadowUppercase}>&#9744;</span>
+                <span className={styles.extraMarginOne}>
+              <span className={styles.inverseShadow}>&#9744;</span>
+              <span className={styles.titleShadowUppercase}>&#9744;</span>
+              </span>
+            </span>
+            <br />
+             <br />
+            <span className={styles.extraMarginTwo}>
+              <span className={styles.inverseShadowUppercase}>r</span>
+              </span>
+              <br/>
+              <span className={styles.extraMarginTwo}>
+              <i>i</i>
+              </span>
+              <br/>
+               <span className={styles.extraMarginTwo}>
+              <span className={styles.titleShadowUppercase}> c</span>
+              </span>
+              <br/>
+              <span className={styles.extraMarginTwo}>
+              <span className={styles.inverseShadow}>e</span>
+              </span>
+          </span>
+        </span>
+      </LeftGrayTitle>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <StandardProse>
+        <span className={styles.shadowOne}>
+          hurry, tense to steel before the pen touches, tense, tense. constant
+          nude, hurry, there’s anything and anyone there, in the wardrobe of
+          your eyes. my camera is saying go out with dignity. yes there’s armor
+          no won’t sink you don’t be petty about it. you gonna, you gonna?
+        </span>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <span className={styles.shadowOne}>
+          no no you got it wrong we have to panic before we absolve anyone or
+          sit and breathe with the broken candy of it all. you have to run for
+          your life as if it wasn’t already over, don’t you think it absurd when
+          you’re on miles and miles of cloud and he’s behind you, the towering
+          angel with cotton in his ears, in blindfolds, how he takes only one
+          step to cover your hundred, do you think he’ll ever need to catch his
+          breath. if death bleeds you as you’re dreaming like a needle stalked
+          through the mind, and you feel another blade slipping down to touch
+          the teeth of your spine, sweep and jig and run along, let it unzip
+          everything, cut in half, the hemispheres hardly prised but giving
+          themselves over, blurting out their contents:
+        </span>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <span className={styles.shadowOne}>
+          imitation gold. lapis, brine, the glowing map. a ball of rubber bands
+          which was which was the sweet suffering of all hands.
+        </span>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <span className={styles.shadowOne}>
+          breathless fast, you had a particle of angel in you running, hardly
+          knew its own scale against anything, but it ran before you and cast
+          your time receding like a silhouette, amazed by how far behind.
+        </span>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <span className={styles.shadowOne}>
+          that particle, that beat of silence, pinched laugh, a likeness to
+          everything else. did you live up to it? did it leave you too alone? i
+          don’t know either but now now you’ve landed here, damaged goods. so
+          you gonna do it in the right order, strong thing, open bag, you still
+          gonna run?
+        </span>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+      </StandardProse>
+
+      <Footer />
+    </>
+  );
+}
