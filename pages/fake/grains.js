12d0a4dbb1afba0706acc609668c6a9ae7ed5ede
Steve Barbaro <steve.barbaro@gmail.com>
2025-11-20 14:04:39 -0600

    gets yet further into issue9 grains entry

--- pages/fake/grains.js
+++ pages/fake/grains.js
@@ -4,9 +4,10 @@ import LeftGrayTitle from "../../components/leftGrayTitle.js";
 import StandardProse from "../../components/standardProse.js";
 import Footer from "../../components/footer.js";
 import styles from "../../styles/Grains.module.css";
-Grains.title = "atsushi ikeda ~ needle stalked — new_sinews";
-Grains.pieceName = "atsushi ikeda ~ needle stalked — new_sinews";
-Grains.description = "_Atsushi Ikeda_fiction_NEEDLE STALKED_new_sinews_issue7_";
+Grains.title = "brandon shimoda ~ grains of rice — new_sinews";
+Grains.pieceName = "brandon shimoda ~ grains of rice — new_sinews";
+Grains.description =
+  "_Brandon Shimoda_poetry_GRAINS OF RICE_new_sinews_issue9_";
 Grains.image = "https://www.newnewsinews.com/images/NeedleBackground.jpg";
 
 export default function Grains() {
@@ -23,17 +24,20 @@ export default function Grains() {
             &#9744;
             <span className={styles.inverseShadowUppercase}>&#9744;</span>
           </span>
-          <br />b<span className={styles.inverseShadowUppercase}>r</span>
+          <br />b
+          <span className={styles.inverseShadowUppercase}>
+            <i>r</i>
+          </span>
           <br />
           <span className={styles.inverseShadow}>
             <i>a </i>
           </span>
           <span className={styles.inverseShadowUppercase}>n</span>
           <br />
-          <span className={styles.titleShadowUppercase}>d</span>
+          <span className={styles.titleShadowUppercase}>d </span>
           <span className={styles.inverseShadow}>o</span>
           <br />
-          <span className={styles.titleShadowUppercase}>n</span>
+          <span className={styles.titleShadowUppercase}>n </span>
           <span className={styles.inverseShadowUppercase}>&#9744;</span>
           <br />
           &#9744;
@@ -124,13 +128,15 @@ export default function Grains() {
             <span className={styles.extraMarginTwo}>
               <span className={styles.titleShadowUppercase}>c</span>
             </span>
-              <span className={styles.inverseShadow}><i>e</i></span>
+            <span className={styles.inverseShadow}>
+              <i>e</i>
+            </span>
           </span>
         </span>
       </LeftGrayTitle>
-      <br/>
-      <br/>
-       <LeftGrayTitle>
+      <br />
+      <br />
+      <LeftGrayTitle>
         <span className={styles.titleShadowTwo}>
           <span className={styles.boxSpacing}>
             <span className={styles.titleShadowUppercase}>&#9744;</span>
@@ -152,7 +158,7 @@ export default function Grains() {
         </span>
       </LeftGrayTitle>
 
-       <LeftGrayTitle>
+      <LeftGrayTitle>
         <span className={styles.titleShadowTwo}>
           <span className={styles.boxSpacing}>
             <span className={styles.titleShadowUppercase}>&#9744;</span>
@@ -242,166 +248,486 @@ export default function Grains() {
       <br />
       <br />
       <br />
-      <StandardProse>
-        <span className={styles.shadowOne}>
-          hurry, tense to steel before the pen touches, tense, tense. constant
-          nude, hurry, there’s anything and anyone there, in the wardrobe of
-          your eyes. my camera is saying go out with dignity. yes there’s armor
-          no won’t sink you don’t be petty about it. you gonna, you gonna?
-        </span>
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <span className={styles.shadowOne}>
-          no no you got it wrong we have to panic before we absolve anyone or
-          sit and breathe with the broken candy of it all. you have to run for
-          your life as if it wasn’t already over, don’t you think it absurd when
-          you’re on miles and miles of cloud and he’s behind you, the towering
-          angel with cotton in his ears, in blindfolds, how he takes only one
-          step to cover your hundred, do you think he’ll ever need to catch his
-          breath. if death bleeds you as you’re dreaming like a needle stalked
-          through the mind, and you feel another blade slipping down to touch
-          the teeth of your spine, sweep and jig and run along, let it unzip
-          everything, cut in half, the hemispheres hardly prised but giving
-          themselves over, blurting out their contents:
-        </span>
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <span className={styles.shadowOne}>
-          imitation gold. lapis, brine, the glowing map. a ball of rubber bands
-          which was which was the sweet suffering of all hands.
-        </span>
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <span className={styles.shadowOne}>
-          breathless fast, you had a particle of angel in you running, hardly
-          knew its own scale against anything, but it ran before you and cast
-          your time receding like a silhouette, amazed by how far behind.
-        </span>
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <span className={styles.shadowOne}>
-          that particle, that beat of silence, pinched laugh, a likeness to
-          everything else. did you live up to it? did it leave you too alone? i
-          don’t know either but now now you’ve landed here, damaged goods. so
-          you gonna do it in the right order, strong thing, open bag, you still
-          gonna run?
-        </span>
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-      </StandardProse>
+      <pre className={styles.poem}>
+        {`We were eating fries from Wendy’s
+at an Arby’s in New Jersey
+when my father’s wife told us
+that when her mother was young
+her father was beheaded
+by the Japanese imperial army
 
+
+She watched the Japanese cut their heads off
+she said
+
+
+What she saw
+went inside her   grew inside her
+
+
+one of several
+voices
+in her head
+
+
+the oldest   the one
+who knew everything saw everything
+
+
+and kept it
+to herself
+
+
+except when weakness permitted
+
+
+She had `}
+        <i>seen</i>
+        {` it
+She was not a witness
+
+
+`}
+        <i>that</i>
+        {`
+enforces
+an escalating distance
+
+
+like looking through a glass box
+or a fence
+
+
+or `}
+        <i>down</i>
+        {`
+at the heads of people
+
+
+To witness   To have witnessed
+would have been
+to not have had a head
+
+
+or to be unfamiliar
+with the lament
+of carrying the head off
+
+
+because it rained
+and the heads were raining too
+
+
+
+
+She said it like their home was replaced
+with the afterimage
+of everyone’s arrested face
+
+
+and the faces of the Japanese men
+as they turned
+
+
+her caretakers and protectors
+playmates and storytellers
+into ghosts
+
+
+carrying the lantern
+they lit together
+
+
+shaky, it rustled
+grew heavier shadows
+on the long, mottled stalks
+growing out of her heart
+
+
+to the flowers that hardened
+on the end
+
+
+
+
+She was spared
+or escaped
+
+
+and her baby brother too
+
+
+took him   and the rice
+and ran through mud
+
+
+they were running in their sleep
+through mud   up their ribs
+
+
+but   like winter
+everything was pushed down
+
+
+through the roots
+
+
+
+
+She saved grains of rice, my father’s wife said
+
+
+When she dropped a grain in the mud, even a single grain
+
+
+she stopped
+and picked up the grain   every grain
+
+
+rescued them   saved them
+to survive
+
+
+into another sphere
+to retrieve what proved to be missing
+
+
+
+
+
+The mud fortified the rice, she said
+to her daughter   lifetimes later
+
+
+thick
+with hands
+
+
+heavy
+with footprints
+
+
+people scattering
+into the rupture
+
+
+bearing the soul
+of hunger
+
+
+that would not end
+when the bodies fell
+
+
+still cradling the rice   still ruling the future
+
+
+that turned each family member
+into a torch
+
+
+or a mailbox
+
+
+
+
+
+
+They hid in a cave, she said
+
+
+The cave was still in her, grew
+a shell filled with water
+around her
+
+
+Day became an interpretation of the dream of running
+away from their dead
+
+
+each day   each night
+each hour, each step
+detached and dispensed demagnetized
+
+
+the soul
+so the self could keep running
+
+
+you would think   lighter and lighter
+you would think the selves would fall off
+
+
+but the soul is what absorbed
+the sight of the bodies
+
+
+
+
+
+
+
+We listened while eating fries
+two, three fries at a time
+
+
+like the gates were coming down
+
+
+people advancing up
+the greasy floor
+
+
+kids in a crevice somewhere
+salting earth
+
+
+My father was looking at me
+like the atrocity was playing on my face
+
+
+which made my face real   for a moment
+
+
+sliding
+like a shadow
+down the soldiers’
+
+
+
+
+
+
+
+
+How does a boy find himself
+in an occupation force
+
+
+Does a boy? lose himself
+by law
+
+
+I was old enough to be their older brother
+to have gone through the process
+of becoming a chest
+with drawers,   every drawer empty
+
+
+gotten dirty and loose
+
+
+my father might look like those men
+if he was my younger brother
+
+
+the soldiers’ age   and aged with them
+into the re-hallucination of Japanese faces
+
+
+
+
+
+
+
+
+
+The first time my father’s wife’s mother
+saw my father’s mother
+she screamed
+
+
+a black hole in the center of the room
+whirled
+the winds
+of generations around
+the space between her and my grandmother
+
+
+she could see the bodies
+more clearly
+
+
+in the current
+
+
+and my grandmother’s face
+summoned by the gravity of each body
+
+
+pulled the river
+into her
+
+
+she saw a ghost   not a soldier
+the soldier’s mother   staring back
+
+
+and had to be carried out of the room
+
+
+Her body became the doorway   it could not hold her 
+without disclosing the radiation   it kept hidden
+
+to keep the generations moving
+
+
+past the countenance of traumas
+
+
+Devil! she screamed
+
+
+My grandmother did not understand
+she was the terror
+
+
+she must have seen her elders
+in the faces of the young men
+strung up like clothes
+
+
+My grandmother, Japanese, had the luxury
+of not being terrorized
+by the woman’s ghost
+
+
+or the ghost of the family members
+
+
+she could eat and stare, process the role
+
+
+her murderous present
+was holding onto
+
+
+
+
+
+
+
+
+
+The young girl became
+an entire lightning
+distilled
+
+
+into the strength of an old woman
+with eyes on the back of her head
+
+
+the faces of loved ones   slipping
+
+
+into the sociopathy
+of the twenty-first century
+
+
+where her daughter brings home a Japanese boyfriend
+
+
+his face still on his head, smiling
+
+
+a complete lack of history
+
+
+holding hands with an overflowing
+absence of history
+
+
+
+
+
+
+
+
+
+
+It was justified, my father said
+about the atomic bombings
+of Hiroshima and Nagasaki
+
+
+both bombs linked
+by the dream of living forever
+away from being tortured
+by reality
+
+
+The sky fell
+a wave
+growing inland
+
+
+harvesting the histories of many thousands of people
+the division of energy
+
+
+It was justified, he said
+three, four fries at a time
+
+
+with the attitude of a woman
+on her deathbed
+
+
+resigned, free of nostalgia
+many lifetimes of children
+carrying on
+
+
+Was he speaking as an American
+a Japanese American
+a Japanese American man
+
+
+one face rotated
+to catch the sun in its facets
+light reflecting in heterogeneous ways
+
+
+the psyche running backwards
+into the familiar estrangement of a white American
+deal
+at the crossroads
+
+
+
+
+
+
+
+
+
+
+
+I stared past my father
+at the restroom door
+counted the seconds until it opened
+
+
+Someone going in
+Someone coming out
+
+
+I imagined myself
+in the restroom
+turning the water on in the sink
+
+
+Looking in the mirror and seeing
+men walking in and out
+of the turquoise sky
+pink iridescent green stalks of brown smoke
+
+
+reorganizing deaths
+into denominations
+of democracy`}
+      </pre>
       <Footer />
     </>
   );
