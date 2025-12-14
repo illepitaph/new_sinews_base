498655124e76218a5494a6210d990ab77623f11f
Steve Barbaro <steve.barbaro@gmail.com>
2025-12-14 16:18:50 -0600

    gets yet deeper into issue9 blazes entry

--- pages/fake/blazes.js
+++ pages/fake/blazes.js
@@ -33,14 +33,12 @@ export default function Blazes() {
           <i>s </i>
         </span>
       </RightBlackTitle>
-
       <br />
       <br />
       <br />
       <br />
       <br />
       <br />
-
       <LeftBlackTitle>
         <span className={styles.inverseShadowAlternate}>&#40; </span>
         <span className={styles.titleShadow}>
@@ -58,11 +56,8 @@ export default function Blazes() {
           </span>
           <span className={styles.specialCursive}> s </span>
         </span>
-        <span className={styles.blueAlternate}>
-          &#41;
-        </span>
+        <span className={styles.blueAlternate}>&#41;</span>
       </LeftBlackTitle>
-
       <br />
       <br />
       <br />
@@ -75,7 +70,6 @@ export default function Blazes() {
       <br />
       <br />
       <br />
-
       <RightBlackTitle>
         <span className={styles.bareHeader}>
           ... h <span className={styles.uppercase}>a</span>n
@@ -86,19 +80,185 @@ export default function Blazes() {
           <span className={styles.uppercase}>tl</span> ...
         </span>
       </RightBlackTitle>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <pre className={styles.poem}>
+        {`Involved with blazing
 
+In a canyon in a crevasse on a ruin by a stream invented body 
+invented lovethorn leaving history
+
+w/out something in it`}
+      </pre>
       <br />
       <br />
       <br />
+      <p className={styles.grayShadowUpper}>Note 98</p>
       <br />
       <br />
       <br />
+      <pre className={styles.poem}>
+        {`—spongy, sporish, the departicular way I’m sitting at 
+dr eyemed or dr eye dr med or whatever 
+open to the croon`}
+      </pre>
       <br />
       <br />
       <br />
+      <p className={styles.grayShadowUpper}>Maybe tuning</p>
       <br />
       <br />
       <br />
+       <pre className={styles.poem}>
+        {`Porsche from Vermont
+Fit in Massachusetts 
+The problem is that of knowledge I knowwwww 
+No it’s about action inaction I’m waiting for a covid test it’s about Privilege always optimism when I’m  
+In a thought about poetry “now” but need my hand to further it`}
+      </pre>
+       <br />
+      <br />
+      <br />
+      <p className={styles.grayShadowUpper}>The future fell down</p>
+      <br />
+      <br />
+      <br />
+       <pre className={styles.poem}>
+        {`Was pulled
+The past shouted
+Rising 
+The past leashed us
+Intermittently 
+In one direction 
+It was substance 
+Green joggers 
+            w white sneakers 
+
+Russian sounds like sharp stones 
+in pretty water the million little conversations 
+people are so so happy to have change the ecology of the monolith 
+starting to look. The bubble of island
+striped sweater pleated skirt and a big audience 
+“I can give birth to only one baby at a time” 
+“I like it, I like it” 
+and the water bottle 
+tangling I mean like making the sound of tang do you need
+a device charger
+`}
+      </pre>
+       <br />
+      <br />
+      <br />
+      <p className={styles.grayShadowUpper}>lorineBMB</p>
+      <br />
+      <br />
+      <br />
+             <pre className={styles.poem}>
+        {`“The confusion of small once connected things” 
+
+“A joy practice”`}
+      </pre>
+        <br />
+      <br />
+      <br />
+      <p className={styles.grayShadowUpper}>Lorine01!</p>
+      <br />
+      <br />
+      <br />
+      <pre className={styles.poem}>
+        {`Something is in misery 
+Put it out 
+Roads that were gravel are now paved this accounted for certain 
+loneliness 
+
+Lonelinesses put them out 
+
+This that the other  
+
+I won’t survive so try to get interested in what will 
+A group of women in puffy coats and masks, haul between the barn
+and little green library, one fancy corner in town  
+
+Honor humble bobbing 
+First daffodil 
+
+Abyss at lesser street 
+Iris tips talons on railroad street`}
+      </pre>
+       <br />
+      <br />
+      <br />
+      <p className={styles.grayShadowUpper}>Lax flight</p>
+      <br />
+      <br />
+      <br />
+      <pre className={styles.poem}>
+        {`Hdz99n 
+9920160#
+Room 225
+
+Sunday: book lyft for 5:00 am
+7:15-2:32/ Atlanta 1 hr layover 
+3:43-6:03
+
+I looked at found
+Thought does not arrive in a sentence 
+A Tesla does! Its sentence of riches
+Pollutes us
+And light on the palms, my palms typing 
+Big blue bus big blue in moms 
+Los Angeles not where the movies are but burned up burning trashed 
+woman fake farting by the pier 
+she was exaggeratedly lifting her leg blowing her lips a young
+someone 
+in the bus shelters canopy and the pretty dog in its Ford 
+Talking to you it’s shockingly good
+That’s why a box of Amstel in the shaft, analogous epideictic singing
+along 
+I thought the argument was not in the direction of the field the field
+was going toward the management of abundance abundances
+emerging clarified the clear present w all that esotericism, well be
+careful. 2 men from somewhere vaguely south sit in moms talking
+about dumb shit they gotta do. Games. A friend is really excited to
+meet the one guy, he’s been asking. Earlier they discussed
+Pentecostalism, ordinary dudes. A woman goes to get shawarma from
+across the street. What has happened in the bar called moms?
+A vaper asked to leave refuses. 
+His refusal ignites a series of events. 
+All actions ignite a series of events.
+All dominoing spiraling out.`}
+ </pre>
+  <br />
+      <br />
+      <br />
+      <p className={styles.grayShadowUpper}>Baltimore secac</p>
+      <br />
+      <br />
+      <br />
+       <pre className={styles.poem}>
+        {`Spirit of the visionary saying poverty raised me I don’t abandon
+poverty it doesn’t hold me though
+This is how we stayed alive
+It had some wood and bottle bits, a painted screen
+A picture comes from my head, pretty picture w borders you see
+Whatever fantasy that comes to their mind
+Old people painting representationally mimetically a big snow-
+covered mountain with a cross by an empty glass globe
+Instead of decoration you made a painting
+The sun going down the colors in the sky they bring tears to your eyes
+The people are dying
+Fad and tradition`}
+      </pre>
       <Footer />
     </>
   );
