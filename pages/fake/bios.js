fd1687a27f6fc61874c1585dbdd0d24f743e86ad
Steve Barbaro <steve.barbaro@gmail.com>
2025-12-22 19:52:21 -0600

    refines issue9 bios page

--- pages/fake/bios.js
+++ pages/fake/bios.js
@@ -91,7 +91,7 @@ export default function Issue9Bios() {
         John Goodhue (<i>Cleveland Review of Books</i>) calls &ldquo;the glitch which
         reveals the dark data of a world that one has a moral imperative to
         rebuff.&rdquo; Extending this process into sound, he records as The Spanish
-        Trail Motel. His debut EP, <i>The Spanish Trail Motel Descrambler</i>,
+        Trail Motel. His debut EP, <i>The Spanish Trail Motel Descrambler </i>,
         <i>Issue 1</i> (2025), and follow-up <i>Vaporleonard</i> (2025), are
         available on all major streaming platforms.
         <br />
