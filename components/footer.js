4682aa9aac29d467f99e0c07f5b2ab5204fa1d7a
Steve Barbaro <steve.barbaro@gmail.com>
2026-05-13 17:00:32 -0500

    injects instagram link

--- components/footer.js
+++ components/footer.js
@@ -3,7 +3,10 @@ import styles from "../styles/Footer.module.css";
 export default function Footer() {
   return (
     <footer className={styles.socialLinkFooter}>
-      <p>&#x1F92B;</p>
+      <a
+      href="https://www.instagram.com/new_sinews/"
+        target="_blank"
+        rel="noreferrer">i<i>n_</i>s<i>t</i>a</a>
     </footer>
   );
 }
