a3ff6445acafb7d7d6a0e02aafd461b2a1ffd802
Steve Barbaro <steve.barbaro@gmail.com>
2026-07-30 09:55:34 -0500

    instantiates issue10 symmetry entry

---
+++ pages/fake/symmetry.js
@@ -0,0 +1,291 @@
+import Head from "next/head";
+import Header from "../../components/header.js";
+import LeftGrayTitle from "../../components/leftGrayTitle.js";
+import RightBlackTitle from "../../components/rightBlackTitle.js";
+import StandardProse from "../../components/standardProse.js";
+import Footer from "../../components/footer.js";
+import styles from "../../styles/Symmetry.module.css";
+Symmetry.title = "jancie_creaney ~ one_eyed, no_symmetry  — new_sinews";
+Symmetry.pieceName = "jancie_creaney ~ one_eyed, no_symmetry  — new_sinews";
+Symmetry.description =
+  "_Jancie Creaney_fiction_ONE EYED, NO SYMMETRY_new_sinews_issue10_";
+Symmetry.image = "https://www.newnewsinews.com/images/ChronologyBackground.jpg";
+
+export default function Symmetry() {
+  return (
+    <>
+      <Head />
+
+      <Header headerBackground="symmetryBackground" />
+      <LeftGrayTitle>
+        <span className={styles.inverseShadowUppercase}>
+          {" "}
+          one-eyed<span className={styles.titleShadowUppercase}>, </span>
+        </span>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <span className={styles.inverseShadowUppercase}>
+          _ <span className={styles.inverseShadow}> _</span>
+        </span>
+        <span className={styles.specialCursive}> n</span>
+        <span className={styles.titleShadowUppercase}>o </span>
+        <span className={styles.titleShadow}>
+          <span className={styles.marginRight}></span>
+          <span className={styles.inverseShadowUppercase}>s </span>
+          <span className={styles.skewDown}>
+            <span className={styles.alternateTitleColor}>y </span>
+          </span>
+          <span className={styles.skewUp}>
+            <span className={styles.titleShadowUppercase}>
+              <i>m </i>
+            </span>
+          </span>
+          <span className={styles.skewDown}>
+            <span className={styles.inverseShadowUppercase}>m</span>
+          </span>
+          <span className={styles.skewLeft}>
+            <span className={styles.alternateTitleColorBigger}>e </span>
+            <span className={styles.inverseShadowUppercase}>t </span>
+            <span className={styles.alternateTitleColor}>r </span>
+            <span className={styles.titleShadowUppercase}>y </span>
+          </span>
+          <br />
+          <br />{" "}
+        </span>
+      </LeftGrayTitle>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <RightBlackTitle>
+        <span className={styles.nameSpacing}>
+          ... ... j<span className={styles.uppercase}>A nc</span> i
+          <span className={styles.nameIndent}>e</span> ... c
+          <span className={styles.uppercase}>r</span>e
+          <span className={styles.uppercase}>a </span> n
+          <span className={styles.nameIndent}>i</span>
+          <span className={styles.uppercase}>e</span> ... ...
+          <br />
+        </span>
+      </RightBlackTitle>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <StandardProse>
+        Though I might feign a structure it is useless to defend myself against
+        the reality of my cloth-like heave, flapping and pinched by the peg: I
+        am a product of the one-eyed squirrel seeing me from its limb-cut roost.
+        Admiring its twitchiness, I wonder if it has trouble scaling the trunk
+        of the grooveless oak in search of a nut called acorn. I discover the
+        animal is one-eyed by looking at it straight on, into a black pearl
+        protruding on the right-side—no symmetry. One dark marble silent such
+        that my single purpose suddenly is—relax in red metal chair, communicate
+        broad acceptance, turn my looking into a safe zone wherein the rodent
+        can shut that one eye and sleep.
+        <br />
+        <br />
+        Strange thing: a shyness around what I notice. That what I take note of
+        is this. That by disclosing it I reveal a life, flat and futile and grey
+        as this February day, my life a sampling of observations. The
+        consequence of this shyness being that the tunnel narrows. What is
+        permitted to pass through to a point where I say it, what I see—I am
+        prone to demote a clean and simple picture because it did not scale the
+        heights of—what?—and then the shame of my vain standards.
+        <br />
+        <br />
+        Biking back from the YMCA I rode over the bridge and slowed at the apex
+        to look at the cityscape encased in a blue fog, thin violet lights
+        piercing through from the tops of towers. The view was cushioned by the
+        bright orange corduroy of my jacket sleeve—held out to the handlebar.
+        That I could be reduced to this outline—weatherworn corduroy purchased
+        at Value Village when I was fifteen.
+        <br />
+        <br />
+        On the snowy street Michael said, It’s no fun. No, it’s not. What I
+        can’t believe is, so, half the year we live in heaven? The picture of
+        the lake radiates. Smaller squirrels, auburn. Frogs and toads, all
+        through the night. Falling asleep, or trying—to a tumbling cacophony.
+        Half-conscious I translated each bellow of the bullfrogs’ serenade to a
+        lump on the earth, confusing roundness of sound with roundness of
+        topography.
+        <br />
+        <br />
+        Why do squirrels steal my twine? I asked. My rendition of Karen
+        Carpenter’s <i>why do birds suddenly appear</i>. I had a marine
+        biologist—my teacher in college—he said—said everything in life is
+        searching for balance. It seems hard to be a baby. Anyway, we’ve ran
+        out.
+        <br />
+        <br />
+        If form is authenticity’s chaos meeting a desire to be understood—if
+        only as an object apprehensible by another through the senses—the issue
+        is being at once form and producer of form, able to clam up the shore
+        and look instantly back at the slime trail.
+        <br />
+        <br />I want to be understood. But I want also to find how far I can go
+        with my private unkillable wilderness. I test, and through testing
+        discover the addictive euphoria experienced when I’ve married my feral
+        lonely interiority to a communicable event. The meeting draws the
+        outline. I see a shape. And reconsider—
+        <pre className={styles.poemlike}>
+          {" "}
+          {` 
+            was I unfettered enough? did I hold back for the sake of comprehension?—then redraw 
+            the line. 
+
+            The form morphs.
+            `}{" "}
+        </pre>
+        Admiring my twitchiness, I wonder if I have trouble scaling the ledge of
+        the edgeless cosmos in search of a nut called salvation.
+        <br />
+        <br />
+        Double shyness: that nothing is novel about my observations and that I
+        want there to be something novel about my observations. That I dream of
+        our vacation at the lake, of summertime, hope I do something interesting
+        before I die, but tears well up when reading the simplest words: I’m
+        writing, the pen ran out, birds are chirping.
+        <br/>
+        <br/>
+        Well-insulated in the city beneath hat, scarf, headphone, and the breeze-breaking 
+        collar of my coat, I decided to give in—loosely inspired by three young 
+        women, bundled up, carrying bookbags, a few steps ahead. I wondered how each girl 
+        experiences time. Then spotted myself drifting, toying with the thought I was one
+        of them. The escalator separated us. I sunk into the metro. I was singing Lana
+        softly, certain the underground hum camouflaged my voice, but a man, younger than
+        me, peered up, and I quieted. A woman with a bright red scar across her forehead
+        entered the car. She took off her puffer quickly. The young man read 1984. A red
+        ribbon tied to his black backpack twirled in the metro wind.
+        <br/>
+        <br/>
+        Daydream: the foot was last, to push off the deck. Losing balance (not totally) the
+        toes dip and skim the pristine surface. The knee bent, leg tucked, pulled into the
+        body. I sat on my butt, spine straight. We glided gently to the middle of Lac Clair
+        to a spot far from shore. Our paddle board wobbled between loons. One of two
+        disappeared. The loon’s wail was so dignified at the start. Cam guided us through
+        a meditation on the water, Notice how sounds come and go, arise from nothing and
+        disappear back. Our eyes were shut. I opened mine to see his face flecked by
+        rippling golden lines that traveled down his neck. I shut them again. Late
+        afternoon sun pinched my shoulders. I’ll have a burn by tonight, thought. Then, 
+        acceleration, the loon’s call pitched up, ascending to a yodel-screech that sounded
+        frantic, desperate, hyperbolic. 
+         <br/>
+        <br/>
+        Had to open our eyes and look because it was almost worrisome, this bird losing
+        its mind. It was funny but I was crying.
+        <br/>
+        <br/>
+        Problems: one, blankets of shame—conscious and not—blanket access to reality. They
+        erect monuments, orchestrate a path across the day—I avoid, I draw near—why—and
+        how intensely, mysteriously this occurs in relation to what the mind forbids us
+        apprehending. Out of a sense of sanity, protection, order. Seeing all would be
+        chaos. Forgetting allows something to take the stage. And two: the locus can be
+        displaced: you can interpret a moment from the vantage of your eyes, or from the 
+        point-of-view of sunlight on the cheek of a passerby—or from the perspective of 
+        the sliding door, whose existence is a shuttering, gliding, threshold. Two is that
+        we hide inside everything and can outsource the POV to particles and clouds.
+          <br/>
+        <br/>
+        What’s a twine? It’s the rope I use to tie the trellis to the balcony railing. Ah
+        yes they bring more to the nest babies coming soon. Thought so. I caught one
+        jamming a bunch into its mouth.
+ <br/>
+        <br/>
+        Listening to a man sing about burying his mom and dad by a lake, on my way to the 
+        YMCA. Waiting, jacket open, for the light to be green. Thought, peering into the
+        wet pavement and the brown slush in the craters, I could see reality composing
+        itself—like acetate papers overlapping. I strained my eyes on the rocks to 
+        apprehend this fact: finite material infinitely shuffled into different sets of
+        hands, held out as an offering.
+        <br/>
+        <br/>
+        Shuffling like television static. 
+        <br/>
+        <br/>
+        I renamed the power line outside the back window in the kitchen squirrel
+        highway. With a nod I bless the creatures when they pass in the corner of my 
+        eye as I stir a potful of squash. An old friend years ago said I’d stir even 
+        an empty pot just to have something to do while I submerge myself in a revery.
+        <br/>
+        <br/>
+        From the dining table, watched sunlight spill through the courtyard cedar onto
+        the sage green wall.
+ <br/>
+        <br/>
+        All of that is knowing itself, thought. The illusory barrier—I am—all that as
+        this. So when I turn around on the white swivel chair, I see myself as Cooper’s
+        Hawk, and startle at the sight of me, broad-chested and flecked, mistaken firstly
+        for a wasps’ nest or an inordinately oversized pinecone.
+        <br/>
+        <br/>
+        He says they’re smaller up north because, didn’t migrate there, too cold. When we
+        were little, me and my brother fed them from the porch. They bit our toes, you know
+        why? Toes look like peanuts. 
+         <br/>
+        <br/>
+        When I met that one-eyed squirrel it scared me. It saw nothing wrong with its
+        condition. And it looked monstrous. I believed it would at any moment leap from
+        the tree and eat my face or something, though its paws were tucked under its
+        body. After my initial repulsion, I was overcome with remorse. I felt so bad about
+        my reaction. I wanted it to know I was not scared anymore, of its nonexistent eye
+        or it being so close to me.
+      </StandardProse>
+      <br />
+      <br />
+      <br />
+      <Footer />
+    </>
+  );
+}
