6080f97e851bbbb2401a0224a6370211e3d5fc7e
Steve Barbaro <steve.barbaro@gmail.com>
2026-08-18 22:53:51 -0500

    instantiates issue10 spell entry

---
+++ pages/fake/spell.js
@@ -0,0 +1,205 @@
+import Head from "next/head";
+import Header from "../../components/header.js";
+import LeftBlackTitle from "../../components/leftBlackTitle.js";
+import CenterBlackTitle from "../../components/centerBlackTitle.js";
+import Image from "next/legacy/image";
+import Memory1 from "../../public/images/Memory1.jpg";
+import Memory2 from "../../public/images/Memory2.jpg";
+import Memory3 from "../../public/images/Memory3.jpg";
+import Memory4 from "../../public/images/Memory4.jpg";
+import Footer from "../../components/footer.js";
+import styles from "../../styles/Spell.module.css";
+Spell.title =
+  "liola_mattheis ~ the spell keeps on casting ++ forming by forming in — new_sinews";
+Spell.pieceName =
+  "liola_mattheis ~ the spell keeps on casting ++ forming by forming in — new_sinews";
+Spell.description = "_LIOLA MATTHEIS_poetry_5 POEMS_new_sinews_issue10_";
+Spell.image = "https://www.newnewsinews.com/images/MemoryBackground.jpg";
+
+export default function Spell() {
+  return (
+    <>
+      <Header headerBackground="memoryBackground" />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <CenterBlackTitle>
+        <span className={styles.titleShadow}>
+          <span className={styles.uppercase}>l</span><br/>
+          <span className={styles.fade}>i</span><br/>
+          <i>o</i><br/>
+          <span className={styles.blueUpper}>l</span><br/>
+          <span className={styles.uppercase}><i>a</i></span><br/><br/><br/><br/>m
+          <span className={styles.fadeUpper}>a</span>
+          <i>t<span className={styles.blue}>t</span></i>
+          <span className={styles.uppercase}>
+            h<i>e </i>
+          </span>
+         <span className={styles.blue}>i</span>
+          <span className={styles.uppercase}>s</span>
+          <span className={styles.fadeUpper}>++</span>
+        </span>
+      </CenterBlackTitle>
+
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+           <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+           <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+
+      <h3 className={styles.poemTitle}>[No Big Deal!?.]</h3>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+          <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <span className={styles.boldHeader}>No !</span>
+      <div className={styles.poemBox}>
+        <pre className={styles.poem}>
+          <br />
+          {`World without end!
+However it falls!
+The aesthetics of my politics are not for sale!
+I wonder about these mouldy webpages like all the time!
+In this archive you shall encounter some genuine lovers but don’t expect too many
+other things!
+It’s giving me the ick how you continue to grope that screen of yours!
+Can’t you just finally own up to the good life!
+It’s never too late to grow up!
+Damn right I run this book club on my own!
+Anyone would have made a French turn then!
+Every day is a new chance to be more feminine!
+We get to realize the baby!
+Now it’s time for us to fuck some hammocks into the ground!
+I’m only asking you to lend me a speculum!
+And take my dog for the walk of his life!
+I was going to get that golden handshake!
+The best way something pops must clearly be the berry in a gendered compound!
+But what an adorable value you have entertained us with tonight!
+What a tiny leaflet thrown into my naughty bank account!
+Mail the list some squashy remedies!
+I see the double bind in the sky!
+And the rest is natural history!`}
+        </pre>
+      </div>
+        <br />
+      <br />
+      <br />
+      <br />
+      <br />
+          <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <span className={styles.boldHeader}>Big ?</span>
+      <div className={styles.poemBox}>
+        <pre className={styles.poem}>
+          <br />
+          {`Besides that lukewarm property?
+Is that a yes or are you still waiting to hear back?
+Did I ask you to turn around?
+And who am I to be horny tonight?
+Likewise?`}
+        </pre>
+      </div>
+       <br />
+      <br />
+      <br />
+      <br />
+      <br />
+          <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <span className={styles.boldHeader}>deal .</span>
+      <div className={styles.poemBox}>
+        <pre className={styles.poem}>
+          <br />
+          {`I went diving, knees first.
+Water comes from the breasts.
+Like an unhinged simile.
+Later, a message would be text.
+Now that’s a problem if there ever was one.
+And yet, I’d make for a good photo.
+I feel like it’s grey and striped.
+Before the owner goes.
+Mostly another night.
+Let’s not be too hard on your disappointments.
+In fact, once someone had furnished a thorough assessment it was thorough enough to change.
+Ravishing, truly ravishing.
+Likewise.
+Whereas I always assumed the loveliest of them all would have come from far away.
+I wanna say fire.
+I mean I mean it.
+Everyone gets some and only some.
+And some is repeated in every child.
+For starters, I want something rich.
+I mean I need it.
+And so the acolytes flew in their face.
+All he could do was blow her nose`}
+        </pre>
+      </div>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+  
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <Footer />
+    </>
+  );
+}
